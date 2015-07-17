2b7a43bfcbdeb9e21723ac131f3849ca7df57782
elibixby <elibixby@google.com>
2015-07-17 16:26:18 -0700

    Fixed packaging

--- resourcemanager/tests/test_resource_manager.py
+++ resourcemanager/tests/test_resource_manager.py
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from resourcemanager.samples import create_project, delete_project
+from resourcemanager import create_project, delete_project
 from tests import CloudBaseTest
 
 
