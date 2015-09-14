7e9e500025ce82dab9da334dd6623c05799838fa
Jon Wayne Parrott <jon.wayne.parrott@gmail.com>
2015-09-14 11:29:26 -0700

    Renaming blog folder for datastore samples to be a proper package name, increasing coverage

--- blog/introduction_to_data_models_in_cloud_datastore/test_blog.py
+++ blog/introduction_to_data_models_in_cloud_datastore/test_blog.py
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from blog import main
+from .blog import main
 from tests import CloudBaseTest
 
 
---
+++ blog/introduction_to_data_models_in_cloud_datastore/test_wiki.py
@@ -0,0 +1,22 @@
+# Copyright 2015, Google, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from .wiki import main
+from tests import CloudBaseTest
+
+
+class WikiTestCase(CloudBaseTest):
+    """Simple test case that ensures the wiki code doesn't throw any errors."""
+
+    def test_main(self):
+        main(self.constants['projectId'])
