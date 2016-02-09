9a4114d8a958096a6c40601feec78a68444ca5f8
Jon Wayne Parrott <jon.wayne.parrott@gmail.com>
2016-02-09 12:31:58 -0800

    Moving mvm samples to runtime: python

--- managed_vms/memcache/main.py
+++ managed_vms/memcache/main.py
@@ -45,5 +45,5 @@ def index():
 
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
-    # application on Google App Engine. See CMD in Dockerfile.
+    # application on Google App Engine. See entrypoint in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
