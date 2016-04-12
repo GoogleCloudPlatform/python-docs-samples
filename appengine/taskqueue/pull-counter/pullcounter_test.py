718417d347fde625e08d835c6070b38e61efb16d
Jeff Mendoza <jeffmendoza@google.com>
2016-04-12 13:54:07 -0700

    App Engine Task Queue example using pull queues.
    Copied from: https://github.com/GoogleCloudPlatform/appengine-pullqueue-counter
    and tests added.

---
+++ appengine/taskqueue/pull-counter/pullcounter_test.py
@@ -0,0 +1,33 @@
+# Copyright 2016 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from google.appengine.ext import testbed as gaetestbed
+import main
+import webtest
+
+
+def test_app(testbed):
+    key_name = 'foo'
+
+    testbed.init_taskqueue_stub(root_path=os.path.dirname(__file__))
+
+    app = webtest.TestApp(main.app)
+    app.post('/', {'key': key_name})
+
+    tq_stub = testbed.get_stub(gaetestbed.TASKQUEUE_SERVICE_NAME)
+    tasks = tq_stub.get_filtered_tasks()
+    assert len(tasks) == 1
+    assert tasks[0].name == 'task1'
