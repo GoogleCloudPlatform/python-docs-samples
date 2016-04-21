9d37f1603a0b32622c478ad523fef03ea2dd244e
Sandeep Dinesh <sandeepdinesh@google.com>
2016-04-21 14:58:51 -0700

    moved image guestbook, added basic image example

--- appengine/images/api/README.md
+++ appengine/images/api/README.md
@@ -6,8 +6,8 @@ API](https://cloud.google.com/appengine/docs/python/images/usingimages).
 <!-- auto-doc-link -->
 These samples are used on the following documentation page:
 
-> https://cloud.google.com/appengine/docs/python/images/usingimages
+> https://cloud.google.com/appengine/docs/python/images/
 
 <!-- end-auto-doc-link -->
 
-Refer to the [App Engine Samples README](../README.md) for information on how to run and deploy this sample.
+Refer to the [App Engine Samples README](../../README.md) for information on how to run and deploy this sample.
---
+++ appengine/images/api/main.py
@@ -0,0 +1,56 @@
+# Copyright 2015 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Sample application that demonstrates how to use the App Engine Images API.
+
+For more information, see README.md.
+"""
+
+# [START all]
+# [START thumbnailer]
+from google.appengine.api import images
+from google.appengine.ext import ndb
+
+import webapp2
+
+
+class Photo(ndb.Model):
+    title = ndb.StringProperty()
+    full_size_image = ndb.BlobProperty()
+
+
+class Thumbnailer(webapp2.RequestHandler):
+    def get(self):
+        if self.request.get("id"):
+            photo = Photo.get_by_id(int(self.request.get("id")))
+
+            if photo:
+                img = images.Image(photo.full_size_image)
+                img.resize(width=80, height=100)
+                img.im_feeling_lucky()
+                thumbnail = img.execute_transforms(output_encoding=images.JPEG)
+
+                self.response.headers['Content-Type'] = 'image/jpeg'
+                self.response.out.write(thumbnail)
+                return
+
+        # Either "id" wasn't provided, or there was no image with that ID
+        # in the datastore.
+        self.error(404)
+# [END thumbnailer]
+
+
+app = webapp2.WSGIApplication([('/img', Thumbnailer)], debug=True)
+# [END all]
