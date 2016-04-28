75106f591f43b709d1beae129359612c8ee3de94
Chris Broadfoot <cbro@golang.org>
2016-04-28 13:37:22 -0700

    Adding endpoints sample

---
+++ managed_vms/endpoints/main.py
@@ -0,0 +1,91 @@
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Google Cloud Endpoints sample application.
+
+Demonstrates how to create a simple echo API as well as how to deal with
+various authentication methods.
+"""
+
+import base64
+import json
+
+from flask import Flask, jsonify, request, send_from_directory
+from six.moves import http_client
+import yaml
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    """Shows the index page."""
+    return send_from_directory('.', 'index.html')
+
+
+@app.route('/api-docs', methods=['GET'])
+def swagger_json():
+    """Serves up the Swagger spec for the API."""
+    with open('swagger.yaml', 'r') as f:
+        spec = yaml.safe_load(f)
+    return jsonify(spec)
+
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    """Simple echo service."""
+    message = request.get_json().get('message', '')
+    return jsonify({'message': message})
+
+
+def auth_info():
+    """Retrieves the authenication information from Google Cloud Endpoints."""
+    encoded_info = request.headers.get('X-Endpoint-API-UserInfo', None)
+
+    if encoded_info:
+        info_json = base64.b64decode(encoded_info).decode('utf-8')
+        user_info = json.loads(info_json)
+    else:
+        user_info = {'id': 'anonymous'}
+
+    return jsonify(user_info)
+
+
+@app.route('/auth/info/googlejwt', methods=['GET'])
+def auth_info_google_jwt():
+    """Auth info with Google signed JWT."""
+    return auth_info()
+
+
+@app.route('/auth/info/googleidtoken', methods=['GET'])
+def auth_info_google_id_token():
+    """Auth info with Google ID token."""
+    return auth_info()
+
+
+@app.errorhandler(http_client.INTERNAL_SERVER_ERROR)
+def unexpected_error(e):
+    """Handle exceptions by returning swagger-compliant json."""
+    response = jsonify({
+        'error': http_client.INTERNAL_SERVER_ERROR,
+        'message': 'Exception: {}'.format(e)})
+    response.status_code = http_client.INTERNAL_SERVER_ERROR
+    return response
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
