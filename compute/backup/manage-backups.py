ee74fb9b0a7bb53c97f3f2feb0da5218a9c53400
Andrew Grumet <aegrumet@alum.mit.edu>
2016-06-25 11:47:41 -0700

    Applied first round of pull request review changes.

--- compute/backup/manage-backups.py
+++ compute/backup/manage-backups.py
@@ -1,41 +1,66 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-# pip install --upgrade google-api-python-client
-# pip install --upgrade iso8601
-# pip install --upgrade rfc3339
-# pip install --upgrade pytz
+# Copyright (C) 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-import simplejson as json
-import iso8601
-import datetime
-import pytz
-import sys
-from oauth2client.client import GoogleCredentials
-from googleapiclient import discovery
-import re
+"""Example of using the Compute Engine API to manage a set of rolling
+disk snapshots.
+
+./manage-backups.py  --disk <disk1>,<zone1> \
+    --disk <disk2>,<zone2> \
+    --project <project>
+
+"""
+
 import argparse
+import datetime
+import json
+import re
+import sys
+
+from googleapiclient import discovery
+import iso8601
+from oauth2client.client import GoogleCredentials
+import pytz
+
 
 DISK_ZONE_MAP = {}
 
 def list_snapshots(compute, project, filter=None, pageToken=None):
-    result = compute.snapshots().list(project=project, pageToken=pageToken, filter=filter).execute()
-    return result
+    return compute.snapshots().list(project=project, pageToken=pageToken, filter=filter).execute()
 
-# Given a list of snapshot items, return True if a snapshot should be
-# taken, False if not.
 def should_snapshot(items, minimum_delta):
-    _items = items[:]
-    _items.sort(key=lambda x: x['creationTimestamp'])
-    _items.reverse()
-    if datetime.datetime.now(pytz.utc) > iso8601.parse_date(_items[0]['creationTimestamp']) \
-        + minimum_delta:
+    """Given a list of snapshot items, return True if a snapshot should be
+    taken, False if not.
+    """
+    sorted_items = items[:]
+    sorted_items.sort(key=lambda x: x['creationTimestamp'])
+    sorted_items.reverse()
+
+    now = datetime.datetime.now(pytz.utc)
+    created = iso8601.parse_date(sorted_items[0]['creationTimestamp'])
+
+    if now > created + minimum_delta:
         return True
+
     return False
 
 
-# Given a list of snapshot items, return the snapshots than can be
-# deleted.
 def deletable_items(items):
+    """Given a list of snapshot items, return the snapshots than can be
+    deleted.
+    """
     _items = items[:]
     _items.sort(key=lambda x: x['creationTimestamp'])
     _items.reverse()
@@ -53,7 +78,7 @@ def deletable_items(items):
     # Global reasons
 
     if len(items) < minimum_number:
-        print "Fewer than %d snapshots, not deleting any" % minimum_number
+        print("Fewer than {0} snapshots, not deleting any".format(minimum_number))
         return result
 
     # Item-specific reasons
@@ -63,58 +88,58 @@ def deletable_items(items):
         item_timestamp = iso8601.parse_date(item['creationTimestamp'])
 
         if now - item_timestamp < one_week:
-            print "Snapshot '%s' too new, not deleting." % item['name']
+            print("Snapshot '{0}' too new, not deleting.".format(item['name']))
             continue
 
         if item_timestamp.weekday() == 1 and now - item_timestamp < three_months:
-            print "Snapshot '%s' is weekly timestamp and too new, not deleting." % item['name']
+            print("Snapshot '{0}' is weekly timestamp and too new, not deleting.".format(item['name']))
             continue
 
         if item_timestamp.day == 1 and now - item_timestamp < one_year:
-            print "Snapshot '%s' is monthly timestamp and too new, not deleting." % item['name']
+            print("Snapshot '{0}' is monthly timestamp and too new, not deleting.".format(item['name']))
             continue
 
-        print "Adding snapshot '%s' to the delete list" % item['name']
+        print("Adding snapshot '{0}' to the delete list".format(item['name']))
         result.append(item)
 
     return result
 
-def create_snapshot(compute,project,disk,dry_run):
+def create_snapshot(compute, project, disk, dry_run):
 
     now = datetime.datetime.now(pytz.utc)
-    name = "%s-%s" % (disk,now.strftime('%Y-%m-%d'))
+    name = "{0}-{1}" % (disk, now.strftime('%Y-%m-%d'))
     zone = zone_from_disk(disk)
-    print "Creating snapshot '%s' in zone '%s'" % (disk,zone)
+    print("Creating snapshot '{0}' in zone '{1}'".format(disk, zone))
     if not dry_run:
         result = compute.disks().createSnapshot(project=project, disk=disk, body={"name":name}, zone=zone).execute()
 
-def delete_snapshots(compute,project,snapshots,dry_run):
+def delete_snapshots(compute, project, snapshots, dry_run):
     for snapshot in snapshots:
-        print "Deleting snapshot '%s'" % snapshot['name']
+        print("Deleting snapshot '{0}'".format(snapshot['name']))
         if not dry_run:
             result = compute.snapshots().delete(project=project, snapshot=snapshot['name']).execute()
 
 def zone_from_disk(disk):
     return DISK_ZONE_MAP[disk]
 
-def update_snapshots(compute,project,disk,dry_run):
+def update_snapshots(compute, project, disk, dry_run):
 
-    filter = "name eq %s-[0-9]{4}-[0-9]{2}-[0-9]{2}" % disk
-    result = list_snapshots(compute,project,filter=filter)
+    filter = "name eq {0}-[0-9]{{4}}-[0-9]{{2}}-[0-9]{{2}}".format(disk)
+    result = list_snapshots(compute, project, filter=filter)
 
     if not result.has_key('items'):
-        print "Disk '%s' has no snapshots. Possibly it's new or you have a typo." % disk
+        print("Disk '{0}' has no snapshots. Possibly it's new or you have a typo.".format(disk))
         snapshot_p = True
         items_to_delete = []
     else:
-        snapshot_p = should_snapshot(result['items'],datetime.timedelta(days=1))
+        snapshot_p = should_snapshot(result['items'], datetime.timedelta(days=1))
         items_to_delete = deletable_items(result['items'])
 
     if snapshot_p:
-        create_snapshot(compute,project,disk,dry_run)
+        create_snapshot(compute, project, disk, dry_run)
 
     if len(items_to_delete):
-        delete_snapshots(compute,project,items_to_deelete,dry_run)
+        delete_snapshots(compute, project, items_to_delete, dry_run)
 
 def main(args):
 
@@ -129,11 +154,12 @@ def main(args):
     project = args.project
 
     for disk in disks:
-        update_snapshots(compute,project,disk,args.dry_run)
+        update_snapshots(compute, project, disk, args.dry_run)
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Make and manage disk snapshots.')
+    parser = argparse.ArgumentParser(
+        description=__doc__)
     parser.add_argument('--dry-run', help="Show what actions would be run, but don't actually run them.",
                         action="store_true")
     parser.add_argument('--project', help="GCE project.", required=True)
@@ -146,7 +172,7 @@ if __name__ == "__main__":
         try:
             diskzone.index(',')
         except ValueError:
-            print "Disk '%s' has no comma.  Should be <disk,zone>." % disk
+            print("Disk '{0}' has no comma.  Should be <disk,zone>.".format(disk))
             sys.exit(1)
 
     main(args)
