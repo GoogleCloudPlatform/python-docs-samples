c24489ce81583b3a0a8a5373fcaf65be0ce4c1f9
Jon Wayne Parrott <jonwayne@google.com>
2016-08-16 14:58:28 -0700

    Fix lint.
    Change-Id: Idd758ddb7e5beb6d16489955cbf794011aa145cc

--- appengine/standard/urlfetch/async/rpc_test.py
+++ appengine/standard/urlfetch/async/rpc_test.py
@@ -12,12 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+from google.appengine.api import urlfetch
 import mock
 import pytest
 import rpc
 import webtest
-
-from google.appengine.api import urlfetch
 
 
 @pytest.fixture
