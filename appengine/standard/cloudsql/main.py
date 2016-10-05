967407b795f843ad48548ad886fc973c70487251
Jon Wayne Parrott <jonwayne@google.com>
2016-10-05 13:44:12 -0700

    Update standard cloudsql sample to use env vars, cloudsql v2 (#556)

--- appengine/standard/cloudsql/main.py
+++ appengine/standard/cloudsql/main.py
@@ -14,40 +14,57 @@
 
 """
 Sample App Engine application demonstrating how to connect to Google Cloud SQL
-using App Engine's native unix socket.
+using App Engine's native unix socket or using TCP when running locally.
 
 For more information, see the README.md.
 """
 
 # [START all]
-
 import os
 
 import MySQLdb
 import webapp2
 
 
-CLOUDSQL_PROJECT = '<your-project-id>'
-CLOUDSQL_INSTANCE = '<your-cloud-sql-instance>'
+# These environment variables are configured in app.yaml.
+CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
+CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
+
+
+def connect_to_cloudsql():
+    # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
+    # will be set to 'Google App Engine/version'.
+    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+        # Connect using the unix socket located at
+        # /cloudsql/cloudsql-connection-name.
+        cloudsql_unix_socket = os.path.join(
+            '/cloudsql', CLOUDSQL_CONNECTION_NAME)
+
+        db = MySQLdb.connect(
+            unix_socket=cloudsql_unix_socket,
+            user=CLOUDSQL_USER,
+            passwd=CLOUDSQL_PASSWORD)
+
+    # If the unix socket is unavailable, then try to connect using TCP. This
+    # will work if you're running a local MySQL server or using the Cloud SQL
+    # proxy, for example:
+    #
+    #   $ cloud_sql_proxy -instances=your-connection-name=tcp:3306
+    #
+    else:
+        db = MySQLdb.connect(
+            host='127.0.0.1', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
+
+    return db
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+        """Simple request handler that shows all of the MySQL variables."""
         self.response.headers['Content-Type'] = 'text/plain'
 
-        # When running on Google App Engine, use the special unix socket
-        # to connect to Cloud SQL.
-        if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
-            db = MySQLdb.connect(
-                unix_socket='/cloudsql/{}:{}'.format(
-                    CLOUDSQL_PROJECT,
-                    CLOUDSQL_INSTANCE),
-                user='root')
-        # When running locally, you can either connect to a local running
-        # MySQL instance, or connect to your Cloud SQL instance over TCP.
-        else:
-            db = MySQLdb.connect(host='localhost', user='root')
-
+        db = connect_to_cloudsql()
         cursor = db.cursor()
         cursor.execute('SHOW VARIABLES')
 
