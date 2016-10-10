e6ac821769b63102604645d2b906c049afafa7fc
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-10-10 14:51:13 -0700

    make small changes and add licenses

--- appengine/flexible/multiple_services/api_gateway/gateway/services_config.py
+++ appengine/flexible/multiple_services/api_gateway/gateway/services_config.py
@@ -1,3 +1,17 @@
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 from flask import Flask
 
@@ -10,7 +24,8 @@ SERVICES = {
 
 def make_app(name):
     app = Flask(name)
-    environment = 'production' if os.environ.get('GAE_LONG_APP_ID') else 'development'
+    deployed = os.environ.get('GAE_INSTANCE', os.environ.get('GAE_MODULE_INSTANCE'))
+    environment = environment or 'development'
     app.config['SERVICE_MAP'] = map_services(environment)
     return app
 
