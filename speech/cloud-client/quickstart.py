a122a2f2585e7573a473d3ee2227320a0aa67712
Jason Dobry <jason.dobry@gmail.com>
2016-11-15 14:28:54 -0800

    Address comments.

--- speech/cloud-client/quickstart.py
+++ speech/cloud-client/quickstart.py
@@ -27,13 +27,19 @@ def run_quickstart():
     speech_client = speech.Client()
 
     # The name of the audio file to transcribe
-    file_name = os.path.join(os.path.dirname(__file__), 'resources/audio.raw')
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        'resources',
+        'audio.raw')
 
     # Loads the audio into memory
     with io.open(file_name, 'rb') as audio_file:
         content = audio_file.read()
         audio_sample = speech_client.sample(
-            content, None, 'LINEAR16', 16000)
+            content,
+            source_uri=None,
+            encoding='LINEAR16',
+            sample_rate=16000)
 
     # Detects speech in the audio file
     alternatives = speech_client.speech_api.sync_recognize(audio_sample)
