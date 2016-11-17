f4fab1cdab1e6e7545713b9d688e354c01fa3581
Wencheng Lu <wlu@google.com>
2016-11-17 14:50:46 -0800

    fix import order

--- endpoints/bookstore-grpc/status.py
+++ endpoints/bookstore-grpc/status.py
@@ -12,14 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import status
-
 from contextlib import contextmanager
 
 import grpc
 
-
-Code = grpc.StatusCode
+import status
 
 
 @contextmanager
