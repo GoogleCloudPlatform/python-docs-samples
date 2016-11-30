6f6134bdcc086f07b831bdfc3ca2fecd84c89317
Ryan Matsumoto <ryanmats@google.com>
2016-11-29 17:00:10 -0800

    Added set -e to prepare-testing-project.sh script as per Jerjou's comments

--- scripts/prepare-testing-project.sh
+++ scripts/prepare-testing-project.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+
 GCLOUD_PROJECT=$(gcloud config list project --format="value(core.project)" 2>/dev/null)
 
 echo "Configuring project $GCLOUD_PROJECT for system tests."
