53050d2cbcdf786b5a0da57d02a2b611e3a6f613
Ryan Matsumoto <ryanmats@google.com>
2016-12-13 14:00:17 -0800

    Added comment about starting proxy from command line

--- appengine/standard/django/mysite/settings.py
+++ appengine/standard/django/mysite/settings.py
@@ -94,8 +94,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # [START db_setup]
 if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine'):
-    # Running on production App Engine, so connect to Google Cloud SQL using the unix socket
-    # at /cloudsql/<your-cloudsql-connection string>
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
@@ -107,7 +107,10 @@ if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine'):
     }
 else:
     # Running locally so connect to either a local MySQL instance or connect to
-    # Cloud SQL via the proxy. To start the proxy:
+    # Cloud SQL via the proxy. To start the proxy via command line:
+
+    #     $ ./cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
+
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
