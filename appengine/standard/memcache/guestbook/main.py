c24ce632b5df38a98db0f7745b28c97570806519
Ryan Matsumoto <ryanmats@google.com>
2017-01-06 12:46:12 -0800

    Updated memcache error checking to handle large object exception (#748)

--- appengine/standard/memcache/guestbook/main.py
+++ appengine/standard/memcache/guestbook/main.py
@@ -85,9 +85,13 @@ class MainPage(webapp2.RequestHandler):
         greetings = memcache.get('{}:greetings'.format(guestbook_name))
         if greetings is None:
             greetings = self.render_greetings(guestbook_name)
-            if not memcache.add('{}:greetings'.format(guestbook_name),
-                                greetings, 10):
-                logging.error('Memcache set failed.')
+            try:
+                added = memcache.add(
+                    '{}:greetings'.format(guestbook_name), greetings, 10)
+                if not added:
+                    logging.error('Memcache set failed.')
+            except ValueError:
+                logging.error('Memcache set failed - data larger than 1MB')
         return greetings
     # [END check_memcache]
 
