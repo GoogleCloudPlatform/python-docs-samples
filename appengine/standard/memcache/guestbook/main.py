97d1434c93e567a23a2fc027845f36f38d6dc936
Ryan Matsumoto <ryanmats@google.com>
2017-01-06 11:36:16 -0800

    Fixed style issues

--- appengine/standard/memcache/guestbook/main.py
+++ appengine/standard/memcache/guestbook/main.py
@@ -86,14 +86,12 @@ class MainPage(webapp2.RequestHandler):
         if greetings is None:
             greetings = self.render_greetings(guestbook_name)
             try:
-                added = memcache.add('{}:greetings'.format(guestbook_name),
-                                     greetings, 10)
+                added = memcache.add(
+                    '{}:greetings'.format(guestbook_name), greetings, 10)
                 if not added:
                     logging.error('Memcache set failed.')
             except ValueError:
                 logging.error('Memcache set failed - data larger than 1MB')
-            except Exception as e:
-                logging.error('Memcache set failed - {}'.format(e))
         return greetings
     # [END check_memcache]
 
