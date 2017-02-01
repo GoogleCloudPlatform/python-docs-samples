6efa8c84717b966519e490a41b638ba29f5d21e9
Gus Class <class@google.com>
2017-02-01 15:20:35 -0800

    Review feedback

--- speech/cloud-client/transcribe.py
+++ speech/cloud-client/transcribe.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-# Copyright 2016 Google Inc. All Rights Reserved.
+
+# Copyright 2017 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """Google Cloud Speech API sample application using the REST API for batch
 processing.
 
