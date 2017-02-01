228ddf822b1c7eceea704c8678099a1cd70970a9
Gus Class <class@google.com>
2017-02-01 13:24:35 -0800

    Changes example code to use cloud client and adds usage examples.

--- speech/api-client/transcribe_async.py
+++ speech/api-client/transcribe_async.py
@@ -13,33 +13,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Google Cloud Speech API sample application using the REST API for async
-batch processing."""
+batch processing.
+
+Example usage: python transcribe_async.py resources/audio.raw
+"""
 
 # [START import_libraries]
 import argparse
-import base64
-import json
+import io
 import time
-
-from googleapiclient import discovery
-import httplib2
-from oauth2client.client import GoogleCredentials
 # [END import_libraries]
-
-
-# [START authenticating]
-
-
-# Application default credentials provided by env variable
-# GOOGLE_APPLICATION_CREDENTIALS
-def get_speech_service():
-    credentials = GoogleCredentials.get_application_default().create_scoped(
-        ['https://www.googleapis.com/auth/cloud-platform'])
-    http = httplib2.Http()
-    credentials.authorize(http)
-
-    return discovery.build('speech', 'v1beta1', http=http)
-# [END authenticating]
 
 
 def main(speech_file):
@@ -48,60 +31,49 @@ def main(speech_file):
     Args:
         speech_file: the name of the audio file.
     """
+    # [START authenticating]
+    # Application default credentials provided by env variable
+    # GOOGLE_APPLICATION_CREDENTIALS
+    from google.cloud import speech
+    speech_client = speech.Client()
+    # [END authenticating]
+
     # [START construct_request]
-    with open(speech_file, 'rb') as speech:
-        # Base64 encode the binary audio file for inclusion in the request.
-        speech_content = base64.b64encode(speech.read())
-
-    service = get_speech_service()
-    service_request = service.speech().asyncrecognize(
-        body={
-            'config': {
-                # There are a bunch of config options you can specify. See
-                # https://goo.gl/KPZn97 for the full list.
-                'encoding': 'LINEAR16',  # raw 16-bit signed LE samples
-                'sampleRate': 16000,  # 16 khz
-                # See http://g.co/cloud/speech/docs/languages for a list of
-                # supported languages.
-                'languageCode': 'en-US',  # a BCP-47 language tag
-            },
-            'audio': {
-                'content': speech_content.decode('UTF-8')
-                }
-            })
+    # Loads the audio into memory
+    with io.open(speech_file, 'rb') as audio_file:
+        content = audio_file.read()
+        audio_sample = speech_client.sample(
+            content,
+            source_uri=None,
+            encoding='LINEAR16',
+            sample_rate=16000)
     # [END construct_request]
+
     # [START send_request]
-    response = service_request.execute()
-    print(json.dumps(response))
+    operation = speech_client.speech_api.async_recognize(audio_sample)
+
+    retry_count = 100
+    while retry_count > 0 and not operation.complete:
+        retry_count -= 1
+        time.sleep(2)
+        operation.poll()
+
+    if not operation.complete:
+        print("Operation not complete and retry limit reached.")
+        return
+
+    alternatives = operation.results
+    for alternative in alternatives:
+        print('Transcript: {}'.format(alternative.transcript))
+        print('Confidence: {}'.format(alternative.confidence))
     # [END send_request]
-
-    name = response['name']
-    # Construct a GetOperation request.
-    service_request = service.operations().get(name=name)
-
-    while True:
-        # Give the server a few seconds to process.
-        print('Waiting for server processing...')
-        time.sleep(1)
-        # Get the long running operation with response.
-        response = service_request.execute()
-
-        if 'done' in response and response['done']:
-            break
-
-    # First print the raw json response
-    print(json.dumps(response['response'], indent=2))
-
-    # Now print the actual transcriptions
-    for result in response['response'].get('results', []):
-        print('Result:')
-        for alternative in result['alternatives']:
-            print(u'  Alternative: {}'.format(alternative['transcript']))
 
 
 # [START run_application]
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument(
         'speech_file', help='Full path of audio file to be recognized')
     args = parser.parse_args()
