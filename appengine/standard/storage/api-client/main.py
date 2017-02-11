6e8807a225b74343a93a99e47925bb4ff4e038d7
Ryan Matsumoto <ryanmats@google.com>
2017-02-10 16:35:12 -0800

    Updated storage api-client code to upload/delete objects - now testing works

--- appengine/standard/storage/api-client/main.py
+++ appengine/standard/storage/api-client/main.py
@@ -23,27 +23,52 @@ For more information about Google App Engine, see README.md in /appengine.
 """
 
 import json
+import StringIO
 
 from googleapiclient import discovery
+from googleapiclient import http
 from oauth2client.client import GoogleCredentials
 import webapp2
 
 
 # The bucket that will be used to list objects.
-BUCKET_NAME = '<your-bucket-name>'
+BUCKET_NAME = 'unique-apricot-158222'
+
+# The filename for an object to be uploaded.
+FILE_NAME = 'storage-api-client-sample-file.txt'
 
 credentials = GoogleCredentials.get_application_default()
 storage = discovery.build('storage', 'v1', credentials=credentials)
 
 
 class MainPage(webapp2.RequestHandler):
-    def get(self):
-        response = storage.objects().list(bucket=BUCKET_NAME).execute()
+    def upload_object(self, bucket, filename):
+        body = {
+            'name': FILE_NAME,
+        }
+        req = storage.objects().insert(
+            bucket=bucket, body=body, media_body=http.MediaIoBaseUpload(
+                filename, 'application/octet-stream'))
+        resp = req.execute()
+        return resp
 
+    def delete_object(self, bucket, filename):
+        req = storage.objects().delete(bucket=bucket, object=filename)
+        resp = req.execute()
+        return resp
+
+    def get(self):
+        string_io_file = StringIO.StringIO()
+        string_io_file.write('Hello World!')
+        self.upload_object(BUCKET_NAME, string_io_file)
+
+        response = storage.objects().list(bucket=BUCKET_NAME).execute()
         self.response.write(
             '<h3>Objects.list raw response:</h3>'
             '<pre>{}</pre>'.format(
                 json.dumps(response, sort_keys=True, indent=2)))
+
+        self.delete_object(BUCKET_NAME, FILE_NAME)
 
 
 app = webapp2.WSGIApplication([
