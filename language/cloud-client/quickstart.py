534c13783b426b881fc84d4aa5489451be11b439
Gus Class <gguuss@gmail.com>
2017-02-28 11:24:31 -0800

    Updates client library to version 0.23.0 (#832)

--- language/cloud-client/quickstart.py
+++ language/cloud-client/quickstart.py
@@ -28,7 +28,7 @@ def run_quickstart():
     document = language_client.document_from_text(text)
 
     # Detects the sentiment of the text
-    sentiment = document.analyze_sentiment()
+    sentiment = document.analyze_sentiment().sentiment
 
     print('Text: {}'.format(text))
     print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
