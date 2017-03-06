54e2bd3f7647301a3d406b5c52beab9b7c0ed041
Yogesh Garg (Clic) <yg2482@columbia.edu>
2017-03-06 00:00:07 -0500

    style updates

--- speech/cloud-client/transcribe_streaming.py
+++ speech/cloud-client/transcribe_streaming.py
@@ -19,9 +19,6 @@
 Example usage:
     python transcribe_stream resources/audio.raw
  
-sources:
- * https://googlecloudplatform.github.io/google-cloud-python/stable/speech-usage.html#streaming-recognition
-
 """
 
 # [START import_libraries]
@@ -31,24 +28,28 @@ from google.cloud import speech
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-SAMPLE_RATE=16000
+SAMPLE_RATE = 16000
 speech_client = speech.Client()
 
+
 def transcribe_stream(stream):
+    """Transcribe a stream of audio."""
     logging.debug('{} is closed: {}'.format(str(stream), stream.closed))
 
     sample = speech_client.sample(stream=stream,
-                                    encoding=speech.Encoding.LINEAR16,
-                                    sample_rate=SAMPLE_RATE)
+        encoding=speech.Encoding.LINEAR16,
+        sample_rate=SAMPLE_RATE)
     results = list(sample.streaming_recognize())
     print('results len:{}'.format(len(results)))
     for result in results:
         for alternative in result.alternatives:
             print(str(alternative.confidence)+' '+str(alternative.transcript))
 
+
 def main(args):
     with open(args.filename, 'rb') as stream:
-        transcribe_stream( stream )
+        transcribe_stream(stream)
+
 
 def parse_args():
     import argparse
@@ -56,6 +57,7 @@ def parse_args():
     p.add_argument('filename', type=str)
     args = p.parse_args()
     return args
+
 
 if __name__ == '__main__':
     args = parse_args()
