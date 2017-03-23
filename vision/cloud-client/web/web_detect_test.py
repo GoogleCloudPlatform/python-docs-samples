677201632ccc63af543123beac3d150858ce4c90
Gus Class <gguuss@gmail.com>
2017-03-23 15:37:12 -0700

    Adds web detection tutorial (#874)

---
+++ vision/cloud-client/web/web_detect_test.py
@@ -0,0 +1,36 @@
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import web_detect
+
+
+def test_detect_file(cloud_config, capsys):
+    file_name = ('../detect/resources/landmark.jpg')
+    web_detect.report(web_detect.annotate(file_name))
+    out, _ = capsys.readouterr()
+    assert 'Description: Palace of Fine Arts Theatre' in out
+
+
+def test_detect_web_gsuri(cloud_config, capsys):
+    file_name = ('gs://{}/vision/landmark.jpg'.format(
+                 cloud_config.storage_bucket))
+    web_detect.report(web_detect.annotate(file_name))
+    out, _ = capsys.readouterr()
+    assert 'Description: Palace of Fine Arts Theatre' in out
+
+
+def test_detect_web_http(cloud_config, capsys):
+    web_detect.report(web_detect.annotate('https://goo.gl/X4qcB6'))
+    out, _ = capsys.readouterr()
+    assert 'https://cloud.google.com/vision/' in out
