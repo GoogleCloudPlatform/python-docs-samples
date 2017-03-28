005b710f3fb506c28ef5cb9beb260602b3b35cf1
Gus Class <class@google.com>
2017-03-28 12:46:51 -0700

    Nudge for travis

--- vision/cloud-client/face_detection/faces.py
+++ vision/cloud-client/face_detection/faces.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Draws squares around faces in the given image."""
+"""Draws squares around detected faces in the given image."""
 
 import argparse
 
