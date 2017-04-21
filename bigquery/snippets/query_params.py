536f05ab95e14d92d9186df13effdbec2cdd87ca
Tim Swast <swast@google.com>
2017-04-21 16:51:55 -0700

    Rename query file and use async method.
    Update to latest version of google-cloud-bigquery.

--- bigquery/snippets/query_params.py
+++ bigquery/snippets/query_params.py
@@ -14,20 +14,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Command-line app to perform synchronous queries with parameters in BigQuery.
+"""Command-line app to perform queries with parameters in BigQuery.
 
 For more information, see the README.md under /bigquery.
 
 Example invocation:
-    $ python sync_query_params.py --use-named-params 'romeoandjuliet' 100
-    $ python sync_query_params.py --use-positional-params 'romeoandjuliet' 100
+    $ python query_params.py --use-named-params 'romeoandjuliet' 100
+    $ python query_params.py --use-positional-params 'romeoandjuliet' 100
 """
 
 import argparse
 import datetime
+import time
+import uuid
 
 from google.cloud import bigquery
 import pytz
+
+
+def wait_for_job(job):
+    while True:
+        job.reload()  # Refreshes the state via a GET request.
+        if job.state == 'DONE':
+            if job.error_result:
+                raise RuntimeError(job.errors)
+            return
+        time.sleep(1)
 
 
 def print_results(query_results):
@@ -46,7 +58,7 @@ def print_results(query_results):
             break
 
 
-def sync_query_positional_params(corpus, min_word_count):
+def query_positional_params(corpus, min_word_count):
     client = bigquery.Client()
     query = """SELECT word, word_count
     FROM `bigquery-public-data.samples.shakespeare`
@@ -54,7 +66,8 @@ def sync_query_positional_params(corpus, min_word_count):
     AND word_count >= ?
     ORDER BY word_count DESC;
     """
-    query_results = client.run_sync_query(
+    query_job = client.run_async_query(
+        str(uuid.uuid4()),
         query,
         query_parameters=(
             bigquery.ScalarQueryParameter(
@@ -68,12 +81,15 @@ def sync_query_positional_params(corpus, min_word_count):
 
     # Only standard SQL syntax supports parameters in queries.
     # See: https://cloud.google.com/bigquery/sql-reference/
-    query_results.use_legacy_sql = False
-    query_results.run()
-    print_results(query_results)
+    query_job.use_legacy_sql = False
+
+    # Start the query and wait for the job to complete.
+    query_job.begin()
+    wait_for_job(query_job)
+    print_results(query_job.results())
 
 
-def sync_query_named_params(corpus, min_word_count):
+def query_named_params(corpus, min_word_count):
     client = bigquery.Client()
     query = """SELECT word, word_count
     FROM `bigquery-public-data.samples.shakespeare`
@@ -81,7 +97,8 @@ def sync_query_named_params(corpus, min_word_count):
     AND word_count >= @min_word_count
     ORDER BY word_count DESC;
     """
-    query_results = client.run_sync_query(
+    query_job = client.run_async_query(
+        str(uuid.uuid4()),
         query,
         query_parameters=(
             bigquery.ScalarQueryParameter('corpus', 'STRING', corpus),
@@ -89,12 +106,15 @@ def sync_query_named_params(corpus, min_word_count):
                 'min_word_count',
                 'INT64',
                 min_word_count)))
-    query_results.use_legacy_sql = False
-    query_results.run()
-    print_results(query_results)
+    query_job.use_legacy_sql = False
+
+    # Start the query and wait for the job to complete.
+    query_job.begin()
+    wait_for_job(query_job)
+    print_results(query_job.results())
 
 
-def sync_query_array_params(gender, states):
+def query_array_params(gender, states):
     client = bigquery.Client()
     query = """SELECT name, sum(number) as count
     FROM `bigquery-public-data.usa_names.usa_1910_2013`
@@ -104,20 +124,25 @@ def sync_query_array_params(gender, states):
     ORDER BY count DESC
     LIMIT 10;
     """
-    query_results = client.run_sync_query(
+    query_job = client.run_async_query(
+        str(uuid.uuid4()),
         query,
         query_parameters=(
             bigquery.ScalarQueryParameter('gender', 'STRING', gender),
             bigquery.ArrayQueryParameter('states', 'STRING', states)))
-    query_results.use_legacy_sql = False
-    query_results.run()
-    print_results(query_results)
+    query_job.use_legacy_sql = False
+
+    # Start the query and wait for the job to complete.
+    query_job.begin()
+    wait_for_job(query_job)
+    print_results(query_job.results())
 
 
-def sync_query_timestamp_params(year, month, day, hour, minute):
+def query_timestamp_params(year, month, day, hour, minute):
     client = bigquery.Client()
     query = 'SELECT TIMESTAMP_ADD(@ts_value, INTERVAL 1 HOUR);'
-    query_results = client.run_sync_query(
+    query_job = client.run_async_query(
+        str(uuid.uuid4()),
         query,
         query_parameters=[
             bigquery.ScalarQueryParameter(
@@ -125,24 +150,31 @@ def sync_query_timestamp_params(year, month, day, hour, minute):
                 'TIMESTAMP',
                 datetime.datetime(
                     year, month, day, hour, minute, tzinfo=pytz.UTC))])
-    query_results.use_legacy_sql = False
-    query_results.run()
-    print_results(query_results)
+    query_job.use_legacy_sql = False
+
+    # Start the query and wait for the job to complete.
+    query_job.begin()
+    wait_for_job(query_job)
+    print_results(query_job.results())
 
 
-def sync_query_struct_params(x, y):
+def query_struct_params(x, y):
     client = bigquery.Client()
     query = 'SELECT @struct_value AS s;'
-    query_results = client.run_sync_query(
+    query_job = client.run_async_query(
+        str(uuid.uuid4()),
         query,
         query_parameters=[
             bigquery.StructQueryParameter(
                 'struct_value',
                 bigquery.ScalarQueryParameter('x', 'INT64', x),
                 bigquery.ScalarQueryParameter('y', 'STRING', y))])
-    query_results.use_legacy_sql = False
-    query_results.run()
-    print_results(query_results)
+    query_job.use_legacy_sql = False
+
+    # Start the query and wait for the job to complete.
+    query_job.begin()
+    wait_for_job(query_job)
+    print_results(query_job.results())
 
 
 if __name__ == '__main__':
@@ -197,15 +229,15 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.sample == 'named':
-        sync_query_named_params(args.corpus, args.min_word_count)
+        query_named_params(args.corpus, args.min_word_count)
     elif args.sample == 'positional':
-        sync_query_positional_params(args.corpus, args.min_word_count)
+        query_positional_params(args.corpus, args.min_word_count)
     elif args.sample == 'array':
-        sync_query_array_params(args.gender, args.states)
+        query_array_params(args.gender, args.states)
     elif args.sample == 'timestamp':
-        sync_query_timestamp_params(
+        query_timestamp_params(
                 args.year, args.month, args.day, args.hour, args.minute)
     elif args.sample == 'struct':
-        sync_query_struct_params(args.x, args.y)
+        query_struct_params(args.x, args.y)
     else:
         print('Unexpected value for sample')
