67c02997328428c4ed97090b7f6fd94c78b1bcd2
rhaertel80 <robbie.haertel@gmail.com>
2017-04-27 03:56:21 -0700

    Changed the endpoint from v1beta1 to v1

--- ml_engine/online_prediction/predict.py
+++ ml_engine/online_prediction/predict.py
@@ -80,7 +80,7 @@ def predict_examples(project,
         Mapping[str: any]: dictionary of prediction results defined by the
             model.
     """
-    service = googleapiclient.discovery.build('ml', 'v1beta1')
+    service = googleapiclient.discovery.build('ml', 'v1')
     name = 'projects/{}/models/{}'.format(project, model)
 
     if version is not None:
