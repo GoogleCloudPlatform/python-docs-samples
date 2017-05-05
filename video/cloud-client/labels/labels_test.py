29b006045f6ce5a9e762f09fbe00be1257bfa4fc
Gus Class <gguuss@gmail.com>
2017-05-05 10:58:17 -0700

    Adds tutorials using Cloud Client (#930)
    * Adds tutorials.
    
    * Removes unused enumerate

---
+++ video/cloud-client/labels/labels_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# Copyright 2017 Google, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+import pytest
+
+import labels
+
+BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
+LABELS_FILE_PATH = '/video/cat.mp4'
+
+
+@pytest.mark.slow
+def test_feline_video_labels(capsys):
+    labels.analyze_labels(
+        'gs://{}{}'.format(BUCKET, LABELS_FILE_PATH))
+    out, _ = capsys.readouterr()
+    assert 'Whiskers' in out
