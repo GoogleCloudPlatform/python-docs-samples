674b1430260aac1f1599f4480dbb61bde600d51c
Eli Bixby <elibixby@google.com>
2017-05-18 12:44:40 -0700

    Update predict.py

--- ml_engine/online_prediction/predict.py
+++ ml_engine/online_prediction/predict.py
@@ -43,7 +43,7 @@ def predict_json(project, model, instances, version=None):
     # Create the ML Engine service object.
     # To authenticate set the environment variable
     # GOOGLE_APPLICATION_CREDENTIALS=<path_to_service_account_file>
-    service = googleapiclient.discovery.build('ml', 'v1beta1')
+    service = googleapiclient.discovery.build('ml', 'v1')
     name = 'projects/{}/models/{}'.format(project, model)
 
     if version is not None:
