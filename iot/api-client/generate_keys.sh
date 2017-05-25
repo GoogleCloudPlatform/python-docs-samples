ea106235351ce3a4c57528edd788e7793ff5f2fd
Gus Class <gguuss@gmail.com>
2017-05-25 10:31:58 -0700

    Cloud iot core (#946)
    * Initial commit.
    
    * Changes oauth2 client
    
    * Fixes google.auth and addresses changes in v1beta1

---
+++ iot/api-client/generate_keys.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+openssl req -x509 -newkey rsa:2048 -keyout rsa_private.pem -nodes -out \
+    rsa_cert.pem -subj "/CN=unused"
+openssl ecparam -genkey -name prime256v1 -noout -out ec_private.pem
+openssl ec -in ec_private.pem -pubout -out ec_public.pem
