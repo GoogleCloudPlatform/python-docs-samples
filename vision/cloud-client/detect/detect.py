aa03a599036fe75226871eef3430d467dca5ca8a
Yu-Han Liu <yuhanliu@google.com>
2017-06-27 16:03:11 -0700

    Migrate detect_properties to gapic

--- vision/cloud-client/detect/detect.py
+++ vision/cloud-client/detect/detect.py
@@ -264,10 +264,11 @@ def detect_properties(path):
 
     image = types.Image(content=content)
 
-    props = image.detect_properties()
+    response = client.image_properties(image=image)
+    props = response.image_properties_annotation
     print('Properties:')
 
-    for color in props.colors:
+    for color in props.dominant_colors.colors:
         print('fraction: {}'.format(color.pixel_fraction))
         print('\tr: {}'.format(color.color.red))
         print('\tg: {}'.format(color.color.green))
@@ -279,12 +280,14 @@ def detect_properties_uri(uri):
     """Detects image properties in the file located in Google Cloud Storage or
     on the Web."""
     client = ImageAnnotatorClient()
-    image = types.Image(source_uri=uri)
+    image = types.Image()
+    image.source.image_uri = uri
 
-    props = image.detect_properties()
+    response = client.image_properties(image=image)
+    props = response.image_properties_annotation
     print('Properties:')
 
-    for color in props.colors:
+    for color in props.dominant_colors.colors:
         print('frac: {}'.format(color.pixel_fraction))
         print('\tr: {}'.format(color.color.red))
         print('\tg: {}'.format(color.color.green))
