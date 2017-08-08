811256830d985dc818dc00a753f4984e6474ad8d
Yu-Han Liu <yuhanliu@google.com>
2017-08-08 12:19:06 -0700

    update v1beta2

--- language/cloud-client/v1beta2/snippets.py
+++ language/cloud-client/v1beta2/snippets.py
@@ -92,7 +92,7 @@ def entities_text(text):
     for entity in entities:
         print('=' * 20)
         print(u'{:<16}: {}'.format('name', entity.name))
-        print(u'{:<16}: {}'.format('type', entity.type))
+        print(u'{:<16}: {}'.format('type', entity_type[entity.type]))
         print(u'{:<16}: {}'.format('metadata', entity.metadata))
         print(u'{:<16}: {}'.format('salience', entity.salience))
         print(u'{:<16}: {}'.format('wikipedia_url',
@@ -119,7 +119,7 @@ def entities_file(gcs_uri):
     for entity in entities:
         print('=' * 20)
         print(u'{:<16}: {}'.format('name', entity.name))
-        print(u'{:<16}: {}'.format('type', entity.type))
+        print(u'{:<16}: {}'.format('type', entity_type[entity.type]))
         print(u'{:<16}: {}'.format('metadata', entity.metadata))
         print(u'{:<16}: {}'.format('salience', entity.salience))
         print(u'{:<16}: {}'.format('wikipedia_url',
