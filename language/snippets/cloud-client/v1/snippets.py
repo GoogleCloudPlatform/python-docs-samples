31d3b5bd0dd94846afdf7c96b38f9c90d623d877
Yu-Han Liu <dizcology@hotmail.com>
2017-08-08 15:55:47 -0700

    show entity type name [(#1062)](https://github.com/GoogleCloudPlatform/python-docs-samples/issues/1062)
    * show entity type name
    
    * update v1beta2
    
    * correct indent

--- language/snippets/cloud-client/v1/snippets.py
+++ language/snippets/cloud-client/v1/snippets.py
@@ -94,10 +94,14 @@ def entities_text(text):
     #   document.type == enums.Document.Type.HTML
     entities = client.analyze_entities(document).entities
 
+    # entity types from enums.Entity.Type
+    entity_type = ('UNKNOWN', 'PERSON', 'LOCATION', 'ORGANIZATION',
+                   'EVENT', 'WORK_OF_ART', 'CONSUMER_GOOD', 'OTHER')
+
     for entity in entities:
         print('=' * 20)
         print(u'{:<16}: {}'.format('name', entity.name))
-        print(u'{:<16}: {}'.format('type', entity.type))
+        print(u'{:<16}: {}'.format('type', entity_type[entity.type]))
         print(u'{:<16}: {}'.format('metadata', entity.metadata))
         print(u'{:<16}: {}'.format('salience', entity.salience))
         print(u'{:<16}: {}'.format('wikipedia_url',
@@ -120,10 +124,14 @@ def entities_file(gcs_uri):
     #   document.type == enums.Document.Type.HTML
     entities = client.analyze_entities(document).entities
 
+    # entity types from enums.Entity.Type
+    entity_type = ('UNKNOWN', 'PERSON', 'LOCATION', 'ORGANIZATION',
+                   'EVENT', 'WORK_OF_ART', 'CONSUMER_GOOD', 'OTHER')
+
     for entity in entities:
         print('=' * 20)
         print(u'{:<16}: {}'.format('name', entity.name))
-        print(u'{:<16}: {}'.format('type', entity.type))
+        print(u'{:<16}: {}'.format('type', entity_type[entity.type]))
         print(u'{:<16}: {}'.format('metadata', entity.metadata))
         print(u'{:<16}: {}'.format('salience', entity.salience))
         print(u'{:<16}: {}'.format('wikipedia_url',
