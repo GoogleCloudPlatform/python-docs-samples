f0f2632c137fb9febbea990162d67ae6d3c7b175
Daniel De Leo <ddeleo@google.com>
2017-08-17 14:18:58 -0400

    Adding sample for making IAP request using service account with user-managed keys

--- iap/make_iap_request_external.py
+++ iap/make_iap_request_external.py
@@ -12,16 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Example use of a service account with user-managed keys
+   to authenticate to Identity-Aware Proxy."""
+
+# [START iap_make_request_external]
+import json
 import google.auth
 from google.auth.transport.requests import Request
 from google.oauth2.service_account import Credentials
 import requests
-import json
+
 
 def make_iap_request(url, client_id, json_private_key):
-<<<<<<< HEAD
-  """Makes a request to an application protected by Identity-Aware Proxy.
-=======
     """Makes a request to an application protected by Identity-Aware Proxy.
 
       Args:
@@ -43,59 +45,41 @@ def make_iap_request(url, client_id, json_private_key):
     # which is a JWT signed by Google.
     google_open_id_connect_token = get_google_open_id_connect_token(
       service_account_credentials)
->>>>>>> 990b77b... Fixing unused imports
 
-    Args:
-      url: The Identity-Aware Proxy-protected URL to fetch.
-      client_id: The client ID used by Identity-Aware Proxy.
-      json_private_key: Path to the json private key file
+    # Fetch the Identity-Aware Proxy-protected URL, including an
+    # Authorization header containing "Bearer " followed by a
+    # Google-issued OpenID Connect token for the service account.
+    resp = requests.get(
+        url,
+        headers={'Authorization': 'Bearer {}'.format(
+            google_open_id_connect_token)},
+        # Do not set verify=False in production!!
+        # This is only for sample using self-signed certs
+        verify=False)
+    return resp.text
 
-    Returns:
-      The page body, or raises an exception if the page couldn't be retrieved.
-    """
-  # Construct OAuth 2.0 service account credentials using the 
-  # service account's associated json private key file 
-  service_account_credentials = get_service_account_credentials(
-    client_id,json_private_key)
-
-  # service_account_credentials gives us a JWT signed by the service
-  # account. Next, we use that to obtain an OpenID Connect token,
-  # which is a JWT signed by Google.
-  google_open_id_connect_token = get_google_open_id_connect_token(
-    service_account_credentials)
-
-  # Fetch the Identity-Aware Proxy-protected URL, including an
-  # Authorization header containing "Bearer " followed by a
-  # Google-issued OpenID Connect token for the service account.
-  resp = requests.get(
-      url,
-      headers={'Authorization': 'Bearer {}'.format(
-          google_open_id_connect_token)},
-      # Do not set verify=False in production!!
-      # This is only for sample using self-signed certs
-      verify=False)
-  return resp.text
 
 def get_service_account_credentials(iap_client_id, json_private_key):
     """Create Service Account credential from downloaded JSON private key file.
 
     Args:
       iap_client_id: The client ID used by Identity-Aware Proxy.
-      json_private_key: The json private key file for the service account 
+      json_private_key: The json private key file for the service account
 
     Returns:
       OAuth 2.0 signed JWT-based access token (JWT-bAT)
     """
     # Create Service Account credential from downloaded JSON private key file.
-    # Note: Additional claim of 'target_audience' is required and must be 
+    # Note: Additional claim of 'target_audience' is required and must be
     # set to the clientId of the IAP Service Account
-    credentials = Credentials.from_service_account_file(json_private_key
-                 ).with_claims({'target_audience': iap_client_id})
-    # Must change credential's token uri to point to Google's 
+    credentials = Credentials.from_service_account_file(
+      json_private_key).with_claims({'target_audience': iap_client_id})
+    # Must change credential's token uri to point to Google's
     # OpenId token endpoint instead of Google's authorization endpoint
     credentials._token_uri = get_token_endpoint()
     # Generate and return OAuth 2.0 signed JWT-based access token (JWT-bAT)
     return credentials
+
 
 def get_google_open_id_connect_token(service_account_credentials):
     """Get an OpenID Connect token issued by Google for the service account.
@@ -120,13 +104,8 @@ def get_google_open_id_connect_token(service_account_credentials):
     """
     service_account_jwt = (
       service_account_credentials._make_authorization_grant_assertion())
-<<<<<<< HEAD
-    # Request an OpenID Connect (OIDC) token for the Cloud IAP-secured client ID.
-    request = google.auth.transport.requests.Request()
-=======
     # Request OpenID Connect (OIDC) token for Cloud IAP-secured client ID.
     request = Request()
->>>>>>> 990b77b... Fixing unused imports
     body = {
         'assertion': service_account_jwt,
         'grant_type': google.oauth2._client._JWT_GRANT_TYPE,
@@ -135,11 +114,14 @@ def get_google_open_id_connect_token(service_account_credentials):
         request, get_token_endpoint(), body)
     return token_response['id_token']
 
+
 def get_token_endpoint():
-  """Makes a request to Google's openid endpoint and returns the oauth token uri
-  This function eliminates the need to hardcode the oauth token uri which is subject
-  to future changes. 
-  """
-  response = requests.get("https://accounts.google.com/.well-known/openid-configuration")
-  return json.loads(response.text)["token_endpoint"]
-  
+    """Makes a request to Google's openid endpoint and returns
+    the oauth token uri. This function eliminates the need to
+    hardcode the oauth token uri which is subject to future changes.
+    """
+    response = requests.get(
+      "https://accounts.google.com/.well-known/openid-configuration")
+    return json.loads(response.text)["token_endpoint"]
+
+# [END iap_make_request_external]
