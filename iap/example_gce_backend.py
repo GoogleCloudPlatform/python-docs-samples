aa5fa11faacc06975391238d5003f3aac941e7a2
Paul Buser <buser@google.com>
2017-09-12 13:05:30 -0700

    Update IAP example backend and requirements.

--- iap/example_gce_backend.py
+++ iap/example_gce_backend.py
@@ -1,30 +1,46 @@
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 CLOUD_PROJECT_ID = 'YOUR_PROJECT_ID'
 BACKEND_SERVICE_ID = 'YOUR_BACKEND_SERVICE_ID'
 
-from flask import Flask
-from flask import request
-
 import platform
 
-from validate_jwt import validate_iap_jwt_from_compute_engine
+import flask
 
-app = Flask(__name__)
+import validate_jwt
+
+app = flask.Flask(__name__)
+
 
 @app.route('/')
 def root():
-    jwt = request.headers.get('x-goog-iap-jwt-assertion')
+    jwt = flask.request.headers.get('x-goog-iap-jwt-assertion')
     if jwt is None:
         return 'Unauthorized request.', 401
-    user_id, user_email, error_str = validate_iap_jwt_from_compute_engine(
-            jwt, CLOUD_PROJECT_ID, BACKEND_SERVICE_ID)
+    user_id, user_email, error_str = validate_jwt.validate_iap_jwt_from_compute_engine(
+        jwt, CLOUD_PROJECT_ID, BACKEND_SERVICE_ID)
     if error_str:
-        return "Error: %s" % error_str
+        return 'Error: {}'.format(error_str)
     else:
-      return "Hi, {}. I am {}.".format(user_email, platform.node())
+        return 'Hi, {}. I am {}.'.format(user_email, platform.node())
+
 
 @app.route('/healthz')
 def health():
     return 'OK', 200
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run()
