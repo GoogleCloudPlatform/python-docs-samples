7cce976f2be3ad4d2ca69fa8c7a25f3b85a88b67
Jon Wayne Parrott <jonwayne@google.com>
2017-09-13 12:44:30 -0700

    Update testing to use Python 3.6
    Change-Id: I33e152ff826cd2fac966e420d8251f4c43cc3648

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -6,7 +6,7 @@ if [[ $TRAVIS_SECURE_ENV_VARS == "true" ]]; then
     source ${TRAVIS_BUILD_DIR}/testing/test-env.sh;
     export GOOGLE_APPLICATION_CREDENTIALS=${TRAVIS_BUILD_DIR}/testing/service-account.json
     export GOOGLE_CLIENT_SECRETS=${TRAVIS_BUILD_DIR}/testing/client-secrets.json
-    nox --envdir /tmp --stop-on-first-error -s lint gae py35 -- -m "not slow";
+    nox --envdir /tmp --stop-on-first-error -s lint gae py36 -- -m "not slow";
 else
     # only run lint on external PRs
     echo 'External PR: only running lint.'
