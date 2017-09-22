fd722263cae4ae3e742bb049f3bc7d4270279733
Brandon Yarbrough <yarbrough@google.com>
2017-09-22 13:40:09 -0700

    Fix ordered list ordinals.

--- storage/cloud-client/notification_polling.py
+++ storage/cloud-client/notification_polling.py
@@ -31,19 +31,19 @@ below:
 2. Activate the Google Cloud Pub/Sub API, if you have not already done so.
    https://console.cloud.google.com/flows/enableapi?apiid=pubsub
 
-2. Create a Google Cloud Storage bucket:
+3. Create a Google Cloud Storage bucket:
    $ gsutil mb gs://testbucket
 
-3. Create a Cloud Pub/Sub topic and publish bucket notifications there:
+4. Create a Cloud Pub/Sub topic and publish bucket notifications there:
    $ gsutil notification create -f json -t testtopic gs://testbucket
 
-4. Create a subscription for your new topic:
+5. Create a subscription for your new topic:
    $ gcloud beta pubsub subscriptions create testsubscription --topic=testtopic
 
-5. Run this program:
+6. Run this program:
    $ python notification_polling my-project-id testsubscription
 
-6. While the program is running, upload and delete some files in the testbucket
+7. While the program is running, upload and delete some files in the testbucket
    bucket (you could use the console or gsutil) and watch as changes scroll by
    in the app.
 """
