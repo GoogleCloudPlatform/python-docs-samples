cb9c8d2a26b11a05f1f12d2c70ed4034314b1ed1
Jon Wayne Parrott <jonwayne@google.com>
2017-10-24 11:11:20 -0700

    Upgrade pip in kokoro
    Change-Id: I9e0b8bedd6cd80cf1abb63733b81b6e491f82346

--- .kokoro/system_tests.sh
+++ .kokoro/system_tests.sh
@@ -27,6 +27,7 @@ export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/testing/service-account.json
 export GOOGLE_CLIENT_SECRETS=$(pwd)/testing/client-secrets.json
 
 # Run tests
+pip install --upgrade pip
 pip install --upgrade nox-automation
 nox -k "${NOX_SESSION}" || ret_code=$?
 
