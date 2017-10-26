937455f1233030fd12d2b89d3b2d813be812f16e
Alix Hamilton <alixh@google.com>
2017-10-26 10:36:53 -0700

    fixes typo in sample

--- bigquery/cloud-client/quickstart.py
+++ bigquery/cloud-client/quickstart.py
@@ -26,7 +26,7 @@ def run_quickstart():
     # The name for the new dataset
     dataset_id = 'my_new_dataset'
 
-    # Prepares a reference the new dataset
+    # Prepares a reference to the new dataset
     dataset_ref = bigquery_client.dataset(dataset_id)
     dataset = bigquery.Dataset(dataset_ref)
 
