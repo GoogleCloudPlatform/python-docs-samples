35fbe61f69db77f9b03b15d2df50d9598f4dbcfa
michaelawyu <chenyumic@google.com>
2017-12-06 14:53:23 -0800

    Updated the certificate (#1251)

--- iot/api-client/generate_keys.sh
+++ iot/api-client/generate_keys.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-openssl req -x509 -newkey rsa:2048 -keyout rsa_private.pem -nodes -out \
+openssl req -x509 -newkey rsa:2048 -days 3650 -keyout rsa_private.pem -nodes -out \
     rsa_cert.pem -subj "/CN=unused"
 openssl ecparam -genkey -name prime256v1 -noout -out ec_private.pem
 openssl ec -in ec_private.pem -pubout -out ec_public.pem
