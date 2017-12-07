3e5ab295e0f537f66c7bd379ef06b5a09cae9a34
Tim Swast <swast@google.com>
2017-12-07 09:46:43 -0800

    Fix broken link to core Client service account helper. (#1256)

--- bigquery/cloud-client/auth_snippets.py
+++ bigquery/cloud-client/auth_snippets.py
@@ -35,8 +35,7 @@ def explicit():
 
     # Explicitly use service account credentials by specifying the private key
     # file. All clients in google-cloud-python have this helper, see
-    # https://google-cloud-python.readthedocs.io/en/latest/core/modules.html
-    #   #google.cloud.client.Client.from_service_account_json
+    # https://googlecloudplatform.github.io/google-cloud-python/latest/core/auth.html#service-accounts
     bigquery_client = bigquery.Client.from_service_account_json(
         'service_account.json')
 
