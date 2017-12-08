0ef8f56a3f02687b573d31c499ca72afc43105fc
Gus Class <gguuss@gmail.com>
2017-12-08 13:43:44 -0800

    Tests for device over HTTP and adds example for getting configuration. (#1255)
    * Tests for device over HTTP and adds example for getting configuration.
    
    * Fixes circle
    
    * Fixes circle
    
    * Print is a function
    
    * Python3 encodings
    
    * Add note for manager import

--- iot/api-client/http_example/cloudiot_http_example.py
+++ iot/api-client/http_example/cloudiot_http_example.py
@@ -51,7 +51,7 @@ def create_jwt(project_id, private_key_file, algorithm):
     print('Creating JWT using {} from private key file {}'.format(
             algorithm, private_key_file))
 
-    return jwt.encode(token, private_key, algorithm=algorithm)
+    return jwt.encode(token, private_key, algorithm=algorithm).decode('ascii')
 
 
 def publish_message(
@@ -72,15 +72,35 @@ def publish_message(
             url_suffix)
 
     body = None
+    msg_bytes = base64.urlsafe_b64encode(message.encode('utf-8'))
     if message_type == 'event':
-        body = {'binary_data': base64.urlsafe_b64encode(message)}
+        body = {'binary_data': msg_bytes.decode('ascii')}
     else:
         body = {
-          'state': {'binary_data': base64.urlsafe_b64encode(message)}
+          'state': {'binary_data': msg_bytes.decode('ascii')}
         }
 
     resp = requests.post(
             publish_url, data=json.dumps(body), headers=headers)
+
+    return resp
+
+
+def get_config(
+        version, message_type, base_url, project_id, cloud_region, registry_id,
+        device_id, jwt_token):
+    headers = {
+            'authorization': 'Bearer {}'.format(jwt_token),
+            'content-type': 'application/json',
+            'cache-control': 'no-cache'
+    }
+
+    basepath = '{}/projects/{}/locations/{}/registries/{}/devices/{}/'
+    template = basepath + 'config?local_version={}'
+    config_url = template.format(
+        base_url, project_id, cloud_region, registry_id, device_id, version)
+
+    resp = requests.get(config_url, headers=headers)
 
     return resp
 
@@ -142,6 +162,10 @@ def main():
             args.project_id, args.private_key_file, args.algorithm)
     jwt_iat = datetime.datetime.utcnow()
     jwt_exp_mins = args.jwt_expires_minutes
+
+    print('Latest configuration: {}'.format(get_config(
+        '0', args.message_type, args.base_url, args.project_id,
+        args.cloud_region, args.registry_id, args.device_id, jwt_token).text))
 
     # Publish num_messages mesages to the HTTP bridge once per second.
     for i in range(1, args.num_messages + 1):
