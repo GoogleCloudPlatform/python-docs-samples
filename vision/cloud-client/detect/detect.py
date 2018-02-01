f4a0a669d803b4f9ec38cf2364a62b5e490f2302
Yu-Han Liu <yuhanliu@google.com>
2018-02-01 14:42:12 -0800

    update tests, flake

--- vision/cloud-client/detect/detect.py
+++ vision/cloud-client/detect/detect.py
@@ -528,7 +528,8 @@ def detect_crop_hints(path):
     image = vision.types.Image(content=content)
 
     crop_hints_params = vision.types.CropHintsParams(aspect_ratios=[1.77])
-    image_context = vision.types.ImageContext(crop_hints_params=crop_hints_params)
+    image_context = vision.types.ImageContext(
+        crop_hints_params=crop_hints_params)
 
     response = client.crop_hints(image=image, image_context=image_context)
     hints = response.crop_hints_annotation.crop_hints
