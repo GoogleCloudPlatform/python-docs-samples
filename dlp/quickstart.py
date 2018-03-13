96b0e4f5f9898ddcacf16fbabb46c3e4f6debe0a
Andrew Gorcester <gorcester@google.com>
2018-03-13 14:58:02 -0700

    update redact_image, quickstart samples

--- dlp/quickstart.py
+++ dlp/quickstart.py
@@ -22,20 +22,31 @@ def quickstart():
     """Demonstrates use of the Data Loss Prevention API client library."""
 
     # [START quickstart]
+    import os
+    import sys
+
     # Import the client library
-    import google.cloud.dlp_v2beta1
+    import google.cloud.dlp
 
     # Instantiate a client.
-    dlp = google.cloud.dlp_v2beta1.DlpServiceClient()
+    dlp = google.cloud.dlp.DlpServiceClient()
+
+    # Pull the project ID from an environment variable.
+    try:
+        project = os.environ['GCLOUD_PROJECT']
+    except KeyError:
+        print('Please specify a project ID in the GCLOUD_PROJECT environment '
+              'variable.')
+        sys.exit(1)
 
     # The string to inspect
     content = 'Robert Frost'
 
-    # Construct the list of content items to inspect; in this case, only one.
-    items = [{'type': 'text/plain', 'value': content}]
+    # Construct the item to inspect.
+    item = {'value': content}
 
-    # The info types to search for in the content.
-    info_types = [{'name': 'US_MALE_NAME'}, {'name': 'US_FEMALE_NAME'}]
+    # The info types to search for in the content. Required.
+    info_types = [{'name': 'FIRST_NAME'}, {'name': 'LAST_NAME'}]
 
     # The minimum likelihood to constitute a match. Optional.
     min_likelihood = 'LIKELIHOOD_UNSPECIFIED'
@@ -51,16 +62,19 @@ def quickstart():
     inspect_config = {
         'info_types': info_types,
         'min_likelihood': min_likelihood,
-        'max_findings': max_findings,
         'include_quote': include_quote,
+        'limits': {'max_findings_per_request': max_findings},
     }
 
+    # Convert the project id into a full resource id.
+    parent = dlp.project_path(project)
+
     # Call the API.
-    response = dlp.inspect_content(inspect_config, items)
+    response = dlp.inspect_content(parent, inspect_config, item)
 
     # Print out the results.
-    if response.results[0].findings:
-        for finding in response.results[0].findings:
+    if response.result.findings:
+        for finding in response.result.findings:
             try:
                 print('Quote: {}'.format(finding.quote))
             except AttributeError:
