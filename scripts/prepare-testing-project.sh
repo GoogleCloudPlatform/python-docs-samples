bfc4357838394ce9582e6772a250ba86cbc813b7
nnegrey <nnegrey@google.com>
2018-03-21 16:25:03 -0700

    Add Text-To-Speech api to prepare testing script

--- scripts/prepare-testing-project.sh
+++ scripts/prepare-testing-project.sh
@@ -50,4 +50,4 @@ echo "Creating speech resources."
 gsutil cp speech/api-client/resources/audio.raw gs://$GCLOUD_PROJECT/speech/
 
 echo "To finish setup, follow this link to enable APIs."
-echo "https://console.cloud.google.com/flows/enableapi?project=${GCLOUD_PROJECT}&apiid=bigtable.googleapis.com,bigtableadmin.googleapis.com,bigquery,bigquerydatatransfer.googleapis.com,cloudmonitoring,compute_component,datastore,datastore.googleapis.com,dataproc,dns,plus,pubsub,logging,storage_api,vision.googleapis.com"
+echo "https://console.cloud.google.com/flows/enableapi?project=${GCLOUD_PROJECT}&apiid=bigtable.googleapis.com,bigtableadmin.googleapis.com,bigquery,bigquerydatatransfer.googleapis.com,cloudmonitoring,compute_component,datastore,datastore.googleapis.com,dataproc,dns,plus,pubsub,logging,storage_api,texttospeech.googleapis.com,vision.googleapis.com"
