0dcff9a082c6bf5410b2887958afc529db0822e0
Jisha Abubaker <jishaa@google.com>
2018-05-08 23:53:45 -0700

    fixing lint error

--- memorystore/redis/main.py
+++ memorystore/redis/main.py
@@ -23,6 +23,7 @@ redis_host = os.environ.get('REDISHOST', 'localhost')
 redis_port = int(os.environ.get('REDISPORT', 6379))
 redis_client = redis.StrictRedis(host=redis_host, port=redis_port)
 
+
 @app.route('/')
 def index():
     value = redis_client.incr('counter', 1)
