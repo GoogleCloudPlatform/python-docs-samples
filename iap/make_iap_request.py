dc6a338005dab330a0e867532d9ed8697c362090
danieldeleo <danieldeleo@users.noreply.github.com>
2018-05-16 12:27:32 -0400

    Adding ability to make reqeusts of all method types: GET, POST, PUT etc (#1481)

--- iap/make_iap_request.py
+++ iap/make_iap_request.py
@@ -30,12 +30,16 @@ IAM_SCOPE = 'https://www.googleapis.com/auth/iam'
 OAUTH_TOKEN_URI = 'https://www.googleapis.com/oauth2/v4/token'
 
 
-def make_iap_request(url, client_id):
+def make_iap_request(url, client_id, method='GET', **kwargs):
     """Makes a request to an application protected by Identity-Aware Proxy.
 
     Args:
       url: The Identity-Aware Proxy-protected URL to fetch.
       client_id: The client ID used by Identity-Aware Proxy.
+      method: The request method to use
+              ('GET', 'OPTIONS', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE')
+      **kwargs: Any of the parameters defined for the request function:
+                https://github.com/requests/requests/blob/master/requests/api.py
 
     Returns:
       The page body, or raises an exception if the page couldn't be retrieved.
@@ -94,10 +98,10 @@ def make_iap_request(url, client_id):
     # Fetch the Identity-Aware Proxy-protected URL, including an
     # Authorization header containing "Bearer " followed by a
     # Google-issued OpenID Connect token for the service account.
-    resp = requests.get(
-        url,
+    resp = requests.request(
+        method, url,
         headers={'Authorization': 'Bearer {}'.format(
-            google_open_id_connect_token)})
+            google_open_id_connect_token)}, **kwargs)
     if resp.status_code == 403:
         raise Exception('Service account {} does not have permission to '
                         'access the IAP-protected application.'.format(
