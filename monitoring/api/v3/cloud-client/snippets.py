e45b0be7630ef89ad8f4a728f2d1734a4593de29
Jeffrey Rennie <rennie@google.com>
2018-05-18 10:26:34 -0700

    First check GOOGLE_CLOUD_PROJECT environment variable.

--- monitoring/api/v3/cloud-client/snippets.py
+++ monitoring/api/v3/cloud-client/snippets.py
@@ -213,7 +213,8 @@ def project_id():
     Returns:
         str -- the project name
     """
-    project_id = os.environ['GCLOUD_PROJECT']
+    project_id = (os.environ['GOOGLE_CLOUD_PROJECT'] or
+                  os.environ['GCLOUD_PROJECT'])
 
     if not project_id:
         raise MissingProjectIdError(
