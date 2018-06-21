cf3c6c6d61f03ec6346533bf51f6b39c5e2cb072
hirokts <hkchatar@gmail.com>
2018-06-21 20:59:33 +0900

    modify importing of mysite/urls.py

--- container_engine/django_tutorial/mysite/urls.py
+++ container_engine/django_tutorial/mysite/urls.py
@@ -14,8 +14,9 @@
 
 from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
