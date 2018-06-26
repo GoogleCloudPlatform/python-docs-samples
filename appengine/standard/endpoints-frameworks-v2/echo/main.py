f0a29068110cca1cb33038dc878d18a0eb22070e
Rose Davidson <rose@inklesspen.com>
2018-06-26 12:52:04 -0700

    Change Endpoints Frameworks samples to import from endpoints (#1542)
    * Update Endpoints Framework for Python dependencies.
    
    These stopped being auto-updated sometime in March; that's bad.
    
    * Change Endpoints Frameworks samples to import from endpoints
    
    Instead of importing from protorpc, import from endpoints.

--- appengine/standard/endpoints-frameworks-v2/echo/main.py
+++ appengine/standard/endpoints-frameworks-v2/echo/main.py
@@ -17,9 +17,9 @@ Endpoints."""
 
 # [START imports]
 import endpoints
-from protorpc import message_types
-from protorpc import messages
-from protorpc import remote
+from endpoints import message_types
+from endpoints import messages
+from endpoints import remote
 # [END imports]
 
 
