440e5d23d34e665c218dc26441c8aa3f238ea194
happyhuman <shahins@google.com>
2018-07-03 10:23:37 -0700

    Added unit test

--- speech/cloud-client/transcribe_word_level_confidence.py
+++ speech/cloud-client/transcribe_word_level_confidence.py
@@ -26,75 +26,75 @@ import argparse
 
 
 # [START speech_transcribe_word_level_confidence]
-def speech_transcribe_word_level_confidence(speech_file):
-  """Transcribe the given audio file synchronously with
-    the selected model."""
-  from google.cloud import speech_v1p1beta1 as speech
-  client = speech.SpeechClient()
+def speech_transcribe_word_level_conf(speech_file):
+    """Transcribe the given audio file synchronously with
+      word level confidence."""
+    from google.cloud import speech_v1p1beta1 as speech
+    client = speech.SpeechClient()
 
-  with open(speech_file, 'rb') as audio_file:
-    content = audio_file.read()
+    with open(speech_file, 'rb') as audio_file:
+        content = audio_file.read()
 
-  audio = speech.types.RecognitionAudio(content=content)
+    audio = speech.types.RecognitionAudio(content=content)
 
-  config = speech.types.RecognitionConfig(
-      encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
-      sample_rate_hertz=16000,
-      language_code='en-US',
-      enable_word_confidence=True)
+    config = speech.types.RecognitionConfig(
+        encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
+        language_code='en-US',
+        enable_word_confidence=True)
 
-  response = client.recognize(config, audio)
+    response = client.recognize(config, audio)
 
-  for i, result in enumerate(response.results):
-    alternative = result.alternatives[0]
-    print('-' * 20)
-    print('First alternative of result {}'.format(i))
-    print(u'Transcript: {}'.format(alternative.transcript))
-    print(u'First Word and Confidence:{} {}'.format(
-        alternative.words[0].word, alternative.words[0].confidence))
+    for i, result in enumerate(response.results):
+        alternative = result.alternatives[0]
+        print('-' * 20)
+        print('First alternative of result {}'.format(i))
+        print(u'Transcript: {}'.format(alternative.transcript))
+        print(u'First Word and Confidence:{} {}'.format(
+            alternative.words[0].word, alternative.words[0].confidence))
 
 
 # [END speech_transcribe_word_level_confidence]
 
 
 # [START speech_transcribe_word_level_confidence_gcs]
-def speech_transcribe_word_level_confidence_gcs(gcs_uri):
-  """Transcribe the given audio file asynchronously with
-    the selected model."""
-  from google.cloud import speech_v1p1beta1 as speech
-  client = speech.SpeechClient()
+def speech_transcribe_word_level_conf_gcs(gcs_uri):
+    """Transcribe the given audio file asynchronously with
+      word level confidence."""
+    from google.cloud import speech_v1p1beta1 as speech
+    client = speech.SpeechClient()
 
-  audio = speech.types.RecognitionAudio(uri=gcs_uri)
+    audio = speech.types.RecognitionAudio(uri=gcs_uri)
 
-  config = speech.types.RecognitionConfig(
-      encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
-      sample_rate_hertz=16000,
-      language_code='en-US',
-      enable_word_confidence=True)
+    config = speech.types.RecognitionConfig(
+        encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
+        language_code='en-US',
+        enable_word_confidence=True)
 
-  print('Waiting for operation to complete...')
-  response = client.recognize(config, audio)
+    print('Waiting for operation to complete...')
+    response = client.recognize(config, audio)
 
-  for i, result in enumerate(response.results):
-    alternative = result.alternatives[0]
-    print('-' * 20)
-    print('First alternative of result {}'.format(i))
-    print(u'Transcript: {}'.format(alternative.transcript))
-    print(u'First Word and Confidence:{} {}'.format(
-        alternative.words[0].word, alternative.words[0].confidence))
+    for i, result in enumerate(response.results):
+        alternative = result.alternatives[0]
+        print('-' * 20)
+        print('First alternative of result {}'.format(i))
+        print(u'Transcript: {}'.format(alternative.transcript))
+        print(u'First Word and Confidence:{} {}'.format(
+            alternative.words[0].word, alternative.words[0].confidence))
 
 
 # [END speech_transcribe_word_level_confidence_gcs]
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(
-      description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
-  parser.add_argument(
-      'path', help='File or GCS path for audio file to be recognized')
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        'path', help='File or GCS path for audio file to be recognized')
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  if args.path.startswith('gs://'):
-    speech_transcribe_word_level_confidence_gcs(args.path)
-  else:
-    speech_transcribe_word_level_confidence(args.path)
+    if args.path.startswith('gs://'):
+        speech_transcribe_word_level_confidence_gcs(args.path)
+    else:
+        speech_transcribe_word_level_confidence(args.path)
