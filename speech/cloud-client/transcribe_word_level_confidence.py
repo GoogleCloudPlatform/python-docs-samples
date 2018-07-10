61b46b44e4c6519e27aaf98fe5d459b89d65c771
happyhuman <shahins@google.com>
2018-07-10 15:41:48 -0700

    Updates based on Noah' suggestions

--- speech/cloud-client/transcribe_word_level_confidence.py
+++ speech/cloud-client/transcribe_word_level_confidence.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2017 Google LLC. All Rights Reserved.
+# Copyright 2018 Google LLC. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 r"""Google Cloud Speech API sample that demonstrates how to request word
 level confidence information.
 
