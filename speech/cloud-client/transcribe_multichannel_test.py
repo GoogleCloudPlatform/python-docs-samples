b74c4ecc5bdc56a4d9129096abcbbbe9ef7c464b
happyhuman <shahins@google.com>
2018-07-10 15:44:00 -0700

    Updates based on Noah's suggestions

--- speech/cloud-client/transcribe_multichannel_test.py
+++ speech/cloud-client/transcribe_multichannel_test.py
@@ -1,4 +1,4 @@
-# Copyright 2016, Google, LLC.
+# Copyright 2018, Google, LLC.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -17,7 +17,7 @@ import re
 import transcribe_multichannel
 
 RESOURCES = os.path.join(os.path.dirname(__file__), 'resources')
-OUTPUT1 = r'OK Google stream stranger things from Netflix to my TV'
+OUTPUT1 = 'OK Google stream stranger things from Netflix to my TV'
 
 
 def test_transcribe_multichannel_file(capsys):
