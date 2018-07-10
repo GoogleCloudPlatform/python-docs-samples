661a725790f3b60e38fd573134350601d231a055
happyhuman <shahins@google.com>
2018-07-10 15:12:34 -0700

    Updated library version

--- speech/cloud-client/transcribe_word_level_confidence.py
+++ speech/cloud-client/transcribe_word_level_confidence.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2017 Google Inc. All Rights Reserved.
+# Copyright 2017 Google LLC. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
