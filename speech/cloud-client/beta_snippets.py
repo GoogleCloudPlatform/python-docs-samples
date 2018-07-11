18cfacc51a38898abe97a588791e41139190e119
happyhuman <shahins@google.com>
2018-07-11 11:28:56 -0700

    Moved the sample to the right python file

--- speech/cloud-client/beta_snippets.py
+++ speech/cloud-client/beta_snippets.py
@@ -21,6 +21,7 @@ Example usage:
     python beta_snippets.py enhanced-model resources/commercial_mono.wav
     python beta_snippets.py metadata resources/commercial_mono.wav
     python beta_snippets.py punctuation resources/commercial_mono.wav
+    python beta_snippets.py multi-channel resources/commercial_mono.wav
 """
 
 import argparse
@@ -126,6 +127,35 @@ def transcribe_file_with_auto_punctuation(path):
 # [END speech_transcribe_file_with_auto_punctuation]
 
 
+# [START speech_transcribe_multichannel]
+def transcribe_file_with_multichannel(speech_file):
+    """Transcribe the given audio file synchronously with
+      multi channel."""
+    client = speech.SpeechClient()
+
+    with open(speech_file, 'rb') as audio_file:
+        content = audio_file.read()
+
+    audio = speech.types.RecognitionAudio(content=content)
+
+    config = speech.types.RecognitionConfig(
+        encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
+        language_code='en-US',
+        audio_channel_count=1,
+        enable_separate_recognition_per_channel=True)
+
+    response = client.recognize(config, audio)
+
+    for i, result in enumerate(response.results):
+        alternative = result.alternatives[0]
+        print('-' * 20)
+        print('First alternative of result {}'.format(i))
+        print(u'Transcript: {}'.format(alternative.transcript))
+        print(u'Channel Tag: {}'.format(result.channel_tag))
+# [END speech_transcribe_multichannel]
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description=__doc__,
@@ -142,3 +172,5 @@ if __name__ == '__main__':
         transcribe_file_with_metadata(args.path)
     elif args.command == 'punctuation':
         transcribe_file_with_auto_punctuation(args.path)
+    elif args.command == 'multi-channel':
+        transcribe_file_with_multichannel(args.path)
