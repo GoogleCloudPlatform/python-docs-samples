b90133606fd76f539ab9efb0c4e93ace925ba157
Ace Nassri <anassri@google.com>
2018-08-06 16:03:31 -0700

    Add multipart sample
    Change-Id: I1a17e1c72bea6e7a8d18b9ed5910db09bedb753d

--- functions/http/main.py
+++ functions/http/main.py
@@ -16,14 +16,56 @@
 import json
 # [END functions_http_xml]
 
+# [START functions_http_form_data]
+import os
+import tempfile
+# [END functions_http_form_data]
+
+# [START functions_http_form_data]
+from werkzeug.utils import secure_filename
+# [END functions_http_form_data]
+
 # [START functions_http_xml]
 import xmltodict
 # [END functions_http_xml]
 
-# [START functions_http_xml]
 
+# [START functions_http_xml]
 
 def parse_xml(request):
     data = xmltodict.parse(request.data)
     return json.dumps(data, indent=2)
 # [END functions_http_xml]
+
+
+# [START functions_http_form_data]
+
+# Helper function that computes the filepath to save files to
+def get_file_path(filename):
+    # Note: tempfile.gettempdir() points to an in-memory file system
+    # on GCF. Thus, any files in it must fit in the instance's memory.
+    file_name = secure_filename(filename)
+    return os.path.join(tempfile.gettempdir(), file_name)
+
+
+def parse_multipart(request):
+    # This code will process each non-file field in the form
+    fields = {}
+    data = request.form.to_dict()
+    for field in data:
+        fields[field] = data[field]
+        print 'Processed field: %s' % field
+
+    # This code will process each file uploaded
+    files = request.files.to_dict()
+    for file_name, file in files.iteritems():
+        file.save(get_file_path(file_name))
+        print 'Processed file: %s' % file_name
+
+    # Clear temporary directory
+    for file_name in files:
+        file_path = get_file_path(file_name)
+        os.remove(file_path)
+
+    return "Done!"
+# [END functions_http_form_data]
