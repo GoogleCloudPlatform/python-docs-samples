76d2bfa1402d8e01f73eea350ae64ce451fa32bc
David Sabater <davidsabater@google.com>
2018-09-06 22:13:39 +0100

    Refactor master to table_file in code and filename

--- composer/workflows/bq_copy_across_locations.py
+++ composer/workflows/bq_copy_across_locations.py
@@ -89,14 +89,14 @@ logger = logging.getLogger('bq_copy_us_to_eu_01')
 
 def read_table_list(table_list_file):
     """
-    Reads the master CSV file that will help in creating Airflow tasks in
+    Reads the table list file that will help in creating Airflow tasks in
     the DAG dynamically.
-    :param table_list_file: (String) The file location of the master file,
-    e.g. '/home/airflow/framework/master.csv'
-    :return master_record_all: (List) List of Python dictionaries containing
-    the information for a single row in master CSV file.
+    :param table_list_file: (String) The file location of the table list file,
+    e.g. '/home/airflow/framework/table_list.csv'
+    :return table_list: (List) List of tuples containing the source and
+    target tables.
     """
-    master_record_all = []
+    table_list = []
     logger.info('Reading table_list_file from : %s' % str(table_list_file))
     try:
         with open(table_list_file, 'rb') as csv_file:
@@ -104,12 +104,12 @@ def read_table_list(table_list_file):
             next(csv_reader)  # skip the headers
             for row in csv_reader:
                 logger.info(row)
-                master_record = {
+                table_tuple = {
                     'table_source': row[0],
                     'table_dest': row[1]
                 }
-                master_record_all.append(master_record)
-            return master_record_all
+                table_list.append(table_tuple)
+            return table_list
     except IOError as e:
         logger.error('Error opening table_list_file %s: ' % str(
             table_list_file), e)
