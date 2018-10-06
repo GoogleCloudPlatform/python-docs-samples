ae05ad0d4a26c1d256a06148381c0a53f7d10ed5
Ace Nassri <anassri@google.com>
2018-10-05 19:48:45 -0700

    Add Firestore reactive sample
    Change-Id: I8ae1d62356927dca9cf62ff7fdf0f887550ec6ae

--- functions/firebase/main_test.py
+++ functions/firebase/main_test.py
@@ -12,10 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from collections import UserDict
 from datetime import datetime
 import json
-
 import uuid
+
+from mock import MagicMock, patch
 
 import main
 
@@ -83,3 +85,40 @@ def test_auth(capsys):
     assert user_id in out
     assert date_string in out
     assert email_string in out
+
+
+@patch('main.client')
+def test_make_upper_case(firestore_mock, capsys):
+
+    firestore_mock.collection = MagicMock(return_value=firestore_mock)
+    firestore_mock.document = MagicMock(return_value=firestore_mock)
+    firestore_mock.set = MagicMock(return_value=firestore_mock)
+
+    user_id = str(uuid.uuid4())
+    date_string = datetime.now().isoformat()
+    email_string = '%s@%s.com' % (uuid.uuid4(), uuid.uuid4())
+
+    data = {
+        'uid': user_id,
+        'metadata': {'createdAt': date_string},
+        'email': email_string,
+        'value': {
+            'fields': {
+                'original': {
+                    'stringValue': 'foobar'
+                }
+            }
+        }
+    }
+
+    context = UserDict()
+    context.resource = '/documents/some_collection/path/some/path'
+
+    main.make_upper_case(data, context)
+
+    out, _ = capsys.readouterr()
+
+    assert 'Replacing value: foobar --> FOOBAR' in out
+    firestore_mock.collection.assert_called_with('some_collection')
+    firestore_mock.document.assert_called_with('path/some/path')
+    firestore_mock.set.assert_called_with({'original': 'FOOBAR'})
