2cf1fd8eb7b304bfc34265f8ef9f396c4e017df1
Chen Yu <chenyumic@chenyumic-macbookpro.roam.corp.google.com>
2018-10-10 15:33:09 -0700

    Minor fix.

--- appengine/standard_python37/cloudsql/main_test.py
+++ appengine/standard_python37/cloudsql/main_test.py
@@ -19,12 +19,12 @@ import psycopg2.pool
 
 
 def test_main():
-    import main
-    main.mysql.connector = MagicMock()
-    main.mysql.connector.connect().cursor().fetchall.return_value = [['0']]
+    import main_mysql
+    main_mysql.mysql.connector = MagicMock()
+    main_mysql.mysql.connector.connect().cursor().fetchall.return_value = [['0']]
 
-    main.app.testing = True
-    client = main.app.test_client()
+    main_mysql.app.testing = True
+    client = main_mysql.app.test_client()
 
     r = client.get('/')
     assert r.status_code == 200
@@ -34,13 +34,13 @@ def test_main():
 def test_main_pooling():
     mysql.connector.pooling.MySQLConnectionPool = MagicMock()
 
-    import main_pooling
+    import main_mysql_pooling
 
-    mock_pool = main_pooling.mysql.connector.pooling.MySQLConnectionPool()
+    mock_pool = main_mysql_pooling.mysql.connector.pooling.MySQLConnectionPool()
     mock_pool.get_connection().cursor().fetchall.return_value = [['0']]
 
-    main_pooling.app.testing = True
-    client = main_pooling.app.test_client()
+    main_mysql_pooling.app.testing = True
+    client = main_mysql_pooling.app.test_client()
 
     r = client.get('/')
     assert r.status_code == 200
@@ -48,13 +48,13 @@ def test_main_pooling():
 
 
 def test_main_postgressql():
-    import main_postgressql
-    main_postgressql.psycopg2.connect = MagicMock()
-    mock_cursor = main_postgressql.psycopg2.connect().cursor()
+    import main_postgres
+    main_postgres.psycopg2.connect = MagicMock()
+    mock_cursor = main_postgres.psycopg2.connect().cursor()
     mock_cursor.__enter__().fetchall.return_value = [['0']]
 
-    main_postgressql.app.testing = True
-    client = main_postgressql.app.test_client()
+    main_postgres.app.testing = True
+    client = main_postgres.app.test_client()
 
     r = client.get('/')
     assert r.status_code == 200
@@ -64,13 +64,13 @@ def test_main_postgressql():
 def test_main_postgressql_pooling():
     psycopg2.pool.ThreadedConnectionPool = MagicMock()
 
-    import main_postgressql_pooling
+    import main_postgres_pooling
 
-    mock_pool = main_postgressql_pooling.psycopg2.pool.ThreadedConnectionPool()
+    mock_pool = main_postgres_pooling.psycopg2.pool.ThreadedConnectionPool()
     mock_pool.getconn().cursor().__enter__().fetchall.return_value = [['0']]
 
-    main_postgressql_pooling.app.testing = True
-    client = main_postgressql_pooling.app.test_client()
+    main_postgres_pooling.app.testing = True
+    client =main_postgres_pooling.app.test_client()
 
     r = client.get('/')
     assert r.status_code == 200
