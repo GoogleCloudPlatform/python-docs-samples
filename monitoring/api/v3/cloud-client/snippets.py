3812ee347cf4f24a202ec33b25c1bdf41ac89136
Charles Engelke <engelke@google.com>
2018-10-19 10:17:51 -0700

    Time series tests use random unique name to avoid limits

--- monitoring/api/v3/cloud-client/snippets.py
+++ monitoring/api/v3/cloud-client/snippets.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Google Inc.
+
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,17 +15,21 @@
 import argparse
 import os
 import pprint
+import random
 import time
 
 from google.cloud import monitoring_v3
 
+
+# Avoid collisions with other runs
+RANDOM_SUFFIX = str(random.randint(1000, 9999))
 
 def create_metric_descriptor(project_id):
     # [START monitoring_create_metric]
     client = monitoring_v3.MetricServiceClient()
     project_name = client.project_path(project_id)
     descriptor = monitoring_v3.types.MetricDescriptor()
-    descriptor.type = 'custom.googleapis.com/my_metric'
+    descriptor.type = 'custom.googleapis.com/my_metric' + RANDOM_SUFFIX
     descriptor.metric_kind = (
         monitoring_v3.enums.MetricDescriptor.MetricKind.GAUGE)
     descriptor.value_type = (
@@ -50,7 +54,7 @@ def write_time_series(project_id):
     project_name = client.project_path(project_id)
 
     series = monitoring_v3.types.TimeSeries()
-    series.metric.type = 'custom.googleapis.com/my_metric'
+    series.metric.type = 'custom.googleapis.com/my_metric' + RANDOM_SUFFIX
     series.resource.type = 'gce_instance'
     series.resource.labels['instance_id'] = '1234567890123456789'
     series.resource.labels['zone'] = 'us-central1-f'
