77894554d4c070b8b60fb95a4f577736756735c5
michaelawyu <chenyumic@google.com>
2018-10-22 13:22:56 -0700

    Minor fixes.

--- appengine/standard_python37/spanner/main_test.py
+++ appengine/standard_python37/spanner/main_test.py
@@ -12,10 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from unittest.mock import MagicMock
-
-from google.cloud import spanner
-
 
 def test_main():
     import main
