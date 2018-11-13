bbd968a8d632aea578d9b645cb0a9e905cf2160c
Frank Natividad <franknatividad@google.com>
2018-11-13 15:45:14 -0800

    Update path, add README, and add system test to sample.

---
+++ storage/s3-sdk/list_gcs_buckets.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+# Copyright 2018 Google, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This sample shows how to list buckets using the AWS S3 SDK with
+   Google Cloud Storage (GCS) interoperable XML API.
+
+GCS Credentials are passed in using the following environment variables:
+
+    * AWS_ACCESS_KEY_ID
+    * AWS_SECRET_ACCESS_KEY
+
+Learn how to get GCS interoperable credentials at
+https://cloud.google.com/storage/docs/migrating#keys.
+"""
+
+# [START gcs_s3_sdk_list_buckets]
+import boto3
+
+
+def list_gcs_buckets():
+    """Lists all GCS buckets using boto3 SDK"""
+    # Change the endpoint_url to use the Google Cloud Storage XML API endpoint.
+    s3 = boto3.client('s3', region_name="auto",
+                      endpoint_url="https://storage.googleapis.com")
+
+    # Call GCS to list current buckets
+    response = s3.list_buckets()
+
+    # Print bucket names
+    print("Buckets:")
+    for bucket in response["Buckets"]:
+        print(bucket["Name"])
+# [END gcs_s3_sdk_list_buckets]
+
+
+if __name__ == '__main__':
+    list_gcs_buckets()
--- storage/s3-sdk/list_gcs_buckets_test.py
+++ storage/s3-sdk/list_gcs_buckets_test.py
@@ -1,4 +1,4 @@
-# Copyright 2016 Google, Inc.
+# Copyright 2018 Google, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,17 +14,12 @@
 
 import os
 
-import pytest
-import requests
-
 import list_gcs_buckets
 
 BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
-ACCESS_KEY_ID = os.environ['CLOUD_STORAGE_ACCESS_KEY_ID']
-SECRET_ACCESS_KEY = os.environ['CLOUD_STORAGE_SECRET_ACCESS_KEY']
+
 
 def test_list_blobs(capsys):
-    list_gcs_buckets.list_blobs(google_access_key_id=ACCESS_KEY_ID,
-        google_secret_access_key=SECRET_ACCESS_KEY)
+    list_gcs_buckets.list_gcs_buckets()
     out, _ = capsys.readouterr()
     assert BUCKET in out
