d4ccc35162ba459e9feccc1ff7c8bffab1fc270a
Yu-Han Liu <yuhanliu@google.com>
2018-11-26 11:25:50 -0800

    update to use 0.35.1

--- vision/cloud-client/product_search/reference_image_management.py
+++ vision/cloud-client/product_search/reference_image_management.py
@@ -108,9 +108,9 @@ def get_reference_image(
     client = vision.ProductSearchClient()
 
     # Get the full path of the reference image.
-    reference_image_path = client.image_path(
+    reference_image_path = client.reference_image_path(
         project=project_id, location=location, product=product_id,
-        image=reference_image_id)
+        reference_image=reference_image_id)
 
     # Get complete detail of the reference image.
     image = client.get_reference_image(name=reference_image_path)
@@ -136,9 +136,9 @@ def delete_reference_image(
     client = vision.ProductSearchClient()
 
     # Get the full path of the reference image.
-    reference_image_path = client.image_path(
+    reference_image_path = client.reference_image_path(
         project=project_id, location=location, product=product_id,
-        image=reference_image_id)
+        reference_image=reference_image_id)
 
     # Delete the reference image.
     client.delete_reference_image(name=reference_image_path)
