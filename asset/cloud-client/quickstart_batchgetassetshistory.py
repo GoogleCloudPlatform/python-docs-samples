af07edddd66934bc34a8ac58c1cd28332711caae
Peter Zheng <peterzh@google.com>
2018-11-27 17:40:47 -0800

    [Asset] Print assets instead of response.

--- asset/cloud-client/quickstart_batchgetassetshistory.py
+++ asset/cloud-client/quickstart_batchgetassetshistory.py
@@ -35,7 +35,7 @@ def batch_get_assets_history(project_id, asset_names):
     read_time_window.start_time.GetCurrentTime()
     response = client.batch_get_assets_history(
         parent, content_type, read_time_window, asset_names)
-    print('Member: {}'.format(response.member))
+    print('assets: {}'.format(response.assets))
     # [END asset_quickstart_batch_get_assets_history]
 
 
