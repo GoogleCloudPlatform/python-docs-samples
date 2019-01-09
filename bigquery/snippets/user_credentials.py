2bb70c58a05b514cdf798ca9ff0758b51c6e40ea
Tim Swast <swast@google.com>
2019-01-09 10:18:27 -0800

    Add missing explanation about local_server callback URL
    Per feedback on CL 227863277

--- bigquery/snippets/user_credentials.py
+++ bigquery/snippets/user_credentials.py
@@ -30,10 +30,10 @@ def main(project, launch_browser=True):
     # TODO: Uncomment the line below to set the `launch_browser` variable.
     # launch_browser = True
     #
-    # The `launch_browser` boolean variable indicates if a local server in the
-    # auth flow. A value of `True` is recommended, but a local server does not
-    # work if accessing the application remotely, such as over SSH or from a
-    # remote Jupyter notebook.
+    # The `launch_browser` boolean variable indicates if a local server is used
+    # as the callback URL in the auth flow. A value of `True` is recommended,
+    # but a local server does not work if accessing the application remotely,
+    # such as over SSH or from a remote Jupyter notebook.
 
     appflow = flow.InstalledAppFlow.from_client_secrets_file(
         'client_secrets.json',
