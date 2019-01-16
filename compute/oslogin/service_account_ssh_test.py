ea3bb7be4b47ff88ddb576f1352eef7632c1be9c
Dwight J Lyle <dwightjl@users.noreply.github.com>
2019-01-16 11:45:51 -0800

    Create a test for the OS Login service account example.

---
+++ compute/oslogin/service_account_ssh_test.py
@@ -0,0 +1,36 @@
+# Copyright 2019, Google, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import re
+from gcp_devrel.testing.flaky import flaky
+from service_account_ssh import main
+
+
+@flaky
+def test_main(capsys):
+
+    # Run the main method from service_account_ssh
+    main()
+
+    # Capture the command line output
+    out, _ = capsys.readouterr()
+
+    # Test the command line output. Expect a failed SSH connection to
+    # a non-existant instance. The attempt is what is important to test.
+    expected_output = re.compile(
+        (r'.*Could not resolve hostname '
+         'my-instance-name.us-central1-a.c.my-project-id.internal: '
+         'Name or service not known.*'),
+        re.DOTALL)
+
+    assert re.search(expected_output, out)
