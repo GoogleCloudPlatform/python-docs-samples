49736dfa2c494de3c44bfdf1e53d5f4b66320edf
Yu-Han Liu <yuhanliu@google.com>
2019-02-26 14:15:58 -0800

    remove location_id

--- video/cloud-client/analyze/analyze.py
+++ video/cloud-client/analyze/analyze.py
@@ -377,12 +377,10 @@ def track_objects_gcs(gcs_uri):
     """Object tracking in a video stored on GCS."""
     from google.cloud import videointelligence
 
-    # It is recommended to use location_id as 'us-east1' for the best latency
-    # due to different types of processors used in this region and others.
     video_client = videointelligence.VideoIntelligenceServiceClient()
     features = [videointelligence.enums.Feature.OBJECT_TRACKING]
     operation = video_client.annotate_video(
-        input_uri=gcs_uri, features=features, location_id='us-east1')
+        input_uri=gcs_uri, features=features)
     print('\nProcessing video for object annotations.')
 
     result = operation.result(timeout=300)
@@ -430,10 +428,8 @@ def track_objects(path):
     with io.open(path, 'rb') as file:
         input_content = file.read()
 
-    # It is recommended to use location_id as 'us-east1' for the best latency
-    # due to different types of processors used in this region and others.
     operation = video_client.annotate_video(
-        input_content=input_content, features=features, location_id='us-east1')
+        input_content=input_content, features=features)
     print('\nProcessing video for object annotations.')
 
     result = operation.result(timeout=300)
