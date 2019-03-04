a529fe8da77d685eb5945f6ec11cadb1ff49c5f6
jqll <jianqiaoli@google.com>
2019-03-04 11:11:49 -0800

    Add some functions to make the profile look more interesting

--- profiler/quickstart/main.py
+++ profiler/quickstart/main.py
@@ -18,19 +18,46 @@
 # [START profiler_python_quickstart]
 import googlecloudprofiler
 
-if __name__ == '__main__':
-    # Profiler initialization, best done as early as possible.
+
+def main():
+    # Profiler initialization. It starts a daemon thread which continuously
+    # collects and uploads profiles. Best done as early as possible.
     try:
         googlecloudprofiler.start(
-            service='service-name',
-            service_version='version',
+            service='hello-profiler',
+            service_version='1.0.1',
             verbose=3,
             # project_id must be set if not running on GCP.
             # project_id='my-project-id',
         )
     except (ValueError, NotImplementedError) as e:
         print(e)  # Handles errors here
-
 # [END profiler_python_quickstart]
+    busyloop()
+
+
+# A loop function which spends 30% CPU time on loop3() and 70% CPU time
+# on loop7().
+def busyloop():
     while True:
+        loop3()
+        loop7()
+
+
+def loop3():
+    for _ in range(3):
+        loop()
+
+
+def loop7():
+    for _ in range(7):
+        loop()
+
+
+def loop():
+    for _ in range(10000):
         pass
+
+
+if __name__ == '__main__':
+    main()
