4da30b57b2cc7308135abd8d8ddede5dc5c1097f
Charles Engelke <github@engelke.com>
2019-03-19 10:25:55 -0700

    New library version to address failure. (#2057)
    * New library version to address failure.
    
    * Encoded strings for library call
    
    * Give changes a bit longer to finish
    
    * fix lint error
    
    * Update main.py
    
    * Paren was missing

--- bigtable/hello_happybase/main.py
+++ bigtable/hello_happybase/main.py
@@ -72,7 +72,9 @@ def main(project_id, instance_id, table_name):
             #
             #     https://cloud.google.com/bigtable/docs/schema-design
             row_key = 'greeting{}'.format(i)
-            table.put(row_key, {column_name: value})
+            table.put(
+                row_key, {column_name.encode('utf-8'): value.encode('utf-8')}
+            )
         # [END writing_rows]
 
         # [START getting_a_row]
