b517ec72b7c6939681de5c96e4321c4754f99b9c
Yu-Han Liu <yuhanliu@google.com>
2019-03-18 17:54:47 -0700

    fix urllib2 import

--- video/cloud-client/analyze/beta_snippets_test.py
+++ video/cloud-client/analyze/beta_snippets_test.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 import time
-import urllib2
+from urllib.request import urlopen
 import uuid
 
 import beta_snippets
@@ -29,7 +29,7 @@ POSSIBLE_TEXTS = ['Google', 'SUR', 'SUR', 'ROTO', 'Vice President', '58oo9',
 
 @pytest.fixture(scope='session')
 def video_path(tmpdir_factory):
-    file = urllib2.urlopen(
+    file = urlopen(
         'http://storage.googleapis.com/cloud-samples-data/video/cat.mp4')
     path = tmpdir_factory.mktemp('video').join('file.mp4')
     with open(str(path), 'wb') as f:
