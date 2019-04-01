3175316134123199d0925a63a8cc9e86df594407
Layla Bristol <laylab@google.com>
2019-04-01 16:32:46 -0700

    Made changes to tests per feedback

--- appengine/standard_python37/bigquery/main.py
+++ appengine/standard_python37/bigquery/main.py
@@ -23,9 +23,10 @@ app = flask.Flask(__name__)
 bigquery_client = bigquery.Client()
 
 
-@app.route('/')
+@app.route("/")
 def main():
-    query_job = bigquery_client.query("""
+    query_job = bigquery_client.query(
+        """
         SELECT
         CONCAT(
             'https://stackoverflow.com/questions/',
@@ -35,34 +36,40 @@ def main():
         WHERE tags like '%google-bigquery%'
         ORDER BY view_count DESC
         LIMIT 10
-    """)
+    """
+    )
 
-    return flask.redirect(flask.url_for('results', project_id=query_job.project,
-                                        job_id=query_job.job_id,
-                                        location=query_job.location))
+    return flask.redirect(
+        flask.url_for(
+            "results",
+            project_id=query_job.project,
+            job_id=query_job.job_id,
+            location=query_job.location,
+        )
+    )
 
 
-@app.route('/results')
+@app.route("/results")
 def results():
-    project_id = flask.request.args.get('project_id')
-    job_id = flask.request.args.get('job_id')
-    location = flask.request.args.get('location')
+    project_id = flask.request.args.get("project_id")
+    job_id = flask.request.args.get("job_id")
+    location = flask.request.args.get("location")
 
     bigquery_client = bigquery.Client(project=project_id, location=location)
-    job = bigquery_client.get_job(job_id)
+    query_job = bigquery_client.get_job(job_id)
 
     try:
         # Set a timeout because queries could take longer than one minute.
-        results = job.result(timeout=30)
+        results = query_job.result(timeout=30)
     except concurrent.futures.TimeoutError:
-        return flask.render_template('timeout.html', job_id=query_job.job_id)
+        return flask.render_template("timeout.html", job_id=query_job.job_id)
 
-    return flask.render_template('query_result.html', results=results)
+    return flask.render_template("query_result.html", results=results)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
 # [END gae_python37_bigquery]
