b63488a0ae0c8d2bde799131093f0dbe734ba6cc
Tim Swast <swast@google.com>
2019-04-02 15:11:03 -0700

    Fix flake8 lint error in main.py

--- appengine/standard_python37/bigquery/main.py
+++ appengine/standard_python37/bigquery/main.py
@@ -55,7 +55,11 @@ def results():
     job_id = flask.request.args.get("job_id")
     location = flask.request.args.get("location")
 
-    query_job = bigquery_client.get_job(job_id, project=project_id, location=location)
+    query_job = bigquery_client.get_job(
+        job_id,
+        project=project_id,
+        location=location,
+    )
 
     try:
         # Set a timeout because queries could take longer than one minute.
