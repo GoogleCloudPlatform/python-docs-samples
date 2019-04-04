6e96d54b6e99c840666293b07a23a9405576d18a
Yu-Han Liu <yuhanliu@google.com>
2019-04-04 12:33:56 -0700

    add label_text test

--- datalabeling/label_text.py
+++ datalabeling/label_text.py
@@ -18,28 +18,29 @@ import argparse
 
 
 # [START datalabeling_label_text_beta]
-def label_text(dataset_resource_name, instruction_resource_name,
-                             annotation_spec_set_resource_name):
+def label_text(dataset_resource_name, instruction_resource_name, annotation_spec_set_resource_name):
     """Labels a text dataset."""
     from google.cloud import datalabeling_v1beta1 as datalabeling
     client = datalabeling.DataLabelingServiceClient()
 
     basic_config = datalabeling.types.HumanAnnotationConfig(
-            instruction=instruction_resource_name,
-            annotated_dataset_display_name='YOUR_ANNOTATED_DATASET_DISPLAY_NAME',
-            label_group='YOUR_LABEL_GROUP',
-            replica_count=1)
+        instruction=instruction_resource_name,
+        annotated_dataset_display_name='YOUR_ANNOTATED_DATASET_DISPLAY_NAME',
+        label_group='YOUR_LABEL_GROUP',
+        replica_count=1
+    )
 
     feature = datalabeling.enums.LabelTextRequest.Feature.TEXT_ENTITY_EXTRACTION
 
     text_entity_extraction_config = datalabeling.types.TextEntityExtractionConfig(
-            annotation_spec_set=annotation_spec_set_resource_name)
+        annotation_spec_set=annotation_spec_set_resource_name)
 
     response = client.label_text(
-            dataset_resource_name,
-            basic_config,
-            feature,
-            text_entity_extraction_config=text_entity_extraction_config)
+        dataset_resource_name,
+        basic_config,
+        feature,
+        text_entity_extraction_config=text_entity_extraction_config
+    )
 
     print('Label_text operation name: {}'.format(response.operation.name))
     return response
@@ -47,24 +48,28 @@ def label_text(dataset_resource_name, instruction_resource_name,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-            description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
 
     parser.add_argument(
-            '--dataset-resource-name',
-            help='Dataset resource name. Required.',
-            required=True)
+        '--dataset-resource-name',
+        help='Dataset resource name. Required.',
+        required=True
+    )
 
     parser.add_argument(
-            '--instruction-resource-name',
-            help='Instruction resource name. Required.',
-            required=True)
+        '--instruction-resource-name',
+        help='Instruction resource name. Required.',
+        required=True
+    )
 
     parser.add_argument(
-            '--annotation-spec-set-resource-name',
-            help='Annotation spec set resource name. Required.',
-            required=True)
+        '--annotation-spec-set-resource-name',
+        help='Annotation spec set resource name. Required.',
+        required=True
+    )
 
     args = parser.parse_args()
 
-    label_text(args.dataset_resource_name, args.instruction_resource_name,
-                         args.annotation_spec_set_resource_name)
+    label_text(args.dataset_resource_name, args.instruction_resource_name, args.annotation_spec_set_resource_name)
