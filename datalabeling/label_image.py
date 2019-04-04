364765b24aa5f1b44db6505b8390ac5489c8b5d5
Yu-Han Liu <yuhanliu@google.com>
2019-04-04 11:35:04 -0700

    add label image and test

--- datalabeling/label_image.py
+++ datalabeling/label_image.py
@@ -18,31 +18,33 @@ import argparse
 
 
 # [START datalabeling_label_image_beta]
-def label_image(dataset_resource_name, instruction_resource_name,
-                                annotation_spec_set_resource_name):
+def label_image(dataset_resource_name, instruction_resource_name, annotation_spec_set_resource_name):
     """Labels an image dataset."""
     from google.cloud import datalabeling_v1beta1 as datalabeling
     client = datalabeling.DataLabelingServiceClient()
 
     basic_config = datalabeling.types.HumanAnnotationConfig(
-            instruction=instruction_resource_name,
-            annotated_dataset_display_name='YOUR_ANNOTATED_DATASET_DISPLAY_NAME',
-            label_group='YOUR_LABEL_GROUP',
-            replica_count=1)
+        instruction=instruction_resource_name,
+        annotated_dataset_display_name='YOUR_ANNOTATED_DATASET_DISPLAY_NAME',
+        label_group='YOUR_LABEL_GROUP',
+        replica_count=1
+    )
 
     feature = datalabeling.enums.LabelImageRequest.Feature.CLASSIFICATION
 
     image_classification_config = datalabeling.types.ImageClassificationConfig(
-            annotation_spec_set=annotation_spec_set_resource_name,
-            allow_multi_label=False,
-            answer_aggregation_type=datalabeling.enums.StringAggregationType
-            .MAJORITY_VOTE)
+        annotation_spec_set=annotation_spec_set_resource_name,
+        allow_multi_label=False,
+        answer_aggregation_type=datalabeling.enums.StringAggregationType
+        .MAJORITY_VOTE
+    )
 
     response = client.label_image(
-            dataset_resource_name,
-            basic_config,
-            feature,
-            image_classification_config=image_classification_config)
+        dataset_resource_name,
+        basic_config,
+        feature,
+        image_classification_config=image_classification_config
+    )
 
     print('Label_image operation name: {}'.format(response.operation.name))
     return response
@@ -50,24 +52,29 @@ def label_image(dataset_resource_name, instruction_resource_name,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-            description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
 
     parser.add_argument(
-            '--dataset-resource-name',
-            help='Dataset resource name. Required.',
-            required=True)
+        '--dataset-resource-name',
+        help='Dataset resource name. Required.',
+        required=True
+    )
 
     parser.add_argument(
-            '--instruction-resource-name',
-            help='Instruction resource name. Required.',
-            required=True)
+        '--instruction-resource-name',
+        help='Instruction resource name. Required.',
+        required=True
+    )
 
     parser.add_argument(
-            '--annotation-spec-set-resource-name',
-            help='Annotation spec set resource name. Required.',
-            required=True)
+        '--annotation-spec-set-resource-name',
+        help='Annotation spec set resource name. Required.',
+        required=True
+    )
 
     args = parser.parse_args()
 
     label_image(args.dataset_resource_name, args.instruction_resource_name,
-                            args.annotation_spec_set_resource_name)
+        args.annotation_spec_set_resource_name)
