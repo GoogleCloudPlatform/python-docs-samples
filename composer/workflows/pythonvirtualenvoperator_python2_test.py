d22810524c54a4c618ff1cb6840f8c19fcda2603
Leah Cole <coleleah@google.com>
2019-04-05 11:05:54 -0700

    Comment out unused imports to pass linter

--- composer/workflows/pythonvirtualenvoperator_python2_test.py
+++ composer/workflows/pythonvirtualenvoperator_python2_test.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
+# import sys
 
-import pytest
+# import pytest
 
 from . import unit_testing
 
