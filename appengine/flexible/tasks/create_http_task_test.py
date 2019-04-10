014d40bed5a109759ad65c3d3c992ba43c09ddc3
Averi Kitsch <akitsch@google.com>
2019-04-10 10:17:41 -0700

    [Cloud Tasks] Add Http Push Queue Sample (#2046)
    * Http push queue sample
    
    * Updates to requirements and license
    
    * update comments
    
    * Update app.yaml
    
    * update readme
    
    * fix versioning errors
    
    * linting error fixed

--- appengine/flexible/tasks/create_http_task_test.py
+++ appengine/flexible/tasks/create_http_task_test.py
@@ -14,7 +14,7 @@
 
 import os
 
-import create_app_engine_queue_task
+import create_http_task
 
 TEST_PROJECT_ID = os.getenv('GCLOUD_PROJECT')
 TEST_LOCATION = os.getenv('TEST_QUEUE_LOCATION', 'us-central1')
@@ -22,6 +22,7 @@ TEST_QUEUE_NAME = os.getenv('TEST_QUEUE_NAME', 'my-appengine-queue')
 
 
 def test_create_task():
-    result = create_app_engine_queue_task.create_task(
-        TEST_PROJECT_ID, TEST_QUEUE_NAME, TEST_LOCATION)
+    url = 'https://' + TEST_PROJECT_ID + '.appspot.com/example_task_handler'
+    result = create_http_task.create_http_task(
+        TEST_PROJECT_ID, TEST_QUEUE_NAME, TEST_LOCATION, url)
     assert TEST_QUEUE_NAME in result.name
