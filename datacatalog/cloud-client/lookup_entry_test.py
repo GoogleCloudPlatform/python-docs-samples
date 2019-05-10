10450b2313d38f995179179aaa35d8a6091bdbe9
Ricardo Mendes <ricardosm@ciandt.com>
2019-05-10 18:20:24 -0300

    Refer to GCP public assets in tests

--- datacatalog/cloud-client/lookup_entry_test.py
+++ datacatalog/cloud-client/lookup_entry_test.py
@@ -14,38 +14,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
 import lookup_entry
 
-PROJECT = os.environ['GCLOUD_PROJECT']
-BIGQUERY_DATASET = os.environ['BIGQUERY_DATASET']
-BIGQUERY_TABLE = os.environ['BIGQUERY_TABLE']
-PUBSUB_TOPIC = os.environ['PUBSUB_TOPIC']
+BIGQUERY_PROJECT = 'bigquery-public-data'
+BIGQUERY_DATASET = 'new_york_taxi_trips'
+BIGQUERY_TABLE = 'taxi_zone_geom'
+
+PUBSUB_PROJECT = 'pubsub-public-data'
+PUBSUB_TOPIC = 'taxirides-realtime'
 
 
 def test_lookup_bigquery_dataset():
-    assert lookup_entry.lookup_bigquery_dataset(PROJECT, BIGQUERY_DATASET)
+    assert lookup_entry.lookup_bigquery_dataset(
+        BIGQUERY_PROJECT, BIGQUERY_DATASET)
 
 
 def test_lookup_bigquery_dataset_sql_resource():
     assert lookup_entry.lookup_bigquery_dataset_sql_resource(
-        PROJECT, BIGQUERY_DATASET)
+        BIGQUERY_PROJECT, BIGQUERY_DATASET)
 
 
 def test_lookup_bigquery_table():
     assert lookup_entry.lookup_bigquery_table(
-        PROJECT, BIGQUERY_DATASET, BIGQUERY_TABLE)
+        BIGQUERY_PROJECT, BIGQUERY_DATASET, BIGQUERY_TABLE)
 
 
 def test_lookup_bigquery_table_sql_resource():
     assert lookup_entry.lookup_bigquery_table_sql_resource(
-        PROJECT, BIGQUERY_DATASET, BIGQUERY_TABLE)
+        BIGQUERY_PROJECT, BIGQUERY_DATASET, BIGQUERY_TABLE)
 
 
 def test_lookup_pubsub_topic():
-    assert lookup_entry.lookup_pubsub_topic(PROJECT, PUBSUB_TOPIC)
+    assert lookup_entry.lookup_pubsub_topic(PUBSUB_PROJECT, PUBSUB_TOPIC)
 
 
 def test_lookup_pubsub_topic_sql_resource():
-    assert lookup_entry.lookup_pubsub_topic_sql_resource(PROJECT, PUBSUB_TOPIC)
+    assert lookup_entry.lookup_pubsub_topic_sql_resource(
+        PUBSUB_PROJECT, PUBSUB_TOPIC)
