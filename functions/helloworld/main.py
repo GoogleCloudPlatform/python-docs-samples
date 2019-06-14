e8ba8c35e4d1885ff06c2e9968a8969b59d1f6bd
Tianzi Cai <anguillanneuf@gmail.com>
2019-06-13 17:46:26 -0700

    reformat

--- functions/helloworld/main.py
+++ functions/helloworld/main.py
@@ -83,9 +83,9 @@ def hello_http(request):
 def hello_pubsub(event, context):
     """Background Cloud Function to be triggered by Pub/Sub.
     Args:
-         event (dict):  The dictionary with data specific to this type of event.
-         The `data` field contains the PubsubMessage message. The `attributes`
-         field will contain custom attributes if there are any.
+         event (dict):  The dictionary with data specific to this type of
+         event. The `data` field contains the PubsubMessage message. The
+         `attributes` field will contain custom attributes if there are any.
          context (google.cloud.functions.Context): The Cloud Functions event
          metadata. The `event_id` field contains the Pub/Sub message ID. The
          `timestamp` field contains the publish time.
