0054f9e7ef8243f39e9032f5b02e11a6b8a46514
cclauss <cclauss@me.com>
2019-06-21 08:50:58 +0200

    Use print() function in both Python 2 and Python 3
    Legacy __print__ statements are syntax errors in Python 3 but __print()__ function works as expected in both Python 2 and Python 3.

--- appengine/standard/i18n/i18n_utils.py
+++ appengine/standard/i18n/i18n_utils.py
@@ -19,6 +19,7 @@
 The idea of this example, especially for how to translate strings in
 Javascript is originally from an implementation of Django i18n.
 """
+from __future__ import print_function
 
 
 import gettext
@@ -49,7 +50,7 @@ def _get_plural_forms(js_translations):
         for l in js_translations._catalog[''].split('\n'):
             if l.startswith('Plural-Forms:'):
                 plural = l.split(':', 1)[1].strip()
-                print "plural is %s" % plural
+                print("plural is %s" % plural)
     if plural is not None:
         for raw_element in plural.split(';'):
             element = raw_element.strip()
@@ -57,7 +58,7 @@ def _get_plural_forms(js_translations):
                 n_plural = int(element.split('=', 1)[1])
             elif element.startswith('plural='):
                 plural = element.split('=', 1)[1]
-                print "plural is now %s" % plural
+                print("plural is now %s" % plural)
     else:
         n_plural = 2
         plural = '(n == 1) ? 0 : 1'
