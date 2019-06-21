f13dcb36d25cd66f332830daeb9bacbc5ddb4ed9
Ryan Chen <ryachen@google.com>
2019-06-21 15:47:30 -0700

    Update cloud-sql/postgres sample (#2235)

--- cloud-sql/mysql/sqlalchemy/main.py
+++ cloud-sql/mysql/sqlalchemy/main.py
@@ -36,14 +36,14 @@ logger = logging.getLogger()
 # managing a pool of connections to your database
 db = sqlalchemy.create_engine(
     # Equivalent URL:
-    # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
+    # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_sock=/cloudsql/<cloud_sql_instance_name>
     sqlalchemy.engine.url.URL(
         drivername='mysql+pymysql',
         username=db_user,
         password=db_pass,
         database=db_name,
         query={
-            'unix_socket': '/cloudsql/{}'.format(cloud_sql_connection_name)
+            'unix_socket': '/cloudsql/{}/'.format(cloud_sql_connection_name)
         }
     ),
     # ... Specify additional properties here.
