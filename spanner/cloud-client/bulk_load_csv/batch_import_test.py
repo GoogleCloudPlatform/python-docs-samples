cf17c22b934a98cf7fdb2ecb9912a16f961ec59a
Oluwatoni Oshikanlu <39501338+tonioshikanlu@users.noreply.github.com>
2019-07-23 16:31:40 -0700

    Fixed Formatting issues

--- spanner/cloud-client/bulk_load_csv/batch_import_test.py
+++ spanner/cloud-client/bulk_load_csv/batch_import_test.py
@@ -9,7 +9,8 @@ from google.cloud import spanner
 
 def unique_database_id():
     """ Creates a unique id for the database. """
-    return 'test-db-{}'.format(''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(5)))
+    return 'test-db-{}'.format(''.join(random.choice(string.ascii_lowercase +
+        string.digits) for _ in range(5)))
 
 
 INSTANCE_ID = os.environ['SPANNER_INSTANCE']
@@ -28,46 +29,47 @@ def example_database():
     instance = spanner_client.instance(SPANNER_INSTANCE)
     database = instance.database(DATABASE_ID)
 
-     if not database.exists():
-         database = instance.database(DATABASE_ID, ddl_statements=[
-         """
-         CREATE TABLE comments (
-         id INT64,
-         author STRING(MAX),
-         `by` STRING(MAX),
-         dead BOOL,
-         deleted BOOL,
-         parent INT64,
-         ranking INT64,
-         text STRING(MAX),
-         time INT64,
-         time_ts TIMESTAMP,
-         ) PRIMARY KEY(parent, id);
-         CREATE INDEX CommentsByAuthor ON comments(author);
-         CREATE TABLE stories (
-         id INT64,
-         author STRING(MAX),
-         `by` STRING(MAX),
-         dead BOOL,
-         deleted BOOL,
-         descendants INT64,
-         score INT64,
-         text STRING(MAX),
-         time INT64,
-         time_ts TIMESTAMP,
-         title STRING(MAX),
-         url STRING(MAX),
-         ) PRIMARY KEY(id);
-         CREATE INDEX StoriesByAuthor ON stories(author);
-         CRETE INDEX StoriesByScoreURL ON stories(score, url);
-         CREATE INDEX StoriesByTitleTimeScore ON stories(title) STORING (time_ts, score)"""
-         ])
-         database.create()
+    if not database.exists():
+        database = instance.database(DATABASE_ID, ddl_statements=[
+        """
+        CREATE TABLE comments (
+        id INT64,
+        author STRING(MAX),
+        `by` STRING(MAX),
+        dead BOOL,
+        deleted BOOL,
+        parent INT64,
+        ranking INT64,
+        text STRING(MAX),
+        time INT64,
+        time_ts TIMESTAMP,
+        ) PRIMARY KEY(parent, id);
+        CREATE INDEX CommentsByAuthor ON comments(author);
+        CREATE TABLE stories (
+        id INT64,
+        author STRING(MAX),
+        `by` STRING(MAX),
+        dead BOOL,
+        deleted BOOL,
+        descendants INT64,
+        score INT64,
+        text STRING(MAX),
+        time INT64,
+        time_ts TIMESTAMP,
+        title STRING(MAX),
+        url STRING(MAX),
+        ) PRIMARY KEY(id);
+        CREATE INDEX StoriesByAuthor ON stories(author);
+        CRETE INDEX StoriesByScoreURL ON stories(score, url);
+        CREATE INDEX StoriesByTitleTimeScore ON stories(title) STORING (time_ts, score)
+        """
+        ])
+        database.create()
 
-         yield database
-             database.drop()
-                        
-                        
+        yield database
+            database.drop()
+
+
 def test_insert_data(capsys):
     batch_import.main(INSTANCE_ID, DATABASE_ID)
     out, _ = capsys.readouterr()
