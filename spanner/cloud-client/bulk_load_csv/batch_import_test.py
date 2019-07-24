e941c097d4472bb12799f3e30307f7241c7c9448
Oluwatoni Oshikanlu <39501338+tonioshikanlu@users.noreply.github.com>
2019-07-23 17:23:46 -0700

    Update batch_import_test.py

--- spanner/cloud-client/bulk_load_csv/batch_import_test.py
+++ spanner/cloud-client/bulk_load_csv/batch_import_test.py
@@ -31,41 +31,9 @@ def example_database():
     database = instance.database(DATABASE_ID)
 
     if not database.exists():
-        database = instance.database(DATABASE_ID, ddl_statements=[
-        ("""
-        CREATE TABLE comments (
-        id INT64,
-        author STRING(MAX),
-        `by` STRING(MAX),
-        dead BOOL,
-        deleted BOOL,
-        parent INT64,
-        ranking INT64,
-        text STRING(MAX),
-        time INT64,
-        time_ts TIMESTAMP,
-        ) PRIMARY KEY(parent, id);
-        CREATE INDEX CommentsByAuthor ON comments(author);
-        CREATE TABLE stories (
-        id INT64,
-        author STRING(MAX),
-        `by` STRING(MAX),
-        dead BOOL,
-        deleted BOOL,
-        descendants INT64,
-        score INT64,
-        text STRING(MAX),
-        time INT64,
-        time_ts TIMESTAMP,
-        title STRING(MAX),
-        url STRING(MAX),
-        ) PRIMARY KEY(id);
-        CREATE INDEX StoriesByAuthor ON stories(author);
-        CRETE INDEX StoriesByScoreURL ON stories(score, url);
-        CREATE INDEX StoriesByTitleTimeScore
-        ON stories(title) STORING (time_ts, score)
-        """)
-        ])
+        with open('schema.ddl', 'r') as myfile:
+            schema = myfile.read()
+        database = instance.database(DATABASE_ID, ddl_statements=[schema])
         database.create()
 
         yield database
