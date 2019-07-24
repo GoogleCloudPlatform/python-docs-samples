08cb1193c12d111253a821cf2950a9dd33476d6a
Frank Natividad <frankyn@users.noreply.github.com>
2019-07-24 10:51:49 -0700

    Fix lint issues

--- storage/cloud-client/snippets.py
+++ storage/cloud-client/snippets.py
@@ -142,7 +142,8 @@ def list_blobs_with_prefix(bucket_name, prefix, delimiter=None):
 
     """
     storage_client = storage.Client()
-    blobs = storage_client.list_blobs(bucket_name, prefix=prefix, delimiter=delimiter)
+    blobs = storage_client.list_blobs(bucket_name, prefix=prefix,
+                                      delimiter=delimiter)
 
     print('Blobs:')
     for blob in blobs:
