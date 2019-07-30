d304f0219dfa5107f120f134242bc6223e9212e5
Oluwatoni Oshikanlu <39501338+tonioshikanlu@users.noreply.github.com>
2019-07-30 16:28:27 -0700

    Added tests for two functions

--- spanner/cloud-client/bulk_load_csv/batch_import_test.py
+++ spanner/cloud-client/bulk_load_csv/batch_import_test.py
@@ -48,6 +48,15 @@ def example_database():
         database.drop()
 
 
+def  test_is_bool_null():
+    assert is_bool_null(['12','true','','12','jkl','']) == [['12'], [True], [], ['12'], ['jkl'], []]
+
+
+def test_divide_chunks():
+    res = list(divide_chunks(['12','true','','12','jkl',''], 2))
+    assert res == [['12', 'true'], ['', '12'], ['jkl', '']]
+
+
 def test_insert_data(capsys):
     batch_import.main(INSTANCE_ID, DATABASE_ID)
     out, _ = capsys.readouterr()
