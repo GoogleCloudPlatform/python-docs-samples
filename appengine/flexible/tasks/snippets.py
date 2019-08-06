7dd0ac5bc9532c1ceb8e8a54261c14894653b9ba
averikitsch <akitsch@google.com>
2019-08-05 17:06:53 -0700

    snippets for migration guide - tests added

--- appengine/flexible/tasks/snippets.py
+++ appengine/flexible/tasks/snippets.py
@@ -14,16 +14,20 @@
 
 from google.cloud import tasks
 
-def create_queue():
+def create_queue(project, location, queue_blue_name, queue_red_name):
     # [START taskqueues_using_yaml]
     client = tasks.CloudTasksClient()
 
-    project = 'my-project-id'
-    location = 'us-central1'
+    # TODO(developer): Uncomment these lines and replace with your values.
+    # project = 'my-project-id'
+    # location = 'us- central1'
+    # queue_blue_name = 'queue-blue'
+    # queue_red_name = 'queue-red'
+
     parent = client.location_path(project, location)
 
     queue_blue = {
-        'name': client.queue_path(project, location, 'queue-blue'),
+        'name': client.queue_path(project, location, queue_blue_name),
         'rate_limits': {
             'max_dispatches_per_second': 5
         },
@@ -34,7 +38,7 @@ def create_queue():
     }
 
     queue_red = {
-        'name': client.queue_path(project, location, 'queue-red'),
+        'name': client.queue_path(project, location, queue_red_name),
         'rate_limits': {
             'max_dispatches_per_second': 1
         }
@@ -43,19 +47,23 @@ def create_queue():
     queues = [queue_blue, queue_red]
     for queue in queues:
         response = client.create_queue(parent, queue)
+        print("Created queue: ")
         print(response)
     # [END taskqueues_using_yaml]
+    return response
 
 
-def update_queue():
+def update_queue(project, location, queue):
     # [START taskqueues_processing_rate]
     client = tasks.CloudTasksClient()
 
-    project = 'my-project-id'
-    location = 'us-central1'
+    # TODO(developer): Uncomment these lines and replace with your values.
+    # project = 'my-project-id'
+    # location = 'us- central1'
+    # queue = 'queue-blue'
 
     # Get queue object
-    queue_path = client.queue_path(project, location, 'queue-blue')
+    queue_path = client.queue_path(project, location, queue)
     queue = client.get_queue(queue_path)
 
     # Update queue object
@@ -63,18 +71,22 @@ def update_queue():
     queue.rate_limits.max_concurrent_dispatches = 10
 
     response = client.update_queue(queue)
+    print("Updated queue: ")
     print(response)
     # [END taskqueues_processing_rate]
+    return response
 
 
-def create_task():
+def create_task(project, location, queue):
     # [START taskqueues_new_task]
     client = tasks.CloudTasksClient()
 
-    project = 'my-project-id'
-    location = 'us-central1'
-    queue = 'default'
+    # TODO(developer): Uncomment these lines and replace with your values.
+    # project = 'my-project-id'
+    # location = 'us- central1'
+    # queue = 'default'
     amount = 10
+
     parent = client.queue_path(project, location, queue)
 
     task = {
@@ -92,15 +104,18 @@ def create_task():
     eta = response.schedule_time.ToDatetime().strftime("%m/%d/%Y, %H:%M:%S")
     print('Task {} enqueued, ETA {}.'.format(response.name, eta))
     # [END taskqueues_new_task]
+    return response
 
-def create_tasks_with_data():
+def create_tasks_with_data(project, location, queue):
     # [START taskqueues_passing_data]
     import json
     client = tasks.CloudTasksClient()
 
-    project = 'my-project-id'
-    location = 'us-central1'
-    queue = 'default'
+    # TODO(developer): Uncomment these lines and replace with your values.
+    # project = 'my-project-id'
+    # location = 'us- central1'
+    # queue = 'default'
+
     parent = client.queue_path(project, location, queue)
 
     task1 = {
@@ -127,24 +142,28 @@ def create_tasks_with_data():
         }
     }
 
-    tasks = [task1, task2]
-    for task in tasks:
-        response = client.create_task(parent, task)
-        print(response)
+    response = client.create_task(parent, task1)
+    print(response)
+    response = client.create_task(parent, task2)
+    print(response)
     # [END taskqueues_passing_data]
+    return response
 
 
-def create_task_with_name():
+def create_task_with_name(project, location, queue, task_name):
     # [START taskqueues_naming_tasks]
     client = tasks.CloudTasksClient()
 
-    project = 'my-project-id'
-    location = 'us-central1'
-    queue = "default"
+    # TODO(developer): Uncomment these lines and replace with your values.
+    # project = 'my-project-id'
+    # location = 'us- central1'
+    # queue = 'default'
+    # task_name = 'first-try'
+
     parent = client.queue_path(project, location, queue)
 
     task =  {
-        'name': client.task_path(project, location, queue, 'first-try'),
+        'name': client.task_path(project, location, queue, task_name),
         'app_engine_http_request': {
             'http_method': 'GET',
             'relative_uri': '/url/path'
@@ -153,52 +172,72 @@ def create_task_with_name():
     response = client.create_task(parent, task)
     print(response)
     # [END taskqueues_naming_tasks]
+    return response
 
 
-def delete_tasks():
+def delete_task(project, location, queue):
     # [START taskqueues_setup]
     client = tasks.CloudTasksClient()
 
-    project = 'my-project-id'
-    location = 'us-central1'
+    # TODO(developer): Uncomment these lines and replace with your values.
+    # project = 'my-project-id'
+    # location = 'us- central1'
+    # queue = 'queue1'
     # [START taskqueues_setup]
 
     # [START taskqueues_deleting_tasks]
-    task_path = client.task_path(project, location, 'queue1', 'foo')
+    task_path = client.task_path(project, location, queue, 'foo')
     response = client.delete_task(task_path)
     # [END taskqueues_deleting_tasks]
 
     # [START taskqueues_purging_tasks]
-    queue_path = client.queue_path(project, location, 'queue1')
+    queue_path = client.queue_path(project, location, queue)
     response = client.purge_queue(queue_path)
     # [END taskqueues_purging_tasks]
 
     # [START taskqueues_pause_queue]
-    queue_path = client.queue_path(project, location, 'queue1')
+    queue_path = client.queue_path(project, location, queue)
     response = client.pause_queue(queue_path)
     # [END taskqueues_pause_queues]
+    return response
+
+
+def delete_queue(project, location, queue):
+    client = tasks.CloudTasksClient()
+
+    # TODO(developer): Uncomment these lines and replace with your values.
+    # project = 'my-project-id'
+    # location = 'us- central1'
+    # queue = 'queue1'
 
     # [START taskqueues_deleting_queues]
-    queue_path = client.queue_path(project, location, 'queue1')
+    queue_path = client.queue_path(project, location, queue)
     response = client.delete_queue(queue_path)
     # [END taskqueues_deleting_queues]
+    return response
 
 
-def retry_task():
+def retry_task(project, location, fooqueue, barqueue, bazqueue):
     # [START taskqueues_retrying_tasks]
     from google.protobuf import duration_pb2
 
     client = tasks.CloudTasksClient()
 
-    project = 'my-project-id'
-    location = 'us-central1'
+    # TODO(developer): Uncomment these lines and replace with your values.
+    # project = 'my-project-id'
+    # location = 'us- central1'
+    # fooqueue = 'fooqueue'
+    # barqueue = 'barqueue'
+    # bazqueue = 'bazqueue'
+
+
     parent = client.location_path(project, location)
 
     max_retry = duration_pb2.Duration()
     max_retry.seconds = 2*60*60*24
 
-    fooqueue = {
-        'name': client.queue_path(project, location, 'fooqueue'),
+    foo = {
+        'name': client.queue_path(project, location, fooqueue),
         'rate_limits': {
             'max_dispatches_per_second': 1
         },
@@ -214,33 +253,34 @@ def retry_task():
     max = duration_pb2.Duration()
     max.seconds = 200
 
-    barqueue = {
-        'name': client.queue_path(project, location, 'barqueue'),
+    bar = {
+        'name': client.queue_path(project, location, barqueue),
         'rate_limits': {
             'max_dispatches_per_second': 1
         },
         'retry_config': {
             'min_backoff': min,
             'max_backoff': max,
-            'nax_doublings': 0
+            'max_doublings': 0
         }
     }
 
     max.seconds = 300
-    bazqueue = {
-        'name': client.queue_path(project, location, 'bazqueue'),
+    baz = {
+        'name': client.queue_path(project, location, bazqueue),
         'rate_limits': {
             'max_dispatches_per_second': 1
         },
         'retry_config': {
             'min_backoff': min,
             'max_backoff': max,
-            'nax_doublings': 3
+            'max_doublings': 3
         }
     }
 
-    queues = [fooqueue, barqueue, bazqueue]
+    queues = [foo, bar, baz]
     for queue in queues:
         response = client.create_queue(parent, queue)
         print(response)
     # [END taskqueues_retrying_tasks]
+    return response
