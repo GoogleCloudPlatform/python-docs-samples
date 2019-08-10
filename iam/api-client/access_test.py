b6fe450559b161cfac39448265485d10314b759a
DSdatsme <darsuratwala@gmail.com>
2019-08-10 19:24:09 +0530

    [IAM] test case to add/remove member

--- iam/api-client/access_test.py
+++ iam/api-client/access_test.py
@@ -13,17 +13,41 @@
 # limitations under the License.
 
 import os
+import random
 
 import access
-
+import service_accounts
 
 def test_access(capsys):
-    project = os.environ['GCLOUD_PROJECT']
+    
+    # Setting up variables for testing
+    project_id = os.environ['GCLOUD_PROJECT']
+    gcp_role = 'roles/owner'
 
-    policy = access.get_policy(project)
+    # section to create service account to test policy updates.
+    rand = str(random.randint(0, 1000))
+    name = 'python-test-' + rand
+    email = 'serviceAccount:' + name + '@' + project_id + '.iam.gserviceaccount.com'
+    service_accounts.create_service_account(
+        project_id, name, 'Py Test Account')
+
+    policy = access.get_policy(project_id)
     out, _ = capsys.readouterr()
     assert 'etag' in out
 
-    policy = access.set_policy(project, policy)
+    policy = access.modify_policy_add_role(policy, gcp_role, email)
     out, _ = capsys.readouterr()
     assert 'etag' in out
+
+    policy = access.modify_policy_remove_member(policy, gcp_role, email)
+    out, _ = capsys.readouterr()
+    assert 'etag' in out
+
+    policy = access.set_policy(project_id, policy)
+    out, _ = capsys.readouterr()
+    assert 'etag' in out
+
+    # deleting the service account created above
+    service_accounts.delete_service_account(
+        email)
+
