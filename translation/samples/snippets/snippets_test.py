9c93b19e30753e708a3d93b7a8ff1a4819dbb3db
Elizabeth Crowdus <elcrowdus@gmail.com>
2019-08-12 08:47:07 -0700

    Translate beta samples fix [(#2327)](https://github.com/GoogleCloudPlatform/python-docs-samples/issues/2327)
    * fixing translate-with-glossary bug
    
    * tests passing
    
    * reverting to python3 compatibility
    
    * snippets test fix

--- translation/samples/snippets/snippets_test.py
+++ translation/samples/snippets/snippets_test.py
@@ -46,4 +46,4 @@ def test_translate_utf8(capsys):
     text = u'나는 파인애플을 좋아한다.'
     snippets.translate_text('en', text)
     out, _ = capsys.readouterr()
-    assert u'I like pineapples.' in out
+    assert u'I like pineapple' in out
