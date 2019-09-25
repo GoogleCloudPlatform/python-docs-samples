fb5ba6d5528f86a0efab18e1dd29b86f489bb4d9
Leah E. Cole <6719667+leahecole@users.noreply.github.com>
2019-09-25 14:31:14 -0700

    Update functions/composer/composer_storage_trigger.py
    fix nit
    
    Co-Authored-By: Tim Swast <swast@google.com>

--- functions/composer/composer_storage_trigger.py
+++ functions/composer/composer_storage_trigger.py
@@ -34,7 +34,9 @@ def trigger_dag(data, context=None):
     For more info, refer to
     https://cloud.google.com/functions/docs/writing/background#functions_background_parameters-python
 
-    When called via Python, a context object is not needed,
+    To call this function from a Python script, omit the ``context`` argument
+    and pass in a non-null value for the ``data`` argument.
+    """
     and a non-null data object must be present."""
 
     # Fill in with your Composer info here
