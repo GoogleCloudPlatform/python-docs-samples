771cce5453827165aed851e846f0f65ed7f05df2
Tim Swast <swast@google.com>
2019-10-15 15:40:10 -0700

    feat(bigquery): add Dataset.default_partition_expiration_ms and Table.require_partition_filter properties (#9464)
    * feat(bigquery): add Dataset.default_partiion_expiration_ms and Table.require_partition_filter properties
    
    Samples double as system tests. I aim to use them here:
    
    * https://cloud.google.com/bigquery/docs/managing-partitioned-tables#require-filter
    * https://cloud.google.com/bigquery/docs/updating-datasets#partition-expiration
    
    Note: this also (silently) deprecates
    TimePartitioning.require_partition_filter, as that's duplicating the
    same functionality.
    
    I was curious why the expiration_ms wasn't also moving up, but then I
    realized that property only makes sense if a partition is assocatied
    with a timestamp.
    
    * add deprecation warning to require_partition_filter
    
    * blacken

---
+++ bigquery/tests/test_update_dataset_default_partition_expiration.py
@@ -0,0 +1,31 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .. import update_dataset_default_partition_expiration
+
+
+def test_update_dataset_default_partition_expiration(capsys, client, dataset_id):
+
+    ninety_days_ms = 90 * 24 * 60 * 60 * 1000  # in milliseconds
+
+    update_dataset_default_partition_expiration.update_dataset_default_partition_expiration(
+        client, dataset_id
+    )
+    out, _ = capsys.readouterr()
+    assert (
+        "Updated dataset {} with new default partition expiration {}".format(
+            dataset_id, ninety_days_ms
+        )
+        in out
+    )
---
+++ bigquery/update_dataset_default_partition_expiration.py
@@ -0,0 +1,43 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def update_dataset_default_partition_expiration(client, dataset_id):
+
+    # [START bigquery_update_dataset_partition_expiration]
+    # TODO(developer): Import the client library.
+    # from google.cloud import bigquery
+
+    # TODO(developer): Construct a BigQuery client object.
+    # client = bigquery.Client()
+
+    # TODO(developer): Set dataset_id to the ID of the dataset to fetch.
+    # dataset_id = 'your-project.your_dataset'
+
+    dataset = client.get_dataset(dataset_id)  # Make an API request.
+
+    # Set the default partition expiration (applies to new tables, only) in
+    # milliseconds. This example sets the default expiration to 90 days.
+    dataset.default_partition_expiration_ms = 90 * 24 * 60 * 60 * 1000
+
+    dataset = client.update_dataset(
+        dataset, ["default_partition_expiration_ms"]
+    )  # Make an API request.
+
+    print(
+        "Updated dataset {}.{} with new default partition expiration {}".format(
+            dataset.project, dataset.dataset_id, dataset.default_partition_expiration_ms
+        )
+    )
+    # [END bigquery_update_dataset_partition_expiration]
