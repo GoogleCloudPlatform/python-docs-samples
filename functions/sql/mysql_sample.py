59fc01fe26cb383c216820ffbeafdac675c3aed5
Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>
2019-10-18 08:24:55 -0700

    Update functions/sql example to use more clear connection logic.
    Fixes #2127

--- functions/sql/mysql_sample.py
+++ functions/sql/mysql_sample.py
@@ -24,6 +24,8 @@ DB_USER = getenv('MYSQL_USER', '<YOUR DB USER>')
 DB_PASSWORD = getenv('MYSQL_PASSWORD', '<YOUR DB PASSWORD>')
 DB_NAME = getenv('MYSQL_DATABASE', '<YOUR DB NAME>')
 
+DEBUG = false # set to true to test locally using Cloud SQL proxy listening on a TCP port
+
 mysql_config = {
   'user': DB_USER,
   'password': DB_PASSWORD,
@@ -56,12 +58,11 @@ def mysql_demo(request):
     # GCF instance. Doing so minimizes the number of active SQL connections,
     # which helps keep your GCF instances under SQL connection limits.
     if not mysql_conn:
-        try:
-            mysql_conn = pymysql.connect(**mysql_config)
-        except OperationalError:
-            # If production settings fail, use local development ones
-            mysql_config['unix_socket'] = f'/cloudsql/{CONNECTION_NAME}'
-            mysql_conn = pymysql.connect(**mysql_config)
+        if DEBUG:
+            # try to connect using localling running Cloud SQL proxy (local development only)
+            mysql_conn = pymysql.connect(**mysql_config, host='127.0.0.1', port=3306)
+        else:
+            mysql_conn = pymysql.connect(**mysql_config, unix_socket=f'/cloudsql/{CONNECTION_NAME}')
 
     # Remember to close SQL resources declared while running this function.
     # Keep any declared in global scope (e.g. mysql_conn) for later reuse.
