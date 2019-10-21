483871fce831600274f9805f1e781a84d323c20f
nnegrey <nnegrey@google.com>
2019-10-21 13:41:16 -0600

    Add data residency samples

---
+++ automl/beta/data_residency.py
@@ -0,0 +1,38 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def data_residency(project_id):
+    """Set a regional data endpoint using data residency"""
+    # [START automl_client_library]
+    # [START automl_data_residency]
+    from google.cloud import automl_v1beta1 as automl
+
+    client_options = {'api_endpoint': 'automl.googleapis.com:443'}
+
+    # Instantiates a client
+    client = automl.AutoMlClient(client_options=client_options)
+    # [END automl_data_residency]
+
+    # A resource that represents Google Cloud Platform location.
+    # project_id = 'YOUR_PROJECT_ID'
+    project_location = client.location_path(project_id, 'us-central1')
+
+    # List all the datasets available in the region.
+    response = client.list_datasets(
+        project_location, 'translation_dataset_metadata:*')
+
+    for dataset in response:
+        print(dataset)
+    # [END automl_client_library]
---
+++ vision/cloud-client/detect/data_residency.py
@@ -0,0 +1,37 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def data_residency():
+    """Set a regional data endpoint using data residency"""
+    # [START vision_client_library]
+    # [START vision_data_residency]
+    from google.cloud import vision
+
+    client_options = {'api_endpoint': 'vision.googleapis.com:443'}
+
+    client = vision.ImageAnnotatorClient(client_options=client_options)
+    # [END vision_data_residency]
+
+    image_source = vision.types.ImageSource(
+        image_uri='gs://cloud-samples-data/vision/label/setagaya.jpeg')
+
+    image = vision.types.Image(source=image_source)
+
+    response = client.label_detection(image=image)
+
+    print('Labels:')
+    for label in response.label_annotations:
+        print(label.description)
+    # [END vision_client_library]
