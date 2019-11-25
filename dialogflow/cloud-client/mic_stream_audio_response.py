927001367f71183bd519bff0e5d37287c14d4f5e
Dale Markowitz <dalequark@google.com>
2019-11-25 10:29:38 -0500

    some minor refactoring and formatting

--- dialogflow/cloud-client/mic_stream_audio_response.py
+++ dialogflow/cloud-client/mic_stream_audio_response.py
@@ -30,11 +30,11 @@ import uuid
 from dialogflow import enums
 
 # Audio recording parameters
-SAMPLE_RATE = 16000
+SAMPLE_RATE = 44100  # Most modern mics support modeling at this rate
 CHUNK_SIZE = int(SAMPLE_RATE / 10)
 
 
-def grab_intent(projectId, sessionId, languageCode):
+def grab_intent(project_id, session_id, language_code):
     """Start stream from microphone input to dialogflow API"""
 
     session_client = dialogflow.SessionsClient()
@@ -42,32 +42,41 @@ def grab_intent(projectId, sessionId, languageCode):
 
     final_request_received = False
 
-    def __request_generator():
-        input_stream = pyaudio.PyAudio().open(
-            channels=1, rate=SAMPLE_RATE, format=pyaudio.paInt16, input=True)
-        audio_encoding = enums.AudioEncoding.AUDIO_ENCODING_LINEAR_16
-        session_path = session_client.session_path(projectId, sessionId)
-        print('Session path: {}\n'.format(session_path))
+    def _build_initial_request():
+        """The first request should always contain the configuration"""
+        session_path = session_client.session_path(
+            project=project_id, session=session_id
+        )
 
         input_audio_config = dialogflow.types.InputAudioConfig(
-            audio_encoding=audio_encoding,
-            language_code=languageCode,
-            sample_rate_hertz=SAMPLE_RATE)
-        speech_config = dialogflow.types.SynthesizeSpeechConfig(
-            voice=dialogflow.types.VoiceSelectionParams(
-                ssml_gender=enums.SsmlVoiceGender.SSML_VOICE_GENDER_FEMALE))
-        output_audio_config = dialogflow.types.OutputAudioConfig(
-            audio_encoding=enums.OutputAudioEncoding.OUTPUT_AUDIO_ENCODING_LINEAR_16,
+            audio_encoding=enums.AudioEncoding.AUDIO_ENCODING_LINEAR_16,
+            language_code=language_code,
             sample_rate_hertz=SAMPLE_RATE,
-            synthesize_speech_config=speech_config)
+        )
         query_input = dialogflow.types.QueryInput(
             audio_config=input_audio_config)
 
-        # The first request contains the configuration.
-        yield dialogflow.types.StreamingDetectIntentRequest(
+        voice = dialogflow.types.VoiceSelectionParams(
+            ssml_gender=enums.SsmlVoiceGender.SSML_VOICE_GENDER_FEMALE
+        )
+        speech_config = dialogflow.types.SynthesizeSpeechConfig(voice=voice)
+        output_audio_config = dialogflow.types.OutputAudioConfig(
+            audio_encoding=enums.OutputAudioEncoding.OUTPUT_AUDIO_ENCODING_LINEAR_16,
+            sample_rate_hertz=SAMPLE_RATE,
+            synthesize_speech_config=speech_config,
+        )
+
+        return dialogflow.types.StreamingDetectIntentRequest(
             session=session_path,
             query_input=query_input,
-            output_audio_config=output_audio_config)
+            output_audio_config=output_audio_config,
+        )
+
+    def _request_generator():
+        input_stream = pyaudio.PyAudio().open(
+            channels=1, rate=SAMPLE_RATE, format=pyaudio.paInt16, input=True)
+
+        yield _build_initial_request()
 
         while True:
             if final_request_received:
@@ -81,7 +90,7 @@ def grab_intent(projectId, sessionId, languageCode):
 
     while True:
         print('=' * 20)
-        requests = __request_generator()
+        requests = _request_generator()
         responses = session_client.streaming_detect_intent(requests)
 
         for response in responses:
@@ -112,7 +121,7 @@ def main(project_id, session_id, language_code):
 if __name__ == "__main__":
 
     # TODO: developer uncomment and replace these variables with your own
-    # project_id = 'helloworld-ceyxpf'
+    # project_id = 'YOUR_PROJECT_ID'
     # session_id = uuid.uuid4()
     # language_code = 'en-US'
     main(project_id, session_id, language_code)
