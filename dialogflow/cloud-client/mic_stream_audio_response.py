5aabf27cebeded6a2d8568c9788208e9d168f070
Dale Markowitz <dalequark@google.com>
2019-11-25 10:22:06 -0500

    format/style changes

--- dialogflow/cloud-client/mic_stream_audio_response.py
+++ dialogflow/cloud-client/mic_stream_audio_response.py
@@ -19,14 +19,15 @@ speaks voice responses through the speaker.
 
 Examples:
   python mic_stream_audio_response.py
-  python mic_stream_audio_response.py--project-id PROJECT_ID
 """
+
+# [START dialogflow_microphone_streaming]
 
 import dialogflow
 import pyaudio
 import simpleaudio as sa
-import argparse
 import uuid
+from dialogflow import enums
 
 # Audio recording parameters
 SAMPLE_RATE = 16000
@@ -35,46 +36,48 @@ CHUNK_SIZE = int(SAMPLE_RATE / 10)
 
 def grab_intent(projectId, sessionId, languageCode):
     """Start stream from microphone input to dialogflow API"""
-   
+
     session_client = dialogflow.SessionsClient()
     # Audio output stream
-    
+
     final_request_received = False
 
     def __request_generator():
-        input_stream = pyaudio.PyAudio().open(channels=1,
-                rate=SAMPLE_RATE, format=pyaudio.paInt16, input=True)
-        audio_encoding = dialogflow.enums.AudioEncoding.AUDIO_ENCODING_LINEAR_16
+        input_stream = pyaudio.PyAudio().open(
+            channels=1, rate=SAMPLE_RATE, format=pyaudio.paInt16, input=True)
+        audio_encoding = enums.AudioEncoding.AUDIO_ENCODING_LINEAR_16
         session_path = session_client.session_path(projectId, sessionId)
         print('Session path: {}\n'.format(session_path))
-        
-        input_audio_config = dialogflow.types.InputAudioConfig(audio_encoding=audio_encoding, 
+
+        input_audio_config = dialogflow.types.InputAudioConfig(
+            audio_encoding=audio_encoding,
             language_code=languageCode,
             sample_rate_hertz=SAMPLE_RATE)
         speech_config = dialogflow.types.SynthesizeSpeechConfig(
-            voice=dialogflow.types.VoiceSelectionParams(ssml_gender=dialogflow.enums.SsmlVoiceGender.SSML_VOICE_GENDER_FEMALE))
+            voice=dialogflow.types.VoiceSelectionParams(
+                ssml_gender=enums.SsmlVoiceGender.SSML_VOICE_GENDER_FEMALE))
         output_audio_config = dialogflow.types.OutputAudioConfig(
-            audio_encoding=dialogflow.enums.OutputAudioEncoding.OUTPUT_AUDIO_ENCODING_LINEAR_16,
+            audio_encoding=enums.OutputAudioEncoding.OUTPUT_AUDIO_ENCODING_LINEAR_16,
             sample_rate_hertz=SAMPLE_RATE,
             synthesize_speech_config=speech_config)
-        query_input = dialogflow.types.QueryInput(audio_config=input_audio_config)
+        query_input = dialogflow.types.QueryInput(
+            audio_config=input_audio_config)
 
         # The first request contains the configuration.
         yield dialogflow.types.StreamingDetectIntentRequest(
-            session=session_path, query_input=query_input, output_audio_config=output_audio_config)
+            session=session_path,
+            query_input=query_input,
+            output_audio_config=output_audio_config)
 
         while True:
             if final_request_received:
-                print("received final request")
                 input_stream.close()
-                print("closed stream")
                 return
             if input_stream.is_active():
-                content = input_stream.read(CHUNK_SIZE, exception_on_overflow = False)
-                yield dialogflow.types.StreamingDetectIntentRequest(input_audio=content)
-        
-
-
+                content = input_stream.read(
+                    CHUNK_SIZE, exception_on_overflow=False)
+                yield dialogflow.types.StreamingDetectIntentRequest(
+                    input_audio=content)
 
     while True:
         print('=' * 20)
@@ -82,12 +85,15 @@ def grab_intent(projectId, sessionId, languageCode):
         responses = session_client.streaming_detect_intent(requests)
 
         for response in responses:
-            print(f'Intermediate transcription result: {response.recognition_result.transcript}')
+            print('Intermediate transcription result: {}'.format(
+                response.recognition_result.transcript))
             if response.recognition_result.is_final:
                 final_request_received = True
             if response.query_result.query_text:
-                print(f'Fullfilment Text: {response.query_result.fulfillment_text}')
-                print(f'Intent: {response.query_result.intent}')
+                print('Fullfilment Text: {}'.format(
+                    response.query_result.fulfillment_text))
+                print('Intent: {}'.format(
+                    response.query_result.intent.display_name))
             if response.output_audio:
                 return response
 
@@ -96,28 +102,19 @@ def play_audio(audio):
     audio_obj = sa.play_buffer(audio, 1, 2, SAMPLE_RATE)
     audio_obj.wait_done()
 
-def main():
+
+def main(project_id, session_id, language_code):
     while(True):
-        response = grab_intent(args.project_id, args.session_id, args.language_code)
+        response = grab_intent(project_id, session_id, language_code)
         play_audio(response.output_audio)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument(
-        '--project-id',
-        help='Project/agent id.  Required.',
-        required=True)
-    parser.add_argument(
-        '--session-id',
-        help='Identifier of the DetectIntent session. '
-             'Defaults to a random UUID.',
-        default=str(uuid.uuid4()))
-    parser.add_argument(
-        '--language-code',
-        help='Language code of the query. Defaults to "en-US".',
-        default='en-US')
 
-    args = parser.parse_args()
-    main()
+if __name__ == "__main__":
+
+    # TODO: developer uncomment and replace these variables with your own
+    # project_id = 'helloworld-ceyxpf'
+    # session_id = uuid.uuid4()
+    # language_code = 'en-US'
+    main(project_id, session_id, language_code)
+
+# [END dialogflow_microphone_streaming]
