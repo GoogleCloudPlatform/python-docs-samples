93eb3ca684efcfed8895a662f837c15bb30bac4f
kurtisvg <31518063+kurtisvg@users.noreply.github.com>
2019-12-06 11:45:17 -0800

    Run black.

--- dlp/redact_test.py
+++ dlp/redact_test.py
@@ -20,11 +20,11 @@ import pytest
 
 import redact
 
-GCLOUD_PROJECT = os.getenv('GCLOUD_PROJECT')
-RESOURCE_DIRECTORY = os.path.join(os.path.dirname(__file__), 'resources')
+GCLOUD_PROJECT = os.getenv("GCLOUD_PROJECT")
+RESOURCE_DIRECTORY = os.path.join(os.path.dirname(__file__), "resources")
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def tempdir():
     tempdir = tempfile.mkdtemp()
     yield tempdir
@@ -32,14 +32,12 @@ def tempdir():
 
 
 def test_redact_image_file(tempdir, capsys):
-    test_filepath = os.path.join(RESOURCE_DIRECTORY, 'test.png')
-    output_filepath = os.path.join(tempdir, 'redacted.png')
+    test_filepath = os.path.join(RESOURCE_DIRECTORY, "test.png")
+    output_filepath = os.path.join(tempdir, "redacted.png")
 
     redact.redact_image(
-        GCLOUD_PROJECT,
-        test_filepath,
-        output_filepath,
-        ['FIRST_NAME', 'EMAIL_ADDRESS'])
+        GCLOUD_PROJECT, test_filepath, output_filepath, ["FIRST_NAME", "EMAIL_ADDRESS"]
+    )
 
     out, _ = capsys.readouterr()
     assert output_filepath in out
