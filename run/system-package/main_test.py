d0b577999c0a0333473b666e6c8e92667a718f1a
Dina Graves Portman <dinagraves@google.com>
2019-12-10 12:36:41 -0800

    Stronger assertion in test

--- run/system-package/main_test.py
+++ run/system-package/main_test.py
@@ -43,4 +43,4 @@ def test_bad_dot_parameter(client):
 
 def test_good_dot_parameter(client):
     r = client.get("/diagram.png?dot=digraph G { A -> {B, C, D} -> {F} }")
-    assert r.status_code == 200
+    assert r.content_type == "image/png"
