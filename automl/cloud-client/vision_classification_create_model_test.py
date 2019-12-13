1295aea885625716224c1226256252105689517f
nnegrey <nnegrey@google.com>
2019-12-13 11:25:30 -0700

    automl: add vision classification samples for automl ga

---
+++ automl/cloud-client/vision_classification_create_model_test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+import pytest
+
+import vision_classification_create_model
+
+PROJECT_ID = os.environ["GCLOUD_PROJECT"]
+DATASET_ID = 'ICN3946265060617537378'
+
+
+@pytest.mark.slow
+def test_deploy_undeploy_model_with_node_count(capsys):
+    vision_classification_create_model.create_model(
+        PROJECT_ID, DATASET_ID, 'classification_test_create_model')
+    out, _ = capsys.readouterr()
+    assert 'Training started' in out
+
+    # Cancel the operation
+    operation_id = out.split('Training operation name: ')[1].split('\n')[0]
+    from google.cloud import automl
+    client = automl.AutoMlClient()
+    client.transport._operations_client.cancel_operation(operation_id)
---
+++ automl/cloud-client/vision_classification_deploy_model_node_count_test.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import sys
+
+import pytest
+
+import vision_classification_deploy_model_node_count
+
+PROJECT_ID = os.environ["GCLOUD_PROJECT"]
+
+version_info = sys.version_info
+if version_info.major == 3 and version_info.minor == 6:
+    MODEL_ID = "ICN5430958520562352128"
+else:
+    MODEL_ID = "ICN7383667271543079510"
+
+
+@pytest.fixture(scope="function")
+def verify_model_state():
+    from google.cloud import automl
+
+    client = automl.AutoMlClient()
+    model_full_id = client.model_path(PROJECT_ID, "us-central1", MODEL_ID)
+
+    model = client.get_model(model_full_id)
+    if model.deployment_state == automl.enums.Model.DeploymentState.DEPLOYED:
+        # Undeploy model if it is deployed
+        response = client.undeploy_model(model_full_id)
+        response.result()
+
+
+@pytest.mark.slow
+def test_deploy_undeploy_model_with_node_count(capsys, verify_model_state):
+    verify_model_state
+    vision_classification_deploy_model_node_count.deploy_model_with_node_count(
+        PROJECT_ID, MODEL_ID
+    )
+    out, _ = capsys.readouterr()
+    assert "Model deployment finished." in out
