7825b1a2ef782831ccab8b19ab5ecbcbe2ee1b85
Leah Cole <coleleah@google.com>
2019-12-16 16:55:34 -0500

    fix: datastore flaky

--- datastore/cloud-client/tasks_test.py
+++ datastore/cloud-client/tasks_test.py
@@ -14,7 +14,7 @@
 import os
 
 from gcp_devrel.testing import eventually_consistent
-from gcp_devrel.testing.flaky import flaky
+from flaky import flaky
 from google.cloud import datastore
 import pytest
 
