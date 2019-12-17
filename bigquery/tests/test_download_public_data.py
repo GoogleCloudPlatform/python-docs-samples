6d74dce3cc0db2d37e5413e0faea171737c98f41
Tim Swast <swast@google.com>
2019-12-16 16:50:44 -0800

    feat(bigquery): add `create_bqstorage_client` param to `to_dataframe` and `to_arrow` (#9573)
    * feat(bigquery): add `create_bqstorage_client` param to `to_dataframe` and `to_arrow`
    
    When the `create_bqstorage_client` parameter is set to `True`, the
    BigQuery client constructs a BigQuery Storage API client for you. This
    removes the need for boilerplate code to manually construct both clients
    explitly with the same credentials.
    
    Does this make the `bqstorage_client` parameter unnecessary? In most
    cases, yes, but there are a few cases where we'll want to continue using
    it.
    
    * When partner tools use `to_dataframe`, they should continue to use
      `bqstorage_client` so that they can set the correct amended user-agent
      strings.
    * When a developer needs to override the default API endpoint for the BQ
      Storage API, they'll need to manually supply a `bqstorage_client`.
    
    * test for BQ Storage API usage in samples tests.
    
    * fix: close bqstorage client if created by to_dataframe/to_arrow
    
    * chore: blacken
    
    * doc: update versionadded
    
    * doc: update versionadded

---
+++ bigquery/tests/test_download_public_data.py
@@ -0,0 +1,34 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+
+from .. import download_public_data
+
+
+def test_download_public_data(caplog, capsys, client):
+    # Enable debug-level logging to verify the BigQuery Storage API is used.
+    caplog.set_level(logging.DEBUG)
+
+    download_public_data.download_public_data(client)
+    out, _ = capsys.readouterr()
+    assert "year" in out
+    assert "gender" in out
+    assert "name" in out
+
+    assert any(
+        "Started reading table 'bigquery-public-data.usa_names.usa_1910_current' with BQ Storage API session"
+        in message
+        for message in caplog.messages
+    )
---
+++ bigquery/tests/test_download_public_data_sandbox.py
@@ -0,0 +1,34 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+
+from .. import download_public_data_sandbox
+
+
+def test_download_public_data_sandbox(caplog, capsys, client):
+    # Enable debug-level logging to verify the BigQuery Storage API is used.
+    caplog.set_level(logging.DEBUG)
+
+    download_public_data_sandbox.download_public_data_sandbox(client)
+    out, err = capsys.readouterr()
+    assert "year" in out
+    assert "gender" in out
+    assert "name" in out
+
+    assert any(
+        # An anonymous table is used because this sample reads from query results.
+        ("Started reading table" in message and "BQ Storage API session" in message)
+        for message in caplog.messages
+    )
