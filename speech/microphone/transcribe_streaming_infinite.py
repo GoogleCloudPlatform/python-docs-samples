1eb9c52a4ca8ced2fb34d714126a83fd0f648ac6
nnegrey <nnegrey@google.com>
2019-12-17 12:10:36 -0700

    Remove similar samples, update streaming limit

--- speech/microphone/transcribe_streaming_infinite.py
+++ speech/microphone/transcribe_streaming_infinite.py
@@ -38,7 +38,7 @@ import pyaudio
 from six.moves import queue
 
 # Audio recording parameters
-STREAMING_LIMIT = 10000
+STREAMING_LIMIT = 240000  # 4 minutes
 SAMPLE_RATE = 16000
 CHUNK_SIZE = int(SAMPLE_RATE / 10)  # 100ms
 
