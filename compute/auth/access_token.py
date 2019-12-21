82237a742e1e4a216f0c8553d6fefdaf6bafba53
Aman Raj <56553835+dgnext@users.noreply.github.com>
2019-12-21 15:19:43 +0530

    Update access_token.py

--- compute/auth/access_token.py
+++ compute/auth/access_token.py
@@ -1,3 +1,58 @@
+
+import argparse
+
+import requests
+
+
+METADATA_URL = 'http://metadata.google.internal/computeMetadata/v1/'
+METADATA_HEADERS = {'Metadata-Flavor': 'Google'}
+SERVICE_ACCOUNT = 'default'
+
+
+def get_access_token():
+    url = '{}instance/service-accounts/{}/token'.format(
+        METADATA_URL, SERVICE_ACCOUNT)
+
+    # Request an access token from the metadata server.
+    r = requests.get(url, headers=METADATA_HEADERS)
+    r.raise_for_status()
+
+    # Extract the access token from the response.
+    access_token = r.json()['access_token']
+
+    return access_token
+
+
+def list_buckets(project_id, access_token):
+    url = 'https://www.googleapis.com/storage/v1/b'
+    params = {
+        'project': project_id
+    }
+    headers = {
+        'Authorization': 'Bearer {}'.format(access_token)
+    }
+
+    r = requests.get(url, params=params, headers=headers)
+    r.raise_for_status()
+
+    return r.json()
+
+
+def main(project_id):
+    access_token = get_access_token()
+    buckets = list_buckets(project_id, access_token)
+    print(buckets)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('project_id', help='Your Google Cloud project ID.')
+
+    args = parser.parse_args()
+
+    main(args.project_id)
 #!/usr/bin/env python
 
 # Copyright 2016 Google Inc. All Rights Reserved.
