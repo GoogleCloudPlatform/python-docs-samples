f7d346f78594b8e9802470a668c2083fc44287c4
Chris Wilcox <crwilcox@google.com>
2019-12-30 16:36:16 -0800

    fix: lint

--- firestore/cloud-client/snippets_test.py
+++ firestore/cloud-client/snippets_test.py
@@ -23,6 +23,7 @@ os.environ['GOOGLE_CLOUD_PROJECT'] = os.environ['FIRESTORE_PROJECT']
 
 UNIQUE_STRING = str(uuid.uuid4()).split("-")[0]
 
+
 class TestFirestoreClient(firestore.Client):
     def __init__(self, *args, **kwargs):
         self._UNIQUE_STRING = UNIQUE_STRING
@@ -33,11 +34,14 @@ class TestFirestoreClient(firestore.Client):
         collection_name += '-{}'.format(self._UNIQUE_STRING)
         return self._super.collection(collection_name, *args, **kwargs)
 
+
 snippets.firestore.Client = TestFirestoreClient
+
 
 @pytest.fixture
 def db():
     yield snippets.firestore.Client()
+
 
 def test_quickstart_new_instance():
     snippets.quickstart_new_instance()
