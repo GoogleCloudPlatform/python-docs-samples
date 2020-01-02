f94bb7894cb7c92a0d5a1cd36875838f1077822e
Christopher Wilcox <crwilcox@google.com>
2020-01-02 09:31:15 -0800

    fix: Improve Firestore tests by separating test runs, improving cleanup (#2667)
    * fix: harden the wait for consistency for firestore multiple listen snippet
    
    * fix: formatting
    
    * add a setup step in CLI (#2611)
    
    * fix: formatting
    
    * fix: formatting
    
    * fix: add setup to snippets for tests
    
    * fix: rename sydney_query to denver_query as it refers to denver
    
    * fix: Use stream instead of get in samples
    
    * fix: stop deleting LA, delete DEN instead
    
    * fix: move delete tests to end of file
    
    * fix: move BJ to top to work with snapshot cursors
    
    * fix: Beijing -> BJ
    
    * during delete collection delete all collections created for these tests
    
    * fix: add cleanup test to distributed counters
    
    * wrap db.collections calls to use unique names
    
    * fix: remove unused import
    
    * fix: remove more complicated fix for time now that we have separate collections
    
    * fix: reorder tests
    
    * fix: move test firestore client to test file
    
    * fix: lint
    
    * fix: use old-style classes for Python 2.7
    
    Co-authored-by: Tianzi Cai <tianzi@google.com>

--- firestore/cloud-client/distributed_counters_test.py
+++ firestore/cloud-client/distributed_counters_test.py
@@ -47,3 +47,15 @@ def test_distributed_counters(fs_client):
     counter.increment_counter(doc_ref)
     counter.increment_counter(doc_ref)
     assert counter.get_count(doc_ref) == 2
+
+
+def test_distributed_counters_cleanup(fs_client):
+    col = fs_client.collection("dc_samples")
+    doc_ref = col.document("distributed_counter")
+
+    shards = doc_ref.collection("shards").list_documents()
+    shards_list = [shard for shard in shards]
+    for shard in shards_list:
+        shard.delete()
+
+    doc_ref.delete()
