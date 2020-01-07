fba4c12d5f55fbd3f615b5d96bf36e799fd1ca44
Noah Negrey <nnegrey@users.noreply.github.com>
2020-01-07 11:12:01 -0700

    automl: add natural language sentiment analysis ga samples (#2677)
    * automl: add natural language sentiment analysis ga samples
    
    * Add links to documentation
    
    * Update tests to use centralized project
    
    * Fix environment variable, make translate test less flaky

--- automl/cloud-client/language_sentiment_analysis_create_dataset.py
+++ automl/cloud-client/language_sentiment_analysis_create_dataset.py
@@ -15,28 +15,28 @@
 
 def create_dataset(project_id, display_name):
     """Create a dataset."""
-    # [START automl_vision_classification_create_dataset]
+    # [START automl_language_sentiment_analysis_create_dataset]
     from google.cloud import automl
 
     # TODO(developer): Uncomment and set the following variables
     # project_id = "YOUR_PROJECT_ID"
-    # display_name = "your_datasets_display_name"
+    # display_name = "YOUR_DATASET_NAME"
 
     client = automl.AutoMlClient()
 
     # A resource that represents Google Cloud Platform location.
     project_location = client.location_path(project_id, "us-central1")
-    # Specify the classification type
-    # Types:
-    # MultiLabel: Multiple labels are allowed for one example.
-    # MultiClass: At most one label is allowed per example.
-    # https://cloud.google.com/automl/docs/reference/rpc/google.cloud.automl.v1#classificationtype
-    metadata = automl.types.ImageClassificationDatasetMetadata(
-        classification_type=automl.enums.ClassificationType.MULTILABEL
-    )
+
+    # Each dataset requires a sentiment score with a defined sentiment_max
+    # value, for more information on TextSentimentDatasetMetadata, see:
+    # https://cloud.google.com/natural-language/automl/docs/prepare#sentiment-analysis
+    # https://cloud.google.com/automl/docs/reference/rpc/google.cloud.automl.v1#textsentimentdatasetmetadata
+    metadata = automl.types.TextSentimentDatasetMetadata(
+        sentiment_max=4
+    )  # Possible max sentiment score: 1-10
+
     dataset = automl.types.Dataset(
-        display_name=display_name,
-        image_classification_dataset_metadata=metadata,
+        display_name=display_name, text_sentiment_dataset_metadata=metadata
     )
 
     # Create a dataset with the dataset metadata in the region.
@@ -47,4 +47,4 @@ def create_dataset(project_id, display_name):
     # Display the dataset information
     print("Dataset name: {}".format(created_dataset.name))
     print("Dataset id: {}".format(created_dataset.name.split("/")[-1]))
-    # [END automl_vision_classification_create_dataset]
+    # [END automl_language_sentiment_analysis_create_dataset]
