f0e04c7efe1f11350ab52420ef21f8a4418c4822
Bu Sun Kim <busunkim@google.com>
2020-01-08 16:34:40 -0800

    more tweaks to get quickstart to run

--- texttospeech/cloud-client/quickstart.py
+++ texttospeech/cloud-client/quickstart.py
@@ -28,23 +28,23 @@ def run_quickstart():
     Note: ssml must be well-formed according to:
         https://www.w3.org/TR/speech-synthesis/
     """
-    from google.cloud import texttospeech
+    from google.cloud import texttospeech_v1 as texttospeech
 
     # Instantiates a client
     client = texttospeech.TextToSpeechClient()
 
     # Set the text input to be synthesized
-    synthesis_input = texttospeech.types.SynthesisInput(text="Hello, World!")
+    synthesis_input = texttospeech.SynthesisInput(text="Hello, World!")
 
     # Build the voice request, select the language code ("en-US") and the ssml
     # voice gender ("neutral")
-    voice = texttospeech.types.VoiceSelectionParams(
+    voice = texttospeech.VoiceSelectionParams(
         language_code='en-US',
-        ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+        ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL)
 
     # Select the type of audio file you want returned
-    audio_config = texttospeech.types.AudioConfig(
-        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3)
 
     # Perform the text-to-speech request on the text input with the selected
     # voice parameters and audio file type
