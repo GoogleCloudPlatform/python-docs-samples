fc14e9d796fb8143d00baffda31cfba4e2f99c54
Sarath Kaul <sarath.kaul@searce.com>
2020-01-15 12:20:42 +0530

    Http Task Tests updated for Task Name

--- tasks/create_http_task_test.py
+++ tasks/create_http_task_test.py
@@ -16,13 +16,19 @@ import os
 
 import create_http_task
 
-TEST_PROJECT_ID = os.getenv('GCLOUD_PROJECT')
-TEST_LOCATION = os.getenv('TEST_QUEUE_LOCATION', 'us-central1')
-TEST_QUEUE_NAME = os.getenv('TEST_QUEUE_NAME', 'my-queue')
+TEST_PROJECT_ID = os.getenv("GCLOUD_PROJECT")
+TEST_LOCATION = os.getenv("TEST_QUEUE_LOCATION", "us-central1")
+TEST_QUEUE_NAME = os.getenv("TEST_QUEUE_NAME", "my-queue")
+TEST_TASK_NAME = os.getenv("TEST_TASK_NAME", "my-task")
 
 
 def test_create_http_task():
-    url = 'https://example.com/task_handler'
+    url = "https://example.com/task_handler"
     result = create_http_task.create_http_task(
-        TEST_PROJECT_ID, TEST_QUEUE_NAME, TEST_LOCATION, url)
+        project=TEST_PROJECT_ID,
+        queue=TEST_QUEUE_NAME,
+        location=TEST_LOCATION,
+        url=url,
+        task_name=TEST_TASK_NAME,
+    )
     assert TEST_QUEUE_NAME in result.name
