218a73a109614ab825cd761b514d1c72bd73d78c
nnegrey <nnegrey@google.com>
2020-01-17 16:27:40 -0700

    run black on new files

--- dialogflow/cloud-client/create_session_entity_type_test.py
+++ dialogflow/cloud-client/create_session_entity_type_test.py
@@ -22,11 +22,12 @@ import dialogflow_v2 as dialogflow
 
 import session_entity_type_management
 
-PROJECT_ID = os.getenv('GCLOUD_PROJECT')
-SESSION_ID = 'session_' + datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-ENTITY_TYPE_DISPLAY_NAME = 'test_type_' \
-                           + datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-ENTITY_VALUES = ['fake_entity_value_1', 'fake_entity_value_2']
+PROJECT_ID = os.getenv("GCLOUD_PROJECT")
+SESSION_ID = "session_" + datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+ENTITY_TYPE_DISPLAY_NAME = "test_type_" + datetime.datetime.now().strftime(
+    "%Y%m%d%H%M%S"
+)
+ENTITY_VALUES = ["fake_entity_value_1", "fake_entity_value_2"]
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -36,23 +37,29 @@ def setup_teardown():
     parent = entity_types_client.project_agent_path(PROJECT_ID)
     entity_type = dialogflow.types.EntityType(
         display_name=ENTITY_TYPE_DISPLAY_NAME,
-        kind=dialogflow.enums.EntityType.Kind.KIND_MAP)
+        kind=dialogflow.enums.EntityType.Kind.KIND_MAP,
+    )
 
     response = entity_types_client.create_entity_type(parent, entity_type)
-    entity_type_id = response.name.split('agent/entityTypes/')[1]
+    entity_type_id = response.name.split("agent/entityTypes/")[1]
 
     yield
 
     # Delete the created entity type
     entity_type_path = entity_types_client.entity_type_path(
-        PROJECT_ID, entity_type_id)
+        PROJECT_ID, entity_type_id
+    )
     entity_types_client.delete_entity_type(entity_type_path)
 
 
 def test_create_session_entity_type(capsys):
     session_entity_type_management.create_session_entity_type(
-        PROJECT_ID, SESSION_ID, ENTITY_VALUES, ENTITY_TYPE_DISPLAY_NAME,
-        'ENTITY_OVERRIDE_MODE_SUPPLEMENT')
+        PROJECT_ID,
+        SESSION_ID,
+        ENTITY_VALUES,
+        ENTITY_TYPE_DISPLAY_NAME,
+        "ENTITY_OVERRIDE_MODE_SUPPLEMENT",
+    )
 
     out, _ = capsys.readouterr()
 
