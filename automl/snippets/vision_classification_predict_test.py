767240efc5d38b72ea669e0e34c8bfc574552010
Noah Negrey <nnegrey@users.noreply.github.com>
2020-01-28 13:52:54 -0700

    Automl ga base samples [(#2613)](https://github.com/GoogleCloudPlatform/python-docs-samples/issues/2613)
    * automl: add base samples
    
    * automl: add base set of samples
    
    * Clean up tests
    
    * License year 2020, drop python2 print statement unicode
    
    * use centralized automl testing project
    
    * Fix GCS path typo
    
    * Use fake dataset for batch predict
    
    * lint: line length
    
    * fix fixture naming and use
    
    * Fix fixture changes
    
    * Catch resource exhausted error
    
    * use fake data for import test
    
    * update how to access an operation id
    
    Co-authored-by: Leah E. Cole <6719667+leahecole@users.noreply.github.com>

--- automl/snippets/vision_classification_predict_test.py
+++ automl/snippets/vision_classification_predict_test.py
@@ -23,8 +23,9 @@ PROJECT_ID = os.environ["AUTOML_PROJECT_ID"]
 MODEL_ID = os.environ["VISION_CLASSIFICATION_MODEL_ID"]
 
 
-@pytest.fixture(scope="function")
-def verify_model_state():
+@pytest.fixture(scope="function", autouse=True)
+def setup():
+    # Verify the model is deployed before tyring to predict
     client = automl.AutoMlClient()
     model_full_id = client.model_path(PROJECT_ID, "us-central1", MODEL_ID)
 
@@ -35,8 +36,7 @@ def verify_model_state():
         response.result()
 
 
-def test_vision_classification_predict(capsys, verify_model_state):
-    verify_model_state
+def test_vision_classification_predict(capsys):
     file_path = "resources/test.png"
     vision_classification_predict.predict(PROJECT_ID, MODEL_ID, file_path)
     out, _ = capsys.readouterr()
