16f190087cae6b6caeceaa60cfa21704b09c111e
Charles Engelke <engelke@google.com>
2020-02-18 13:59:05 -0800

    Added tests

--- appengine/standard/migration/urlfetch/requests/main.py
+++ appengine/standard/migration/urlfetch/requests/main.py
@@ -42,3 +42,8 @@ def server_error(e):
     See logs for full stacktrace.
     """.format(e), 500
 # [END app]
+
+
+if __name__ == '__main__':
+    # This is used when running locally.
+    app.run(host='127.0.0.1', port=8080, debug=True)
