73428409d1792a62568a2264465d58f00c2de230
Charles Engelke <engelke@google.com>
2020-02-25 14:39:18 -0800

    Remove old debugging line.

--- appengine/standard/migration/storage/main.py
+++ appengine/standard/migration/storage/main.py
@@ -55,14 +55,6 @@ def get():
     return response
 
 
-
-
-
-
-
-    return 'Hello there', 200
-
-
 if __name__ == '__main__':
     # This is used when running locally.
     app.run(host='127.0.0.1', port=8080, debug=True)
