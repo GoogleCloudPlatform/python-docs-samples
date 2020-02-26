beace62dceb1ea48ad6d102f3e5609ed0eb09214
kurtisvg <31518063+kurtisvg@users.noreply.github.com>
2020-02-25 16:26:15 -0800

    Fix lint error

--- opencensus/metrics_quickstart_test.py
+++ opencensus/metrics_quickstart_test.py
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
-
-from. import metrics_quickstart
+import metrics_quickstart
 
 
 def test_quickstart_main(capsys):
