273210f8b1b57b6b741bafabb13bbcd70a737b00
Noah Negrey <nnegrey@users.noreply.github.com>
2020-02-28 16:09:11 -0700

    Speech sample cleanup [(#2628)](https://github.com/GoogleCloudPlatform/python-docs-samples/issues/2628)
    * Remove similar samples, update streaming limit
    
    * Update to v1 library
    
    * Fix requirements.txt
    
    * Update library

--- speech/microphone/transcribe_streaming_infinite.py
+++ speech/microphone/transcribe_streaming_infinite.py
@@ -32,13 +32,12 @@ import time
 import re
 import sys
 
-# uses result_end_time currently only avaialble in v1p1beta, will be in v1 soon
-from google.cloud import speech_v1p1beta1 as speech
+from google.cloud import speech
 import pyaudio
 from six.moves import queue
 
 # Audio recording parameters
-STREAMING_LIMIT = 10000
+STREAMING_LIMIT = 240000  # 4 minutes
 SAMPLE_RATE = 16000
 CHUNK_SIZE = int(SAMPLE_RATE / 10)  # 100ms
 
