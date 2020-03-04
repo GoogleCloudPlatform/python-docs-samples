582b4b89abbce48e6b7865dbbc0538c20adca3b6
shollyman <shollyman@google.com>
2020-03-03 16:34:47 -0800

    chore: update quickstart to use v1 endpoint (#19)
    * chore: update quickstart to leverage v1 endpoint

--- bigquery_storage/quickstart.py
+++ bigquery_storage/quickstart.py
@@ -17,7 +17,7 @@ import argparse
 
 def main(project_id="your-project-id", snapshot_millis=0):
     # [START bigquerystorage_quickstart]
-    from google.cloud import bigquery_storage_v1beta1
+    from google.cloud import bigquery_storage_v1
 
     # TODO(developer): Set the project_id variable.
     # project_id = 'your-project-id'
@@ -25,51 +25,44 @@ def main(project_id="your-project-id", snapshot_millis=0):
     # The read session is created in this project. This project can be
     # different from that which contains the table.
 
-    client = bigquery_storage_v1beta1.BigQueryStorageClient()
+    client = bigquery_storage_v1.BigQueryReadClient()
 
     # This example reads baby name data from the public datasets.
-    table_ref = bigquery_storage_v1beta1.types.TableReference()
-    table_ref.project_id = "bigquery-public-data"
-    table_ref.dataset_id = "usa_names"
-    table_ref.table_id = "usa_1910_current"
+    table = "projects/{}/datasets/{}/tables/{}".format(
+        "bigquery-public-data", "usa_names", "usa_1910_current"
+    )
+
+    requested_session = bigquery_storage_v1.types.ReadSession()
+    requested_session.table = table
+    # This API can also deliver data serialized in Apache Arrow format.
+    # This example leverages Apache Avro.
+    requested_session.data_format = bigquery_storage_v1.enums.DataFormat.AVRO
 
     # We limit the output columns to a subset of those allowed in the table,
     # and set a simple filter to only report names from the state of
     # Washington (WA).
-    read_options = bigquery_storage_v1beta1.types.TableReadOptions()
-    read_options.selected_fields.append("name")
-    read_options.selected_fields.append("number")
-    read_options.selected_fields.append("state")
-    read_options.row_restriction = 'state = "WA"'
+    requested_session.read_options.selected_fields.append("name")
+    requested_session.read_options.selected_fields.append("number")
+    requested_session.read_options.selected_fields.append("state")
+    requested_session.read_options.row_restriction = 'state = "WA"'
 
     # Set a snapshot time if it's been specified.
     modifiers = None
     if snapshot_millis > 0:
-        modifiers = bigquery_storage_v1beta1.types.TableModifiers()
-        modifiers.snapshot_time.FromMilliseconds(snapshot_millis)
+        requested_session.table_modifiers.snapshot_time.FromMilliseconds(
+            snapshot_millis
+        )
 
     parent = "projects/{}".format(project_id)
     session = client.create_read_session(
-        table_ref,
         parent,
-        table_modifiers=modifiers,
-        read_options=read_options,
-        # This API can also deliver data serialized in Apache Arrow format.
-        # This example leverages Apache Avro.
-        format_=bigquery_storage_v1beta1.enums.DataFormat.AVRO,
-        # We use a LIQUID strategy in this example because we only read from a
-        # single stream. Consider BALANCED if you're consuming multiple streams
-        # concurrently and want more consistent stream sizes.
-        sharding_strategy=(bigquery_storage_v1beta1.enums.ShardingStrategy.LIQUID),
-    )  # API request.
-
-    # We'll use only a single stream for reading data from the table. Because
-    # of dynamic sharding, this will yield all the rows in the table. However,
-    # if you wanted to fan out multiple readers you could do so by having a
-    # reader process each individual stream.
-    reader = client.read_rows(
-        bigquery_storage_v1beta1.types.StreamPosition(stream=session.streams[0])
+        requested_session,
+        # We'll use only a single stream for reading data from the table. However,
+        # if you wanted to fan out multiple readers you could do so by having a
+        # reader process each individual stream.
+        max_stream_count=1,
     )
+    reader = client.read_rows(session.streams[0].name)
 
     # The read stream contains blocks of Avro-encoded bytes. The rows() method
     # uses the fastavro library to parse these blocks as an interable of Python
