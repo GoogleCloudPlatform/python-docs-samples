81bb4c8542b33a76a6f30f53b1f27abef16580eb
Mike Ganbold <munkhbayarg@google.com>
2020-03-05 08:09:14 -0800

    added missing region tags and link to supported langs

--- translate/cloud-client/translate_v3_get_supported_languages_with_target.py
+++ translate/cloud-client/translate_v3_get_supported_languages_with_target.py
@@ -12,21 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# [START translate_v3_get_supported_languages_for_target]
 from google.cloud import translate
 
 
-def get_supported_languages_with_target(
-    language_code="YOUR_TARGET_LANG_CODE", project_id="YOUR_PROJECT_ID"
-):
+def get_supported_languages_with_target(project_id="YOUR_PROJECT_ID"):
     """Listing supported languages with target language name."""
 
     client = translate.TranslationServiceClient()
     parent = client.location_path(project_id, "global")
 
+    # Supported language codes: https://cloud.google.com/translate/docs/languages
     response = client.get_supported_languages(
-        display_language_code=language_code, parent=parent
+        display_language_code="is",  # target language code
+        parent=parent
     )
     # List language codes of supported languages
     for language in response.languages:
         print(u"Language Code: {}".format(language.language_code))
         print(u"Display Name: {}".format(language.display_name))
+
+# [END translate_v3_get_supported_languages_for_target]
