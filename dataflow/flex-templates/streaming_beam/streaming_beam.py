4d264bdd3698ab5b61367262bf7735785bfce55f
Manav Garg <manavgarg123@gmail.com>
2020-03-10 11:42:56 -0700

    Addressing review comments.

--- dataflow/flex-templates/streaming_beam/streaming_beam.py
+++ dataflow/flex-templates/streaming_beam/streaming_beam.py
@@ -20,8 +20,6 @@ It reads JSON encoded messages from Pub/Sub, transforms the message data and
 writes the results to BigQuery.
 """
 
-from __future__ import absolute_import
-
 import argparse
 import json
 import logging
@@ -41,42 +39,40 @@ SCHEMA = ','.join([
 ])
 
 
+def parse_json_message(message):
+  """Parse the input json message and add 'score' & 'processing_time' keys."""
+  row = json.loads(message)
+  return {
+      'url': row['url'],
+      'score': 1.0 if row['review'] == 'positive' else 0.0,
+      'processing_time': int(time.time()),
+  }
+
+
+def get_statistics(url_messages):
+  """Get statistics from the input URL messages."""
+  url, messages = url_messages
+  return {
+      'url': url,
+      'num_reviews': len(messages),
+      'score': sum(msg['score'] for msg in messages) / len(messages),
+      'first_date': min(msg['processing_time'] for msg in messages),
+      'last_date': max(msg['processing_time'] for msg in messages),
+  }
+
+
 def run(args, input_subscription, output_table):
   """Build and run the pipeline."""
   options = PipelineOptions(args, save_main_session=True, streaming=True)
 
   with beam.Pipeline(options=options) as pipeline:
 
-    # Read from PubSub into a PCollection.
+    # Read the messages from PubSub and process them.
     messages = (
         pipeline
         | 'Read form Pub/Sub' >> beam.io.ReadFromPubSub(
             subscription=input_subscription).with_output_types(bytes)
-        | 'UTF-8 bytes to string' >> beam.Map(lambda msg: msg.decode('utf-8')))
-
-    # Parse the input json message and add 'score' & 'processing_time' keys.
-    def parse_json_message(message):
-      row = json.loads(message)
-      return {
-          'url': row['url'],
-          'score': 1.0 if row['review'] == 'positive' else 0.0,
-          'processing_time': int(time.time()),
-      }
-
-    # Count the occurrences of each url, average score and the corresponding
-    # min/max timestamps.
-    def get_statistics(url_messages):
-      url, messages = url_messages
-      return {
-          'url': url,
-          'num_reviews': len(messages),
-          'score': sum(msg['score'] for msg in messages) / len(messages),
-          'first_date': min(msg['processing_time'] for msg in messages),
-          'last_date': max(msg['processing_time'] for msg in messages),
-      }
-
-    url_values = (
-        messages
+        | 'UTF-8 bytes to string' >> beam.Map(lambda msg: msg.decode('utf-8'))
         | 'Parse JSON messages' >> beam.Map(parse_json_message)
         | 'Fixed-size windows' >> beam.WindowInto(window.FixedWindows(
             60, 0))  # 1 minute
@@ -85,7 +81,7 @@ def run(args, input_subscription, output_table):
         | 'Get statistics' >> beam.Map(get_statistics))
 
     # Output the results into BigQuery table.
-    _ = url_values | 'Write' >> beam.io.WriteToBigQuery(
+    _ = messages | 'Write' >> beam.io.WriteToBigQuery(
         output_table, schema=SCHEMA)
 
 if __name__ == '__main__':
@@ -93,14 +89,12 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument(
       '--output_table',
-      help=(
-          'Output BigQuery table for results specified as: '
-          'PROJECT:DATASET.TABLE or DATASET.TABLE.'))
+      help='Output BigQuery table for results specified as: '
+      'PROJECT:DATASET.TABLE or DATASET.TABLE.')
   parser.add_argument(
       '--input_subscription',
-      help=(
-          'Input PubSub subscription of the form '
-          '"projects/<PROJECT>/subscriptions/<SUBSCRIPTION>."'))
+      help='Input PubSub subscription of the form '
+      '"projects/<PROJECT>/subscriptions/<SUBSCRIPTION>."')
   known_args, pipeline_args = parser.parse_known_args()
   run(pipeline_args, known_args.input_subscription, known_args.output_table)
 
