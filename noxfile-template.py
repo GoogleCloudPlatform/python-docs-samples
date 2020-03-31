93e3b464a62740f917adbf9ce0e65fd95c2418c2
kurtisvg <31518063+kurtisvg@users.noreply.github.com>
2020-03-31 16:51:02 -0700

    Fix issue with flake8 args.

--- noxfile-template.py
+++ noxfile-template.py
@@ -56,10 +56,10 @@ def _determine_local_import_names(start_dir):
 
 FLAKE8_COMMON_ARGS = [
     "--show-source",
-    "--builtin='gettext'",
+    "--builtin=gettext",
     "--max-complexity=20",
     "--import-order-style=google",
-    "--exclude='.nox,.cache,env,lib,generated_pb2,*_pb2.py,*_pb2_grpc.py'",
+    "--exclude=.nox,.cache,env,lib,generated_pb2,*_pb2.py,*_pb2_grpc.py",
     "--ignore=E121,E123,E126,E203,E226,E24,E266,E501,E704,W503,W504,I100,I201,I202",
     "--max-line-length=88",
 ]
