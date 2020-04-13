5e15796d5093811da01fc1efb2585d6ede30c995
Benedict Noero <noero@google.com>
2020-04-13 18:13:36 -0400

    healthcare API: update all DICOM v1beta1 samples to v1

---
+++ healthcare/api-client/v1/dicom/dicom_stores_test.py
@@ -0,0 +1,166 @@
+# Copyright 2020 Google LLC All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import pytest
+import sys
+import uuid
+
+# Add datasets for bootstrapping datasets for testing
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'datasets')) # noqa
+import datasets
+import dicom_stores
+
+cloud_region = 'us-central1'
+project_id = os.environ['GOOGLE_CLOUD_PROJECT']
+
+dataset_id = 'test_dataset-{}'.format(uuid.uuid4())
+dicom_store_id = 'test_dicom_store_{}'.format(uuid.uuid4())
+pubsub_topic = 'test_pubsub_topic_{}'.format(uuid.uuid4())
+
+RESOURCES = os.path.join(os.path.dirname(__file__), 'resources')
+bucket = os.environ['CLOUD_STORAGE_BUCKET']
+dcm_file_name = 'dicom_00000001_000.dcm'
+content_uri = bucket + '/' + dcm_file_name
+dcm_file = os.path.join(RESOURCES, dcm_file_name)
+
+
+@pytest.fixture(scope='module')
+def test_dataset():
+    dataset = datasets.create_dataset(
+        project_id,
+        cloud_region,
+        dataset_id)
+
+    yield dataset
+
+    # Clean up
+    datasets.delete_dataset(
+        project_id,
+        cloud_region,
+        dataset_id)
+
+
+@pytest.fixture(scope='module')
+def test_dicom_store():
+    dicom_store = dicom_stores.create_dicom_store(
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id)
+
+    yield dicom_store
+
+    # Clean up
+    dicom_stores.delete_dicom_store(
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id)
+
+
+def test_CRUD_dicom_store(test_dataset, capsys):
+    dicom_stores.create_dicom_store(
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id)
+
+    dicom_stores.get_dicom_store(
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id)
+
+    dicom_stores.list_dicom_stores(
+        project_id,
+        cloud_region,
+        dataset_id)
+
+    dicom_stores.delete_dicom_store(
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id)
+
+    out, _ = capsys.readouterr()
+
+    # Check that create/get/list/delete worked
+    assert 'Created DICOM store' in out
+    assert 'Name' in out
+    assert 'dicomStores' in out
+    assert 'Deleted DICOM store' in out
+
+
+def test_patch_dicom_store(test_dataset, test_dicom_store, capsys):
+    dicom_stores.patch_dicom_store(
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        pubsub_topic)
+
+    out, _ = capsys.readouterr()
+
+    assert 'Patched DICOM store' in out
+
+
+def test_import_dicom_instance(test_dataset, test_dicom_store, capsys):
+    dicom_stores.import_dicom_instance(
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        content_uri)
+
+    out, _ = capsys.readouterr()
+
+    assert 'Imported DICOM instance' in out
+
+
+def test_export_dicom_instance(test_dataset, test_dicom_store, capsys):
+    dicom_stores.export_dicom_instance(
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        bucket)
+
+    out, _ = capsys.readouterr()
+
+    assert 'Exported DICOM instance' in out
+
+
+def test_get_set_dicom_store_iam_policy(test_dataset, test_dicom_store, capsys):
+    get_response = dicom_stores.get_dicom_store_iam_policy(
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id)
+
+    set_response = dicom_stores.set_dicom_store_iam_policy(
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        'serviceAccount:python-docs-samples-tests@appspot.gserviceaccount.com',
+        'roles/viewer')
+
+    out, _ = capsys.readouterr()
+
+    assert 'etag' in get_response
+    assert 'bindings' in set_response
+    assert len(set_response['bindings']) == 1
+    assert 'python-docs-samples-tests' in str(set_response['bindings'])
+    assert 'roles/viewer' in str(set_response['bindings'])
---
+++ healthcare/api-client/v1/dicom/dicomweb_test.py
@@ -0,0 +1,277 @@
+# Copyright 2020 Google LLC All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import pytest
+import sys
+import uuid
+
+# Add datasets for bootstrapping datasets for testing
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'datasets'))  # noqa
+import datasets
+import dicom_stores
+import dicomweb
+
+cloud_region = 'us-central1'
+base_url = 'https://healthcare.googleapis.com/v1'
+project_id = os.environ['GOOGLE_CLOUD_PROJECT']
+
+dataset_id = 'test_dataset-{}'.format(uuid.uuid4())
+dicom_store_id = 'test_dicom_store_{}'.format(uuid.uuid4())
+
+RESOURCES = os.path.join(os.path.dirname(__file__), 'resources')
+dcm_file_name = 'dicom_00000001_000.dcm'
+dcm_file = os.path.join(RESOURCES, dcm_file_name)
+# The study_uid, series_uid, and instance_uid are not assigned by the
+# server and are part of the metadata of dcm_file
+study_uid = '1.3.6.1.4.1.11129.5.5.111396399361969898205364400549799252857604'
+series_uid = '1.3.6.1.4.1.11129.5.5.195628213694300498946760767481291263511724'
+instance_uid = '{}.{}'.format(
+    '1.3.6.1.4.1.11129.5.5',
+    '153751009835107614666834563294684339746480')
+
+
+@pytest.fixture(scope='module')
+def test_dataset():
+    dataset = datasets.create_dataset(
+        project_id,
+        cloud_region,
+        dataset_id)
+
+    yield dataset
+
+    # Clean up
+    datasets.delete_dataset(
+        project_id,
+        cloud_region,
+        dataset_id)
+
+
+@pytest.fixture(scope='module')
+def test_dicom_store():
+    dicom_store = dicom_stores.create_dicom_store(
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id)
+
+    yield dicom_store
+
+    # Clean up
+    dicom_stores.delete_dicom_store(
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id)
+
+
+def test_dicomweb_store_instance(test_dataset, test_dicom_store, capsys):
+    dicomweb.dicomweb_store_instance(
+        base_url,
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        dcm_file)
+
+    out, _ = capsys.readouterr()
+
+    # Check that store instance worked
+    assert 'Stored DICOM instance' in out
+
+    dicomweb.dicomweb_delete_study(
+        base_url,
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        study_uid)
+
+
+def test_dicomweb_search_instance_studies(
+        test_dataset, test_dicom_store, capsys):
+    dicomweb.dicomweb_store_instance(
+        base_url,
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        dcm_file)
+
+    dicomweb.dicomweb_search_instance(
+        base_url,
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id)
+
+    dicomweb.dicomweb_search_studies(
+        base_url,
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id)
+
+    out, _ = capsys.readouterr()
+
+    # Check that search instance worked
+    assert 'Instances:' in out
+    # Check that search studies worked
+    assert 'Studies found: response is <Response [204]>' in out
+
+    dicomweb.dicomweb_delete_study(
+        base_url,
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        study_uid)
+
+
+def test_dicomweb_retrieve_study(test_dataset, test_dicom_store, capsys):
+    dicomweb.dicomweb_store_instance(
+        base_url,
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        dcm_file)
+
+    dicomweb.dicomweb_retrieve_study(
+        base_url,
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        study_uid)
+
+    # Assert study was downloaded
+    assert os.path.isfile('study.multipart')
+
+    out, _ = capsys.readouterr()
+
+    # Check that retrieve study worked
+    assert 'Retrieved study' in out
+
+    # Delete downloaded study
+    os.remove('study.multipart')
+
+    dicomweb.dicomweb_delete_study(
+        base_url,
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        study_uid)
+
+
+def test_dicomweb_retrieve_instance(test_dataset, test_dicom_store, capsys):
+    dicomweb.dicomweb_store_instance(
+        base_url,
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        dcm_file)
+
+    dicomweb.dicomweb_retrieve_instance(
+        base_url,
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        study_uid,
+        series_uid,
+        instance_uid)
+
+    # Assert instance was downloaded
+    assert os.path.isfile('instance.dcm')
+
+    out, _ = capsys.readouterr()
+
+    # Check that retrieve instance worked
+    assert 'Retrieved DICOM instance' in out
+
+    # Delete downloaded instance
+    os.remove('instance.dcm')
+
+    dicomweb.dicomweb_delete_study(
+        base_url,
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        study_uid)
+
+
+def test_dicomweb_retrieve_rendered(test_dataset, test_dicom_store, capsys):
+    dicomweb.dicomweb_store_instance(
+        base_url,
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        dcm_file)
+
+    dicomweb.dicomweb_retrieve_rendered(
+        base_url,
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        study_uid,
+        series_uid,
+        instance_uid)
+
+    # Assert rendered image was downloaded
+    assert os.path.isfile('rendered_image.png')
+
+    out, _ = capsys.readouterr()
+
+    # Check that retrieve rendered image worked
+    assert 'Retrieved rendered image' in out
+
+    # Delete downloaded rendered image
+    os.remove('rendered_image.png')
+
+    dicomweb.dicomweb_delete_study(
+        base_url,
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        study_uid)
+
+
+def test_dicomweb_delete_study(test_dataset, test_dicom_store, capsys):
+    dicomweb.dicomweb_store_instance(
+        base_url,
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        dcm_file)
+
+    dicomweb.dicomweb_delete_study(
+        base_url,
+        project_id,
+        cloud_region,
+        dataset_id,
+        dicom_store_id,
+        study_uid)
+
+    out, _ = capsys.readouterr()
+
+    # Check that store instance worked
+    assert 'Deleted study.' in out
