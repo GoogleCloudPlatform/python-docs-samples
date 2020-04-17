ca985b0e12993cdfcca5e98248785e01731898f7
Seth Hollyman <shollyman@google.com>
2020-04-17 23:44:03 +0000

    reorder imports

--- bigquery/cloud-client/quickstart_test.py
+++ bigquery/cloud-client/quickstart_test.py
@@ -12,9 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import uuid
+
 from google.cloud import bigquery
 import pytest
-import uuid
 
 import quickstart
 
