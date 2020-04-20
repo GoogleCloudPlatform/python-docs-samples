a5bdd4c78137de9d2a5caeed0dbac7b93bc6bf1b
Sofia Leon <sofialeon@google.com>
2020-04-20 15:21:49 -0700

    fix: import functions from helper file to test

--- composer/workflows/dataflowtemplateoperator_create_dataset_and_table_helper.py
+++ composer/workflows/dataflowtemplateoperator_create_dataset_and_table_helper.py
@@ -2,46 +2,61 @@
 # It helps the user set up a BigQuery dataset and table that is needed
 # for the tutorial.
 
+
 # [START composer_dataflow_dataset_table_creation]
 
 from google.cloud import bigquery
 
-# Construct a BigQuery client object.
-client = bigquery.Client()
+# [END composer_dataflow_dataset_table_creation]
 
-# TODO(developer): Set client.project to your GCP Project ID.
-# client.project = "your-client-project"
+def create_dataset():   
+    # [START composer_dataflow_dataset_table_creation] 
 
-dataset_id = f"{client.project}.average_weather"
+    # Construct a BigQuery client object.
+    client = bigquery.Client()
 
-# Construct a full Dataset object to send to the API.
-dataset = bigquery.Dataset(dataset_id)
+    # TODO(developer): Set client.project to your GCP Project ID.
+    # client.project = "your-client-project"
 
-# TODO(developer): Fill out this location appropriately.
-# dataset.location = your-data-location
+    dataset_id = f"{client.project}.average_weather"
 
-# Send the dataset to the API for creation.
-# Raises google.api_core.exceptions.Conflict if the Dataset already
-# exists within the project.
-dataset = client.create_dataset(dataset)  # Make an API request.
-print(f"Created dataset {client.project} {dataset.dataset_id}")
+    # Construct a full Dataset object to send to the API.
+    dataset = bigquery.Dataset(dataset_id)
 
+    # TODO(developer): Fill out this location appropriately.
+    # dataset.location = your-data-location
 
-# Create a table from this dataset.
+    # Send the dataset to the API for creation.
+    # Raises google.api_core.exceptions.Conflict if the Dataset already
+    # exists within the project.
+    dataset = client.create_dataset(dataset) # Make an API request.
 
-table_id = f"{client.project}.average_weather.average_weather"
+    print(f"Created dataset {client.project} {dataset.dataset_id}")
+    # [END composer_dataflow_dataset_table_creation]
 
-schema = [
-    bigquery.SchemaField("location", "GEOGRAPHY", mode="REQUIRED"),
-    bigquery.SchemaField("average_temperature", "INTEGER", mode="REQUIRED"),
-    bigquery.SchemaField("month", "STRING", mode="REQUIRED"),
-    bigquery.SchemaField("inches_of_rain", "NUMERIC", mode="NULLABLE"),
-    bigquery.SchemaField("is_current", "BOOLEAN", mode="NULLABLE"),
-    bigquery.SchemaField("latest_measurement", "DATE", mode="NULLABLE"),
-]
+    return dataset_id
 
-table = bigquery.Table(table_id, schema=schema)
-table = client.create_table(table)  # Make an API request.
-print(f"Created table {table.project}.{table.dataset_id}.{table.table_id}")
+def create_table():
+
+    client = bigquery.Client()
+    # [START composer_dataflow_dataset_table_creation]
+
+    # Create a table from this dataset.
+
+    table_id = f"{client.project}.average_weather.average_weather"
+
+    schema = [
+        bigquery.SchemaField("location", "GEOGRAPHY", mode="REQUIRED"),
+        bigquery.SchemaField("average_temperature", "INTEGER", mode="REQUIRED"),
+        bigquery.SchemaField("month", "STRING", mode="REQUIRED"),
+        bigquery.SchemaField("inches_of_rain", "NUMERIC", mode="NULLABLE"),
+        bigquery.SchemaField("is_current", "BOOLEAN", mode="NULLABLE"),
+        bigquery.SchemaField("latest_measurement", "DATE", mode="NULLABLE"),
+    ]
+
+    table = bigquery.Table(table_id, schema=schema)
+    table = client.create_table(table)  # Make an API request.
+    print(f"Created table {table.project}.{table.dataset_id}.{table.table_id}")
 
 # [END composer_dataflow_dataset_table_creation]
+    return table
--- composer/workflows/dataflowtemplateoperator_create_dataset_and_table_test.py
+++ composer/workflows/dataflowtemplateoperator_create_dataset_and_table_test.py
@@ -1,64 +1,49 @@
-import uuid
-
+from . import dataflowtemplateoperator_create_dataset_and_table_helper as helper
 from google.cloud import bigquery
 import google.cloud.exceptions
+import google.api_core.exceptions
 import pytest
 
 client = bigquery.Client()
-dataset_UUID = str(uuid.uuid4()).split("-")[0]
-dataset_id = f"{client.project}.sample_dataset_{dataset_UUID}"
 
+expected_schema = schema = [
+    bigquery.SchemaField("location", "GEOGRAPHY", mode="REQUIRED"),
+    bigquery.SchemaField("average_temperature", "INTEGER", mode="REQUIRED"),
+    bigquery.SchemaField("month", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("inches_of_rain", "NUMERIC", mode="NULLABLE"),        
+    bigquery.SchemaField("is_current", "BOOLEAN", mode="NULLABLE"),
+    bigquery.SchemaField("latest_measurement", "DATE", mode="NULLABLE"),
+]
+
+expected_table_id = f"{client.project}.average_weather.average_weather"
+expected_dataset_id = f"{client.project}.average_weather"
 
 @pytest.fixture(scope="module")
-def dataset():
-
-    dataset = bigquery.Dataset(dataset_id)
-
+def dataset_id():
     try:
-        dataset = client.get_dataset(dataset_id)
+        dataset = client.get_dataset(expected_dataset_id)
+        dataset_id = dataset.dataset_id
     except google.cloud.exceptions.NotFound:
-        dataset = client.create_dataset(dataset)
+        dataset_id = helper.create_dataset()
 
-    yield dataset
+    yield dataset_id
 
     client.delete_dataset(dataset_id, delete_contents=True, not_found_ok=True)
 
-
 @pytest.fixture(scope="module")
 def table():
+    try:
+        table = client.get_table(expected_table_id)
+    except google.api_core.exceptions.NotFound:
+        table = helper.create_table()
 
-    table_id = f"{dataset_id}.average_weather"
+    yield table
 
-    schema = [
-        bigquery.SchemaField("location", "GEOGRAPHY", mode="REQUIRED"),
-        bigquery.SchemaField("average_temperature", "INTEGER", mode="REQUIRED"),
-        bigquery.SchemaField("month", "STRING", mode="REQUIRED"),
-        bigquery.SchemaField("inches_of_rain", "NUMERIC", mode="NULLABLE"),
-        bigquery.SchemaField("is_current", "BOOLEAN", mode="NULLABLE"),
-        bigquery.SchemaField("latest_measurement", "DATE", mode="NULLABLE"),
-    ]
+    client.delete_table(table, not_found_ok=True)
 
-    table = bigquery.Table(table_id, schema=schema)
-    table = client.create_table(table, exists_ok=True)
+def test_creation(dataset_id, table):
 
-    yield table_id
-
-    client.delete_table(table_id, not_found_ok=True)
-
-
-def test_dataset_creation(dataset, table):
-
-    table = client.get_table(table)
-    schema = table.schema
-
-    expected_schema = schema = [
-        bigquery.SchemaField("location", "GEOGRAPHY", mode="REQUIRED"),
-        bigquery.SchemaField("average_temperature", "INTEGER", mode="REQUIRED"),
-        bigquery.SchemaField("month", "STRING", mode="REQUIRED"),
-        bigquery.SchemaField("inches_of_rain", "NUMERIC", mode="NULLABLE"),
-        bigquery.SchemaField("is_current", "BOOLEAN", mode="NULLABLE"),
-        bigquery.SchemaField("latest_measurement", "DATE", mode="NULLABLE"),
-    ]
     assert table.table_id == "average_weather"
-    assert schema == expected_schema
-    assert f"{client.project}.{dataset.dataset_id}" == dataset_id
+    assert dataset_id == expected_dataset_id
+    assert table.schema == expected_schema
+    
