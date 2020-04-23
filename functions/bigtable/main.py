acbc3e8140a6c6370d42274b322c328d61aa9998
Billy Jacobson <thebilly@google.com>
2020-04-23 16:14:06 -0400

    working function

---
+++ functions/bigtable/main.py
@@ -0,0 +1,50 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START bigtable_functions_quickstart]
+from google.cloud import bigtable
+from google.cloud.bigtable.row_set import RowSet
+
+instance_id = 'testing-instance'
+table_id = 'mobile-time-series'
+
+bigtable = bigtable.Client()
+instance = bigtable.instance(instance_id)
+table = instance.table(table_id)
+
+def bigtable_read_data(request):
+    prefix = 'phone#'
+    end_key = prefix[:-1] + chr(ord(prefix[-1]) + 1)
+
+    outputs = []
+    row_set = RowSet()
+    row_set.add_row_range_from_keys(prefix.encode("utf-8"),
+                                    end_key.encode("utf-8"))
+
+    rows = table.read_rows(row_set=row_set)
+    print(rows)
+    for row in rows:
+        print("in loop")
+        print(row.row_key.decode('utf-8'))
+        output = 'Rowkey: {}, os_build: {}'.format(row.row_key.decode('utf-8'),
+                                                   row.cells["stats_summary"][
+                                                       "os_build".encode(
+                                                         'utf-8')][
+                                                       0].value.decode(
+                                                     'utf-8'))
+        outputs.append(output)
+
+    return '\n'.join(outputs)
+
+# [END bigtable_functions_quickstart]
---
+++ functions/bigtable/main_test.py
@@ -0,0 +1,38 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from unittest import mock
+from unittest.mock import MagicMock
+
+from google.cloud import bigtable
+
+bigtable.Client = MagicMock()
+
+
+def test_main():
+    import main
+    bigtable_mock = main.bigtable.instance().table().__enter__()
+    bigtable_mock.read_rows.__iter__.return_value = [{
+      "row_key": "phone#1234",
+      "stats_summary": {
+        "os_build".encode("utf-8"): [
+          {"value": "a"}
+        ]
+      }
+    }]
+
+    response = main.bigtable_read_data(None)
+
+    assert 'SingerID' in response
+    assert 'AlbumID' in response
+    assert 'Album' in response
