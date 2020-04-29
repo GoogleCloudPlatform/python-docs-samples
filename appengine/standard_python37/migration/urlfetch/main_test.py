821b32430cd6690446aa6b92e60646571c338283
Charles Engelke <engelke@google.com>
2020-04-29 15:51:09 -0700

    Added a test

--- appengine/standard_python37/migration/urlfetch/main_test.py
+++ appengine/standard_python37/migration/urlfetch/main_test.py
@@ -11,3 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import main
+
+
+def test_index():
+    main.app.testing = True
+    client = main.app.test_client()
+
+    r = client.get('/')
+    assert r.status_code == 200
+    assert 'Make a request' in r.data.decode('utf-8')
