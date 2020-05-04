fa4b530b9a9f83aef49a1977074fc63309c4cddc
Charles Engelke <engelke@google.com>
2020-05-04 16:51:28 -0700

    Update main.py

--- appengine/standard/migration/storage/main.py
+++ appengine/standard/migration/storage/main.py
@@ -54,6 +54,7 @@ def get():
     response.mimetype = 'text/plain'
     return response
 
+
 if __name__ == '__main__':
     # This is used when running locally.
     app.run(host='127.0.0.1', port=8080, debug=True)
