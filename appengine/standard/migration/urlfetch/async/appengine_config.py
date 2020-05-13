80572d5d4778989500c0505966aa4617d8449376
Charles Engelke <engelke@google.com>
2020-05-13 11:23:53 -0700

    Removed unneeded import

--- appengine/standard/migration/urlfetch/async/appengine_config.py
+++ appengine/standard/migration/urlfetch/async/appengine_config.py
@@ -17,7 +17,6 @@ from google.appengine.ext import vendor
 # Add any libraries installed in the "lib" folder.
 vendor.add('lib')
 
-import requests
 import requests_toolbelt.adapters.appengine
 
 # Use the App Engine Requests adapter. This makes sure that Requests uses
