dc444853bda7d3f25c532c2b032bbe69fa4d7e1b
HemangChothani <50404902+HemangChothani@users.noreply.github.com>
2020-05-13 12:10:14 +0530

    refactor(bigquery): update code samples of load table autodetect and truncate (#28)
    Co-authored-by: Peter Lamut <plamut@users.noreply.github.com>

---
+++ bigquery/load_table_uri_truncate_parquet.py
@@ -0,0 +1,55 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def load_table_uri_truncate_parquet(table_id):
+
+    # [START bigquery_load_table_gcs_parquet_truncate]
+    import six
+
+    from google.cloud import bigquery
+
+    # Construct a BigQuery client object.
+    client = bigquery.Client()
+
+    # TODO(developer): Set table_id to the ID of the table to create.
+    # table_id = "your-project.your_dataset.your_table_name
+
+    job_config = bigquery.LoadJobConfig(
+        schema=[
+            bigquery.SchemaField("name", "STRING"),
+            bigquery.SchemaField("post_abbr", "STRING"),
+        ],
+    )
+
+    body = six.BytesIO(b"Washington,WA")
+    client.load_table_from_file(body, table_id, job_config=job_config).result()
+    previous_rows = client.get_table(table_id).num_rows
+    assert previous_rows > 0
+
+    job_config = bigquery.LoadJobConfig(
+        write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+        source_format=bigquery.SourceFormat.PARQUET,
+    )
+
+    uri = "gs://cloud-samples-data/bigquery/us-states/us-states.parquet"
+    load_job = client.load_table_from_uri(
+        uri, table_id, job_config=job_config
+    )  # Make an API request.
+
+    load_job.result()  # Waits for the job to complete.
+
+    destination_table = client.get_table(table_id)
+    print("Loaded {} rows.".format(destination_table.num_rows))
+    # [END bigquery_load_table_gcs_parquet_truncate]
