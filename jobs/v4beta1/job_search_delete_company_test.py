69a175b31c4b3a30af92adf5c672ef0657987aaa
Mike Ganbold <munkhbayarg@google.com>
2020-05-15 12:42:07 -0700

    made requested changes and added return val for samples

---
+++ jobs/v4beta1/job_search_delete_company_test.py
@@ -0,0 +1,46 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import uuid
+
+import pytest
+
+import job_search_delete_company
+import job_search_create_company
+
+PROJECT_ID = os.environ["GOOGLE_CLOUD_PROJECT"]
+TENANT_ID = os.environ["JOB_SEARCH_TENANT_ID"]
+COMPANY_EXT_ID = "COMPANY_EXT_ID_{}".format(uuid.uuid4())
+
+
+@pytest.fixture(scope="module")
+def company():
+    # create a temporary company
+    company_name = job_search_create_company.create_company(
+        PROJECT_ID, TENANT_ID, "Test Company Name", COMPANY_EXT_ID
+    )
+
+    # extract company id
+    company_id = company_name.split("/")[-1]
+
+    yield company_id
+
+
+def test_delete_company(capsys, company):
+    out, _ = capsys.readouterr()
+
+    job_search_delete_company.delete_company(PROJECT_ID, TENANT_ID, company)
+    out, _ = capsys.readouterr()
+    assert "Deleted" in out
---
+++ jobs/v4beta1/job_search_delete_job_test.py
@@ -0,0 +1,45 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import uuid
+
+import pytest
+
+import job_search_create_job
+import job_search_delete_job
+
+PROJECT_ID = os.environ["GOOGLE_CLOUD_PROJECT"]
+TENANT_ID = os.environ["JOB_SEARCH_TENANT_ID"]
+COMPANY_ID = os.environ["JOB_SEARCH_COMPANY_ID"]
+POST_UNIQUE_ID = "TEST_POST_{}".format(uuid.uuid4())[:20]
+
+
+@pytest.fixture(scope="module")
+def job():
+    # create a temporary company
+    job_name = job_search_create_job.create_job(
+        PROJECT_ID, TENANT_ID, COMPANY_ID, POST_UNIQUE_ID, "www.jobUrl.com"
+    )
+
+    # extract company id
+    job_id = job_name.split("/")[-1]
+
+    yield job_id
+
+
+def test_delete_job(capsys, job):
+    job_search_delete_job.delete_job(PROJECT_ID, TENANT_ID, job)
+    out, _ = capsys.readouterr()
+    assert "Deleted" in out
