c601780bee07d7e4a0e0f5c2052c0f2cf1c0457b
Takashi Matsuo <tmatsuo@google.com>
2020-05-22 07:53:13 +0000

    fix tab

--- .kokoro/tests/run_tests.sh
+++ .kokoro/tests/run_tests.sh
@@ -136,16 +136,16 @@ for file in **/requirements.txt; do
 
     # If no local noxfile exists, copy the one from root
     if [[ ! -f "noxfile.py" ]]; then
-	PARENT_DIR=$(cd ../ && pwd)
-	while [[ "$PARENT_DIR" != "$ROOT" && ! -f "$PARENT_DIR/noxfile-template.py" ]];
-	do
+        PARENT_DIR=$(cd ../ && pwd)
+        while [[ "$PARENT_DIR" != "$ROOT" && ! -f "$PARENT_DIR/noxfile-template.py" ]];
+        do
             PARENT_DIR=$(dirname "$PARENT_DIR")
-	done
-	cp "$PARENT_DIR/noxfile-template.py" "./noxfile.py"
-	echo -e "\n Using noxfile-template from parent folder ($PARENT_DIR). \n"
-	cleanup_noxfile=1
+        done
+        cp "$PARENT_DIR/noxfile-template.py" "./noxfile.py"
+        echo -e "\n Using noxfile-template from parent folder ($PARENT_DIR). \n"
+        cleanup_noxfile=1
     else
-	cleanup_noxfile=0
+        cleanup_noxfile=0
     fi
 
     # Use nox to execute the tests for the project.
@@ -170,7 +170,7 @@ for file in **/requirements.txt; do
 
     # Remove noxfile.py if we copied.
     if [[ $cleanup_noxfile -eq 1 ]]; then
-	rm noxfile.py
+        rm noxfile.py
     fi
 
 done
