48d6584a8de9a849474f75a0557b329a95614f13
Saketram Durbha <saketramd@google.com>
2020-05-28 19:59:17 -0400

    move config dict initialization into init_connection_engine

--- cloud-sql/postgres/sqlalchemy/main.py
+++ cloud-sql/postgres/sqlalchemy/main.py
@@ -27,49 +27,49 @@ db_pass = os.environ.get("DB_PASS")
 db_name = os.environ.get("DB_NAME")
 cloud_sql_connection_name = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
 
-db_config = {
-    # [START cloud_sql_postgres_sqlalchemy_limit]
-    # Pool size is the maximum number of permanent connections to keep.
-    "pool_size": 5,
-    # Temporarily exceeds the set pool_size if no connections are available.
-    "max_overflow": 2,
-    # The total number of concurrent connections for your application will be
-    # a total of pool_size and max_overflow.
-    # [END cloud_sql_postgres_sqlalchemy_limit]
-
-    # [START cloud_sql_postgres_sqlalchemy_backoff]
-    # SQLAlchemy automatically uses delays between failed connection attempts,
-    # but provides no arguments for configuration.
-    # [END cloud_sql_postgres_sqlalchemy_backoff]
-
-    # [START cloud_sql_postgres_sqlalchemy_timeout]
-    # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
-    # new connection from the pool. After the specified amount of time, an
-    # exception will be thrown.
-    "pool_timeout": 30,  # 30 seconds
-    # [END cloud_sql_postgres_sqlalchemy_timeout]
-
-    # [START cloud_sql_postgres_sqlalchemy_lifetime]
-    # 'pool_recycle' is the maximum number of seconds a connection can persist.
-    # Connections that live longer than the specified amount of time will be
-    # reestablished
-    "pool_recycle": 1800,  # 30 minutes
-    # [END cloud_sql_postgres_sqlalchemy_lifetime]
-}
-
 app = Flask(__name__)
 
 logger = logging.getLogger()
 
 
 def init_connection_engine():
+    db_config = {
+        # [START cloud_sql_postgres_sqlalchemy_limit]
+        # Pool size is the maximum number of permanent connections to keep.
+        "pool_size": 5,
+        # Temporarily exceeds the set pool_size if no connections are available.
+        "max_overflow": 2,
+        # The total number of concurrent connections for your application will be
+        # a total of pool_size and max_overflow.
+        # [END cloud_sql_postgres_sqlalchemy_limit]
+
+        # [START cloud_sql_postgres_sqlalchemy_backoff]
+        # SQLAlchemy automatically uses delays between failed connection attempts,
+        # but provides no arguments for configuration.
+        # [END cloud_sql_postgres_sqlalchemy_backoff]
+
+        # [START cloud_sql_postgres_sqlalchemy_timeout]
+        # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
+        # new connection from the pool. After the specified amount of time, an
+        # exception will be thrown.
+        "pool_timeout": 30,  # 30 seconds
+        # [END cloud_sql_postgres_sqlalchemy_timeout]
+
+        # [START cloud_sql_postgres_sqlalchemy_lifetime]
+        # 'pool_recycle' is the maximum number of seconds a connection can persist.
+        # Connections that live longer than the specified amount of time will be
+        # reestablished
+        "pool_recycle": 1800,  # 30 minutes
+        # [END cloud_sql_postgres_sqlalchemy_lifetime]
+    }
+
     if os.environ.get("DB_HOST"):
-        return init_tcp_connection_engine()
+        return init_tcp_connection_engine(db_config)
     else:
-        return init_unix_connection_engine()
+        return init_unix_connection_engine(db_config)
 
 
-def init_tcp_connection_engine():
+def init_tcp_connection_engine(db_config):
     # [START cloud_sql_postgres_sqlalchemy_create_tcp]
     db_scoket_addr = os.environ.get("DB_HOST").split(":")
 
@@ -96,7 +96,7 @@ def init_tcp_connection_engine():
     # [END cloud_sql_postgres_sqlalchemy_create_tcp]
 
 
-def init_unix_connection_engine():
+def init_unix_connection_engine(db_config):
     # [START cloud_sql_postgres_sqlalchemy_create_socket]
     if os.environ.get("DB_SOCKET_PATH"):
         socket_path = os.environ.get("DB_SOCKET_PATH")
