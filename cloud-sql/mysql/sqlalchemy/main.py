b4bbea0928f46d14e18ff251d075cd2d01d56c0c
Saketram Durbha <saketramd@google.com>
2020-06-03 19:23:27 -0400

    minor code style and performance updates for Cloud SQL PostgreSQL and MySQL samples
    * make necessary environment variables such as "DB_USER" required
    * add comments to show examples for some input arguments
    * rewrite small piece of code to be more condensed and concise

--- cloud-sql/mysql/sqlalchemy/main.py
+++ cloud-sql/mysql/sqlalchemy/main.py
@@ -63,27 +63,25 @@ def init_tcp_connection_engine(db_config):
     # [START cloud_sql_mysql_sqlalchemy_create_tcp]
     # Remember - storing secrets in plaintext is potentially unsafe. Consider using
     # something like https://cloud.google.com/kms/ to help keep secrets secret.
-    db_user = os.environ.get("DB_USER")
-    db_pass = os.environ.get("DB_PASS")
-    db_name = os.environ.get("DB_NAME")
-    db_host = os.environ.get("DB_HOST")
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+    db_host = os.environ["DB_HOST"]
 
-    db_host_parts = db_host.split(":")
-
-    # Extract host and port from db_host socket address
-    db_hostname = db_host_parts[0]
-    db_port = int(db_host_parts[1])
+    # Extract host and port from db_host
+    host_args = db_host.split(":")
+    db_hostname, db_port = host_args[0], int(host_args[1])
 
     pool = sqlalchemy.create_engine(
         # Equivalent URL:
         # mysql+pymysql://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
         sqlalchemy.engine.url.URL(
             drivername="mysql+pymysql",
-            username=db_user,
-            password=db_pass,
-            host=db_hostname,
-            port=db_port,
-            database=db_name,
+            username=db_user,  # e.g. "my-database-user"
+            password=db_pass,  # e.g. "my-database-password"
+            host=db_hostname,  # e.g. "127.0.0.1"
+            port=db_port,  # e.g. 3306
+            database=db_name,  # e.g. "my-database-name"
         ),
         # ... Specify additional properties here.
         # [END cloud_sql_mysql_sqlalchemy_create_tcp]
@@ -99,24 +97,24 @@ def init_unix_connection_engine(db_config):
     # [START cloud_sql_mysql_sqlalchemy_create_socket]
     # Remember - storing secrets in plaintext is potentially unsafe. Consider using
     # something like https://cloud.google.com/kms/ to help keep secrets secret.
-    db_user = os.environ.get("DB_USER")
-    db_pass = os.environ.get("DB_PASS")
-    db_name = os.environ.get("DB_NAME")
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
     db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
-    cloud_sql_connection_name = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
+    cloud_sql_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
 
     pool = sqlalchemy.create_engine(
         # Equivalent URL:
         # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
         sqlalchemy.engine.url.URL(
             drivername="mysql+pymysql",
-            username=db_user,
-            password=db_pass,
-            database=db_name,
+            username=db_user,  # e.g. "my-database-user"
+            password=db_pass,  # e.g. "my-database-password"
+            database=db_name,  # e.g. "my-database-name"
             query={
                 "unix_socket": "{}/{}".format(
-                    db_socket_dir,
-                    cloud_sql_connection_name)
+                    db_socket_dir,  # e.g. "/cloudsql"
+                    cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
             }
         ),
         # ... Specify additional properties here.
