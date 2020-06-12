f39f5e44bcb37f3d09b7c9970b6ef2d133e9cd53
Tushar Khan <tusharkhan@google.com>
2020-06-12 15:56:44 -0400

    multi-param column transforms

--- data-science-onramp/data-processing/clean.py
+++ data-science-onramp/data-processing/clean.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import re
+import datetime
 
 from py4j.protocol import Py4JJavaError
 from pyspark.sql import SparkSession
@@ -9,7 +10,7 @@ from pyspark.sql.types import StringType, IntegerType, FloatType
 
 
 PROJECT_ID = sys.argv[1]
-#BUCKET_NAME = sys.argv[2]
+BUCKET_NAME = sys.argv[2]
 TABLE = f'{PROJECT_ID}.new_york_citibike_trips.RAW_DATA'
 
 def trip_duration(duration):
@@ -61,15 +62,60 @@ def gender(s):
 def angle(a):
     '''Converts DMS notation to angles.'''
     if a:
-        dms = re.match('(-?\d*).(-?\d*)\'(-?\d*)"', a) #todo change . with unicode symbol for degree
+        dms = re.match('(-?\d*).(-?\d*)\'(-?\d*)"', a)
         if dms:
             return int(dms[1]) + int(dms[2])/60 + int(dms[3])/(60 * 60)
         else:
             try:
                 return float(a)
             except ValueError:
-                return 100
+                return None
+
+def compute_time(duration, start, end):
+    '''Calculates duration, start time, and end time from each other if one value is null.'''
+    time_format = '%Y-%m-%dT%H:%M:%S'
+
+    # Convert to datetime/timedelta
+    if start:
+        if '.' in start:
+            start = start[:start.index('.')]
+        start = datetime.datetime.strptime(start, time_format)
+    if end:
+        if '.' in end:
+            end = end[:end.index('.')]
+        end = datetime.datetime.strptime(end, time_format)
+    if duration:
+        duration = datetime.timedelta(seconds=duration)
+
+    # Compute null value
+    if start and end and not duration:
+        duration = end - start
+    elif duration and end and not start:
+        start = end - duration
+    elif duration and start and not end:
+        end = start + duration
+
+    # Convert from datetime/timedelta
+    if duration:
+        duration = int(duration.total_seconds())
+    if start:
+        start = start.strftime(time_format)
+    if end:
+        end = end.strftime(time_format)
+
+    return (duration, start, end)
         
+def compute_duration(duration, start, end):
+    '''Calculates duration from start and end time if null.'''
+    return compute_time(duration, start, end)[0]
+
+def compute_start(duration, start, end):
+    '''Calculates start time from duration and end time if null.'''
+    return compute_time(duration, start, end)[1]
+
+def compute_end(duration, start, end):
+    '''Calculates end time from duration and start time if null.'''
+    return compute_time(duration, start, end)[2]
 
 def main():
     '''...'''
@@ -81,10 +127,11 @@ def main():
     try:
         df = spark.read.format('bigquery').option('table', TABLE).load()
     except Py4JJavaError:
-        print(f"{TABLE} does not exist. ")
+        print(f'{TABLE} does not exist. ')
         return
 
-    udf_map = {
+    # Single-parameter column transformations
+    udfs = {
         'start_station_name': (station_name, StringType()),
         'end_station_name': (station_name, StringType()),
         'tripduration': (trip_duration, IntegerType()),
@@ -96,8 +143,27 @@ def main():
         'end_station_longitude': (angle, FloatType())
     }
 
-    for name, udf in udf_map.items():
-        df = df.withColumn(name, UserDefinedFunction(*udf)(name).alias(name))
+    for name, udf in udfs.items():
+        df = df.withColumn(name, UserDefinedFunction(*udf)(name))
+
+    # Multiple-parameter column transformations
+    multi_udfs = {
+        'tripduration': {
+            'udf': (compute_duration, IntegerType()),
+            'params': ('tripduration', 'starttime', 'stoptime')
+        },
+        'starttime': {
+            'udf': (compute_start, StringType()),
+            'params': ('tripduration', 'starttime', 'stoptime')
+        },
+        'stoptime': {
+            'udf': (compute_end, StringType()),
+            'params': ('tripduration', 'starttime', 'stoptime')
+        }
+    }
+
+    for name, obj in multi_udfs.items():
+        df = df.withColumn(name, UserDefinedFunction(*obj['udf'])(*obj['params']))
 
     df.show(n=100)
 
