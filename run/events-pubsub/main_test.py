a129f94f052e927df7fde0fc4219fb8957fda0ed
Curtis Mason <cumason@google.com>
2020-06-12 16:17:10 -0400

    lint fix: reordered imports

--- run/events-pubsub/main_test.py
+++ run/events-pubsub/main_test.py
@@ -11,9 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import base64
+
 import copy
 
-import base64
 from uuid import uuid4
 
 import pytest
