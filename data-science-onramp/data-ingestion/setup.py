4bf07ee52c06da34a387f65b8e63d83911364a8d
Diego Lopez <lodiego@google.com>
2020-06-16 11:29:46 -0400

    optimize data ingestion

--- data-science-onramp/data-ingestion/setup.py
+++ data-science-onramp/data-ingestion/setup.py
@@ -1,41 +1,43 @@
 import random
 import sys
-from time import time_ns
 
 from google.cloud import bigquery
 from py4j.protocol import Py4JJavaError
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import UserDefinedFunction
+from pyspark.sql.functions import UserDefinedFunction, when, expr
 from pyspark.sql.types import IntegerType, StringType
 
 
 BUCKET_NAME = sys.argv[1]
 TABLE = "bigquery-public-data.new_york_citibike.citibike_trips"
+RAW_DATASET_NAME = "new_york_citibike_trips5"
+RAW_TABLE_NAME = "RAW_DATA"
 
 
 # START MAKING DATA DIRTY
-def random_select(items, weights):
-    '''Picks an item according to the cumulative weights'''
-    return random.choices(items, weights=weights, k=1)[0]
-
-
 def trip_duration(duration):
     '''Converts trip duration to other units'''
+    if duration is None:
+        return None
     seconds = str(duration) + " s"
     minutes = str(float(duration) / 60) + " min"
     hours = str(float(duration) / 3600) + " h"
-    return random_select([seconds, minutes, hours,
+    return random.choices([seconds, minutes, hours,
                          str(random.randint(-1000, -1))],
-                         [0.3, 0.3, 0.3, 0.1])
+                         weights=[0.3, 0.3, 0.3, 0.1])[0]
 
 
 def station_name(name):
     '''Replaces '&' with '/' with a 50% chance'''
+    if name is None:
+        return None
     return random.choice([name, name.replace("&", "/")])
 
 
 def user_type(user):
     '''Manipulates the user type string'''
+    if user is None:
+        return None
     return random.choice([user, user.upper(), user.lower(),
                           "sub" if user == "Subscriber" else user,
                           "cust" if user == "Customer" else user])
@@ -43,6 +45,8 @@ def user_type(user):
 
 def gender(s):
     '''Manipulates the gender string'''
+    if s is None:
+        return None
     return random.choice([s.upper(), s.lower(),
                          s[0].upper() if len(s) > 0 else "",
                          s[0].lower() if len(s) > 0 else ""])
@@ -50,29 +54,15 @@ def gender(s):
 
 def convert_angle(angle):
     '''Converts long and lat to DMS notation'''
+    if angle is None:
+        return None
     degrees = int(angle)
     minutes = int((angle - degrees) * 60)
     seconds = int((angle - degrees - minutes/60) * 3600)
     new_angle = str(degrees) + "\u00B0" + \
         str(minutes) + "'" + str(seconds) + '"'
-    return random_select([str(angle), new_angle], [0.55, 0.45])
-
-
-# This function is nested since a UserDefinedFunction is
-# expected to take a single argument
-def dirty_data(proc_func, allow_none):
-    '''Master function returns a user defined function
-    that transforms the column data'''
-    def udf(col_value):
-        random.seed(hash(col_value) + time_ns())
-        if col_value is None:
-            return col_value
-        elif allow_none:
-            return random_select([None, proc_func(col_value)],
-                                 [0.05, 0.95])
-        else:
-            return proc_func(col_value)
-    return udf
+    return random.choices([str(angle), new_angle],
+                          weights=[0.55, 0.45])[0]
 
 
 def write_to_bigquery(df):
@@ -80,16 +70,18 @@ def write_to_bigquery(df):
 
     # Create BigQuery Dataset
     client = bigquery.Client()
-    dataset_id = f'{client.project}.new_york_citibike_trips'
+    dataset_id = f'{client.project}.{RAW_DATASET_NAME}'
     dataset = bigquery.Dataset(dataset_id)
     dataset.location = "US"
     dataset = client.create_dataset(dataset)
 
     # Saving the data to BigQuery
     df.write.format('bigquery') \
-        .option('table', dataset_id + ".RAW_DATA") \
+        .option('table', dataset_id + f".{RAW_TABLE_NAME}") \
         .option("temporaryGcsBucket", BUCKET_NAME) \
         .save()
+
+    print("Table successfully written to BigQuery")
 
 
 def main():
@@ -112,42 +104,49 @@ def main():
         print(f"{TABLE} does not exist. ")
         return
 
-    # Declare data transformations for each column in dataframe
-    udfs = [
-        (dirty_data(trip_duration, True), StringType()),  # tripduration
-        (dirty_data(lambda x: x, True), StringType()),  # starttime
-        (dirty_data(lambda x: x, True), StringType()),  # stoptime
-        (lambda x: x, IntegerType()),  # start_station_id
-        (dirty_data(station_name, False), StringType()),  # start_station_name
-        (dirty_data(convert_angle, True), StringType()),  # start_station_latitude
-        (dirty_data(convert_angle, True), StringType()),  # start_station_longitude
-        (lambda x: x, IntegerType()),  # end_station_id
-        (dirty_data(station_name, False), StringType()),  # end_station_name
-        (dirty_data(convert_angle, True), StringType()),  # end_station_latitude
-        (dirty_data(convert_angle, True), StringType()),  # end_station_longitude
-        (lambda x: x, IntegerType()),  # bikeid
-        (dirty_data(user_type, False), StringType()),  # usertype
-        (lambda x: x, IntegerType()),  # birth_year
-        (dirty_data(gender, False), StringType()),  # gender
-        (lambda x: x, StringType()),  # customer_plan
+    # Declare dictionary with keys column names and values user defined
+    #  functions and return types
+    udf_map = {
+            'tripduration': (trip_duration, StringType()),
+            'start_station_name': (station_name, StringType()),
+            'start_station_latitude': (convert_angle, StringType()),
+            'start_station_longitude': (convert_angle, StringType()),
+            'end_station_name': (station_name, StringType()),
+            'end_station_latitude': (convert_angle, StringType()),
+            'end_station_longitude': (convert_angle, StringType()),
+            'usertype': (user_type, StringType()),
+            'gender': (gender, StringType()),
+    }
+    
+    # Declare which columns to set some values to null randomly
+    null_columns = [
+            'tripduration',
+            'starttime',
+            'stoptime',
+            'start_station_latitude',
+            'start_station_longitude',
+            'end_station_latitude',
+            'end_station_longitude',
     ]
 
-    # Apply dirty transformations to df
-    names = df.schema.names
-    new_df = df.select(*[UserDefinedFunction(*udf)(column).alias(name)
-                         for udf, column, name in zip(udfs, df.columns, names)])
+    # Dirty the columns
+    for name, udf in udf_map.items():
+        df = df.withColumn(name, UserDefinedFunction(*udf)(name))
 
-    new_df.sample(False, 0.0001).show(n=100)
+    # Randomly set about 5% of the values in some columns to null
+    for name in null_columns:
+        df = df.withColumn(name, when(expr("rand() < 0.05"), None).otherwise(df[name]))
 
     # Duplicate about 0.01% of the rows
-    dup_df = new_df.sample(True, 0.0001)
+    dup_df = df.sample(True, 0.0001)
 
     # Create final dirty dataframe
-    df = new_df.union(dup_df)
+    df = df.union(dup_df)
 
     if upload:
         write_to_bigquery(df)
-
+    else:
+        df.sample(True, 0.0001).show(n=500, truncate=False)
 
 if __name__ == '__main__':
     main()
