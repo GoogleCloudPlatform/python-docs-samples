f5890dadf1bb320cd24ce4d713bd90c8224a8663
Tushar Khan <tusharkhan@google.com>
2020-06-17 12:39:13 -0400

    addressed PR comments 2

--- data-science-onramp/data-cleaning/clean.py
+++ data-science-onramp/data-cleaning/clean.py
@@ -12,7 +12,7 @@ PROJECT_ID = sys.argv[1]
 BUCKET_NAME = sys.argv[2]
 TABLE = f'{PROJECT_ID}.new_york_citibike_trips.RAW_DATA'
 
-def trip_duration(duration):
+def trip_duration_udf(duration):
     '''Convert trip duration to seconds.'''
     if not duration:
         return None
@@ -34,11 +34,11 @@ def trip_duration(duration):
     
     return int(time)
 
-def station_name(name):
+def station_name_udf(name):
     '''Replaces '/' with '&'.'''
     return name.replace('/', '&') if name else None
 
-def user_type(user):
+def user_type_udf(user):
     '''Converts user type to 'Subscriber' or 'Customer'.'''
     if not user:
         return None
@@ -47,52 +47,53 @@ def user_type(user):
         return 'Subscriber'
     elif user.lower().startswith('cust'):
         return 'Customer'
-    else:
-        return None
 
-def gender(g):
+def gender_udf(gender):
     '''Converts gender to 'Male' or 'Female' or '''
-    if not g:
+    if not gender:
         return None
     
-    if g.lower().startswith('m'):
+    if gender.lower().startswith('m'):
         return 'Male'
-    elif g.lower().startswith('f'):
+    elif gender.lower().startswith('f'):
         return 'Female'
-    else:
-        return None
 
-def angle(a):
+def angle_udf(angle):
     '''Converts DMS notation to angles.'''
-    if not a:
+    if not angle:
         return None
     
-    dms = re.match('(-?\d*).(-?\d*)\'(-?\d*)"', a)
+    dms = re.match('(-?\d*).(-?\d*)\'(-?\d*)"', angle)
     if dms:
         return int(dms[1]) + int(dms[2])/60 + int(dms[3])/(60 * 60)
-    else:
-        try:
-            return float(a)
-        except ValueError:
-            return None
+    
+    try:
+        return float(angle)
+    except ValueError:
+        return None
 
 def compute_time(duration, start, end):
     '''Calculates duration, start time, and end time from each other if one value is null.'''
     time_format = '%Y-%m-%dT%H:%M:%S'
 
-    # Convert to datetime/timedelta
+    # Transform to datetime objects
     if start:
+        # Round to nearest second
         if '.' in start:
             start = start[:start.index('.')]
+        # Convert to datetime
         start = datetime.datetime.strptime(start, time_format)
     if end:
+        # Round to nearest second
         if '.' in end:
             end = end[:end.index('.')]
+        # Convert to datetime
         end = datetime.datetime.strptime(end, time_format)
     if duration:
+        # Convert to timedelta
         duration = datetime.timedelta(seconds=duration)
 
-    # Compute null value
+    # Calculate missing value
     if start and end and not duration:
         duration = end - start
     elif duration and end and not start:
@@ -100,7 +101,7 @@ def compute_time(duration, start, end):
     elif duration and start and not end:
         end = start + duration
 
-    # Convert from datetime/timedelta
+    # Transform to primitive types
     if duration:
         duration = int(duration.total_seconds())
     if start:
@@ -110,33 +111,33 @@ def compute_time(duration, start, end):
 
     return (duration, start, end)
         
-def compute_duration(duration, start, end):
+def compute_duration_udf(duration, start, end):
     '''Calculates duration from start and end time if null.'''
     return compute_time(duration, start, end)[0]
 
-def compute_start(duration, start, end):
+def compute_start_udf(duration, start, end):
     '''Calculates start time from duration and end time if null.'''
     return compute_time(duration, start, end)[1]
 
-def compute_end(duration, start, end):
+def compute_end_udf(duration, start, end):
     '''Calculates end time from duration and start time if null.'''
     return compute_time(duration, start, end)[2]
 
-def backfill(df):
+def upload_to_gcs(df):
     '''Writes a dataframe to a GCP bucket.'''
     path = 'gs://' + BUCKET_NAME + '/clean_citibike_data' + '.csv.gz'
     df.write.options(codec='org.apache.hadoop.io.compress.GzipCodec').csv(path)
-    return path
+    print('Data successfully uploaded to ' + path)
 
 def main():
     # Create a SparkSession under the name 'data_cleaning'. Viewable via the Spark UI
     spark = SparkSession.builder.appName('data_cleaning').getOrCreate()
 
-    # Whether to backfill data to GCS bucket
+    # Whether to upload data to GCS bucket
     upload = True
     if '--test' in sys.argv:
         upload = False
-        print('Results will not be backfilled into GCS bucket')
+        print('Results will not be uploaded to GCS')
 
     # Check if table exists
     try:
@@ -147,15 +148,15 @@ def main():
 
     # Single-parameter column transformations
     udfs = {
-        'start_station_name': (station_name, StringType()),
-        'end_station_name': (station_name, StringType()),
-        'tripduration': (trip_duration, IntegerType()),
-        'usertype': (user_type, StringType()),
-        'gender': (gender, StringType()),
-        'start_station_latitude': (angle, FloatType()),
-        'start_station_longitude': (angle, FloatType()),
-        'end_station_latitude': (angle, FloatType()),
-        'end_station_longitude': (angle, FloatType())
+        'start_station_name': (station_name_udf, StringType()),
+        'end_station_name': (station_name_udf, StringType()),
+        'tripduration': (trip_duration_udf, IntegerType()),
+        'usertype': (user_type_udf, StringType()),
+        'gender': (gender_udf, StringType()),
+        'start_station_latitude': (angle_udf, FloatType()),
+        'start_station_longitude': (angle_udf, FloatType()),
+        'end_station_latitude': (angle_udf, FloatType()),
+        'end_station_longitude': (angle_udf, FloatType())
     }
 
     for name, udf in udfs.items():
@@ -164,15 +165,15 @@ def main():
     # Multiple-parameter column transformations
     multi_udfs = {
         'tripduration': {
-            'udf': (compute_duration, IntegerType()),
+            'udf': (compute_duration_udf, IntegerType()),
             'params': ('tripduration', 'starttime', 'stoptime')
         },
         'starttime': {
-            'udf': (compute_start, StringType()),
+            'udf': (compute_start_udf, StringType()),
             'params': ('tripduration', 'starttime', 'stoptime')
         },
         'stoptime': {
-            'udf': (compute_end, StringType()),
+            'udf': (compute_end_udf, StringType()),
             'params': ('tripduration', 'starttime', 'stoptime')
         }
     }
@@ -181,9 +182,9 @@ def main():
         df = df.withColumn(name, UserDefinedFunction(*obj['udf'])(*obj['params']))
 
     if upload:
-        gcs_path = backfill(df)
+        upload_to_gcs(df)
     
-    df.sample(False, 0.0001).show(n=100)
+    df.sample(False, 0.001).show(n=100)
 
 if __name__ == '__main__':
     main()
