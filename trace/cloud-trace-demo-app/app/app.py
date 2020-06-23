4403d32d31a6ed66b7620ffcbc66713c320fe927
Aravin Sivakumar <aravins@google.com>
2020-06-23 19:22:45 +0000

    fixing linter issue

--- trace/cloud-trace-demo-app/app/app.py
+++ trace/cloud-trace-demo-app/app/app.py
@@ -25,6 +25,7 @@ from opencensus.ext.stackdriver.trace_exporter import StackdriverExporter
 from opencensus.trace import execution_context
 from opencensus.trace.propagation import google_cloud_format
 from opencensus.trace.samplers import AlwaysOnSampler
+
 # [END trace_demo_imports]
 import requests
 
@@ -39,46 +40,49 @@ def createMiddleWare(exporter):
     # Configure a flask middleware that listens for each request and applies automatic tracing.
     # This needs to be set up before the application starts.
     middleware = FlaskMiddleware(
-        app,
-        exporter=exporter,
-        propagator=propagator,
-        sampler=AlwaysOnSampler())
+        app, exporter=exporter, propagator=propagator, sampler=AlwaysOnSampler()
+    )
     return middleware
+
+
 # [END trace_demo_middleware]
 
 
-@app.route('/')
+@app.route("/")
 def template_test():
     # Sleep for a random time to imitate a random processing time
     time.sleep(random.uniform(0, 0.5))
     # Keyword that gets passed in will be concatenated to the final output string.
-    output_string = app.config['keyword']
+    output_string = app.config["keyword"]
     # If there is no endpoint, return the output string.
-    url = app.config['endpoint']
+    url = app.config["endpoint"]
     if url == "":
         return output_string, 200
     # Endpoint is the next service to send string to.
-    data = {'body': output_string}
+    data = {"body": output_string}
     # [START trace_context_header]
-    trace_context_header = propagator.to_header(execution_context.get_opencensus_tracer().span_context)
+    trace_context_header = propagator.to_header(
+        execution_context.get_opencensus_tracer().span_context
+    )
     response = requests.get(
-        url,
-        params=data,
-        headers={
-          'X-Cloud-Trace-Context' : trace_context_header}
+        url, params=data, headers={"X-Cloud-Trace-Context": trace_context_header}
     )
     # [END trace_context_header]
-    return response.text + app.config['keyword']
+    return response.text + app.config["keyword"]
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--keyword",  default="", help="name of the service.")
-    parser.add_argument("--endpoint", default="", help="endpoint to dispatch appended string, simply respond if not set")
+    parser.add_argument("--keyword", default="", help="name of the service.")
+    parser.add_argument(
+        "--endpoint",
+        default="",
+        help="endpoint to dispatch appended string, simply respond if not set",
+    )
     args = parser.parse_args()
-    app.config['keyword'] = args.keyword
-    app.config['endpoint'] = args.endpoint
+    app.config["keyword"] = args.keyword
+    app.config["endpoint"] = args.endpoint
     # [START trace_demo_create_exporter]
     createMiddleWare(StackdriverExporter())
     # [END trace_demo_create_exporter]
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
