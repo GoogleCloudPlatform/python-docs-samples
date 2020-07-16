69c53a43993f4ca511a52a3dfd5f29aea083b63d
ace-n <anassri@google.com>
2020-07-16 00:20:54 -0700

    functions: remove redundant GCS sample

--- functions/helloworld/main.py
+++ functions/helloworld/main.py
@@ -102,14 +102,24 @@ def hello_pubsub(event, context):
 
 
 # [START functions_helloworld_storage]
-def hello_gcs(event, context):
+def hello_gcs(data, context):
     """Background Cloud Function to be triggered by Cloud Storage.
+       This generic function logs relevant data when a file is changed.
+
     Args:
-         event (dict): The dictionary with data specific to this type of event.
-         context (google.cloud.functions.Context): The Cloud Functions
-         event metadata.
+        data (dict): The Cloud Functions event payload.
+        context (google.cloud.functions.Context): Metadata of triggering event.
+    Returns:
+        None; the output is written to Stackdriver Logging
     """
-    print("File: {}.".format(event['objectId']))
+
+    print('Event ID: {}'.format(context.event_id))
+    print('Event type: {}'.format(context.event_type))
+    print('Bucket: {}'.format(data['bucket']))
+    print('File: {}'.format(data['name']))
+    print('Metageneration: {}'.format(data['metageneration']))
+    print('Created: {}'.format(data['timeCreated']))
+    print('Updated: {}'.format(data['updated']))
 # [END functions_helloworld_storage]
 
 
