e7f6d1f9044517dfe23ff58b813548cb7e034630
vuppalli <vu8hh@virginia.edu>
2020-07-20 16:09:31 +0000

    clean up util.py

--- data-science-onramp/tf-keras/trainer/util.py
+++ data-science-onramp/tf-keras/trainer/util.py
@@ -1,5 +1,3 @@
-# Lint as: python3
-
 import os
 import tempfile
 import numpy as np
@@ -9,10 +7,11 @@ import tensorflow as tf
 from google.cloud import storage
 from sklearn.model_selection import train_test_split
 
-# Data locations
+# Data information
 DATA_DIR = os.path.join(tempfile.gettempdir(), 'final_data')
 BUCKET_NAME = 'citibikevd'
 DATA_FOLDER = 'feature_engineering'
+NUM_FEATURES = 11
 TRAINING_FILE = 'final_data.csv'
 TRAINING_BLOB = f'{DATA_FOLDER}/{TRAINING_FILE}'
 
@@ -21,29 +20,35 @@ BATCH_SIZE = 128
 NUM_EPOCHS = 20
 LEARNING_RATE = .01
 
-NUM_FEATURES = 11
-
+# Get bucket information
 client = storage.Client()
 bucket = client.bucket(BUCKET_NAME)
 
+
 def extract_labels(df):
+    '''Extract the target columns of the dataset'''
     label_cols = []
-    for i in range(NUM_FEATURES, len(df.columns)):
-        label_cols.append(df.columns[i])
+    for col in range(NUM_FEATURES, len(df.columns)):
+        label_cols.append(df.columns[col])
     return label_cols
 
 def load_data():
+    '''Loads data from GCS bucket into training and testing dataframes'''
+    # Download data from GCS bucket
     tf.io.gfile.makedirs(DATA_DIR)
     training_file_path = f'{DATA_DIR}/training.csv'
     blob = bucket.blob(TRAINING_BLOB).download_to_filename(training_file_path)
     print('Downloaded file: ' + training_file_path)
 
+    # Load data into dataframes
     df = pd.read_csv(training_file_path, low_memory=False)
     df = df.drop(df.columns[0], axis=1)
-
+    
+    # Extract feature and target columns
     x = df.iloc[:, :NUM_FEATURES]
     y = df.iloc[:, NUM_FEATURES:-1]
   
+    # Split datasets into training and testing
     train_x, eval_x, train_y, eval_y = train_test_split(x, y, test_size=0.2)
 
     return train_x, train_y, eval_x, eval_y
