f34c35be9687d1f585a54ae5378fc2c1453f8876
vuppalli <vu8hh@virginia.edu>
2020-07-24 19:30:35 +0000

    clean setup code and add init file

--- data-science-onramp/setup.py
+++ data-science-onramp/setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages
 from setuptools import setup
-
+ 
 setup(
     name='trainer',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
-    description='Keras trainer application'
+    description='Data Science Onramp trainer application'
 )
