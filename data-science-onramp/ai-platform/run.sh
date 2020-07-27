3e17921880b443ea6906045d5ad1bd1597bbbee3
Diego Lopez <lodiego@google.com>
2020-07-27 17:22:04 -0400

    add shell script to locally train model

---
+++ data-science-onramp/ai-platform/run.sh
@@ -0,0 +1,10 @@
+python3 -m venv env
+source env/bin/activate
+pip install -U pip
+pip install -r requirements.txt
+gcloud ai-platform local train \
+	--package-path trainer/ \
+	--module-name trainer.sklearn.task \
+	--job-dir local
+deactivate
+rm -r env
