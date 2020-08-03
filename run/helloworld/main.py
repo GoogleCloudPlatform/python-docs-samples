b3c1d42806e3e560a01bc8ebdc4002674b0f060e
averikitsch <akitsch@google.com>
2020-08-03 14:55:19 -0700

    lint

--- run/helloworld/main.py
+++ run/helloworld/main.py
@@ -19,11 +19,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     name = os.environ.get('NAME', 'World')
     return 'Hello {}!'.format(name)
 
+
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
 # [END run_hello_service]
