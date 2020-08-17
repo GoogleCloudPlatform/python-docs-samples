e46ab97dcb9affb1c3126610fd07d247e131ca17
Dina Graves Portman <dinagraves@google.com>
2020-08-17 15:22:34 -0700

    Adding google_auth_idtoken_serverless tag

--- auth/service-to-service/auth.py
+++ auth/service-to-service/auth.py
@@ -16,6 +16,7 @@
 Demonstrates how to send authenticated service-to-service requests, eg 
 for Cloud Run or Cloud Functions"""
 
+# [START google_auth_idtoken_serverless]
 # [START functions_bearer_token]
 # [START run_service_to_service_auth]
 import urllib
@@ -42,3 +43,4 @@ def make_authorized_get_request(service_url):
     return response.read()
 # [END run_service_to_service_auth]
 # [END functions_bearer_token]
+# [END google_auth_idtoken_serverless]
