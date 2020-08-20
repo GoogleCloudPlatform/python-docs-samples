f77b06cab49a376d6fe951a9c78b002e347e72ba
Tushar Khan <tusharkhan@google.com>
2020-08-20 16:53:49 -0700

    Add pytorch files

---
+++ data-science-onramp/ai-platform/trainer/pytorch/model.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self.linear = nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        return self.linear(x)
