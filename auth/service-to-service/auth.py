55f31d102f4de79062246e5e5b1305d90bde1f61
Dina Graves Portman <dinagraves@google.com>
2020-08-21 16:24:31 -0700

    Debugging failed test

--- auth/service-to-service/auth.py
+++ auth/service-to-service/auth.py
@@ -36,6 +36,7 @@ def make_authorized_get_request(service_url):
 
     auth_req = google.auth.transport.requests.Request()
     id_token = google.oauth2.id_token.fetch_id_token(auth_req, service_url)
+    print(id_token)
 
     req.add_header("Authorization", f"Bearer {id_token}")
     response = urllib.request.urlopen(req)
