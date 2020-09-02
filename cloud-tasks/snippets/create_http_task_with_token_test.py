cd472976c962efd79c007f826160e2bcf066a8e1
arithmetic1728 <58957152+arithmetic1728@users.noreply.github.com>
2020-09-02 12:54:12 -0700

    feat!: migrate to use microgen (#38)
    * feat!: migrate to use microgen
    
    * Update UPGRADING.md
    
    Co-authored-by: Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
    
    Co-authored-by: Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>

--- cloud-tasks/snippets/create_http_task_with_token_test.py
+++ cloud-tasks/snippets/create_http_task_with_token_test.py
@@ -20,34 +20,32 @@ import pytest
 
 import create_http_task_with_token
 
-TEST_PROJECT_ID = os.getenv('GOOGLE_CLOUD_PROJECT')
-TEST_LOCATION = os.getenv('TEST_QUEUE_LOCATION', 'us-central1')
-TEST_QUEUE_NAME = f'my-queue-{uuid.uuid4().hex}'
+TEST_PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT")
+TEST_LOCATION = os.getenv("TEST_QUEUE_LOCATION", "us-central1")
+TEST_QUEUE_NAME = f"my-queue-{uuid.uuid4().hex}"
 TEST_SERVICE_ACCOUNT = (
-    'test-run-invoker@python-docs-samples-tests.iam.gserviceaccount.com')
+    "test-run-invoker@python-docs-samples-tests.iam.gserviceaccount.com"
+)
 
 
 @pytest.fixture()
 def test_queue():
     client = tasks_v2.CloudTasksClient()
-    parent = client.location_path(TEST_PROJECT_ID, TEST_LOCATION)
+    parent = f"projects/{TEST_PROJECT_ID}/locations/{TEST_LOCATION}"
     queue = {
         # The fully qualified path to the queue
-        'name': client.queue_path(
-            TEST_PROJECT_ID, TEST_LOCATION, TEST_QUEUE_NAME),
+        "name": client.queue_path(TEST_PROJECT_ID, TEST_LOCATION, TEST_QUEUE_NAME),
     }
-    q = client.create_queue(parent, queue)
+    q = client.create_queue(request={"parent": parent, "queue": queue})
 
     yield q
 
-    client.delete_queue(q.name)
+    client.delete_queue(request={"name": q.name})
 
 
 def test_create_http_task_with_token(test_queue):
-    url = 'https://example.com/task_handler'
-    result = create_http_task_with_token.create_http_task(TEST_PROJECT_ID,
-                                                          TEST_QUEUE_NAME,
-                                                          TEST_LOCATION,
-                                                          url,
-                                                          TEST_SERVICE_ACCOUNT)
+    url = "https://example.com/task_handler"
+    result = create_http_task_with_token.create_http_task(
+        TEST_PROJECT_ID, TEST_QUEUE_NAME, TEST_LOCATION, url, TEST_SERVICE_ACCOUNT
+    )
     assert TEST_QUEUE_NAME in result.name
