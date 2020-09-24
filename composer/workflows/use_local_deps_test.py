7b7aa9cb23aa7b169f941f10b46a20084cbb2699
Takashi Matsuo <tmatsuo@google.com>
2020-09-24 20:32:18 +0000

    cleanup: replace sanity check with confidence check

--- composer/workflows/use_local_deps_test.py
+++ composer/workflows/use_local_deps_test.py
@@ -36,7 +36,7 @@ def test_dag_import():
     """Test that the DAG file can be successfully imported.
 
     This tests that the DAG can be parsed, but does not run it in an Airflow
-    environment. This is a recommended sanity check by the official Airflow
+    environment. This is a recommended confidence check by the official Airflow
     docs: https://airflow.incubator.apache.org/tutorial.html#testing
     """
     from . import use_local_deps as module
