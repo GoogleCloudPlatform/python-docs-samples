3897badd1e459f34cf94bda11c42a1868f938958
Katie McLaughlin <katie@glasnt.com>
2020-10-02 12:54:01 +1000

    Update secretmanager

---
+++ run/django/mysite/migrations/0001_createsuperuser.py
@@ -0,0 +1,28 @@
+from django.db import migrations
+
+import google.auth
+from google.cloud import secretmanager_v1 as sm
+
+
+def createsuperuser(apps, schema_editor):
+    # Retrieve secret from Secret Manager 
+    _, project = google.auth.default()
+    client = sm.SecretManagerServiceClient()
+    path = f"projects/{project}/secrets/superuser_password/versions/latest"
+    admin_password = client.access_secret_version(path).payload.data.decode("UTF-8")
+
+    # Create a new user using acquired password
+    from django.contrib.auth.models import User
+    User.objects.create_superuser("admin", password=admin_password)
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.RunPython(createsuperuser)
+    ]
