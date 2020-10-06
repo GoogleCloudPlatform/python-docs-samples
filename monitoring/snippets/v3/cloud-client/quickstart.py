a0a2ac86d4e5249c2426dcce76eaaae532fb6db1
arithmetic1728 <58957152+arithmetic1728@users.noreply.github.com>
2020-10-06 12:11:53 -0700

    feat!: move to use microgen (#54)
    * feat!: move to use microgen
    
    * update
    
    * Update google/cloud/monitoring_v3/query.py
    
    Co-authored-by: Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
    
    * Update google/cloud/monitoring_v3/query.py
    
    Co-authored-by: Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
    
    Co-authored-by: Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>

--- monitoring/snippets/v3/cloud-client/quickstart.py
+++ monitoring/snippets/v3/cloud-client/quickstart.py
@@ -13,31 +13,33 @@
 # limitations under the License.
 
 
-def run_quickstart():
+def run_quickstart(project=""):
     # [START monitoring_quickstart]
     from google.cloud import monitoring_v3
 
     import time
 
     client = monitoring_v3.MetricServiceClient()
-    project = 'my-project'  # TODO: Update to your project ID.
-    project_name = client.project_path(project)
+    # project = 'my-project'  # TODO: Update to your project ID.
+    project_name = f"projects/{project}"
 
-    series = monitoring_v3.types.TimeSeries()
-    series.metric.type = 'custom.googleapis.com/my_metric'
-    series.resource.type = 'gce_instance'
-    series.resource.labels['instance_id'] = '1234567890123456789'
-    series.resource.labels['zone'] = 'us-central1-f'
-    point = series.points.add()
-    point.value.double_value = 3.14
+    series = monitoring_v3.TimeSeries()
+    series.metric.type = "custom.googleapis.com/my_metric"
+    series.resource.type = "gce_instance"
+    series.resource.labels["instance_id"] = "1234567890123456789"
+    series.resource.labels["zone"] = "us-central1-f"
     now = time.time()
-    point.interval.end_time.seconds = int(now)
-    point.interval.end_time.nanos = int(
-        (now - point.interval.end_time.seconds) * 10**9)
-    client.create_time_series(project_name, [series])
-    print('Successfully wrote time series.')
+    seconds = int(now)
+    nanos = int((now - seconds) * 10 ** 9)
+    interval = monitoring_v3.TimeInterval(
+        {"end_time": {"seconds": seconds, "nanos": nanos}}
+    )
+    point = monitoring_v3.Point({"interval": interval, "value": {"double_value": 3.14}})
+    series.points = [point]
+    client.create_time_series(request={"name": project_name, "time_series": [series]})
+    print("Successfully wrote time series.")
     # [END monitoring_quickstart]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_quickstart()
