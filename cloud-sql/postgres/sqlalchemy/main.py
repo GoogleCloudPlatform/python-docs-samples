2afd6a2bd919f10d6b1f39088cccb27b3feeeabf
craiglabenz <craig.labenz@gmail.com>
2020-10-07 10:17:18 -0700

    Updating Flask sample to new definition

--- cloud-sql/postgres/sqlalchemy/main.py
+++ cloud-sql/postgres/sqlalchemy/main.py
@@ -26,35 +26,53 @@ logger = logging.getLogger()
 
 
 def init_connection_engine():
+    # [START cloud_sql_postgres_sqlalchemy_limit]
+    # [START cloud_sql_postgres_sqlalchemy_backoff]
+    # [START cloud_sql_postgres_sqlalchemy_timeout]
+    # [START cloud_sql_postgres_sqlalchemy_lifetime]
     db_config = {
-        # [START cloud_sql_postgres_sqlalchemy_limit]
+    # [END cloud_sql_postgres_sqlalchemy_limit]
+    # [END cloud_sql_postgres_sqlalchemy_backoff]
+    # [END cloud_sql_postgres_sqlalchemy_timeout]
+    # [END cloud_sql_postgres_sqlalchemy_lifetime]
+
+    # [START cloud_sql_postgres_sqlalchemy_limit]
         # Pool size is the maximum number of permanent connections to keep.
         "pool_size": 5,
         # Temporarily exceeds the set pool_size if no connections are available.
         "max_overflow": 2,
         # The total number of concurrent connections for your application will be
         # a total of pool_size and max_overflow.
-        # [END cloud_sql_postgres_sqlalchemy_limit]
+    # [END cloud_sql_postgres_sqlalchemy_limit]
 
-        # [START cloud_sql_postgres_sqlalchemy_backoff]
+    # [START cloud_sql_postgres_sqlalchemy_backoff]
         # SQLAlchemy automatically uses delays between failed connection attempts,
         # but provides no arguments for configuration.
-        # [END cloud_sql_postgres_sqlalchemy_backoff]
+    # [END cloud_sql_postgres_sqlalchemy_backoff]
 
-        # [START cloud_sql_postgres_sqlalchemy_timeout]
+    # [START cloud_sql_postgres_sqlalchemy_timeout]
         # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
         # new connection from the pool. After the specified amount of time, an
         # exception will be thrown.
         "pool_timeout": 30,  # 30 seconds
-        # [END cloud_sql_postgres_sqlalchemy_timeout]
+    # [END cloud_sql_postgres_sqlalchemy_timeout]
 
-        # [START cloud_sql_postgres_sqlalchemy_lifetime]
+    # [START cloud_sql_postgres_sqlalchemy_lifetime]
         # 'pool_recycle' is the maximum number of seconds a connection can persist.
         # Connections that live longer than the specified amount of time will be
         # reestablished
         "pool_recycle": 1800,  # 30 minutes
-        # [END cloud_sql_postgres_sqlalchemy_lifetime]
+    # [END cloud_sql_postgres_sqlalchemy_lifetime]
+
+    # [START cloud_sql_postgres_sqlalchemy_limit]
+    # [START cloud_sql_postgres_sqlalchemy_backoff]
+    # [START cloud_sql_postgres_sqlalchemy_timeout]
+    # [START cloud_sql_postgres_sqlalchemy_lifetime]
     }
+    # [END cloud_sql_postgres_sqlalchemy_limit]
+    # [END cloud_sql_postgres_sqlalchemy_backoff]
+    # [END cloud_sql_postgres_sqlalchemy_timeout]
+    # [END cloud_sql_postgres_sqlalchemy_lifetime]
 
     if os.environ.get("DB_HOST"):
         return init_tcp_connection_engine(db_config)
