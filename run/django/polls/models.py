5e1cb0a4cb697ad1c8a38f9956b544cb805146c2
Katie McLaughlin <katie@glasnt.com>
2020-10-20 08:53:50 +1100

    black

--- run/django/polls/models.py
+++ run/django/polls/models.py
@@ -17,7 +17,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField("date published")
 
 
 class Choice(models.Model):
--- run/django/polls/urls.py
+++ run/django/polls/urls.py
@@ -17,5 +17,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
 ]
