c31d959263fd67ee1ba12028449e78c4de880f0d
Leah Cole <coleleah@google.com>
2020-11-05 15:44:50 -0800

    add enforce_type_hints to default config

--- noxfile-template.py
+++ noxfile-template.py
@@ -39,6 +39,10 @@ TEST_CONFIG = {
     # You can opt out from the test for specific Python versions.
     'ignored_versions': ["2.7"],
 
+    # Old samples are opted out of enforcing Python type hints
+    # All new samples should feature them
+    'enforce_type_hints': False,
+
     # An envvar key for determining the project id to use. Change it
     # to 'BUILD_SPECIFIC_GCLOUD_PROJECT' if you want to opt in using a
     # build specific Cloud project. You can also use your own string
