8e7452eae1d10c254301918d9ba5a27b9df8272c
craiglabenz <craig.labenz@gmail.com>
2020-11-13 10:20:23 -0800

    Added tests to Cloud SQL quickstarts

--- .kokoro/tests/run_tests.sh
+++ .kokoro/tests/run_tests.sh
@@ -108,11 +108,15 @@ export DATALABELING_ENDPOINT="test-datalabeling.sandbox.googleapis.com:443"
 # Run Cloud SQL proxy (background process exit when script does)
 wget --quiet https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 \
      -O ${HOME}/cloud_sql_proxy && chmod +x ${HOME}/cloud_sql_proxy
-${HOME}/cloud_sql_proxy -instances="${MYSQL_INSTANCE}"=tcp:3306 &>> \
+${HOME}/cloud_sql_proxy -instances="${MYSQL_INSTANCE}"=tcp:3306,"${MYSQL_INSTANCE}" -dir "${HOME}" &>> \
        ${HOME}/cloud_sql_proxy.log &
-${HOME}/cloud_sql_proxy -instances="${POSTGRES_INSTANCE}"=tcp:5432 &>> \
+echo -e "\Cloud SQL proxy started for MySQL."
+${HOME}/cloud_sql_proxy -instances="${POSTGRES_INSTANCE}"=tcp:5432,"${POSTGRES_INSTANCE}" -dir "${HOME}" &>> \
        ${HOME}/cloud_sql_proxy-postgres.log &
-echo -e "\nCloud SQL proxy started."
+echo -e "\Cloud SQL proxy started for Postgres."
+${HOME}/cloud_sql_proxy -instances="${SQLSERVER_INSTANCE}"=tcp:1433 &>> \
+       ${HOME}/cloud_sql_proxy-sqlserver.log &
+echo -e "\Cloud SQL proxy started for SQL Server."
 
 echo -e "\n******************** TESTING PROJECTS ********************"
 # Switch to 'fail at end' to allow all tests to complete before exiting.
