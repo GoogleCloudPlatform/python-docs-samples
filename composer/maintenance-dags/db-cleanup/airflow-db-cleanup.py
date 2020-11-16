99122da0f1182c4ab1a13405bca97130b342d4db
Maciej Gawron <maciekgawron@google.com>
2020-11-16 16:01:12 +0100

    Add license to the db cleanup file.

--- composer/maintenance-dags/db-cleanup/airflow-db-cleanup.py
+++ composer/maintenance-dags/db-cleanup/airflow-db-cleanup.py
@@ -1,12 +1,17 @@
-"""A maintenance workflow that you can deploy into Airflow to periodically clean out the DagRun, TaskInstance, Log, XCom, Job DB and SlaMiss entries to avoid having too much data in your Airflow MetaStore.
-
-airflow trigger_dag --conf
-'[curly-braces]"maxDBEntryAgeInDays":30[curly-braces]' airflow-db-cleanup
-
---conf options:
-    maxDBEntryAgeInDays:<INT> - Optional
-
-"""
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""A maintenance workflow that you can deploy into Airflow to periodically clean out the DagRun, TaskInstance, Log, XCom, Job DB and SlaMiss entries to avoid having too much data in your Airflow MetaStore."""
 import airflow
 from airflow import settings
 from airflow.configuration import conf
