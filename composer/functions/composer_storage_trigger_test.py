b1e48a580aa5f0044a4fc586ae763a50bc97cea8
Maciej Gawron <mgawron@google.com>
2020-11-20 11:17:01 +0100

    Moved db cleanup and integrate readme into the dag
    Additionally some import order changes to be compliant
    with nox linter.

--- composer/functions/composer_storage_trigger_test.py
+++ composer/functions/composer_storage_trigger_test.py
@@ -11,11 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import composer_storage_trigger
 
 import mock
 import pytest
-
-import composer_storage_trigger
 
 
 # handles error in JSON body
--- composer/workflows/airflow_db_cleanup.py
+++ composer/workflows/airflow_db_cleanup.py
@@ -11,18 +11,53 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""
+A maintenance workflow that you can deploy into Airflow to periodically clean
+out the DagRun, TaskInstance, Log, XCom, Job DB and SlaMiss entries to avoid
+having too much data in your Airflow MetaStore.
+
+## Authors
+
+The DAG is a fork of [teamclairvoyant repository.](https://github.com/teamclairvoyant/airflow-maintenance-dags/tree/master/db-cleanup)
+
+## Usage
+
+1. Update the global variables (SCHEDULE_INTERVAL, DAG_OWNER_NAME,
+  ALERT_EMAIL_ADDRESSES and ENABLE_DELETE) in the DAG with the desired values
+
+2. Modify the DATABASE_OBJECTS list to add/remove objects as needed. Each
+   dictionary in the list features the following parameters:
+    - airflow_db_model: Model imported from airflow.models corresponding to
+      a table in the airflow metadata database
+    - age_check_column: Column in the model/table to use for calculating max
+      date of data deletion
+    - keep_last: Boolean to specify whether to preserve last run instance
+        - keep_last_filters: List of filters to preserve data from deleting
+          during clean-up, such as DAG runs where the external trigger is set to 0.
+        - keep_last_group_by: Option to specify column by which to group the
+          database entries and perform aggregate functions.
+
+3. Create and Set the following Variables in the Airflow Web Server
+  (Admin -> Variables)
+    - airflow_db_cleanup__max_db_entry_age_in_days - integer - Length to retain
+      the log files if not already provided in the conf. If this is set to 30,
+      the job will remove those files that are 30 days old or older.
+
+4. Put the DAG in your gcs bucket.
+"""
+from datetime import datetime, timedelta
+import logging
+import os
+
 import airflow
 from airflow import settings
 from airflow.configuration import conf
-from airflow.models import DAG, DagModel, DagRun, Log, XCom, SlaMiss, \
-    TaskInstance, Variable
 from airflow.jobs import BaseJob
+from airflow.models import DAG, DagModel, DagRun, Log, SlaMiss, \
+    TaskInstance, Variable, XCom
 from airflow.operators.python_operator import PythonOperator
-from datetime import datetime, timedelta
 import dateutil.parser
-import logging
-import os
-from sqlalchemy import func, and_
+from sqlalchemy import and_, func
 from sqlalchemy.exc import ProgrammingError
 from sqlalchemy.orm import load_only
 
