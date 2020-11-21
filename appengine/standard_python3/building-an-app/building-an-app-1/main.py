4ced02bb075affa7bb7377087c297b87ed544155
Charles Engelke <engelke@google.com>
2020-11-20 16:45:38 -0800

    Added gae_python3_ tags in addition to gae_python38_ ones

--- appengine/standard_python3/building-an-app/building-an-app-1/main.py
+++ appengine/standard_python3/building-an-app/building-an-app-1/main.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 # [START gae_python38_render_template]
+# [START gae_python3_render_template]
 import datetime
 
 from flask import Flask, render_template
@@ -41,4 +42,5 @@ if __name__ == '__main__':
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_python3_render_template]
 # [END gae_python38_render_template]
