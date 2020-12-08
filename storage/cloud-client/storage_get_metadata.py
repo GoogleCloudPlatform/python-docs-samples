122e979e4d9b23fb06a61bd61feb3edd313413f8
Chris Wilcox <crwilcox@google.com>
2020-12-08 13:46:54 -0800

    fix: add a comment to draw attention to using get_blob, not blob

--- storage/cloud-client/storage_get_metadata.py
+++ storage/cloud-client/storage_get_metadata.py
@@ -27,6 +27,8 @@ def blob_metadata(bucket_name, blob_name):
 
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
+
+    # Retrieve a blob, and its metadata, from Google Cloud Storage.
     blob = bucket.get_blob(blob_name)
 
     print("Blob: {}".format(blob.name))
