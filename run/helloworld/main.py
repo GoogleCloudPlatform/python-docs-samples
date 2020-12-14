54208af42eeea6b2a2e9485158e244516c03ff8c
Leah E. Cole <6719667+leahecole@users.noreply.github.com>
2020-12-14 14:43:15 -0800

    refactor cloud run tests for container reg cleanup (#5053)
    * refactor hello-broken for cleanup
    
    * refactor helloworld test for better cleanup
    
    * remove debug suffix
    
    * fix logging test, remove nonsense gcloud commands
    
    * delete unused build file
    
    * remove debug statements
    
    * refactor markdown preview tests
    
    * remove debug comments
    
    * refactor system package tests
    
    * shorten suffixes
    
    * refactor image processing test
    
    * add project flags
    
    * refactor pubsub test
    
    * add missing check=true
    
    * run blacken, fix lint

--- run/helloworld/main.py
+++ run/helloworld/main.py
@@ -21,13 +21,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    name = os.environ.get('NAME', 'World')
-    return 'Hello {}!'.format(name)
+    name = os.environ.get("NAME", "World")
+    return "Hello {}!".format(name)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 # [END run_helloworld_service]
 # [END cloudrun_helloworld_service]
