f7225927636c0847adccdbd02b2ddeb5a146c489
bradmiro <bmiro@google.com>
2020-12-17 16:00:24 -0500

    updated test to reflect gcs bucket

--- data-science-onramp/ai-platform/sklearn_test.py
+++ data-science-onramp/ai-platform/sklearn_test.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import uuid
 
 from google.cloud import storage
@@ -7,8 +8,9 @@ import pytest
 
 STAGING_BUCKET = f"sklearn-job-dir-{uuid.uuid4()}"
 INPUT_DIR = "inputs"
+TRAINER_DIR = "trainer"
 MODELS_DIR = "models"
-JOB_DIR = STAGING_BUCKET + "/" + MODELS_DIR
+JOB_DIR = f"{STAGING_BUCKET}/{MODELS_DIR}"
 TRAIN_DATA = "train.csv"
 TRAIN_DATA_PATH = f"gs://{STAGING_BUCKET}/{INPUT_DIR}/{TRAIN_DATA}"
 
@@ -18,11 +20,21 @@ MODEL_NAME = f"sklearn-test-{uuid.uuid4()}"
 JOB_ID = f"sklearn_{str(uuid.uuid4())[:7]}"
 
 
+def copy_dir_to_gcs(local_path, gcs_path, bucket):
+    for path in glob.glob(f"{local_path}/**"):
+        new_gcs_path = f"{gcs_path}/{os.path.basename(path)}"
+        if not os.path.isfile(path):
+            copy_dir_to_gcs(path, new_gcs_path, bucket)
+        else:
+            bucket.blob(new_gcs_path).upload_from_filename(path)
+        
+
 @pytest.fixture(autouse=True)
 def setup_teardown():
     storage_client = storage.Client()
     bucket = storage_client.create_bucket(STAGING_BUCKET)
     bucket.blob(INPUT_DIR).upload_from_filename(TRAIN_DATA)
+    copy_dir_to_gcs(f"{TRAINER_DIR}/", TRAINER_DIR, bucket)
 
     yield bucket
 
@@ -37,8 +49,9 @@ def test_sklearn(setup_teardown):
     request_dict = {
         "jobId": JOB_ID,
         "trainingInput": {
+            "scaleTier": "STANDARD_1",
             "region": REGION,
-            "packageUris": ["trainer/"],
+            "packageUris": [f"gs://{STAGING_BUCKET}/{TRAINER_DIR}/"],
             "pythonModule": "trainer.sklearn_model.task",
             "jobDir": JOB_DIR,
             "runtimeVersion": "2.2",
