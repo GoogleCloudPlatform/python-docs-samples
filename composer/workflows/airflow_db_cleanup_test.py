193337df4ca38f1d2af47e20c5a9fee9f1a0b0bc
Leah Cole <coleleah@google.com>
2021-01-29 15:22:15 -0800

    move under composer directory

--- composer/workflows/airflow_db_cleanup_test.py
+++ composer/workflows/airflow_db_cleanup_test.py
@@ -12,7 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import unit_testing
+import os
+
+import unit_testing
+
 
 
 def test_dag_import():
