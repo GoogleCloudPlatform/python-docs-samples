1fd0c33dacc29c54a0522d4dd8f970ca2c96da3e
Katie McLaughlin <glasnt@google.com>
2021-02-08 14:12:50 +1100

    revert

--- run/django/mysite/migrations/0001_createsuperuser.py
+++ run/django/mysite/migrations/0001_createsuperuser.py
@@ -29,16 +29,18 @@ def createsuperuser(apps: StateApps, schema_editor: DatabaseSchemaEditor) -> Non
     Dynamically create an admin user as part of a migration
     Password is pulled from Secret Manger (previously created as part of tutorial)
     """
-    # TODO(glasnt): removed trampoline CI for testing.
-    client = secretmanager.SecretManagerServiceClient()
+    if os.getenv("TRAMPOLINE_CI", None):
+        admin_password = "test"
+    else:
+        client = secretmanager.SecretManagerServiceClient()
 
-    # Get project value for identifying current context
-    _, project = google.auth.default()
+        # Get project value for identifying current context
+        _, project = google.auth.default()
 
-    # Retrieve the previously stored admin password
-    PASSWORD_NAME = os.environ.get("PASSWORD_NAME", "superuser_password")
-    name = f"projects/{project}/secrets/{PASSWORD_NAME}/versions/latest"
-    admin_pass = client.access_secret_version(name=name).payload.data.decode("UTF-8")
+        # Retrieve the previously stored admin password
+        PASSWORD_NAME = os.environ.get("PASSWORD_NAME", "superuser_password")
+        name = f"projects/{project}/secrets/{PASSWORD_NAME}/versions/latest"
+        admin_pass = client.access_secret_version(name=name).payload.data.decode("UTF-8")
 
     # Create a new user using acquired password, stripping any accidentally stored newline characters
     User.objects.create_superuser("admin", password=admin_pass.strip())
