99c49d77741da24616d200125e6ff9ce836d42ca
Katie McLaughlin <glasnt@google.com>
2021-02-08 14:13:07 +1100

    fix

--- run/django/mysite/migrations/0001_createsuperuser.py
+++ run/django/mysite/migrations/0001_createsuperuser.py
@@ -30,7 +30,7 @@ def createsuperuser(apps: StateApps, schema_editor: DatabaseSchemaEditor) -> Non
     Password is pulled from Secret Manger (previously created as part of tutorial)
     """
     if os.getenv("TRAMPOLINE_CI", None):
-        admin_password = "test"
+        admin_pass = "test"
     else:
         client = secretmanager.SecretManagerServiceClient()
 
