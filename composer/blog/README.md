7bb71bc471ad90024d304f40f98cb604052ffe97
Leah Cole <coleleah@google.com>
2021-02-11 14:52:27 -0800

    Add `blogs` folder for blog post code

---
+++ composer/blog/README.md
@@ -0,0 +1,3 @@
+# Cloud Composer Blog Posts
+
+Beginning in 2021, any code samples written by Cloud Composer DevRel and featured in blog posts are found in this repo. 
---
+++ composer/blog/gcp-tech-blog/unit-test-dags-cloud-build/dags/example_dag.py
@@ -0,0 +1,33 @@
+from airflow.operators.bash_operator import BashOperator
+from airflow.providers.google.cloud.operators.gcs import GCSCreateBucketOperator
+from airflow.providers.google.cloud.operators.gcs import GCSListObjectsOperator
+from airflow.providers.google.cloud.operators.gcs import GCSDeleteBucketOperator
+from airflow import models
+import uuid
+import os
+from airflow.utils.dates import days_ago
+from airflow.utils.state import State
+UUID = uuid.uuid4()
+PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT", "leah-playground")
+BUCKET_NAME = f"leah-{UUID}"
+
+with models.DAG(
+    "example_gcs",
+    start_date=days_ago(1),
+    schedule_interval=None,
+) as dag:
+    create_bucket = GCSCreateBucketOperator(task_id="create_bucket", bucket_name=BUCKET_NAME, project_id=PROJECT_ID)
+
+    list_objects = GCSListObjectsOperator(task_id="list_objects", bucket=BUCKET_NAME)
+    list_buckets_result = BashOperator(
+        task_id="list_buckets_result",
+        bash_command="echo \"{{ task_instance.xcom_pull('list_objects') }}\"",
+    )
+    delete_bucket = GCSDeleteBucketOperator(task_id="delete_bucket", bucket_name=BUCKET_NAME)
+
+    create_bucket >> list_objects >> delete_bucket
+
+
+if __name__ == "__main__":
+    dag.clear(dag_run_state=State.NONE)
+    dag.run()
