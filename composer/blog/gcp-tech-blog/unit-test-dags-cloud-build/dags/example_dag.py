8ab0a307c97ad03347dc181469df6149ba212d3e
Leah Cole <coleleah@google.com>
2021-02-11 15:06:31 -0800

    fix lint

--- composer/blog/gcp-tech-blog/unit-test-dags-cloud-build/dags/example_dag.py
+++ composer/blog/gcp-tech-blog/unit-test-dags-cloud-build/dags/example_dag.py
@@ -1,12 +1,30 @@
+# Copyright 2021 Google LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import uuid
+
+from airflow import models
+
 from airflow.operators.bash_operator import BashOperator
 from airflow.providers.google.cloud.operators.gcs import GCSCreateBucketOperator
-from airflow.providers.google.cloud.operators.gcs import GCSListObjectsOperator
 from airflow.providers.google.cloud.operators.gcs import GCSDeleteBucketOperator
-from airflow import models
-import uuid
-import os
+from airflow.providers.google.cloud.operators.gcs import GCSListObjectsOperator
+
 from airflow.utils.dates import days_ago
 from airflow.utils.state import State
+
 UUID = uuid.uuid4()
 PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT", "leah-playground")
 BUCKET_NAME = f"leah-{UUID}"
--- composer/blog/gcp-tech-blog/unit-test-dags-cloud-build/dags/example_dag_test.py
+++ composer/blog/gcp-tech-blog/unit-test-dags-cloud-build/dags/example_dag_test.py
@@ -11,7 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import internal_unit_testing
+
 
 def test_dag_import():
     from . import example_dag
