1481193590964873b7344b5c032be10614429868
bradmiro <bmiro@google.com>
2021-02-17 15:06:35 -0500

    adding headers and lint fixes

--- data-science-onramp/ai-platform/sklearn_test.py
+++ data-science-onramp/ai-platform/sklearn_test.py
@@ -1,14 +1,25 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https: // www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
-import re
 import tarfile
 import time
 import uuid
 
 from google.cloud import storage
 from google.cloud.aiplatform import gapic as aip
-from googleapiclient import discovery
 
-from typing import Callable
 import pytest
 
 STAGING_BUCKET = f"sklearn-job-dir-{uuid.uuid4()}"
@@ -32,10 +43,12 @@ TERMINAL_STATES = [
     aip.JobState.JOB_STATE_CANCELLED
 ]
 
+
 @pytest.fixture
 def shared_state():
     state = {}
     yield state
+
 
 @pytest.fixture(autouse=True)
 def setup_teardown(shared_state):
@@ -62,6 +75,7 @@ def setup_teardown(shared_state):
     os.remove(TRAINER_TAR)
 
     aip_job_client.delete_custom_job(name=shared_state["model_name"]).result()
+
 
 @pytest.mark.timeout(1200)
 def test_sklearn(setup_teardown, shared_state):
--- data-science-onramp/ai-platform/tfkeras_test.py
+++ data-science-onramp/ai-platform/tfkeras_test.py
@@ -1,3 +1,17 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https: // www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import tarfile
 import time
@@ -34,6 +48,7 @@ def shared_state():
     state = {}
     yield state
 
+
 @pytest.fixture(autouse=True)
 def setup_teardown(shared_state):
     storage_client = storage.Client()
@@ -59,6 +74,7 @@ def setup_teardown(shared_state):
     os.remove(TRAINER_TAR)
 
     aip_job_client.delete_custom_job(name=shared_state["model_name"]).result()
+
 
 @pytest.mark.timeout(1200)
 def test_tfkeras(setup_teardown, shared_state):
