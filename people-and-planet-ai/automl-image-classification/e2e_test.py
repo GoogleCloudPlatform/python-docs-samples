05a60eff3b13210a0db57654667c092f0f365f1e
David Cavazos <dcavazos@google.com>
2021-02-26 12:45:15 -0800

    Moved pipeline test to standalone script

--- people-and-planet-ai/automl-image-classification/e2e_test.py
+++ people-and-planet-ai/automl-image-classification/e2e_test.py
@@ -22,14 +22,10 @@ import pytest
 
 SUFFIX = uuid.uuid4().hex[0:6]
 PROJECT = os.environ["GOOGLE_CLOUD_PROJECT"]
-REGION = "us-central1"
 BUCKET_NAME = f"wildlife-insights-test-{SUFFIX}"
 BIGQUERY_DATASET = f"wildlife_insights_test_{SUFFIX}"
 BIGQUERY_TABLE = "images_database"
-AUTOML_DATASET = "wildlife_insights_test"
-AUTOML_MODEL = "wildlife_insights_test"
-MIN_IMAGES_PER_CLASS = 0
-MAX_IMAGES_PER_CLASS = 0
+REGION = "us-central1"
 
 
 @pytest.fixture(scope="session")
@@ -80,72 +76,15 @@ def bigquery_table(bucket_name: str, bigquery_dataset: str) -> None:
 def test_end_to_end(
     bucket_name: str, bigquery_dataset: str, bigquery_table: str
 ) -> None:
-
-    py_code = f"""
-from apache_beam.options.pipeline_options import PipelineOptions
-from dataclasses import dataclass
-import mock
-import pipeline
-
-@dataclass
-class MockOperation:
-    name: str = "operation_path"
-
-@dataclass
-class MockResponse:
-    name: str
-
-@dataclass
-class MockRequest:
-    def __init__(self, name):
-        self.name = "{{name}}_request"
-        self._response_name = "{{name}}_response"
-        self.operation = MockOperation()
-
-    def result(self: Any):
-        return MockResponse(self._response_name)
-
-@mock.patch(
-    "google.cloud.aiplatform_v1.services.dataset_service.DatasetServiceClient.create_dataset",
-    lambda *args, **kwargs: MockRequest("create_dataset"),
-)
-@mock.patch(
-    "google.cloud.aiplatform_v1.services.dataset_service.DatasetServiceClient.import_data",
-    lambda *args, **kwargs: MockRequest("import_data"),
-)
-@mock.patch(
-    "google.cloud.aiplatform_v1.services.pipeline_service.PipelineServiceClient.create_training_pipeline",
-    lambda *args, **kwargs: MockRequest("create_training_pipeline"),
-)
-def run_mock_pipeline():
-    pipeline_args = [
-        "--project={PROJECT}",
-        "--runner=DataflowRunner",
-        "--requirements_file=requirements.txt",
-        "--region={REGION}",
-    ]
-    pipeline_options = PipelineOptions(
-        pipeline_args,
-        temp_location="gs://{bucket_name}/temp",
-        save_main_session=True,
-    )
-
-    pipeline.run(
-        project={PROJECT},
-        region={REGION},
-        cloud_storage_path="gs://{bucket_name}",
-        bigquery_dataset={bigquery_dataset},
-        bigquery_table={bigquery_table},
-        automl_dataset={AUTOML_DATASET},
-        automl_model={AUTOML_MODEL},
-        min_images_per_class={MIN_IMAGES_PER_CLASS},
-        max_images_per_class={MAX_IMAGES_PER_CLASS},
-        automl_budget_milli_node_hours=8000,
-        pipeline_options=pipeline_options,
-    )
-run_mock_pipeline()"""
-
     subprocess.run(
-        ["python", "-c", py_code],
+        [
+            "python",
+            "pipeline_test.py",
+            f"--project={PROJECT}",
+            f"--region={REGION}",
+            f"--bucket={bucket_name}",
+            f"--bigquery-dataset={bigquery_dataset}",
+            f"--bigquery-table={bigquery_table}",
+        ],
         check=True,
     )
