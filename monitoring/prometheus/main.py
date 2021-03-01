71edf8a0ccb55f89d14dd60775ed1b710c6bd0ee
Yuri Grinshteyn <ygrinshteyn@google.com>
2021-03-01 08:57:38 -0800

    cleaning up for linting

--- monitoring/prometheus/main.py
+++ monitoring/prometheus/main.py
@@ -13,17 +13,17 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from flask import Flask
-from prometheus_client import (
-    Summary,
-    Gauge,
-    Counter,
-    generate_latest,
-    REGISTRY,
-    Histogram,
-)
 import random
 import time
+
+from flask import Flask
+
+from prometheus_client import (
+    Counter,
+    generate_latest,
+    Histogram,
+    REGISTRY,
+)
 
 
 app = Flask(__name__)
