c30b4468115428ab13388f34e5a537639ce3db2c
Yuri Grinshteyn <ygrinshteyn@google.com>
2021-03-04 11:39:50 -0800

    cleaning up license header

--- monitoring/prometheus/main.py
+++ monitoring/prometheus/main.py
@@ -1,4 +1,3 @@
-"""
 # Copyright 2021 Google LLC
 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
 # [START monitoring_sli_metrics_prometheus_setup]
 import random
 import time
