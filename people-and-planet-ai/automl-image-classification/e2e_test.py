352e916105aa20cf88070f6ed638d8d59a9b6ae1
David Cavazos <dcavazos@google.com>
2021-03-09 09:22:35 -0800

    fix tests + delete endpoint

--- people-and-planet-ai/automl-image-classification/e2e_test.py
+++ people-and-planet-ai/automl-image-classification/e2e_test.py
@@ -13,10 +13,12 @@
 # limitations under the License.
 
 import io
+import logging
 import os
 import subprocess
 import uuid
 
+from google.cloud import aiplatform
 from google.cloud import bigquery
 from google.cloud import storage
 import pytest
@@ -40,6 +42,7 @@ def bucket_name() -> str:
     storage_client = storage.Client()
     bucket = storage_client.create_bucket(BUCKET_NAME)
 
+    logging.info(f"bucket_name: {BUCKET_NAME}")
     yield BUCKET_NAME
 
     # This test creates 1 image file per class, creating around 650 files.
@@ -57,6 +60,7 @@ def bigquery_dataset() -> str:
     dataset_id = f"{PROJECT}.{BIGQUERY_DATASET}"
     bigquery_client.create_dataset(bigquery.Dataset(dataset_id))
 
+    logging.info(f"bigquery_dataset: {BIGQUERY_DATASET}")
     yield BIGQUERY_DATASET
 
     bigquery_client.delete_dataset(dataset_id, delete_contents=True, not_found_ok=True)
@@ -65,46 +69,61 @@ def bigquery_dataset() -> str:
 @pytest.fixture(scope="session")
 def bigquery_table(bigquery_dataset: str) -> str:
     # Create a small test table.
-    data = """category,file_name
-alectoris rufa,animals/0059/1810.jpg
-equus quagga,animals/0378/0118.jpg
-fossa fossana,animals/0620/0242.jpg
-human,humans/0379/0877.jpg
-human,humans/0640/0467.jpg
-lophotibis cristataa,animals/0605/1478.jpg
-mazama temama,animals/0532/0525.jpg
-odontophorus balliviani,animals/0523/1368.jpg
-tayassu pecari,animals/0049/0849.jpg
-tayassu pecari,animals/0090/1218.jpg"""
+    schema = [
+        bigquery.SchemaField("category", "STRING"),
+        bigquery.SchemaField("file_name", "STRING"),
+    ]
+    rows = [
+        "alectoris rufa,animals/0059/1810.jpg",
+        "equus quagga,animals/0378/0118.jpg",
+        "fossa fossana,animals/0620/0242.jpg",
+        "human,humans/0379/0877.jpg",
+        "human,humans/0640/0467.jpg",
+        "lophotibis cristataa,animals/0605/1478.jpg",
+        "mazama temama,animals/0532/0525.jpg",
+        "odontophorus balliviani,animals/0523/1368.jpg",
+        "tayassu pecari,animals/0049/0849.jpg",
+        "tayassu pecari,animals/0090/1218.jpg",
+    ]
 
     bigquery_client = bigquery.Client()
-    with io.StringIO(data) as source_file:
+    with io.StringIO("\n".join(rows)) as source_file:
         job = bigquery_client.load_table_from_file(
             source_file,
             f"{PROJECT}.{bigquery_dataset}.{BIGQUERY_TABLE}",
             job_config=bigquery.LoadJobConfig(
                 source_format=bigquery.SourceFormat.CSV,
-                skip_leading_rows=1,
-                autodetect=True,
+                schema=schema,
             ),
         )
         job.result()  # wait for table to be loaded
 
     # The table is deleted when we delete the dataset.
+    logging.info(f"bigquery_table: {BIGQUERY_TABLE}")
     yield BIGQUERY_TABLE
 
 
 @pytest.fixture(scope="session")
 def model_endpoint_id() -> str:
-    yield deploy_model.run(
+    endpoint_id = deploy_model.run(
         project=PROJECT,
         region=REGION,
         model_path=AUTOML_MODEL_PATH,
         model_endpoint_name="wildlife_insights",
     )
 
+    logging.info(f"endpoint_id: {endpoint_id}")
+    yield endpoint_id
 
-def test_create_images_database(bucket_name: str, bigquery_dataset: str) -> None:
+    client = aiplatform.gapic.EndpointServiceClient(
+        client_options={"api_endpoint": "us-central1-aiplatform.googleapis.com"}
+    )
+    name = client.endpoint_path(project=PROJECT, location=REGION, endpoint=endpoint_id)
+    client.delete_endpoint(name=name).result()
+
+
+def test_create_images_database_table(bucket_name: str, bigquery_dataset: str) -> None:
+    # The table is deleted when we delete the dataset.
     subprocess.run(
         [
             "python",
@@ -153,4 +172,4 @@ def test_predict(model_endpoint_id: str) -> None:
         model_endpoint_id=model_endpoint_id,
         image_file="animals/0036/0072.jpg",  # tapirus indicus
     )
-    assert len(predictions) > 0
+    assert len(predictions) > 0, f"predictions: {predictions}"
