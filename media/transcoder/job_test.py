fc6dee24e5af771cd426f65f2a65984fe98038b1
Nicholas Cook <irataxy@users.noreply.github.com>
2021-03-30 08:47:23 -0700

    feat: add code samples and tests for overlay creation (#5464)

--- media/transcoder/job_test.py
+++ media/transcoder/job_test.py
@@ -25,6 +25,8 @@ import create_job_from_ad_hoc
 import create_job_from_preset
 import create_job_from_template
 import create_job_template
+import create_job_with_animated_overlay
+import create_job_with_static_overlay
 import delete_job
 import delete_job_template
 import get_job
@@ -38,22 +40,33 @@ template_id = f"my-python-test-template-{uuid.uuid4()}"
 
 bucket_name = f"python-samples-transcoder-{uuid.uuid4()}"
 test_video_file_name = "ChromeCast.mp4"
+test_overlay_image_file_name = "overlay.jpg"
 input_uri = "gs://" + bucket_name + "/" + test_video_file_name
+overlay_image_uri = "gs://" + bucket_name + "/" + test_overlay_image_file_name
 output_uri_for_preset = "gs://" + bucket_name + "/test-output-preset/"
 output_uri_for_template = "gs://" + bucket_name + "/test-output-template/"
 output_uri_for_adhoc = "gs://" + bucket_name + "/test-output-adhoc/"
+output_uri_for_static_overlay = "gs://" + bucket_name + "/test-output-static-overlay/"
+output_uri_for_animated_overlay = "gs://" + bucket_name + "/test-output-animated-overlay/"
+
 preset = "preset/web-hd"
 job_succeeded_state = "ProcessingState.SUCCEEDED"
 test_data = os.path.join(os.path.dirname(__file__), "..", "testdata")
 test_file = os.path.join(test_data, test_video_file_name)
+test_overlay_file = os.path.join(test_data, test_overlay_image_file_name)
 
 
 @pytest.fixture(scope="module")
 def test_bucket():
     storage_client = storage.Client()
     bucket = storage_client.create_bucket(bucket_name)
+
     blob = bucket.blob(test_video_file_name)
     blob.upload_from_filename(test_file)
+
+    blob = bucket.blob(test_overlay_image_file_name)
+    blob.upload_from_filename(test_overlay_file)
+
     yield bucket
     bucket.delete(force=True)
 
@@ -134,6 +147,74 @@ def test_create_job_from_template(capsys, test_bucket):
 def test_create_job_from_ad_hoc(capsys, test_bucket):
     create_job_from_ad_hoc.create_job_from_ad_hoc(
         project_id, location, input_uri, output_uri_for_adhoc
+    )
+    out, _ = capsys.readouterr()
+    job_name_prefix = f"projects/{project_number}/locations/{location}/jobs/"
+    assert job_name_prefix in out
+
+    str_slice = out.split("/")
+    job_id = str_slice[len(str_slice) - 1].rstrip("\n")
+    job_name = f"projects/{project_number}/locations/{location}/jobs/{job_id}"
+    assert job_name in out
+
+    get_job.get_job(project_id, location, job_id)
+    out, _ = capsys.readouterr()
+    assert job_name in out
+
+    time.sleep(30)
+
+    _get_job_state(capsys, job_id)
+
+    list_jobs.list_jobs(project_id, location)
+    out, _ = capsys.readouterr()
+    assert job_name in out
+
+    delete_job.delete_job(project_id, location, job_id)
+    out, _ = capsys.readouterr()
+    assert "Deleted job" in out
+
+
+def test_create_job_with_static_overlay(capsys, test_bucket):
+    create_job_with_static_overlay.create_job_with_static_overlay(
+        project_id,
+        location,
+        input_uri,
+        overlay_image_uri,
+        output_uri_for_static_overlay,
+    )
+    out, _ = capsys.readouterr()
+    job_name_prefix = f"projects/{project_number}/locations/{location}/jobs/"
+    assert job_name_prefix in out
+
+    str_slice = out.split("/")
+    job_id = str_slice[len(str_slice) - 1].rstrip("\n")
+    job_name = f"projects/{project_number}/locations/{location}/jobs/{job_id}"
+    assert job_name in out
+
+    get_job.get_job(project_id, location, job_id)
+    out, _ = capsys.readouterr()
+    assert job_name in out
+
+    time.sleep(30)
+
+    _get_job_state(capsys, job_id)
+
+    list_jobs.list_jobs(project_id, location)
+    out, _ = capsys.readouterr()
+    assert job_name in out
+
+    delete_job.delete_job(project_id, location, job_id)
+    out, _ = capsys.readouterr()
+    assert "Deleted job" in out
+
+
+def test_create_job_with_animated_overlay(capsys, test_bucket):
+    create_job_with_animated_overlay.create_job_with_animated_overlay(
+        project_id,
+        location,
+        input_uri,
+        overlay_image_uri,
+        output_uri_for_animated_overlay,
     )
     out, _ = capsys.readouterr()
     job_name_prefix = f"projects/{project_number}/locations/{location}/jobs/"
