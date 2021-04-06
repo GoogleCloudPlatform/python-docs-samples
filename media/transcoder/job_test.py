ba94cda566e40b1b942f6f1415545ee41281336c
Nicholas Cook <irataxy@users.noreply.github.com>
2021-04-06 11:25:33 -0700

    feat: add spritesheet samples and tests (#5604)

--- media/transcoder/job_test.py
+++ media/transcoder/job_test.py
@@ -26,6 +26,8 @@ import create_job_from_preset
 import create_job_from_template
 import create_job_template
 import create_job_with_animated_overlay
+import create_job_with_periodic_images_spritesheet
+import create_job_with_set_number_images_spritesheet
 import create_job_with_static_overlay
 import delete_job
 import delete_job_template
@@ -47,8 +49,21 @@ output_uri_for_preset = "gs://" + bucket_name + "/test-output-preset/"
 output_uri_for_template = "gs://" + bucket_name + "/test-output-template/"
 output_uri_for_adhoc = "gs://" + bucket_name + "/test-output-adhoc/"
 output_uri_for_static_overlay = "gs://" + bucket_name + "/test-output-static-overlay/"
-output_uri_for_animated_overlay = "gs://" + bucket_name + "/test-output-animated-overlay/"
+output_uri_for_animated_overlay = (
+    "gs://" + bucket_name + "/test-output-animated-overlay/"
+)
+small_spritesheet_file_prefix = "small-sprite-sheet"
+large_spritesheet_file_prefix = "large-sprite-sheet"
+spritesheet_file_suffix = "0000000000.jpeg"
 
+output_dir_for_set_number_spritesheet = "test-output-set-number-spritesheet/"
+output_uri_for_set_number_spritesheet = (
+    "gs://" + bucket_name + "/" + output_dir_for_set_number_spritesheet
+)
+output_dir_for_periodic_spritesheet = "test-output-periodic-spritesheet/"
+output_uri_for_periodic_spritesheet = (
+    "gs://" + bucket_name + "/" + output_dir_for_periodic_spritesheet
+)
 preset = "preset/web-hd"
 job_succeeded_state = "ProcessingState.SUCCEEDED"
 test_data = os.path.join(os.path.dirname(__file__), "..", "testdata")
@@ -90,7 +105,7 @@ def test_create_job_from_preset(capsys, test_bucket):
 
     time.sleep(30)
 
-    _get_job_state(capsys, job_id)
+    _assert_job_state_succeeded(capsys, job_id)
 
     list_jobs.list_jobs(project_id, location)
     out, _ = capsys.readouterr()
@@ -129,7 +144,7 @@ def test_create_job_from_template(capsys, test_bucket):
 
     time.sleep(30)
 
-    _get_job_state(capsys, job_id)
+    _assert_job_state_succeeded(capsys, job_id)
 
     list_jobs.list_jobs(project_id, location)
     out, _ = capsys.readouterr()
@@ -163,7 +178,7 @@ def test_create_job_from_ad_hoc(capsys, test_bucket):
 
     time.sleep(30)
 
-    _get_job_state(capsys, job_id)
+    _assert_job_state_succeeded(capsys, job_id)
 
     list_jobs.list_jobs(project_id, location)
     out, _ = capsys.readouterr()
@@ -197,7 +212,7 @@ def test_create_job_with_static_overlay(capsys, test_bucket):
 
     time.sleep(30)
 
-    _get_job_state(capsys, job_id)
+    _assert_job_state_succeeded(capsys, job_id)
 
     list_jobs.list_jobs(project_id, location)
     out, _ = capsys.readouterr()
@@ -231,7 +246,109 @@ def test_create_job_with_animated_overlay(capsys, test_bucket):
 
     time.sleep(30)
 
-    _get_job_state(capsys, job_id)
+    _assert_job_state_succeeded(capsys, job_id)
+
+    list_jobs.list_jobs(project_id, location)
+    out, _ = capsys.readouterr()
+    assert job_name in out
+
+    delete_job.delete_job(project_id, location, job_id)
+    out, _ = capsys.readouterr()
+    assert "Deleted job" in out
+
+
+def test_create_job_with_set_number_spritesheet(capsys, test_bucket):
+    create_job_with_set_number_images_spritesheet.create_job_with_set_number_images_spritesheet(
+        project_id,
+        location,
+        input_uri,
+        output_uri_for_set_number_spritesheet,
+    )
+    out, _ = capsys.readouterr()
+    job_name_prefix = f"projects/{project_number}/locations/{location}/jobs/"
+    assert job_name_prefix in out
+
+    str_slice = out.split("/")
+    job_id = str_slice[len(str_slice) - 1].rstrip("\n")
+    job_name = f"projects/{project_number}/locations/{location}/jobs/{job_id}"
+    assert job_name in out
+
+    get_job.get_job(project_id, location, job_id)
+    out, _ = capsys.readouterr()
+    assert (
+        job_name in out
+    )  # Get the job name so you can use it later to get the job and delete the job.
+
+    time.sleep(
+        30
+    )  # Transcoding jobs need time to complete. Once the job completes, check the job state.
+
+    _assert_job_state_succeeded(capsys, job_id)
+    _assert_file_in_bucket(
+        capsys,
+        test_bucket,
+        output_dir_for_set_number_spritesheet
+        + small_spritesheet_file_prefix
+        + spritesheet_file_suffix,
+    )
+    _assert_file_in_bucket(
+        capsys,
+        test_bucket,
+        output_dir_for_set_number_spritesheet
+        + large_spritesheet_file_prefix
+        + spritesheet_file_suffix,
+    )
+
+    list_jobs.list_jobs(project_id, location)
+    out, _ = capsys.readouterr()
+    assert job_name in out
+
+    delete_job.delete_job(project_id, location, job_id)
+    out, _ = capsys.readouterr()
+    assert "Deleted job" in out
+
+
+def test_create_job_with_periodic_spritesheet(capsys, test_bucket):
+    create_job_with_periodic_images_spritesheet.create_job_with_periodic_images_spritesheet(
+        project_id,
+        location,
+        input_uri,
+        output_uri_for_periodic_spritesheet,
+    )
+    out, _ = capsys.readouterr()
+    job_name_prefix = f"projects/{project_number}/locations/{location}/jobs/"
+    assert job_name_prefix in out
+
+    str_slice = out.split("/")
+    job_id = str_slice[len(str_slice) - 1].rstrip("\n")
+    job_name = f"projects/{project_number}/locations/{location}/jobs/{job_id}"
+    assert job_name in out
+
+    get_job.get_job(project_id, location, job_id)
+    out, _ = capsys.readouterr()
+    assert (
+        job_name in out
+    )  # Get the job name so you can use it later to get the job and delete the job.
+
+    time.sleep(
+        30
+    )  # Transcoding jobs need time to complete. Once the job completes, check the job state.
+
+    _assert_job_state_succeeded(capsys, job_id)
+    _assert_file_in_bucket(
+        capsys,
+        test_bucket,
+        output_dir_for_periodic_spritesheet
+        + small_spritesheet_file_prefix
+        + spritesheet_file_suffix,
+    )
+    _assert_file_in_bucket(
+        capsys,
+        test_bucket,
+        output_dir_for_periodic_spritesheet
+        + large_spritesheet_file_prefix
+        + spritesheet_file_suffix,
+    )
 
     list_jobs.list_jobs(project_id, location)
     out, _ = capsys.readouterr()
@@ -244,7 +361,7 @@ def test_create_job_with_animated_overlay(capsys, test_bucket):
 
 # Retrying up to 10 mins.
 @backoff.on_exception(backoff.expo, AssertionError, max_time=600)
-def _get_job_state(capsys, job_id):
+def _assert_job_state_succeeded(capsys, job_id):
     try:
         get_job_state.get_job_state(project_id, location, job_id)
     except HttpError as err:
@@ -252,3 +369,8 @@ def _get_job_state(capsys, job_id):
 
     out, _ = capsys.readouterr()
     assert job_succeeded_state in out
+
+
+def _assert_file_in_bucket(capsys, test_bucket, directory_and_filename):
+    blob = test_bucket.blob(directory_and_filename)
+    assert blob.exists()
