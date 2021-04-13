90c273008d8a4a3f8d884bc9c38a346be3ffa43a
David Cavazos <dcavazos@google.com>
2021-04-13 15:32:58 -0700

    dataflow: update Dockerfile and modularize tests

---
+++ dataflow/flex-templates/streaming_beam/e2e_test.py
@@ -0,0 +1,112 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+import os
+import subprocess
+import time
+import uuid
+
+from google.cloud import bigquery
+import pytest
+
+from . import testing_utils
+
+
+SUFFIX = uuid.uuid4().hex[0:6]
+PROJECT = os.environ["GOOGLE_CLOUD_PROJECT"]
+BUCKET_NAME = f"flex-templates-streaming-beam-{SUFFIX}"
+BIGQUERY_DATASET = f"flex_templates_{SUFFIX}"
+BIGQUERY_TABLE = "streaming_beam"
+TOPIC = f"flex-templates-streaming-beam-{SUFFIX}"
+SUBSCRIPTION = TOPIC
+IMAGE_NAME = f"gcr.io/{PROJECT}/dataflow/flex-templates/streaming-beam-{SUFFIX}:latest"
+TEMPLATE_FILE = "template.json"
+REGION = "us-central1"
+
+
+@pytest.fixture(scope="session")
+def bucket_name() -> str:
+    return testing_utils.storage_bucket(BUCKET_NAME)
+
+
+@pytest.fixture(scope="session")
+def topic_path() -> str:
+    return testing_utils.pubsub_topic(PROJECT, TOPIC)
+
+
+@pytest.fixture(scope="session")
+def subscription_path(topic_path: str) -> str:
+    return testing_utils.pubsub_subscription(PROJECT, topic_path, SUBSCRIPTION)
+
+
+@pytest.fixture(scope="session")
+def bigquery_dataset() -> str:
+    return testing_utils.bigquery_dataset(PROJECT, BIGQUERY_DATASET)
+
+
+@pytest.fixture(scope="session")
+def publisher(topic_path: str) -> bool:
+    return testing_utils.pubsub_publisher(topic_path)
+
+
+@pytest.fixture(scope="session")
+def template_image() -> str:
+    return testing_utils.container_image(PROJECT, IMAGE_NAME)
+
+
+@pytest.fixture(scope="session")
+def template_path(bucket_name: str, template_image: str) -> str:
+    return testing_utils.dataflow_flex_template_build(
+        bucket_name=bucket_name,
+        template_file=TEMPLATE_FILE,
+        template_image=template_image,
+        metadata_file="metadata.json",
+    )
+
+
+def test_run_template(
+    publisher: str,
+    bucket_name: str,
+    template_path: str,
+    dataset: str,
+    subscription_path: str,
+) -> None:
+
+    job_name = f"flex-templates-streaming-beam-{SUFFIX}"
+    subprocess.call(
+        [
+            "gcloud",
+            "dataflow",
+            "flex-template",
+            "run",
+            job_name,
+            f"--template-file-gcs-location={template_path}",
+            f"--temp_location=gs://{bucket_name}/temp",
+            f"--parameters=input_subscription={subscription_path}",
+            f"--parameters=output_table={dataset}.{BIGQUERY_TABLE}",
+            f"--region={REGION}",
+        ],
+        check=True,
+    )
+
+    # Wait for 10 minutes, and then cancel the job.
+    time.sleep(10 * 60)
+    testing_utils.dataflow_jobs_cancel(PROJECT, job_name)
+
+    # Check for output data in BigQuery.
+    bigquery_client = bigquery.Client()
+    query = f"SELECT * FROM {PROJECT}.{BIGQUERY_DATASET}.{BIGQUERY_TABLE}"
+    query_job = bigquery_client.query(query)
+    rows = query_job.result()
+    assert rows.total_rows > 0
+    for row in rows:
+        assert row["score"] == 1
---
+++ dataflow/testing_utils.py
@@ -0,0 +1,210 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+from datetime import time
+import itertools
+import subprocess
+import multiprocessing as mp
+from typing import Callable, List, Optional
+
+import backoff
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.cloud import bigquery
+from google.cloud import pubsub
+from google.cloud import storage
+
+dataflow = build("dataflow", "v1b3")
+
+
+RETRY_MAX_TIME = 5 * 60  # 5 minutes in seconds
+
+
+def storage_bucket(bucket_name: str) -> str:
+    storage_client = storage.Client()
+    bucket = storage_client.create_bucket(bucket_name)
+
+    print(f"storage_bucket: {repr(bucket_name)}")
+    yield bucket_name
+
+    bucket.delete(force=True)
+
+
+def bigquery_dataset(project: str, dataset_name: str) -> str:
+    bigquery_client = bigquery.Client()
+    dataset = bigquery.Dataset(f"{project}.{dataset_name}")
+    dataset = bigquery_client.create_dataset(dataset)
+
+    print(f"bigquery_dataset: {dataset.full_dataset_id}")
+    yield dataset.full_dataset_id
+
+    bigquery_client.delete_dataset(dataset.full_dataset_id, delete_contents=True)
+
+
+def pubsub_topic(project: str, topic_name: str) -> str:
+    publisher_client = pubsub.PublisherClient()
+    topic_path = publisher_client.topic_path(project, topic_name)
+    topic = publisher_client.create_topic(topic_path)
+
+    print(f"pubsub_topic: {repr(topic.name)}")
+    yield topic.name
+
+    # Due to the pinned library dependencies in apache-beam, client
+    # library throws an error upon deletion.
+    # We use gcloud for a workaround. See also:
+    # https://github.com/GoogleCloudPlatform/python-docs-samples/issues/4492
+    subprocess.check_call(
+        ["gcloud", "pubsub", "--project", project, "topics", "delete", topic],
+        check=True,
+    )
+
+
+def pubsub_subscription(project: str, topic_path: str, subscription_name: str) -> str:
+    subscriber = pubsub.SubscriberClient()
+    subscription_path = subscriber.subscription_path(project, subscription_name)
+    subscription = subscriber.create_subscription(subscription_path, topic_path)
+
+    print(f"pubsub_subscription: {repr(subscription.name)}")
+    yield subscription.name
+
+    # Due to the pinned library dependencies in apache-beam, client
+    # library throws an error upon deletion.
+    # We use gcloud for a workaround. See also:
+    # https://github.com/GoogleCloudPlatform/python-docs-samples/issues/4492
+    subprocess.check_call(
+        [
+            "gcloud",
+            "pubsub",
+            "--project",
+            project,
+            "subscriptions",
+            "delete",
+            subscription_name,
+        ],
+        check=True,
+    )
+
+
+def pubsub_publisher(topic_path: str, new_msg: Callable[[int], str]) -> bool:
+    def _infinite_publish_job() -> None:
+        publisher_client = pubsub.PublisherClient()
+        for i in itertools.count():
+            publisher_client.publish(topic_path, new_msg(i).encode("utf-8")).result()
+            time.sleep(1)
+
+    # Start a subprocess in the background to do the publishing.
+    p = mp.Process(target=_infinite_publish_job)
+    p.start()
+
+    yield p.is_alive()
+
+    # For cleanup, terminate the background process.
+    p.join(timeout=0)
+    p.terminate()
+
+
+def container_image(project: str, image_name: str) -> str:
+    subprocess.run(["gcloud", "auth", "configure-docker"], check=True)
+    subprocess.run(
+        [
+            "gcloud",
+            "builds",
+            "submit",
+            f"--project={project}",
+            f"--tag={image_name}",
+            ".",
+        ],
+        check=True,
+    )
+
+    yield image_name
+
+    subprocess.run(
+        [
+            "gcloud",
+            "container",
+            "images",
+            "delete",
+            image_name,
+            f"--project={project}",
+            "--quiet",
+        ],
+        check=True,
+    )
+
+
+def dataflow_job_id_from_job_name(project: str, job_name: str) -> Optional[str]:
+    # Only return the 50 most recent results - our job is likely to be in here.
+    # If the job is not found, first try increasing this number.
+    # For more info see:
+    #   https://cloud.google.com/dataflow/docs/reference/rest/v1b3/projects.jobs/list
+    jobs_request = (
+        dataflow.projects()
+        .jobs()
+        .list(
+            projectId=project,
+            filter="ACTIVE",
+            pageSize=50,
+        )
+    )
+    response = jobs_request.execute()
+
+    # Search for the job in the list that has our name (names are unique)
+    for job in response["jobs"]:
+        if job["name"] == job_name:
+            return job["id"]
+    return None
+
+
+@backoff.on_exception(backoff.expo, HttpError, max_time=RETRY_MAX_TIME)
+def dataflow_jobs_cancel(project: str, job_name: str) -> None:
+    # To cancel a dataflow job, we need its ID, not its name
+    job_id = dataflow_job_id_from_job_name(project, job_name)
+
+    if job_id is not None:
+        # Cancel the Dataflow job if it exists.
+        # If it doesn't, job_id will be equal to None.
+        # For more info, see:
+        #   https://cloud.google.com/dataflow/docs/reference/rest/v1b3/projects.jobs/update
+        request = (
+            dataflow.projects()
+            .jobs()
+            .update(
+                projectId=project,
+                jobId=job_id,
+                body={"requestedState": "JOB_STATE_CANCELLED"},
+            )
+        )
+        request.execute()
+
+
+def dataflow_flex_template_build(
+    bucket_name: str, template_file: str, template_image: str, metadata_file: str
+) -> str:
+    subprocess.call(
+        [
+            "gcloud",
+            "dataflow",
+            "flex-template",
+            "build",
+            f"gs://{bucket_name}/{template_file}",
+            f"--image={template_image}",
+            "--sdk-language=PYTHON",
+            f"--metadata-file={metadata_file}",
+        ],
+        check=True,
+    )
+
+    yield f"gs://{bucket_name}/{template_file}"
+
+    storage_client = storage.Client()
+    storage_client.bucket(bucket_name).blob(template_file).delete()
