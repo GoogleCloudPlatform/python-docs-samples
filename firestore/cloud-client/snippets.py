7f58d0f0bed08a4c4f83a6ff22deed8d8da189f0
Juan Lara <1543140+jlara310@users.noreply.github.com>
2021-04-23 13:23:21 -0700

    Remove unnecessary order_by field in a query
    The start_at_name query has an extra order_by that causes an error.
    
    Removes the following error:
    
    ValueError: The "order by" field path 'state' is not present in the cursor data {'name': 'Springfield'}. All fields sent to ``order_by()`` must be present in the fields if passed to one of ``start_at()`` / ``start_after()`` / ``end_before()`` / ``end_at()`` to define a cursor.

--- firestore/cloud-client/snippets.py
+++ firestore/cloud-client/snippets.py
@@ -902,7 +902,6 @@ def cursor_multiple_conditions():
     start_at_name = (
         db.collection(u'cities')
         .order_by(u'name')
-        .order_by(u'state')
         .start_at({
             u'name': u'Springfield'
         })
