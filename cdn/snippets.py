0139e1b5850a16f9eee7081d4c08daa1cc51b6a0
Takashi Matsuo <tmatsuo@google.com>
2021-05-04 19:21:54 +0000

    fix lint

--- cdn/snippets.py
+++ cdn/snippets.py
@@ -29,6 +29,7 @@ import hmac
 
 from six.moves import urllib
 
+
 # [START sign_url]
 def sign_url(url, key_name, base64_key, expiration_time):
     """Gets the Signed URL string for the specified URL and configuration.
