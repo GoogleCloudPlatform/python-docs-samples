b061533bba5aae19dfef3b6aed017560f25e7f3e
Leah Cole <coleleah@google.com>
2021-05-07 13:31:16 -0700

    style change

--- noxfile_config.py
+++ noxfile_config.py
@@ -32,7 +32,9 @@ TEST_CONFIG_OVERRIDE = {
     # to use your own Cloud project.
     "gcloud_project_env": "GOOGLE_CLOUD_PROJECT",
     # 'gcloud_project_env': 'BUILD_SPECIFIC_GCLOUD_PROJECT',
-    # If you need to use a specific version of pip, change _pip_version_override to the string representation of the version number, for example, "20.2.4"
+    # If you need to use a specific version of pip,
+    # change pip_version_override to the string representation
+    # of the version number, for example, "20.2.4"
     "pip_version_override": None,
     # A dictionary you want to inject into your test. Don't put any
     # secrets here. These values will override predefined values.
