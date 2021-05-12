4c692571e54a9f3966648eeabed2d424b7c59900
Katie McLaughlin <katie@glasnt.com>
2021-05-12 16:13:19 +1000

    Update doc version links, remove older links

--- appengine/flexible/django_cloudsql/mysite/settings.py
+++ appengine/flexible/django_cloudsql/mysite/settings.py
@@ -150,7 +150,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.8/topics/i18n/
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
 
--- run/django/mysite/settings.py
+++ run/django/mysite/settings.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Google LLC
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -130,7 +130,6 @@ if not os.environ.get("K_SERVICE"):
 # [END cloudrun_django_dbconfig]
 
 # Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -149,7 +148,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
 
