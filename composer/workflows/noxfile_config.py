b1b4f21f423091c85ed0e844123e886604c72a4d
Leah Cole <coleleah@google.com>
2021-05-14 15:34:44 -0700

    add pip version override

--- composer/workflows/noxfile_config.py
+++ composer/workflows/noxfile_config.py
@@ -40,6 +40,10 @@ TEST_CONFIG_OVERRIDE = {
     # to use your own Cloud project.
     "gcloud_project_env": "GOOGLE_CLOUD_PROJECT",
     # 'gcloud_project_env': 'BUILD_SPECIFIC_GCLOUD_PROJECT',
+    # If you need to use a specific version of pip,
+    # change pip_version_override to the string representation
+    # of the version number, for example, "20.2.4"
+    "pip_version_override": "20.2.4",
     # A dictionary you want to inject into your test. Don't put any
     # secrets here. These values will override predefined values.
     "envs": {"AIRFLOW_HOME": _tmpdir.name},
