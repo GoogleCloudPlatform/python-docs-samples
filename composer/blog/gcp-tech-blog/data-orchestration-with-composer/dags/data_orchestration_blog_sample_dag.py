e6e3a6f456819577ef33f79195f12f5dd27e1531
rachael-ds <deaconsmith@google.com>
2021-06-16 15:16:02 +0100

    formatting changes after running nox black

--- composer/blog/gcp-tech-blog/data-orchestration-with-composer/dags/data_orchestration_blog_sample_dag.py
+++ composer/blog/gcp-tech-blog/data-orchestration-with-composer/dags/data_orchestration_blog_sample_dag.py
@@ -24,8 +24,8 @@ from airflow.utils.dates import days_ago
 from airflow.utils.state import State
 
 # Sample data
-BUCKET_NAME = 'gs://cloud-samples-data/composer/data-orchestration-blog-example'
-DATA_FILE_NAME = 'bike_station_data.csv'
+BUCKET_NAME = "gs://cloud-samples-data/composer/data-orchestration-blog-example"
+DATA_FILE_NAME = "bike_station_data.csv"
 
 # Assumes existence of the following Airflow Variables
 PROJECT_ID = models.Variable.get("gcp_project")
@@ -36,7 +36,7 @@ TABLE = models.Variable.get("bigquery_table")
 # Slack error notification example taken from Kaxil Naik's blog on Slack Integration:
 # https://medium.com/datareply/integrating-slack-alerts-in-airflow-c9dcd155105
 def on_failure_callback(context):
-    ti = context.get('task_instance')
+    ti = context.get("task_instance")
     slack_msg = """
             :red_circle: Task Failed.
             *Task*: {task}
@@ -44,54 +44,63 @@ def on_failure_callback(context):
             *Execution Time*: {exec_date}
             *Log Url*: {log_url}
             """.format(
-            task=ti.task_id,
-            dag=ti.dag_id,
-            log_url=ti.log_url,
-            exec_date=context.get('execution_date'),
-        )
-    slack_webhook_token = BaseHook.get_connection('slack_connection').password
+        task=ti.task_id,
+        dag=ti.dag_id,
+        log_url=ti.log_url,
+        exec_date=context.get("execution_date"),
+    )
+    slack_webhook_token = BaseHook.get_connection("slack_connection").password
     slack_error = SlackWebhookOperator(
-        task_id='post_slack_error',
-        http_conn_id='slack_connection',
-        channel='#airflow-alerts',
+        task_id="post_slack_error",
+        http_conn_id="slack_connection",
+        channel="#airflow-alerts",
         webhook_token=slack_webhook_token,
-        message=slack_msg)
+        message=slack_msg,
+    )
     slack_error.execute(context)
 
 
 with models.DAG(
-    'dataflow_to_bq_workflow',
+    "dataflow_to_bq_workflow",
     schedule_interval=None,
     start_date=days_ago(1),
-    default_args={'on_failure_callback': on_failure_callback}
+    default_args={"on_failure_callback": on_failure_callback},
 ) as dag:
 
     validate_file_exists = GCSObjectExistenceSensor(
-        task_id="validate_file_exists",
-        bucket=BUCKET_NAME,
-        object=DATA_FILE_NAME
+        task_id="validate_file_exists", bucket=BUCKET_NAME, object=DATA_FILE_NAME
     )
 
-# See Launching Dataflow pipelines with Cloud Composer tutorial for further guidance
-# https://cloud.google.com/composer/docs/how-to/using/using-dataflow-template-operator
+    # See Launching Dataflow pipelines with Cloud Composer tutorial for further guidance
+    # https://cloud.google.com/composer/docs/how-to/using/using-dataflow-template-operator
     start_dataflow_job = DataflowTemplatedJobStartOperator(
         task_id="start-dataflow-template-job",
-        job_name='csv_to_bq_transform',
+        job_name="csv_to_bq_transform",
         template="gs://dataflow-templates/latest/GCS_Text_to_BigQuery",
         parameters={
             "javascriptTextTransformFunctionName": "transform",
-            "javascriptTextTransformGcsPath": "gs://{bucket}/udf_transform.js".format(bucket=BUCKET_NAME),
+            "javascriptTextTransformGcsPath": "gs://{bucket}/udf_transform.js".format(
+                bucket=BUCKET_NAME
+            ),
             "JSONPath": "gs://{bucket}/bq_schema.json".format(bucket=BUCKET_NAME),
-            "inputFilePattern": "gs://{bucket}/{filename}".format(bucket=BUCKET_NAME, filename=DATA_FILE_NAME),
-            "bigQueryLoadingTemporaryDirectory": "gs://{bucket}/tmp/".format(bucket=BUCKET_NAME),
-            "outputTable": "{project_id}:{dataset}.{table}".format(project_id=PROJECT_ID, dataset=DATASET, table=TABLE)
-        }
+            "inputFilePattern": "gs://{bucket}/{filename}".format(
+                bucket=BUCKET_NAME, filename=DATA_FILE_NAME
+            ),
+            "bigQueryLoadingTemporaryDirectory": "gs://{bucket}/tmp/".format(
+                bucket=BUCKET_NAME
+            ),
+            "outputTable": "{project_id}:{dataset}.{table}".format(
+                project_id=PROJECT_ID, dataset=DATASET, table=TABLE
+            ),
+        },
     )
 
     execute_bigquery_sql = BigQueryCheckOperator(
-        task_id='execute_bigquery_sql',
-        sql='SELECT COUNT(*) FROM `{project_id}.{dataset}.{table}` LIMIT 1000'.format(project_id=PROJECT_ID, dataset=DATASET, table=TABLE),
-        use_legacy_sql=False
+        task_id="execute_bigquery_sql",
+        sql="SELECT COUNT(*) FROM `{project_id}.{dataset}.{table}`".format(
+            project_id=PROJECT_ID, dataset=DATASET, table=TABLE
+        ),
+        use_legacy_sql=False,
     )
 
     validate_file_exists >> start_dataflow_job >> execute_bigquery_sql
--- composer/blog/gcp-tech-blog/data-orchestration-with-composer/dags/data_orchestration_blog_sample_dag_test.py
+++ composer/blog/gcp-tech-blog/data-orchestration-with-composer/dags/data_orchestration_blog_sample_dag_test.py
@@ -17,23 +17,24 @@ import internal_unit_testing
 import pytest
 
 # user should substitute their project ID
-PROJECT_ID = 'your-project-id'
-DATASET = 'your-bq-output-dataset'
-TABLE = 'your-bq-output-table'
+PROJECT_ID = "your-project-id"
+DATASET = "your-bq-output-dataset"
+TABLE = "your-bq-output-table"
 
 
 @pytest.fixture(autouse=True, scope="function")
 # The fixture `airflow_database` lives in ./conftest.py.
 def set_variables(airflow_database):
-    models.Variable.set('gcp_project', PROJECT_ID)
-    models.Variable.set('bigquery_dataset', DATASET)
-    models.Variable.set('bigquery_table', TABLE)
+    models.Variable.set("gcp_project", PROJECT_ID)
+    models.Variable.set("bigquery_dataset", DATASET)
+    models.Variable.set("bigquery_table", TABLE)
     yield
-    models.Variable.delete('gcp_project')
-    models.Variable.delete('bigquery_dataset')
-    models.Variable.delete('bigquery_table')
+    models.Variable.delete("gcp_project")
+    models.Variable.delete("bigquery_dataset")
+    models.Variable.delete("bigquery_table")
 
 
 def test_dag_import():
     from . import data_orchestration_blog_sample_dag
+
     internal_unit_testing.assert_has_valid_dag(data_orchestration_blog_sample_dag)
