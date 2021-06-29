9a4e9517196fef9639ac743ad1df2c09a8103125
Anthonios Partheniou <partheniou@google.com>
2021-06-29 19:58:23 -0400

    chore: clean up alert policies older than 1 hour in snippets test (#170)
    As a next step in troubleshooting the failure from #149, I'd like to clean up the existing alert policies in the test environment before running tests.

--- monitoring/snippets/v3/alerts-client/snippets_test.py
+++ monitoring/snippets/v3/alerts-client/snippets_test.py
@@ -14,6 +14,7 @@
 
 from __future__ import print_function
 
+from datetime import datetime
 import random
 import string
 import time
@@ -61,6 +62,17 @@ class PochanFixture:
         self.notification_channel_client = (
             monitoring_v3.NotificationChannelServiceClient()
         )
+
+        # delete all existing policies older than 1 hour prior to testing
+        for policy in self.alert_policy_client.list_alert_policies(name=self.project_name):
+            seconds_since_creation = datetime.timestamp(datetime.utcnow())-datetime.timestamp(policy.creation_record.mutate_time)
+            if seconds_since_creation > 3600:
+                try:
+                    self.alert_policy_client.delete_alert_policy(
+                        name=policy.name
+                    )
+                except NotFound:
+                    print("Ignored NotFound when deleting a policy.")
 
     def __enter__(self):
         @retry(
