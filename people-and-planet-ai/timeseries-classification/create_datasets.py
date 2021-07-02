7b604372aae911a3773e551c6637852dcba33f60
David Cavazos <dcavazos@google.com>
2021-07-02 15:46:45 -0700

    modularized functions

--- people-and-planet-ai/timeseries-classification/create_datasets.py
+++ people-and-planet-ai/timeseries-classification/create_datasets.py
@@ -12,97 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from datetime import datetime, timedelta
 import logging
-import os
 import random
-import time
-from typing import Dict, Iterable, Tuple
+from typing import Tuple
 
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
-import numpy as np
 import pandas as pd
 import tensorflow as tf
 
+import data_utils
 import trainer
-
-
-# Duration of a time step in the timeseries.
-# Training and prediction data must be resampled to this time step delta.
-TIME_STEP_DELTA = timedelta(hours=1)
-
-
-def to_unix_time(timestamp: datetime) -> int:
-    return time.mktime(timestamp.timetuple())
-
-
-def read_data(data_file: str) -> pd.DataFrame:
-    mmsi = os.path.splitext(os.path.basename(data_file))[0]
-    with tf.io.gfile.GFile(data_file, "rb") as f:
-        ship_time_steps = (
-            pd.DataFrame(np.load(f)["x"])
-            .assign(timestamp=lambda df: df["timestamp"].map(datetime.utcfromtimestamp))
-            .resample(TIME_STEP_DELTA, on="timestamp")
-            .mean()
-            .reset_index()
-            .interpolate()
-            .assign(
-                mmsi=lambda df: df["mmsi"].map(lambda _: int(mmsi)),
-                timestamp=lambda df: df["timestamp"].map(to_unix_time),
-            )
-        )
-        return ship_time_steps
-
-
-def read_labels(labels_file: str) -> pd.DataFrame:
-    with tf.io.gfile.GFile(labels_file, "r") as f:
-        return (
-            pd.read_csv(f, parse_dates=["start_time", "end_time"])
-            .astype({"mmsi": int})
-            .assign(
-                start_time=lambda df: df["start_time"].map(to_unix_time),
-                end_time=lambda df: df["end_time"].map(to_unix_time),
-            )
-        )
-
-
-def label_data(data: pd.DataFrame, labels: pd.DataFrame) -> pd.DataFrame:
-    data_with_labels = (
-        pd.merge_asof(
-            left=data,
-            right=labels,
-            left_on="timestamp",
-            right_on="start_time",
-            by="mmsi",
-        )
-        .query("timestamp <= end_time")
-        .drop(columns=["start_time", "end_time"])
-    )
-
-    labeled_data = data.assign(is_fishing=lambda _: np.nan)
-    labeled_data.update(data_with_labels)
-    return labeled_data.sort_values(["mmsi", "timestamp"]).drop(
-        columns=["mmsi", "timestamp"]
-    )
-
-
-def generate_training_points(data: pd.DataFrame) -> Iterable[Dict[str, np.ndarray]]:
-    padding = trainer.PADDING
-    midpoints = data[padding:-padding].query("is_fishing == is_fishing").index.tolist()
-    for midpoint in midpoints:
-        inputs = (
-            data.drop(columns=["distance_from_shore", "is_fishing"])
-            .loc[midpoint - padding : midpoint + padding]
-            .to_dict("list")
-        )
-        outputs = (
-            data[["is_fishing"]].loc[midpoint:midpoint].astype("int8").to_dict("list")
-        )
-        yield {
-            name: np.reshape(values, (len(values), 1))
-            for name, values in {**inputs, **outputs}.items()
-        }
 
 
 def run(
@@ -116,7 +36,7 @@ def run(
 
     labels = pd.concat(
         [
-            read_labels(filename)
+            data_utils.read_labels(filename)
             for filename in tf.io.gfile.glob(f"{raw_labels_dir}/*.csv")
         ]
     ).sort_values(by="start_time")
@@ -134,9 +54,9 @@ def run(
         | "Data files" >> beam.Create([f"{raw_data_dir}/*.npz"])
         | "Expand pattern" >> beam.FlatMap(tf.io.gfile.glob)
         | "Reshuffle files" >> beam.Reshuffle()
-        | "Read data" >> beam.Map(read_data)
-        | "Label data" >> beam.Map(label_data, labels)
-        | "Get training points" >> beam.FlatMap(generate_training_points)
+        | "Read data" >> beam.Map(data_utils.read_data)
+        | "Label data" >> beam.Map(data_utils.label_data, labels)
+        | "Get training points" >> beam.FlatMap(data_utils.generate_training_points)
         | "Serialize TFRecords" >> beam.Map(trainer.serialize)
         | "Train-eval split"
         >> beam.Partition(lambda x, n: random.choices([0, 1], train_eval_split)[0], 2)
