d1277f59817381a549c19cdd97ff3f6a5004ca78
David Cavazos <dcavazos@google.com>
2021-07-02 13:44:25 -0700

    configure via env vars

--- people-and-planet-ai/timeseries-classification/main.py
+++ people-and-planet-ai/timeseries-classification/main.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from datetime import datetime
+import os
+
 import flask
 
 import create_datasets
@@ -8,6 +24,19 @@ import train_model
 import predict
 
 app = flask.Flask(__name__)
+
+project = os.environ["PROJECT"]
+bucket = os.environ["BUCKET"]
+region = os.environ["REGION"]
+container_image = os.environ["CONTAINER_IMAGE"]
+storage_path = os.environ.get("STORAGE_PATH", "samples/global-fishing-watch")
+
+raw_data_dir = f"gs://{bucket}/{storage_path}/data"
+raw_labels_dir = f"gs://{bucket}/{storage_path}/labels"
+train_data_dir = f"gs://{bucket}/{storage_path}/datasets/train"
+eval_data_dir = f"gs://{bucket}/{storage_path}/datasets/eval"
+training_dir = f"gs://{bucket}/{storage_path}/training"
+temp_dir = f"gs://{bucket}/{storage_path}/temp"
 
 
 @app.route("/ping", methods=["POST"])
@@ -26,18 +55,18 @@ def run_create_datasets():
         args = flask.request.get_json()
 
         job_id = create_datasets.run(
-            raw_data_dir=args["raw_data_dir"],
-            raw_labels_dir=args["raw_labels_dir"],
-            train_data_dir=args["train_data_dir"],
-            eval_data_dir=args["eval_data_dir"],
+            raw_data_dir=raw_data_dir,
+            raw_labels_dir=raw_labels_dir,
+            train_data_dir=train_data_dir,
+            eval_data_dir=eval_data_dir,
             train_eval_split=args.get("train_eval_split", [80, 20]),
             # Apache Beam runner pipeline options.
             runner="DataflowRunner",
             job_name=f"global-fishing-watch-create-datasets-{datetime.now().strftime('%Y%m%d-%H%M%S')}",
-            project=args["project"],
-            region=args["region"],
-            sdk_container_image=args["container_image"],
-            temp_location=args.get("temp_location"),
+            project=project,
+            region=region,
+            sdk_container_image=container_image,
+            temp_location=temp_dir,
             experiments=["use_runner_v2"],
         )
 
@@ -55,12 +84,12 @@ def run_train_model():
         args = flask.request.get_json()
 
         job_id = train_model.run(
-            project=args["project"],
-            region=args["region"],
-            container_image=args["container_image"],
-            train_data_dir=args["train_data_dir"],
-            eval_data_dir=args["eval_data_dir"],
-            training_dir=args["training_dir"],
+            project=project,
+            region=region,
+            container_image=container_image,
+            train_data_dir=train_data_dir,
+            eval_data_dir=eval_data_dir,
+            training_dir=training_dir,
             train_steps=args.get("train_steps", 10000),
             eval_steps=args.get("eval_steps", 1000),
         )
@@ -79,7 +108,7 @@ def run_predict():
         args = flask.request.get_json()
 
         predictions = predict.run(
-            model_dir=args["model_dir"],
+            model_dir=args.get("model_dir", f"{training_dir}/model"),
             inputs=args["inputs"],
         )
 
