b8cfceeaf1decffdfa81cb9586d38b88cc161198
Katie McLaughlin <katie@glasnt.com>
2021-07-21 15:24:49 +1000

    Add new default auto field value from Django 3.2

--- kubernetes_engine/django_tutorial/mysite/settings.py
+++ kubernetes_engine/django_tutorial/mysite/settings.py
@@ -113,3 +113,8 @@ STATIC_URL = '/static/'
 # [END gke_django_staticconfig]
 
 STATIC_ROOT = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
