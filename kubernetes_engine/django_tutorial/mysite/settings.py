a7a7909fd15c487b69b6e01fbc15aa8acaecb07a
Katie McLaughlin <katie@glasnt.com>
2021-07-21 15:24:27 +1000

    Make database name a variable

--- kubernetes_engine/django_tutorial/mysite/settings.py
+++ kubernetes_engine/django_tutorial/mysite/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         # If you are using Cloud SQL for MySQL rather than PostgreSQL, set
         # 'ENGINE': 'django.db.backends.mysql' instead of the following.
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'polls',
+        'NAME': os.getenv('DATABASE_NAME'),
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': '127.0.0.1',
