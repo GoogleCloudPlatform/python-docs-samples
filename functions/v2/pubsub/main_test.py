b088102af3d2a71a111224ff23db0aab9e9cf27a
Charles Engelke <engelke@google.com>
2021-08-05 13:39:35 -0700

    Remove tests for removed main.py code

--- functions/v2/pubsub/main_test.py
+++ functions/v2/pubsub/main_test.py
@@ -13,33 +13,8 @@
 # limitations under the License.
 
 import base64
-import os
-
-from mock import MagicMock
 
 import main
-
-
-FUNCTIONS_TOPIC = os.getenv("FUNCTIONS_TOPIC")
-
-
-def test_functions_pubsub_publish_should_fail_without_params():
-    request = MagicMock()
-    request.get_json.return_value = {}
-    response = main.publish(request)
-
-    assert 'Missing "topic" and/or "message" parameter.' in response
-
-
-def test_functions_pubsub_publish_should_publish_message():
-    request = MagicMock()
-    request.get_json.return_value = {
-        "topic": FUNCTIONS_TOPIC, "message": "my_message"
-    }
-
-    response = main.publish(request)
-
-    assert response == "Message published."
 
 
 def test_functions_pubsub_subscribe_should_print_message(capsys):
