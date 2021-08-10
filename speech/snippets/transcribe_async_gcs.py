e302bf8453404203c82e67fdf0da5447e803bb03
nicain <nicain.seattle@gmail.com>
2021-08-10 12:02:58 -0700

    docs(samples): remove io dependency in transcribe samples (#223)
    * refactor: Refactor transcribe samples into two separate samples (local-file vs gcs) and remove io dependency. Refactor tests.
    
    * Removing CLI in samples and shebangs
    
    * leaving transcribe_async.py in (to be removed later), because speech_transcribe_async_gcs is a required tag.
    
    * Update year in license headers
    
    Co-authored-by: Nick Cain <nicholascain@google.com>

---
+++ speech/snippets/transcribe_async_gcs.py
@@ -0,0 +1,45 @@
+# Copyright 2021 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Google Cloud Speech-to-Text sample application using the gRPC for async
+batch processing.
+"""
+
+
+# [START speech_transcribe_async_gcs]
+def transcribe_gcs(gcs_uri):
+    """Asynchronously transcribes the audio file specified by the gcs_uri."""
+    from google.cloud import speech
+
+    client = speech.SpeechClient()
+
+    audio = speech.RecognitionAudio(uri=gcs_uri)
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=16000,
+        language_code="en-US",
+    )
+
+    operation = client.long_running_recognize(config=config, audio=audio)
+
+    print("Waiting for operation to complete...")
+    response = operation.result(timeout=90)
+
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        print(u"Transcript: {}".format(result.alternatives[0].transcript))
+        print("Confidence: {}".format(result.alternatives[0].confidence))
+# [END speech_transcribe_async_gcs]
