e0c0d1c5b826ed027c7b336108d8a494f6663880
David Cavazos <dcavazos@google.com>
2021-08-12 18:56:21 -0700

    add suffix to temporary config file

--- people-and-planet-ai/timeseries-classification/e2e_test.py
+++ people-and-planet-ai/timeseries-classification/e2e_test.py
@@ -106,7 +106,7 @@ def raw_labels_dir(bucket_name: str) -> str:
 def container_image(bucket_name: str) -> str:
     # https://cloud.google.com/sdk/gcloud/reference/builds/submit
     container_image = f"gcr.io/{PROJECT}/{NAME}:{UUID}"
-    with tempfile.NamedTemporaryFile("w") as f:
+    with tempfile.NamedTemporaryFile("w", suffix=".json") as f:
         json.dump(
             {
                 "steps": [
