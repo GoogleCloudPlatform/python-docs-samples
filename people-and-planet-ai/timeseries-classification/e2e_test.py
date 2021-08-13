48c5bfca5e619f0ee79d7c0c4915e0a9ae1f6594
David Cavazos <dcavazos@google.com>
2021-08-13 09:23:42 -0700

    remove unused imports

--- people-and-planet-ai/timeseries-classification/e2e_test.py
+++ people-and-planet-ai/timeseries-classification/e2e_test.py
@@ -12,12 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
 import logging
 import os
 import platform
 import subprocess
-import tempfile
 import time
 import uuid
 
