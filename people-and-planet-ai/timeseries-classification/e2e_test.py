664e22978cf5a3513aa624ac65e67cb884f5525d
David Cavazos <dcavazos@google.com>
2021-08-19 11:59:01 -0700

    remove expected failure

--- people-and-planet-ai/timeseries-classification/e2e_test.py
+++ people-and-planet-ai/timeseries-classification/e2e_test.py
@@ -309,5 +309,3 @@ def test_predict(service_url: str, access_token: str, train_model: str) -> None:
     # Check that we get non-empty predictions.
     assert "is_fishing" in predictions
     assert len(predictions["is_fishing"]) > 0
-
-    assert False, "Expected failure to debug logs"
