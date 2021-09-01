c1f8de0862dcc9204043de87c275c77eead1ff5f
Leah E. Cole <6719667+leahecole@users.noreply.github.com>
2021-09-01 10:54:14 -0700

    Update composer/functions/composer_storage_trigger.py

--- composer/functions/composer_storage_trigger.py
+++ composer/functions/composer_storage_trigger.py
@@ -21,6 +21,8 @@ import requests
 
 IAM_SCOPE = 'https://www.googleapis.com/auth/iam'
 OAUTH_TOKEN_URI = 'https://www.googleapis.com/oauth2/v4/token'
+# If you are using the stable API, set this value to False
+# For more info about Airflow APIs see https://cloud.google.com/composer/docs/access-airflow-api
 USE_EXPERIMENTAL_API = True
 
 
