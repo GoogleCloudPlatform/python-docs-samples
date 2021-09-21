e6b6bbf7d5d4177a22e48a3c22e3032a05783a33
David Cavazos <dcavazos@google.com>
2021-09-21 12:06:57 -0700

    add shape validation

--- people-and-planet-ai/timeseries-classification/data_utils.py
+++ people-and-planet-ai/timeseries-classification/data_utils.py
@@ -101,6 +101,7 @@ def generate_training_points(data: pd.DataFrame) -> Iterable[Dict[str, np.ndarra
             .loc[point_index - padding : point_index]
             .to_dict("list")
         )
+        assert len(inputs) == padding + 1
 
         # For the outputs, we only grab the label from the data point itself.
         outputs = (
@@ -109,6 +110,7 @@ def generate_training_points(data: pd.DataFrame) -> Iterable[Dict[str, np.ndarra
             .astype("int8")
             .to_dict("list")
         )
+        assert len(outputs) == 1
 
         yield {
             name: np.reshape(values, (len(values), 1))
