007a9ef99d28dc62e39ab5a5a0925427f6606de2
Nicholas Lee <csudcy@gmail.com>
2021-10-19 22:27:21 +0100

    chore(docs): remove get_default_handler call from setup docs (#421)

--- logging/samples/snippets/handler.py
+++ logging/samples/snippets/handler.py
@@ -28,7 +28,6 @@ def use_logging_handler():
     # you're running in and integrates the handler with the
     # Python logging module. By default this captures all logs
     # at INFO level and higher
-    client.get_default_handler()
     client.setup_logging()
     # [END logging_handler_setup]
 
