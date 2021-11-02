ff136dc32aba8b71f4b202efefec5ebff5458355
Leah Cole <coleleah@google.com>
2021-11-02 15:04:08 -0700

    Address import lint and blacken files

--- composer/rest/composer2/composer2_airflow_rest_api.py
+++ composer/rest/composer2/composer2_airflow_rest_api.py
@@ -22,81 +22,81 @@ Trigger a DAG in Cloud Composer 2 environment using the Airflow 2 stable REST AP
 import google.auth
 from google.auth.transport.requests import AuthorizedSession
 
-AUTH_SCOPE = 'https://www.googleapis.com/auth/cloud-platform'
+AUTH_SCOPE = "https://www.googleapis.com/auth/cloud-platform"
 
-def make_composer2_web_server_request(url, method='GET', **kwargs):
-  """
-  Make a request to Cloud Composer 2 environment's web server.
-  Args:
-    url: The URL to fetch.
-    method: The request method to use ('GET', 'OPTIONS', 'HEAD', 'POST', 'PUT',
-      'PATCH', 'DELETE')
-    **kwargs: Any of the parameters defined for the request function:
-              https://github.com/requests/requests/blob/master/requests/api.py
-                If no timeout is provided, it is set to 90 by default.
-  """
 
-  credentials, _ = google.auth.default(scopes=[AUTH_SCOPE])
-  authed_session = AuthorizedSession(credentials)
-  
-  # Set the default timeout, if missing
-  if 'timeout' not in kwargs:
-    kwargs['timeout'] = 90
-  
-  return authed_session.request(
-    method, 
-    url,
-    **kwargs)
+def make_composer2_web_server_request(url, method="GET", **kwargs):
+    """
+    Make a request to Cloud Composer 2 environment's web server.
+    Args:
+      url: The URL to fetch.
+      method: The request method to use ('GET', 'OPTIONS', 'HEAD', 'POST', 'PUT',
+        'PATCH', 'DELETE')
+      **kwargs: Any of the parameters defined for the request function:
+                https://github.com/requests/requests/blob/master/requests/api.py
+                  If no timeout is provided, it is set to 90 by default.
+    """
+
+    credentials, _ = google.auth.default(scopes=[AUTH_SCOPE])
+    authed_session = AuthorizedSession(credentials)
+
+    # Set the default timeout, if missing
+    if "timeout" not in kwargs:
+        kwargs["timeout"] = 90
+
+    return authed_session.request(method, url, **kwargs)
 
 
 def trigger_dag(web_server_url, dag_id, data):
-  """
-  Make a request to trigger a dag using the stable Airflow 2 REST API.
-  https://airflow.apache.org/docs/apache-airflow/stable/stable-rest-api-ref.html
+    """
+    Make a request to trigger a dag using the stable Airflow 2 REST API.
+    https://airflow.apache.org/docs/apache-airflow/stable/stable-rest-api-ref.html
 
-  Args:
-    web_server_url: The URL of the Airflow 2 web server.
-    dag_id: The DAG ID.
-    data: Additional configuration parameters for the DAG run (json).
-  """
+    Args:
+      web_server_url: The URL of the Airflow 2 web server.
+      dag_id: The DAG ID.
+      data: Additional configuration parameters for the DAG run (json).
+    """
 
-  endpoint = f'api/v1/dags/{dag_id}/dagRuns'
-  request_url = f'{web_server_url}/{endpoint}'
-  json_data = { 'conf': data }
+    endpoint = f"api/v1/dags/{dag_id}/dagRuns"
+    request_url = f"{web_server_url}/{endpoint}"
+    json_data = {"conf": data}
 
-  response = make_composer2_web_server_request(request_url, 
-    method='POST',
-    json=json_data
+    response = make_composer2_web_server_request(
+        request_url, method="POST", json=json_data
     )
 
-  if response.status_code == 403:
-    raise Exception(
-      'You do not have a permission to perform this operation. '
-      'Check Airflow RBAC roles for your account.'
-      f'{response.headers} / {response.text}')
-  elif response.status_code != 200:
-    raise Exception(
-      'Bad request:'
-      f'{response.status_code} / {response.headers} / {response.text}')
-  else:
-    return response.text
+    if response.status_code == 403:
+        raise Exception(
+            "You do not have a permission to perform this operation. "
+            "Check Airflow RBAC roles for your account."
+            f"{response.headers} / {response.text}"
+        )
+    elif response.status_code != 200:
+        raise Exception(
+            "Bad request:"
+            f"{response.status_code} / {response.headers} / {response.text}"
+        )
+    else:
+        return response.text
 
 
 if __name__ == "__main__":
 
-  #TODO(developer): uncomment and replace with your values
-  # dag_id="your-dag-id" # Replace with the ID of the DAG that you want to run.
-  # dag_config={"your-key": "your-value"} # Replace with configuration parameters for the DAG run.
-  # Replace web_server_url with the Airflow web server address. To obtain this
-  # URL, run the following command for your environment:
-  # gcloud composer environments describe example-environment \
-  #  --location=your-composer-region \
-  #  --format="value(config.airflowUri)"
-  # web_server_url="https://example-airflow-ui-url-dot-us-central1.composer.googleusercontent.com"
+    # TODO(developer): uncomment and replace with your values
+    # dag_id="your-dag-id" # Replace with the ID of the DAG that you want to run.
+    # dag_config={"your-key": "your-value"} # Replace with configuration parameters for the DAG run.
+    # Replace web_server_url with the Airflow web server address. To obtain this
+    # URL, run the following command for your environment:
+    # gcloud composer environments describe example-environment \
+    #  --location=your-composer-region \
+    #  --format="value(config.airflowUri)"
+    # web_server_url="https://example-airflow-ui-url-dot-us-central1.composer.googleusercontent.com"
 
-  response_text = trigger_dag(
-    web_server_url=web_server_url, dag_id=dag_id, data=dag_config)
+    response_text = trigger_dag(
+        web_server_url=web_server_url, dag_id=dag_id, data=dag_config
+    )
 
-  print(response_text)
+    print(response_text)
 
 # [END composer_2_trigger_dag]
--- composer/rest/composer2/composer2_airflow_rest_api_test.py
+++ composer/rest/composer2/composer2_airflow_rest_api_test.py
@@ -25,48 +25,72 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import os
-import pytest
+
 from unittest import mock
+
+import pytest
 
 import composer2_airflow_rest_api
 
-COMPOSER2_WEB_SERVER_URL = os.environ['COMPOSER2_WEB_SERVER_URL']
-DAG_CONFIG = { "test": "value" }
+COMPOSER2_WEB_SERVER_URL = os.environ["COMPOSER2_WEB_SERVER_URL"]
+DAG_CONFIG = {"test": "value"}
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def successful_response():
     response_mock = mock.Mock()
     response_mock.status_code = 200
     response_mock.text = '"state": "running"'
-    with mock.patch('composer2_airflow_rest_api.make_composer2_web_server_request', return_value=response_mock):
+    with mock.patch(
+        "composer2_airflow_rest_api.make_composer2_web_server_request",
+        return_value=response_mock,
+    ):
         yield
 
-@pytest.fixture(scope='function')
+
+@pytest.fixture(scope="function")
 def insufficient_permissions_response():
     response_mock = mock.Mock()
     response_mock.status_code = 403
-    response_mock.text = 'Mocked insufficient permissions'
-    with mock.patch('composer2_airflow_rest_api.make_composer2_web_server_request', return_value=response_mock):
+    response_mock.text = "Mocked insufficient permissions"
+    with mock.patch(
+        "composer2_airflow_rest_api.make_composer2_web_server_request",
+        return_value=response_mock,
+    ):
         yield
+
 
 def test_trigger_dag_insufficient_permissions(insufficient_permissions_response):
     with pytest.raises(Exception) as e:
         dag_config = DAG_CONFIG
-        print(composer2_airflow_rest_api.trigger_dag(COMPOSER2_WEB_SERVER_URL, 'airflow_monitoring', dag_config))
-    assert 'You do not have a permission to perform this operation.' in str(e)
+        print(
+            composer2_airflow_rest_api.trigger_dag(
+                COMPOSER2_WEB_SERVER_URL, "airflow_monitoring", dag_config
+            )
+        )
+    assert "You do not have a permission to perform this operation." in str(e)
 
 
 def test_trigger_dag_incorrect_environment():
     with pytest.raises(Exception) as e:
         dag_config = DAG_CONFIG
-        print(composer2_airflow_rest_api.trigger_dag('https://invalid-environment.composer.googleusercontent.com', 'airflow_monitoring', dag_config))
-    assert 'Bad request' in str(e)
+        print(
+            composer2_airflow_rest_api.trigger_dag(
+                "https://invalid-environment.composer.googleusercontent.com",
+                "airflow_monitoring",
+                dag_config,
+            )
+        )
+    assert "Bad request" in str(e)
+
 
 def test_trigger_dag(successful_response, capsys):
     dag_config = DAG_CONFIG
-    print(composer2_airflow_rest_api.trigger_dag(COMPOSER2_WEB_SERVER_URL, 'airflow_monitoring', dag_config))
+    print(
+        composer2_airflow_rest_api.trigger_dag(
+            COMPOSER2_WEB_SERVER_URL, "airflow_monitoring", dag_config
+        )
+    )
     out, _ = capsys.readouterr()
     assert '"state": "running"' in out
