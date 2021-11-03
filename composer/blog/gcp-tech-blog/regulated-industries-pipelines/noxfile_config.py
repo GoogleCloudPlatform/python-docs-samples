9bf9afa44e9508f77997a48168b1ede953506a54
Arjun Gill <arjunsgill89@gmail.com>
2021-11-03 17:22:06 +0000

    Update composer/blog/gcp-tech-blog/regulated-industries-pipelines/noxfile_config.py
    Co-authored-by: Leah E. Cole <6719667+leahecole@users.noreply.github.com>

--- composer/blog/gcp-tech-blog/regulated-industries-pipelines/noxfile_config.py
+++ composer/blog/gcp-tech-blog/regulated-industries-pipelines/noxfile_config.py
@@ -36,7 +36,6 @@ TEST_CONFIG_OVERRIDE = {
         "3.6",
         "3.7",
         "3.9",
-<<<<<<< HEAD
         "3.10",
     ],  # Composer w/ Airflow 2 only supports Python 3.8
     # Old samples are opted out of enforcing Python type hints
