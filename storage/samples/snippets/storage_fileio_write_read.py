704e22c9713950abba704313dbeb68328796b712
Aaron Gabriel Neyer <aarongabriel@google.com>
2021-11-09 11:33:37 -0700

    samples: Add GCS fileio samples (#645)
    * Add GCS file-like io samples
    
    * now with pandas!
    
    * change pandas version
    
    * add to readme
    
    * requests from andrew
    
    * lint
    
    * canonical command line args
    
    * Update storage_fileio_pandas.py
    
    * Update storage_fileio_write_read.py

---
+++ storage/samples/snippets/storage_fileio_write_read.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+# Copyright 2021 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+
+"""Sample that writes and read a blob in GCS using file-like IO
+"""
+
+# [START storage_fileio_write_read]
+from google.cloud import storage
+
+
+def write_read(bucket_name, blob_name):
+    """Write and read a blob from GCS using file-like IO"""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+
+    # The ID of your new GCS object
+    # blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    # Mode can be specified as wb/rb for bytes mode.
+    # See: https://docs.python.org/3/library/io.html
+    with blob.open("w") as f:
+        f.write("Hello world")
+
+    with blob.open("r") as f:
+        print(f.read())
+
+
+# [END storage_fileio_write_read]
+
+if __name__ == "__main__":
+    write_read(
+        bucket_name=sys.argv[1],
+        blob_name=sys.argv[2]
+    )
