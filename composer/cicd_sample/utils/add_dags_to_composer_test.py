44bb84b43ab65e58a8fd9f67d59dc9dd2e91323a
Leah Cole <coleleah@google.com>
2021-11-19 14:34:27 -0800

    cleanup comments

--- composer/cicd_sample/utils/add_dags_to_composer_test.py
+++ composer/cicd_sample/utils/add_dags_to_composer_test.py
@@ -1,28 +1,34 @@
 from unittest import mock
+from shutil import copytree
 
-import requests
+import os
 import pytest
 import tempfile
 import uuid
 import add_dags_to_composer
-from google.api_core.exceptions import Forbidden
 from google.cloud import storage
 
-from google.resumable_media.common import InvalidResponse
-# fixture of valid directory
 
-storage_client = storage.Client()
-# fixture of empty directory
+@pytest.fixture(scope="function")
+def dags_directory():
+    """Copies contents of dags/ folder to a temporary directory"""
+    temp_dir = tempfile.mkdtemp()
+    copytree("../dags/", f"{temp_dir}/", dirs_exist_ok=True)
+    yield temp_dir
+
+
 @pytest.fixture(scope="function")
 def empty_directory():
     temp_dir = tempfile.mkdtemp()
     yield temp_dir
 
-# test bucket used in lieu of an actual Composer environment
+
+# test bucket used in lieu of an actual Composer environment bucket
 @pytest.fixture(scope="module")
 def test_bucket():
     """Yields a bucket that is deleted after the test completes."""
-    
+    storage_client = storage.Client()
+
     bucket_name = f"temp-composer-cicd-test-{str(uuid.uuid4())}"
     bucket = storage_client.bucket(bucket_name)
 
@@ -34,27 +40,30 @@ def test_bucket():
     bucket.delete(force=True)
 
 
-# create dags list
-## test invalid directory
 def test_create_dags_list_invalid_directory():
     with pytest.raises(FileNotFoundError):
         (temp_dir, dags) = add_dags_to_composer._create_dags_list("this-directory-does-not-exist/")
 
-## test valid directory
-#TODO: implement
-def test_create_dags_list():
-    (temp_dir, dags) = add_dags_to_composer._create_dags_list("this-directory-does-not-exist/")
-    assert len(dags) > 0
 
-# upload
-## test no files in directory
+def test_create_dags_list_empty_directory(empty_directory):
+        (temp_dir, dags) = add_dags_to_composer._create_dags_list(empty_directory)
+        assert len(dags) == 0
+        assert len(os.listdir(temp_dir)) == 0
+
+
+def test_create_dags_list(dags_directory):
+    (temp_dir, dags) = add_dags_to_composer._create_dags_list(dags_directory)
+    assert len(dags) == 1
+    assert f"{temp_dir}/__init__.py" not in dags
+    assert f"{temp_dir}/example_dag.py" in dags
+
+
 def test_upload_dags_to_composer_no_files(capsys, empty_directory, test_bucket):
     add_dags_to_composer.upload_dags_to_composer(empty_directory, test_bucket)
     out, _ = capsys.readouterr()
     assert "No DAGs to upload." in out
 
 
-## test successful upload
 def test_upload_dags_to_composer( test_bucket, capsys):
     add_dags_to_composer.upload_dags_to_composer("../dags", test_bucket)
     out, _ = capsys.readouterr()
