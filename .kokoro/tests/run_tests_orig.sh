0644563587e605c1c82086f6a0dbf3b3e413b7e0
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-11-23 13:08:21 -0500

    chore: update reference of master branch to main (#7154)

--- .kokoro/tests/run_tests_orig.sh
+++ .kokoro/tests/run_tests_orig.sh
@@ -179,7 +179,7 @@ for file in **/requirements.txt; do
     # If REPORT_TO_BUILD_COP_BOT is set to "true", send the test log
     # to the FlakyBot.
     # See:
-    # https://github.com/googleapis/repo-automation-bots/tree/master/packages/flakybot.
+    # https://github.com/googleapis/repo-automation-bots/tree/main/packages/flakybot.
     if [[ "${REPORT_TO_BUILD_COP_BOT:-}" == "true" ]]; then
       chmod +x $KOKORO_GFILE_DIR/linux_amd64/flakybot
       $KOKORO_GFILE_DIR/linux_amd64/flakybot
--- video/cloud-client/README.rst
+++ video/cloud-client/README.rst
@@ -1,3 +1,3 @@
 These samples have been moved.
 
-https://github.com/googleapis/python-videointelligence/tree/master/samples
+https://github.com/googleapis/python-videointelligence/tree/main/samples
