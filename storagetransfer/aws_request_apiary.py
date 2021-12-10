31771d8a46398aef03e9da6a3541461d6d76ae13
Daniel Bankhead <dan@danielbankhead.com>
2021-12-10 12:18:56 -0800

    feat: Add samples for STS GAPIC client (#7091)
    * feat: Add samples for GAPIC client
    
    * refactor(storagetransfer): Update region tags
    
    * refactor(storagetransfer)!: Unify `storage-transfer` and `storage/transfer-service` as `storagetransfer`
    
    * refactor(storagetransfer): Update region tags for apiary samples
    
    Removes legacay `all` and `main` tags
    
    * style(storagetransfer): Rename new sample file names to match existing names
    
    Keeps things a bit more uniform
    
    * docs(storagetransfer): Update copyright info for migrated files
    
    * docs(storagetransfer): Update documentation for clarity
    
    * docs(storagetransfer): Make `header-check` happy
    
    * refactor(storagetransfer): Streamline quick start
    
    * refactor(storagetransfer): Updates for `Python Sample Authoring Guide` conformance
    
    * refactor(storagetransfer): Repair apiary import
    
    * chore(storagetransfer): Fix copyright year
    
    * test(storagetransfer): Disable `enforce_type_hints`
    
    This folder has old samples that we don't want to heavily modify
    
    * feat(storagetransfer): Add GAPIC retry sample
    
    * refactor(storagetransfer): cleanup Duration import
    
    * test(storagetransfer): Split up existing test file and add tests for GAPIC
    
    * fix(storagetransfer): Correctly pass `retry` parameter
    
    * feat(storagetransfer): Add create client samples
    
    * feat(storagetransfer): Add GAPIC sample for `storagetransfer_transfer_check`
    
    * refactor(storagetransfer): Get more info from `get_operation`
    
    * refactor(storagetransfer): Use conventional style for argument flags
    
    * test(storagetransfer): Import test reliability with `backoff`
    
    * test(storagetransfer): Cleanup transfer jobs post-test
    
    * test(storagetransfer): Add tests for `transfer_check`
    
    * refactor(storagetransfer): Add `description` to match apiary client
    
    * test(storagetransfer): Add tests for `nearline_request`
    
    * feat(storagetransfer): Improve `TransferOperation` samples by deserializing the payload
    
    * docs(storagetransfer): Clarifications
    
    * style(storagetransfer): line break (80 char limit)
    
    * test(storagetransfer): Source AWS secrets if available
    
    * docs(storagetransfer): Update README input
    
    Update input for consistency, although not necessarily used
    
    * test(storagetransfer): Prepare and use shared, dynamic setup
    
    Use shared config, dynamically prepare buckets
    
    * test(storagetransfer): Add simple tests for create client
    
    * refactor(storagetransfer): Add description to AWS request sample
    
    * test(storagetransfer): Move `job_description` to shared test utility
    
    * test(storagetransfer): Add tests for S3 -> GCS
    
    * fix(storagetransfer): Correct `storagetransfer_create_transfer_client` region tag
    
    * fix(storagetransfer): `BEGIN` -> `START`
    
    * fix(storagetransfer): `BEGIN` -> `START`
    
    * docs(storagetransfer): Typo
    
    * feat(storagetransfer): Add `cloud-storage-dpes` and `python-samples-owners` to CODEOWNERS
    
    * fix(storagetransfer): Correct shebang on apiary samples
    
    * docs: Add comment for AWS secrets
    
    * docs(storagetransfer): Fix product URL for README input
    
    * refactor(storagetransfer): Remove generic `Exception` catch
    
    * test(storagetransfer): Remove unnecessary `noxfile.py`
    
    * docs(storagetransfer): Improve `create_client` test documentation
    
    * docs(storagetransfer): Improve documentation on `aws-secrets.sh`
    
    * docs(storagetransfer): Documentation updates
    
    Co-authored-by: JesseLovelace <43148100+JesseLovelace@users.noreply.github.com>

--- storagetransfer/aws_request_apiary.py
+++ storagetransfer/aws_request_apiary.py
@@ -1,4 +1,6 @@
-# Copyright 2015, Google, Inc.
+#!/usr/bin/env python
+
+# Copyright 2015 Google LLC
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -11,10 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# [START all]
-
-"""Command-line sample that creates a daily transfer from a standard
-GCS bucket to a Nearline GCS bucket for objects untouched for 30 days.
+"""Command-line sample that creates a one-time transfer from Amazon S3 to
+Google Cloud Storage.
 
 This sample is used on this page:
 
@@ -30,10 +30,10 @@ import json
 import googleapiclient.discovery
 
 
-# [START main]
+# [START storagetransfer_transfer_from_aws_apiary]
 def main(description, project_id, start_date, start_time, source_bucket,
-         sink_bucket):
-    """Create a daily transfer from Standard to Nearline Storage class."""
+         access_key_id, secret_access_key, sink_bucket):
+    """Create a one-time transfer from Amazon S3 to Google Cloud Storage."""
     storagetransfer = googleapiclient.discovery.build('storagetransfer', 'v1')
 
     # Edit this template with desired parameters.
@@ -47,6 +47,11 @@ def main(description, project_id, start_date, start_time, source_bucket,
                 'month': start_date.month,
                 'year': start_date.year
             },
+            'scheduleEndDate': {
+                'day': start_date.day,
+                'month': start_date.month,
+                'year': start_date.year
+            },
             'startTimeOfDay': {
                 'hours': start_time.hour,
                 'minutes': start_time.minute,
@@ -54,17 +59,15 @@ def main(description, project_id, start_date, start_time, source_bucket,
             }
         },
         'transferSpec': {
-            'gcsDataSource': {
-                'bucketName': source_bucket
+            'awsS3DataSource': {
+                'bucketName': source_bucket,
+                'awsAccessKey': {
+                    'accessKeyId': access_key_id,
+                    'secretAccessKey': secret_access_key
+                }
             },
             'gcsDataSink': {
                 'bucketName': sink_bucket
-            },
-            'objectConditions': {
-                'minTimeElapsedSinceLastModification': '2592000s'  # 30 days
-            },
-            'transferOptions': {
-                'deleteObjectsFromSourceAfterTransfer': 'true'
             }
         }
     }
@@ -72,7 +75,7 @@ def main(description, project_id, start_date, start_time, source_bucket,
     result = storagetransfer.transferJobs().create(body=transfer_job).execute()
     print('Returned transferJob: {}'.format(
         json.dumps(result, indent=4)))
-# [END main]
+# [END storagetransfer_transfer_from_aws_apiary]
 
 
 if __name__ == '__main__':
@@ -83,8 +86,13 @@ if __name__ == '__main__':
     parser.add_argument('project_id', help='Your Google Cloud project ID.')
     parser.add_argument('start_date', help='Date YYYY/MM/DD.')
     parser.add_argument('start_time', help='UTC Time (24hr) HH:MM:SS.')
-    parser.add_argument('source_bucket', help='Standard GCS bucket name.')
-    parser.add_argument('sink_bucket', help='Nearline GCS bucket name.')
+    parser.add_argument('source_bucket', help='AWS source bucket name.')
+    parser.add_argument('access_key_id', help='Your AWS access key id.')
+    parser.add_argument(
+        'secret_access_key',
+        help='Your AWS secret access key.'
+    )
+    parser.add_argument('sink_bucket', help='GCS sink bucket name.')
 
     args = parser.parse_args()
     start_date = datetime.datetime.strptime(args.start_date, '%Y/%m/%d')
@@ -96,5 +104,6 @@ if __name__ == '__main__':
         start_date,
         start_time,
         args.source_bucket,
+        args.access_key_id,
+        args.secret_access_key,
         args.sink_bucket)
-# [END all]
