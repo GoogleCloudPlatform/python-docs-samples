961d9bd5d005d5d32f644d5f9b96944f5f81e18e
Leah Cole <coleleah@google.com>
2022-01-10 15:56:19 -0800

    also check for test_ files

--- noxfile-template.py
+++ noxfile-template.py
@@ -183,7 +183,7 @@ def _session_tests(
     session: nox.sessions.Session, post_install: Callable = None
 ) -> None:
     # check for presence of tests
-    test_list = glob.glob("*_test.py")
+    test_list = glob.glob("*_test.py") + glob.glob("test_*.py")
     if len(test_list) == 0:
         print("No tests found, skipping directory.")
     else:
