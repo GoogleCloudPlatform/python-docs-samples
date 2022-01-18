67d0da882c2b70b9b3bc35bf7c920c7dd20fcf18
Aaron Gabriel Neyer <breathtender@gmail.com>
2022-01-18 11:47:49 -0700

    feat: remove python 3.6 support (#689)
    * remove python 3.6 support
    
    * few more things
    
    * try deleting kokoro python3.6 samples

--- storage/samples/snippets/noxfile_config.py
+++ storage/samples/snippets/noxfile_config.py
@@ -72,7 +72,7 @@ def get_cloud_kms_key():
 
 TEST_CONFIG_OVERRIDE = {
     # You can opt out from the test for specific Python versions.
-    'ignored_versions': ["2.7"],
+    'ignored_versions': ["2.7", "3.6"],
 
     # An envvar key for determining the project id to use. Change it
     # to 'BUILD_SPECIFIC_GCLOUD_PROJECT' if you want to opt in using a
