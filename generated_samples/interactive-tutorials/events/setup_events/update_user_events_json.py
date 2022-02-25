9d360261b81cb470f78e2475ea810557f046a8f1
tetiana-karasova <62887365+tetiana-karasova@users.noreply.github.com>
2022-02-25 02:59:34 +0100

    docs(samples): add samples for events (#155)
    * feat: Retail. Products importing code samples
    
    * 🦉 Updates from OwlBot
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * lint fix
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * update copyright year
    
    * remove ClientOptions
    
    * update requirements
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * add requirement for pytest-xdist
    
    * test samples on all py3.7+ versions
    
    * add EVENTS_BUCKET_NAME
    
    * importing trsts fix
    
    * importing trsts fix
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * Update import_products_gcs_test.py
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * add google-cloud-testutils==1.3.1 to requirements-test.txt
    
    * rename setup->setup_events
    
    * fix tests
    
    * lint
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * fix flaky tests; address review feedback
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>
    Co-authored-by: Anthonios Partheniou <partheniou@google.com>
    Co-authored-by: Karl Weinmeister <11586922+kweinmeister@users.noreply.github.com>

---
+++ generated_samples/interactive-tutorials/events/setup_events/update_user_events_json.py
@@ -0,0 +1,24 @@
+import datetime
+import re
+
+"""Run the file to update the user_events.json file with more recent timestamp"""
+
+
+def update_events_timestamp(json_file):
+    # Get the yesterday's date
+    request_time = datetime.datetime.now() - datetime.timedelta(days=1)
+    day = request_time.date().strftime("%Y-%m-%d")
+    print(day)
+
+    # Read in the file
+    with open(json_file, 'r') as file:
+        filedata = file.read()
+
+    # Replace the target string  '"eventTime":"YYYY-mm-dd' with yesterday date
+    filedata = re.sub('\"eventTime\":\"([0-9]{4})-([0-9]{2})-([0-9]{2})',
+                      '\"eventTime\":\"' + day, filedata, flags=re.M)
+
+    # Write the file out again
+    with open(json_file, 'w') as file:
+        file.write(filedata)
+    print("The {} is updated".format(json_file))
