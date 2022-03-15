48986880a7fce0018bc6751c390dc8526c5d5fd2
Nikunj Khokhar <khokharnikunj8@gmail.com>
2022-03-15 14:23:13 +0100

    Update airflow_db_cleanup.py

--- composer/airflow_1_samples/airflow_db_cleanup.py
+++ composer/airflow_1_samples/airflow_db_cleanup.py
@@ -361,8 +361,7 @@ def cleanup_function(**context):
 
         if ENABLE_DELETE:
             logging.info("Performing Delete...")
-            if ("do_not_delete_by_dag_id" in context["params"] and 
-                    context["params"].get("do_not_delete_by_dag_id")):
+            if (context["params"].get("do_not_delete_by_dag_id")):
                 query.filter(age_check_column <= max_date,).delete(synchronize_session=False)
                 session.commit()
             else:
