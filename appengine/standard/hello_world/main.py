32fd99d3e326dc00d73b36a7e522119a891822f7
Ricardo Poot <92126133+14Ricardo@users.noreply.github.com>
2022-03-17 11:36:48 -0600

    Update main.py

--- appengine/standard/hello_world/main.py
+++ appengine/standard/hello_world/main.py
@@ -18,7 +18,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        self.response.write('Hello,cruel World!')
 
 
 app = webapp2.WSGIApplication([
