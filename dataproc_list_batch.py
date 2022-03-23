f7300f40e1f9162b04cf25289d03ec09d3626de8
Julian <juliansarajoseph@gmail.com>
2022-03-23 10:19:40 +0530

    Update dataproc_list_batch.py

--- dataproc_list_batch.py
+++ dataproc_list_batch.py
@@ -1,3 +1,24 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START composer_dataproc_dag]
+
+"""Example Airflow DAG that lists the Serverless Dataproc Batches that exist in a project
+This DAG relies on an Airflow variable
+https://airflow.apache.org/docs/apache-airflow/stable/concepts/variables.html
+* project_id - Google Cloud Project ID to use for the Cloud Dataproc Serverless.
+"""
 
 import datetime
 
@@ -21,3 +42,4 @@ with models.DAG(
         project_id=project_id,
         region="us-central1",
     )
+   
