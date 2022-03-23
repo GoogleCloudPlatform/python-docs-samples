441011e734e3765b73b04ee99de02e66e73fa041
Julian <juliansarajoseph@gmail.com>
2022-03-23 10:22:25 +0530

    Update dataproc_get_batch.py

--- dataproc_get_batch.py
+++ dataproc_get_batch.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# [START composer_dataproc_dag]
 
 """Example Airflow DAG that get a specific Serverless Dataproc Batch that exists in a project
 This DAG relies on an Airflow variable
