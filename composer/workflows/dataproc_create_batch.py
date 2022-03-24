72e94f7e5788d4abd2785316a58d5d7b012783a7
Julian <juliansarajoseph@gmail.com>
2022-03-24 14:48:20 +0530

    Add Custom container and metastore create batches

--- composer/workflows/dataproc_create_batch.py
+++ composer/workflows/dataproc_create_batch.py
@@ -34,10 +34,14 @@ from airflow.utils.dates import days_ago
 project_id = "{{ var.value.project_id }}"
 sparkml_file_location = "gs//bucket_name/natality_sparkml.py"  # for e.g.  "gs//my-bucket/natality_sparkml.py"
 # TODO: Add the location where your Spark ML python file is stored.
-#  Start a single node Dataproc Cluster for viewing Persistent History of Spark jobs
+# Start a single node Dataproc Cluster for viewing Persistent History of Spark jobs
 phs_cluster = "projects/project-ID/regions/region-name/clusters/cluster-name"
 # for e.g. projects/my-project/regions/my-region/clusters/my-cluster"
 spark_bigquery_jar_file = "gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar"
+metastore_service_location = "projects/project-id/locations/region-name/services/cluster-name"
+# for e.g. projects/my-project/locations/my-region/services/my-cluster
+custom_container = "us.gcr.io/PROJECT-ID/image-name"
+# for e.g. "us.gcr.io/my-project/quickstart-image",
 
 default_args = {
     # Tell airflow to start one day ago, so that it runs as soon as you upload it
@@ -71,4 +75,53 @@ with models.DAG(
         batch_id="batch-create-phs",
     )
 
-# [END composer_dataproc_create_batch]
+    # [END composer_dataproc_create_batch]
+
+    # [START composer_dataproc_create_metastore_batch]
+
+    create_batch_with_metastore = DataprocCreateBatchOperator(
+        task_id="dataproc-metastore",
+        project_id=project_id,
+        region="us-central1",
+        batch={
+            "pyspark_batch": {
+                "main_python_file_uri": sparkml_file_location,
+                "jar_file_uris": [spark_bigquery_jar_file],
+            },
+            "environment_config": {
+                "peripherals_config": {
+                    "metastore_service": metastore_service_location,
+                    "spark_history_server_config": {
+                        "dataproc_cluster": phs_cluster,
+                    },
+                 },
+            },
+        },
+        batch_id="dataproc-metastore",
+    )
+
+    # [END composer_dataproc_create_metastore_batch]
+    # [START composer_dataproc_create_custom_container]
+    create_batch_with_custom_container = DataprocCreateBatchOperator(
+        task_id="dataproc-custom-container",
+        project_id=project_id,
+        region="us-central1",
+        batch={
+            "pyspark_batch": {
+                "main_python_file_uri": sparkml_file_location,
+                "jar_file_uris": [spark_bigquery_jar_file],
+            },
+            "environment_config": {
+                "peripherals_config": {
+                     "spark_history_server_config": {
+                        "dataproc_cluster": phs_cluster,
+                     },
+                 },
+            },
+            "runtime_config": {
+                    "container_image": custom_container,
+                },
+        },
+        batch_id="batch-custom-container",
+    )
+    # [END composer_dataproc_create_custom_container_batch]
