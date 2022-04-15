b068ebd460e7fd1efc4860958b7c4210ef966f42
Lo Ferris <50979514+loferris@users.noreply.github.com>
2022-04-15 12:12:52 -0700

    docs(samples): create connection sample for MySQL instance (#147)
    * setup for cloudsql connection sample
    
    * fleshed out sample
    
    * complete sample
    
    * changing region tag pattern
    
    * adding new environment variables
    
    * adding missing env variable
    
    * fixing fixture with correct env variable
    
    * refactor
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * refactor test imports
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * kokoro lint errors
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * changing fixture use
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * refactoring variables
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * fixing imports
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * changing fixture syntax
    
    * fixing import
    
    * fixing typecasting
    
    * updating fixture usage
    
    * changing fixtures
    
    * refactor fixtures
    
    Co-authored-by: Anthonios Partheniou <partheniou@google.com>
    Co-authored-by: meredithslota <meredithslota@google.com>
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

---
+++ bigquery-connection/snippets/create_mysql_connection.py
@@ -0,0 +1,64 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START bigqueryconnection_connection_create]
+from google.cloud import bigquery_connection_v1 as bq_connection
+
+"""This sample shows how to create a BigQuery connection with a Cloud SQL for MySQL database"""
+
+
+def main() -> None:
+    # TODO(developer): Set all variables for your Cloud SQL for MySQL connection.
+    project_id = "your-project-id"  # set project_id
+    location = "US"  # set location
+    # See: https://cloud.google.com/bigquery/docs/locations for a list of
+    # available locations.
+    database = "my-database"  # set database name
+    username = "my-username"  # set database username
+    password = "my-password"  # set database password
+    cloud_sql_conn_name = ""  # set the name of your connection
+
+    cloud_sql_credential = bq_connection.CloudSqlCredential(
+        {
+            "username": username,
+            "password": password,
+        }
+    )
+    cloud_sql_properties = bq_connection.CloudSqlProperties(
+        {
+            "type_": bq_connection.CloudSqlProperties.DatabaseType.MYSQL,
+            "database": database,
+            "instance_id": cloud_sql_conn_name,
+            "credential": cloud_sql_credential,
+        }
+    )
+    create_mysql_connection(project_id, location, cloud_sql_properties)
+
+
+def create_mysql_connection(
+    project_id: str,
+    location: str,
+    cloud_sql_properties: bq_connection.CloudSqlProperties,
+) -> None:
+    connection = bq_connection.types.Connection({"cloud_sql": cloud_sql_properties})
+    client = bq_connection.ConnectionServiceClient()
+    parent = client.common_location_path(project_id, location)
+    request = bq_connection.CreateConnectionRequest(
+        {"parent": parent, "connection": connection}
+    )
+    response = client.create_connection(request)
+    print(f"Created connection successfully: {response.name}")
+
+
+# [END bigqueryconnection_connection_create]
