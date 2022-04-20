9093dfc3927e08f4b06f8f5e018ad2874e9773c8
David Cavazos <dcavazos@google.com>
2022-04-20 14:56:01 -0700

    address lint issues

--- people-and-planet-ai/land-cover-classification/train_model.py
+++ people-and-planet-ai/land-cover-classification/train_model.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import tensorflow as tf
 from typing import Dict, Tuple
+
+import tensorflow as tf
 
 INPUT_BANDS = [
     "B1",
