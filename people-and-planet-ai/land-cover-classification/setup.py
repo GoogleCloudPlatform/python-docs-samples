8fd5d35862b62b627dc6d46433d397512fa4cc04
David Cavazos <dcavazos@google.com>
2022-04-21 14:37:29 -0700

    simplified setup.py file

--- people-and-planet-ai/land-cover-classification/setup.py
+++ people-and-planet-ai/land-cover-classification/setup.py
@@ -21,12 +21,5 @@ setup(
     name="ppai-landcover-classification",
     url="https://github.com/GoogleCloudPlatform/python-docs-samples/tree/main/people-and-planet-ai/land-cover-classification",
     packages=find_packages(),
-    # install_requires=[
-    #     "apache-beam[gcp]==2.37.0",
-    #     "earthengine-api==0.1.305",
-    #     "google-cloud-aiplatform==1.12.0",
-    #     "python-snappy==0.6.1",
-    #     "tensorflow==2.8.0",
-    # ],
     install_requires=requirements,
 )
