9846a289f55e7d59650d82b24e182ffc6cb6b405
David Cavazos <dcavazos@google.com>
2022-04-21 14:28:27 -0700

    use requirements.txt in setup.py

--- people-and-planet-ai/land-cover-classification/setup.py
+++ people-and-planet-ai/land-cover-classification/setup.py
@@ -14,15 +14,19 @@
 
 from setuptools import find_packages, setup
 
+with open("requirements.txt") as f:
+    requirements = f.readlines()
+
 setup(
     name="ppai-landcover-classification",
     url="https://github.com/GoogleCloudPlatform/python-docs-samples/tree/main/people-and-planet-ai/land-cover-classification",
     packages=find_packages(),
-    install_requires=[
-        "apache-beam[gcp]==2.37.0",
-        "earthengine-api==0.1.305",
-        "google-cloud-aiplatform==1.12.0",
-        "python-snappy==0.6.1",
-        "tensorflow==2.8.0",
-    ],
+    # install_requires=[
+    #     "apache-beam[gcp]==2.37.0",
+    #     "earthengine-api==0.1.305",
+    #     "google-cloud-aiplatform==1.12.0",
+    #     "python-snappy==0.6.1",
+    #     "tensorflow==2.8.0",
+    # ],
+    install_requires=requirements,
 )
