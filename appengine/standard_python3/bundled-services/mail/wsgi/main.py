bde56afcb53d25eee45d31f54c73868c76748750
Jinglun Dong <1683035+jinglundong@users.noreply.github.com>
2022-05-13 02:47:10 +0000

    Add license headers to the code samples.

--- appengine/standard_python3/bundled-services/mail/wsgi/main.py
+++ appengine/standard_python3/bundled-services/mail/wsgi/main.py
@@ -1,3 +1,17 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from google.appengine.api import wrap_wsgi_app
 from google.appengine.api import mail
 import re, http
@@ -63,6 +77,3 @@ class WSGIApplication():
         return callable(environ, start_response)
 
 app = wrap_wsgi_app(WSGIApplication())
-
-
-
