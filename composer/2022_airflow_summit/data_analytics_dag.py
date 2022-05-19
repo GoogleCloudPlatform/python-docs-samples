f550efb3c3c8188da7cc8452fa5defaefd34db51
Leah Cole <coleleah@google.com>
2022-05-19 08:57:40 -0400

    revert using airflow variable

--- composer/2022_airflow_summit/data_analytics_dag.py
+++ composer/2022_airflow_summit/data_analytics_dag.py
@@ -116,7 +116,7 @@ with models.DAG(
                         "writeDisposition": "WRITE_APPEND",
                     }
                 },
-                location="{{var.value.bq_location}}",
+                location="US",
             )
 
         load_external_dataset >> bq_join_group >> create_batch
