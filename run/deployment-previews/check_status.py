a6138d019bc0c6ffcbac1184d5029eae6c114bb5
Elton Law <eltonlaw296@gmail.com>
2022-06-15 00:03:11 -0400

    fix: remove unused status_sha

--- run/deployment-previews/check_status.py
+++ run/deployment-previews/check_status.py
@@ -18,8 +18,6 @@ import os
 import re
 import sys
 from typing import Callable, List
-import hashlib
-import json
 
 import click
 import github
@@ -241,17 +239,6 @@ def set(
         return
     # [END_EXCLUDE]
 
-    # Create unique sha for specific set status run
-    status_sha = hashlib.sha256(json.dumps({
-        "dry_run": dry_run,
-        "project_id": project_id,
-        "region": region,
-        "service": service,
-        "repo_name": repo_name,
-        "commit_sha": commit_sha,
-        "pull_request": pull_request,
-        "revision_url": revision_url,
-    }, sort_keys=True).encode("utf-8")).hexdigest()
     commit.create_status(
         state="success",
         target_url=revision_url,
