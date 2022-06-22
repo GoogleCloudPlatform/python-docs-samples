0c446c0f35c2a5ab77d648aafe8cd92d4d3ad3f1
Julian <juliansarajoseph@gmail.com>
2022-06-22 20:00:08 +0530

    Update dataproc_batch_operators.py

--- composer/workflows/dataproc_batch_operators.py
+++ composer/workflows/dataproc_batch_operators.py
@@ -11,11 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""
-Check out the Dataproc Serverless workloads with Cloud Composer guide at
-https://cloud.google.com/composer/docs/composer-2/run-dataproc-workloads for more details.
-"""
+# Check out the Dataproc Serverless workloads with Cloud Composer guide at
+# https://cloud.google.com/composer/docs/composer-2/run-dataproc-workloads for more details.
 
 # [START composer_dataproc_create_batch]
 
