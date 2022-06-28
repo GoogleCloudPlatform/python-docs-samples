cbe544555bf41e540d0526765415d0a4ff8adf72
Kaiyang Yu <yukaiyang@google.com>
2022-06-28 18:38:43 +0000

    fixed s3togcsoperator_tutorial_test.py

--- composer/workflows/s3togcsoperator_tutorial_test.py
+++ composer/workflows/s3togcsoperator_tutorial_test.py
@@ -17,20 +17,12 @@ import internal_unit_testing
 
 def test_dag_import():
     """Test that the DAG file can be successfully imported.
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> 803b71b37 (fixed style and format problems according to Leah and Dan)
     This tests that the DAG can be parsed, but does not run it in an Airflow
     environment. This is a recommended confidence check by the official Airflow
     docs: https://airflow.incubator.apache.org/tutorial.html#testing
     """
 
-<<<<<<< HEAD
-    import s3togcsoperator_tutorial
-=======
     from . import s3togcsoperator_tutorial
->>>>>>> 803b71b37 (fixed style and format problems according to Leah and Dan)
 
     internal_unit_testing.assert_has_valid_dag(s3togcsoperator_tutorial)
