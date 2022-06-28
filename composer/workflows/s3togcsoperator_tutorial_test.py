25157c9d2ef34ac51310e7321a3d52a8ce9a5db2
kaiyang-code <57576013+kaiyang-code@users.noreply.github.com>
2022-06-28 15:28:17 +0000

    Update composer/workflows/s3togcsoperator_tutorial_test.py
    Inserted a blank line between the first line of comments and the following description comment
    
    Co-authored-by: Dan Lee <71398022+dandhlee@users.noreply.github.com>

--- composer/workflows/s3togcsoperator_tutorial_test.py
+++ composer/workflows/s3togcsoperator_tutorial_test.py
@@ -17,6 +17,7 @@ import internal_unit_testing
 
 def test_dag_import():
     """Test that the DAG file can be successfully imported.
+    
     This tests that the DAG can be parsed, but does not run it in an Airflow
     environment. This is a recommended confidence check by the official Airflow
     docs: https://airflow.incubator.apache.org/tutorial.html#testing
