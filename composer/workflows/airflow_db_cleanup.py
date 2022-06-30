8a44a70500879f12a9cd781934bf1321d1c44b02
Thinh-NguyenDuc <90733094+thinhnd2104@users.noreply.github.com>
2022-06-30 10:12:52 +0700

    Fix XCom clean up for Airflow >= 2.2.0

--- composer/workflows/airflow_db_cleanup.py
+++ composer/workflows/airflow_db_cleanup.py
@@ -118,7 +118,7 @@ DATABASE_OBJECTS = [{
     "keep_last_group_by": None
 }, {
     "airflow_db_model": XCom,
-    "age_check_column": XCom.execution_date,
+    "age_check_column": XCom.execution_date if AIRFLOW_VERSION < ['2', '2', '0'] else XCom.timestamp,
     "keep_last": False,
     "keep_last_filters": None,
     "keep_last_group_by": None
