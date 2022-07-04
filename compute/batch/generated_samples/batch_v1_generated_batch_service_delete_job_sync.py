a823703e632f0c682cee118389896a44322297f7
Anthonios Partheniou <partheniou@google.com>
2022-07-04 17:50:26 +0000

    feat: generate v1

---
+++ compute/batch/generated_samples/batch_v1_generated_batch_service_delete_job_sync.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Generated code. DO NOT EDIT!
+#
+# Snippet for DeleteJob
+# NOTE: This snippet has been automatically generated for illustrative purposes only.
+# It may require modifications to work in your environment.
+
+# To install the latest published package dependency, execute the following:
+#   python3 -m pip install google-cloud-batch
+
+
+# [START batch_v1_generated_BatchService_DeleteJob_sync]
+from google.cloud import batch_v1
+
+
+def sample_delete_job():
+    # Create a client
+    client = batch_v1.BatchServiceClient()
+
+    # Initialize request argument(s)
+    request = batch_v1.DeleteJobRequest(
+    )
+
+    # Make the request
+    operation = client.delete_job(request=request)
+
+    print("Waiting for operation to complete...")
+
+    response = operation.result()
+
+    # Handle the response
+    print(response)
+
+# [END batch_v1_generated_BatchService_DeleteJob_sync]
