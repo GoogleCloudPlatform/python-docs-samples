ceca6a597c5666672b60606a2b5a850048a4a2a2
Sydney Munro <sydmunro@google.com>
2022-07-27 10:15:09 -0700

    Fix build

--- storagetransfer/posix_download_test.py
+++ storagetransfer/posix_download_test.py
@@ -20,7 +20,7 @@ from google.cloud.storage import Bucket
 import posix_download
 
 
-@backoff.on_exception(backoff.expo, (RetryError, ServiceUnavailable), max_time=60)
+@backoff.on_exception(backoff.expo, (RetryError, ServiceUnavailable,), max_time=60)
 def test_posix_download(
         capsys, project_id: str, job_description_unique: str,
         agent_pool_name: str, posix_root_directory: str,
