8dad00e88bdda2fccff677a97c12e01000ae34b6
Maciej Strzelczyk <strzelczyk@google.com>
2022-08-03 15:23:39 +0200

    Changing debian from 9 to 11

--- compute/oslogin/service_account_ssh_test.py
+++ compute/oslogin/service_account_ssh_test.py
@@ -44,7 +44,7 @@ def test_main(capsys):
     project = os.environ['GOOGLE_CLOUD_PROJECT']
     test_id = 'oslogin-test-{id}'.format(id=str(random.randint(0, 1000000)))
     zone = 'us-east1-d'
-    image_family = 'projects/debian-cloud/global/images/family/debian-9'
+    image_family = 'projects/debian-cloud/global/images/family/debian-11'
     machine_type = 'zones/{zone}/machineTypes/f1-micro'.format(zone=zone)
     account_email = '{test_id}@{project}.iam.gserviceaccount.com'.format(
         test_id=test_id, project=project)
