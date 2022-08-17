5b2898f3c806a8096d42280e85c61fe1e1dc38db
Holt Skinner <13262395+holtskinner@users.noreply.github.com>
2022-08-17 10:45:08 -0500

    docs(samples): Added Human Review Request Sample (#357)
    - Illustrates how to call `review_document()` to send a processed document to Human in the Loop (HITL)
    
    Co-authored-by: Anthonios Partheniou <partheniou@google.com>

---
+++ documentai/snippets/review_document_sample_test.py
@@ -0,0 +1,39 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import os
+
+from samples.snippets import review_document_sample
+
+location = "us"
+project_id = os.environ["GOOGLE_CLOUD_PROJECT"]
+processor_id = "b7054d67d76c39f1"
+file_path = "resources/invoice.pdf"
+mime_type = "application/pdf"
+
+
+def test_process_documents(capsys):
+    review_document_sample.review_document_sample(
+        project_id=project_id,
+        location=location,
+        processor_id=processor_id,
+        file_path=file_path,
+        mime_type=mime_type,
+    )
+    out, _ = capsys.readouterr()
+
+    assert "projects/" in out
+    assert "locations/" in out
+    assert "operations/" in out
