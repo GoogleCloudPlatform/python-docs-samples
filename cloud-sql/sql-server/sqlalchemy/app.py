394a7a04ff49f057350a4340ebba9595c74a345c
Jonathan Simon <jsimonweb@gmail.com>
2022-09-22 13:08:49 -0700

    Update Cloud SQL samples for viewing app on linux GCE VM.

--- cloud-sql/sql-server/sqlalchemy/app.py
+++ cloud-sql/sql-server/sqlalchemy/app.py
@@ -159,4 +159,4 @@ def save_vote(db: sqlalchemy.engine.base.Engine, team: str) -> Response:
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
