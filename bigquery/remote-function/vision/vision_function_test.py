ee9fdd2c21f0af4f67a42c03258e28d3cd6036e8
autoerr <autoerr@google.com>
2022-10-17 14:02:24 -0700

    Nox lint

--- bigquery/remote-function/vision/vision_function_test.py
+++ bigquery/remote-function/vision/vision_function_test.py
@@ -12,10 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from unittest import mock
+
 import flask
 from google.cloud import vision_v1
 import pytest
-from unittest import mock
+
 import vision_function
 
 
