b2b4fc4c3a3d38d9cec9ee2d3e5b6e2bf92d9112
minherz <minherz@gmail.com>
2022-10-18 13:41:04 -0700

    feat: make trace demo application use OpenTelemetry
    remove trace demo application that using OpenCensus from the repo.
    make the removed demo's README file to reference to the active demo.
    modify the OpenTelemetry based demo so it has functional parity with
    the removed demo.

--- trace/cloud-trace-demo-app-opentelemetry/app/app.py
+++ trace/cloud-trace-demo-app-opentelemetry/app/app.py
@@ -11,9 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """
 A sample app demonstrating CloudTraceSpanExporter
 """
+
+import argparse
 import random
 import time
 
@@ -28,10 +31,12 @@ from opentelemetry.sdk.trace.export import SimpleSpanProcessor
 
 # [END trace_demo_imports]
 
+# [START trace_demo_middleware]
 app = flask.Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
+# [END trace_demo_middleware]
 
-
+# [START trace_demo_create_exporter]
 def configure_exporter(exporter):
     trace.set_tracer_provider(TracerProvider())
     trace.get_tracer_provider().add_span_processor(SimpleSpanProcessor(exporter))
@@ -40,20 +45,38 @@ def configure_exporter(exporter):
 
 configure_exporter(CloudTraceSpanExporter())
 tracer = trace.get_tracer(__name__)
-
+# [END trace_demo_create_exporter]
 
 @app.route("/")
 def template_test():
     # Sleep for a random time to imitate a random processing time
     time.sleep(random.uniform(0, 0.5))
 
-    with tracer.start_as_current_span("span1"):
-        with tracer.start_as_current_span("span2"):
-            with tracer.start_as_current_span("span3"):
-                print("Hello world from Cloud Trace Exporter!")
+    # Keyword that gets passed in will be concatenated to the final output string.
+    keyword = app.config['keyword']
+    # If there is no endpoint, return the output string.
+    url = app.config['endpoint']
+    if url == "":
+        return keyword, 200
 
-    return "Hello World"
+    # Endpoint is the next service to send string to.
+    data = {'body': keyword}
 
+    # [START trace_context_header]
+    with tracer.start_as_current_span("span_" + keyword):
+    response = requests.get(
+        url,
+        params=data
+    )
+    # [END trace_context_header]
+    return response.text + keyword
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--keyword",  default="", help="name of the service.")
+    parser.add_argument("--endpoint", default="", help="endpoint to dispatch appended string, simply respond if not set")
+
+    args = parser.parse_args()
+    app.config['keyword'] = args.keyword
+    app.config['endpoint'] = args.endpoint
     app.run(debug=True, host="0.0.0.0", port=8080)
