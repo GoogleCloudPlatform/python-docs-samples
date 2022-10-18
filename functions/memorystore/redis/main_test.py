19b72df9a3605f354417bf4da8f55b27417f33c8
ericschmidtatwork <shibumi@google.com>
2022-10-18 16:33:46 -0700

    Fix lint error

--- functions/memorystore/redis/main_test.py
+++ functions/memorystore/redis/main_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from unittest.mock import patch, Mock
+from unittest.mock import Mock, patch
 
 import flask
 import pytest
