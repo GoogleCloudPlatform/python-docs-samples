31ff224d5c08990757c69c589891f04e635412fe
Sampath M <sampathm@google.com>
2022-11-10 14:30:37 +0100

    Update copyright for all .py files in texttospeecch/snippets/

--- texttospeech/snippets/ssml_addresses_test.py
+++ texttospeech/snippets/ssml_addresses_test.py
@@ -1,4 +1,5 @@
-# Copyright 2019 Google LLC
+#!/usr/bin/env python
+# Copyright 2018 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# All Rights Reserved.
+
 import os
 
 from ssml_addresses import ssml_to_audio, text_to_ssml
