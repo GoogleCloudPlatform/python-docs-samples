449e5a9673d2e5eb1298a4ac50f86391bbea6553
Sampath M <sampathm@google.com>
2022-11-10 14:26:34 +0100

    Update CODEOWNERS file and update Copyright for audio_profile.py and audio_profile_test.py

--- texttospeech/snippets/audio_profile.py
+++ texttospeech/snippets/audio_profile.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-
-# Copyright 2018 Google LLC. All Rights Reserved.
+# Copyright 2018 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# All Rights Reserved.
 
 """Google Cloud Text-To-Speech API sample application for audio profile.
 
