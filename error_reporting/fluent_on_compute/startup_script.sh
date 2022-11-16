a1dc2479d97c33331bec18aa98be0c79111dc194
Sampath M <sampathm@google.com>
2022-11-16 23:12:26 +0100

    Update copyright headers for main.py main_test.py and startup_script.sh

--- error_reporting/fluent_on_compute/startup_script.sh
+++ error_reporting/fluent_on_compute/startup_script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 2016 Google Inc. All rights reserved.
+# Copyright 2016 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# All rights reserved.
 
 set -v
 
