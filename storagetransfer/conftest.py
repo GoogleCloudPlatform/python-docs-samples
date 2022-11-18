9d311a6beb7c0780502b0c6cc20a3ac22145a027
Daniel Bankhead <danielbankhead@google.com>
2022-11-17 20:57:10 -0800

    fix: create AWS bucket

--- storagetransfer/conftest.py
+++ storagetransfer/conftest.py
@@ -170,6 +170,8 @@ def aws_source_bucket(bucket_name: str):
     s3_client = boto3.client('s3', **aws_key_pair())
     s3_resource = boto3.resource('s3', **aws_key_pair())
 
+    s3_client.create_bucket(Bucket=bucket_name)
+
     yield bucket_name
 
     s3_resource.Bucket(bucket_name).objects.all().delete()
