9bdb8d8c8866f51c1dc4e69e65c4e626f1a02744
Maciej Strzelczyk <strzelczyk@google.com>
2022-11-18 14:51:33 +0100

    Fixing license headers

--- language/snippets/sentiment/sentiment_analysis_test.py
+++ language/snippets/sentiment/sentiment_analysis_test.py
@@ -1,4 +1,4 @@
-# Copyright 2016, Google, Inc.
+# Copyright 2016# [END job_discovery_batch_
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
