aee670538ce95c997cd379e1e234394f93d21dd6
Holt Skinner <holtskinner@google.com>
2022-12-15 12:16:28 -0600

    fix(samples): Adjusted Tests for Create/Delete/Cancel

--- enterpriseknowledgegraph/entity_reconciliation/delete_entity_reconciliation_job_sample_test.py
+++ enterpriseknowledgegraph/entity_reconciliation/delete_entity_reconciliation_job_sample_test.py
@@ -15,7 +15,7 @@
 
 import os
 
-from google.api_core.exceptions import FailedPrecondition, NotFound
+from google.api_core.exceptions import InvalidArgument, NotFound
 
 import delete_entity_reconciliation_job_sample
 
@@ -29,7 +29,7 @@ def test_delete_entity_reconciliation_job(capsys):
         delete_entity_reconciliation_job_sample.delete_entity_reconciliation_job_sample(
             project_id=project_id, location=location, job_id=job_id
         )
-    except (FailedPrecondition, NotFound) as e:
+    except (InvalidArgument, NotFound) as e:
         print(e.message)
 
     out, _ = capsys.readouterr()
