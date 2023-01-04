f63653a2b1622e3567874ce33b2a83f9339ce6dc
rbiegacz <36578200+rafalbiegacz@users.noreply.github.com>
2023-01-04 22:06:25 +0100

    adding [Start] tag as suggested by the reviewer
    Co-authored-by: Leah E. Cole <6719667+leahecole@users.noreply.github.com>

--- composer/workflows/c2-kubernetes_pod_operator.py
+++ composer/workflows/c2-kubernetes_pod_operator.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+# [START composer_2_kubernetespodoperator]
 """Example DAG using KubernetesPodOperator."""
 import datetime
 
