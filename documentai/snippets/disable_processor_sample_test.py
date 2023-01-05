6b5e99c9888e8eb824121d05bfd6e43db1c6f610
Holt Skinner <holtskinner@google.com>
2023-01-05 14:09:06 -0600

    Adjusted enable/disable processor tests for new output from API

--- documentai/snippets/disable_processor_sample_test.py
+++ documentai/snippets/disable_processor_sample_test.py
@@ -29,7 +29,7 @@ def test_disable_processor(capsys):
     )
     out, _ = capsys.readouterr()
 
-    assert "projects" in out or "DISABLED" in out
+    assert "projects" in out or "Processor" in out
 
     # Re-Enable Processor
     enable_processor_sample.enable_processor_sample(
