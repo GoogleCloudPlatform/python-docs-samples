66e069f916ffc0c3f19899f5083d32ebc514a8f3
Anthonios Partheniou <partheniou@google.com>
2023-01-09 16:32:42 -0500

    feat: add python 3.11 testing (#8872)
    * feat: add python 3.11 testing
    
    * update dependency scipy to 1.10.0
    
    * update dependency psycopg2-binary to 2.9.5
    
    * exclude python 3.11 for appengine/standard_python3/bigquery
    
    * exclude python 3.11 for tests that use google-cloud-bigquery==2.34.4
    
    * update copyright year

--- composer/workflows/noxfile_config.py
+++ composer/workflows/noxfile_config.py
@@ -30,7 +30,7 @@ _tmpdir = tempfile.TemporaryDirectory()
 
 TEST_CONFIG_OVERRIDE = {
     # You can opt out from the test for specific Python versions.
-    'ignored_versions': ["2.7", "3.6", "3.7", "3.9", "3.10"],  # Composer w/ Airflow 2 only supports Python 3.8
+    'ignored_versions': ["2.7", "3.6", "3.7", "3.9", "3.10", "3.11"],  # Composer w/ Airflow 2 only supports Python 3.8
     # Old samples are opted out of enforcing Python type hints
     # All new samples should feature them
     "enforce_type_hints": False,
