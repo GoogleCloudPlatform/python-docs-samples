deed0b741b5d8710615d03672b17a37f1e9bb502
minherz <minherz@gmail.com>
2023-01-10 14:17:34 -0800

    chore(fix): refactor dataflow
    fix package imports;
    add requirements.txt file to allow import google-cloud-logging
    that is not part of Beam SDK;
    fix logging_v2 client initialization;
    refactor some class, function and variable names;

--- logging/redaction/log_redaction.py
+++ logging/redaction/log_redaction.py
@@ -16,21 +16,20 @@
 import argparse
 import json
 import logging
+from typing import List
 
-from apache_beam import CombineFn, CombineGlobally, DoFn, FixedWindow, io, ParDo, WindowInto
+from apache_beam import CombineFn, CombineGlobally, DoFn, io, ParDo, Pipeline, WindowInto
 from apache_beam.error import PipelineError
 from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.transforms.window import FixedWindows
+
 from google.cloud import logging_v2
 
 
 class PayloadAsJson(DoFn):
   """Convert PubSub message payload to UTF-8 and return as Json"""
-
   def process(self, element):
-    element.decode("utf-8")
-    yield (
-        json.loads(element.decode("utf-8"))
-    )
+    yield json.loads(element.decode("utf-8"))
 
 
 class BatchPayloads(CombineFn):
@@ -56,48 +55,58 @@ class BatchPayloads(CombineFn):
 
 class IngestLogs(DoFn):
   """Ingest payloads into destination log"""
-
   def __init__(self, destination_log_name):
+    self.destination_log_name = destination_log_name
+    self.logger = None
+  
+  def _replace_log_name(self, entry):
+    """Peplace 'original' log name with destination log name"""
+    entry['logName'] = self.logger.name
+    return entry
+
+  def setup(self):
+    """Initialize logging client"""
+    if self.logger:
+      return
+
     logging_client = logging_v2.Client()
     if not logging_client:
       logging.error("Cannot create GCP Logging Client")
       raise PipelineError("Cannot create GCP Logging Client")
-    self.logger = gcp_logging_client.logger(destination_log_name)
+    self.logger = logging_client.logger(self.destination_log_name)
     if not self.logger:
       logging.error("Google client library cannot create Logger object")
       raise PipelineError("Google client library cannot create Logger object")
-  
+
   def process(self, element):
-    def replace_log_name(entry):
-      entrylog['logName'] = self.logger.name
-      return entry
-    
-    # replace 'original' log name with destination log name
-    logs = list(map(replace_log_name, element))
-    self.logger.client.logging_api.write_entries(logs)
+    if self.logger:
+      logs = list(map(self._replace_log_name, element))
+      self.logger.client.logging_api.write_entries(logs)
     yield logs
 
 
-def run(input_subscription: str,
-        output_log_name: str,
+def run(pubsub_subscription: str,
+        destination_log_name: str,
         window_size: float,
         pipeline_args: List[str]=None) -> None:
   """Runs Dataflow pipeline"""
+
   pipeline_options = PipelineOptions(
         pipeline_args, streaming=True, save_main_session=True
     )
-  pipeline = beam.Pipeline(options=pipeline_options)
+  pipeline = Pipeline(options=pipeline_options)
   _ = (
     pipeline
-    | "Read log entries from Pub/Sub" >> io.ReadFromPubSub(subscription=input_subscription)
+    | "Read log entries from Pub/Sub" >> io.ReadFromPubSub(subscription=pubsub_subscription)
     | "Convert log entry payload to Json" >> ParDo(PayloadAsJson())
     | "Aggregate payloads in fixed time intervals" >> WindowInto(FixedWindows(window_size))
     # Optimize Google API consumption and avoid possible throttling
     # by calling APIs for batched data and not per each element
-    | "Batch aggregated payloads" >> CombineGlobally(BatchPayloads())
+    | "Batch aggregated payloads" >> CombineGlobally(BatchPayloads()).without_defaults()
     # TODO: Add redaction logic here
-    | "Ingest to output log" >> ParDo(IngestLogs(output_log_name))
+    | "Ingest to output log" >> ParDo(IngestLogs(destination_log_name))
   )
+  pipeline.run()
 
 
 if __name__ == "__main__":
@@ -105,7 +114,7 @@ if __name__ == "__main__":
 
   parser = argparse.ArgumentParser()
   parser.add_argument(
-      "--input_subscription",
+      "--input_pubsub_subscription",
       help="The Cloud Pub/Sub subscription to read from in the format "
       '"projects/<PROJECT_ID>/subscription/<SUBSCRIPTION_ID>".',
   )
@@ -123,7 +132,7 @@ if __name__ == "__main__":
   known_args, pipeline_args = parser.parse_known_args()
 
   run(
-      known_args.input_subscription,
+      known_args.input_pubsub_subscription,
       known_args.output_log_name,
       known_args.window_size,
       pipeline_args,
