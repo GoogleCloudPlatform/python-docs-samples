49d3f21f8ac990a26701fb0479a9c584ff86d53e
Maciej Strzelczyk <strzelczyk@google.com>
2023-01-13 19:33:07 +0100

    docs(samples): Adding regional disk attach samples

---
+++ compute/client_library/ingredients/disks/attach_disk.py
@@ -0,0 +1,57 @@
+#  Copyright 2023 Google LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# This is an ingredient file. It is not meant to be run directly. Check the samples/snippets
+# folder for complete code samples that are ready to be used.
+# Disabling flake8 for the ingredients file, as it would fail F821 - undefined name check.
+# flake8: noqa
+
+from typing import NoReturn
+
+from google.cloud import compute_v1
+
+
+# <INGREDIENT attach_disk>
+def attach_disk(project_id: str, zone: str, instance_name: str, disk_link: str, mode: str = "READ_ONLY") -> NoReturn:
+    """
+    Attaches a disk resource to a specified compute instance. The disk might be zonal or regional.
+
+    Args:
+        project_id: project ID or project number of the Cloud project you want to use.
+        zone:name of the zone in which the instance you want to use resides.
+        instance_name: name of the compute instance you want to attach a disk to.
+        disk_link: full link to the disk you want to attach. This can be either regional or zonal disk.
+            Expected formats:
+                * https://www.googleapis.com/compute/v1/projects/[project]/zones/[zone]/disks/[disk_name]
+                * /projects/[project]/zones/[zone]/disks/[disk_name]
+                * /projects/[project]/regions/[region]/disks/[disk_name]
+        mode: Specifies in what mode the disk will be attached to the instance. Available options are `READ_ONLY`
+            and `READ_WRITE`. Disk in `READ_ONLY` mode can be attached to multiple instances at once.
+    """
+    instances_client = compute_v1.InstancesClient()
+
+    request = compute_v1.AttachDiskInstanceRequest()
+    request.project = project_id
+    request.zone = zone
+    request.instance = instance_name
+    request.attached_disk_resource = compute_v1.AttachedDisk()
+    request.attached_disk_resource.source = disk_link
+    request.attached_disk_resource.mode = mode
+
+    operation = instances_client.attach_disk(request)
+
+    wait_for_extended_operation(operation, "disk attachement")
+
+    return
+# </INGREDIENT>
--- compute/client_library/snippets/tests/test_disks.py
+++ compute/client_library/snippets/tests/test_disks.py
@@ -11,6 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+import time
 import uuid
 
 from google.api_core.exceptions import NotFound
@@ -18,8 +19,10 @@ import google.auth
 from google.cloud import kms_v1
 import pytest
 
+from ..disks.attach_disk import attach_disk
 from ..disks.clone_encrypted_disk_managed_key import \
     create_disk_from_kms_encrypted_disk
+from ..disks.create_empty_disk import create_empty_disk
 from ..disks.create_from_image import create_disk_from_image
 from ..disks.create_from_source import create_disk_from_disk
 from ..disks.create_kms_encrypted_disk import create_kms_encrypted_disk
@@ -28,11 +31,14 @@ from ..disks.list import list_disks
 from ..disks.regional_create_from_source import create_regional_disk
 from ..disks.regional_delete import delete_regional_disk
 from ..images.get import get_image_from_family
+from ..instances.create import create_instance, disk_from_image
+from ..instances.delete import delete_instance
+from ..instances.get import get_instance
 from ..snapshots.create import create_snapshot
 from ..snapshots.delete import delete_snapshot
 
 PROJECT = google.auth.default()[1]
-ZONE = 'europe-north1-c'
+ZONE = 'europe-central2-c'
 REGION = 'europe-central2'
 KMS_KEYRING_NAME = 'compute-test-keyring'
 KMS_KEY_NAME = 'compute-test-key'
@@ -111,6 +117,71 @@ def autodelete_src_disk(autodelete_disk_name):
     yield disk
 
 
+@pytest.fixture
+def autodelete_instance_name():
+    instance_name = "test-instance-" + uuid.uuid4().hex[:10]
+
+    yield instance_name
+
+    delete_instance(PROJECT, ZONE, instance_name)
+
+
+@pytest.fixture
+def autodelete_regional_blank_disk():
+    disk_name = 'regional-disk-' + uuid.uuid4().hex[:10]
+    replica_zones = [
+        f"projects/{PROJECT}/zones/{REGION}-c",
+        f"projects/{PROJECT}/zones/{REGION}-b",
+    ]
+    disk_type = f"regions/{REGION}/diskTypes/pd-balanced"
+
+    disk = create_regional_disk(PROJECT, REGION, replica_zones, disk_name, disk_type, 11)
+
+    yield disk
+
+    try:
+        # We wait for a while to let instances using this disk to be removed.
+        time.sleep(60)
+        delete_regional_disk(PROJECT, REGION, disk_name)
+    except NotFound:
+        # The disk was already deleted
+        pass
+
+
+@pytest.fixture
+def autodelete_blank_disk():
+    disk_name = 'regional-disk-' + uuid.uuid4().hex[:10]
+    disk_type = f"zones/{ZONE}/diskTypes/pd-standard"
+
+    disk = create_empty_disk(PROJECT, ZONE, disk_name, disk_type, 12)
+
+    yield disk
+
+    try:
+        # We wait for a while to let instances using this disk to be removed.
+        print('Waiting')
+        time.sleep(60)
+        print('Deleting')
+        delete_disk(PROJECT, ZONE, disk_name)
+    except NotFound:
+        # The disk was already deleted
+        pass
+
+
+@pytest.fixture
+def autodelete_compute_instance():
+    instance_name = "test-instance-" + uuid.uuid4().hex[:10]
+    newest_debian = get_image_from_family(project="ubuntu-os-cloud", family="ubuntu-2204-lts")
+    disk_type = f"zones/{ZONE}/diskTypes/pd-standard"
+    disks = [disk_from_image(disk_type, 100, True, newest_debian.self_link)]
+    instance = create_instance(
+        PROJECT, ZONE, instance_name, disks
+    )
+    yield instance
+
+    delete_instance(PROJECT, ZONE, instance_name)
+
+
 def test_disk_create_delete(autodelete_disk_name):
     disk_type = f"zones/{ZONE}/diskTypes/pd-standard"
     debian_image = get_image_from_family('debian-cloud', 'debian-11')
@@ -162,8 +233,8 @@ def test_create_and_delete_regional_disk(test_snapshot):
     disk_name = "test-rdisk-" + uuid.uuid4().hex[:10]
     disk_type = f"regions/{REGION}/diskTypes/pd-balanced"
     replica_zones = [
-        f"projects/diregapic-mestiv/zones/{REGION}-a",
-        f"projects/diregapic-mestiv/zones/{REGION}-b",
+        f"projects/{PROJECT}/zones/{REGION}-a",
+        f"projects/{PROJECT}/zones/{REGION}-b",
     ]
 
     try:
@@ -173,3 +244,16 @@ def test_create_and_delete_regional_disk(test_snapshot):
         assert regional_disk.type_.endswith(disk_type)
     finally:
         delete_regional_disk(PROJECT, REGION, disk_name)
+
+
+def test_disk_attachment(autodelete_blank_disk, autodelete_regional_blank_disk, autodelete_compute_instance):
+    instance = get_instance(PROJECT, ZONE, autodelete_compute_instance.name)
+
+    assert len(list(instance.disks)) == 1
+
+    attach_disk(PROJECT, ZONE, instance.name, autodelete_blank_disk.self_link)
+    attach_disk(PROJECT, ZONE, instance.name, autodelete_regional_blank_disk.self_link)
+
+    instance = get_instance(PROJECT, ZONE, autodelete_compute_instance.name)
+
+    assert len(list(instance.disks)) == 3
