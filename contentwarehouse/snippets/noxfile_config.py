db208c22d157e418abc2fe677a39f291f0009cbc
Holt Skinner <13262395+holtskinner@users.noreply.github.com>
2023-01-18 11:38:13 -0600

    Update noxfile_config.py
    Enabled Type Hints

--- contentwarehouse/snippets/noxfile_config.py
+++ contentwarehouse/snippets/noxfile_config.py
@@ -22,10 +22,10 @@
 
 TEST_CONFIG_OVERRIDE = {
     # You can opt out from the test for specific Python versions.
-    "ignored_versions": ["2.7", "3.6", "3.8", "3.9", "3.11"],
+    "ignored_versions": ["2.7", "3.6", "3.8", "3.9", "3.10", "3.11"],
     # Old samples are opted out of enforcing Python type hints
     # All new samples should feature them
-    "enforce_type_hints": False,
+    "enforce_type_hints": True,
     # An envvar key for determining the project id to use. Change it
     # to 'BUILD_SPECIFIC_GCLOUD_PROJECT' if you want to opt in using a
     # build specific Cloud project. You can also use your own string
