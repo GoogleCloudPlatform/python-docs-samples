f25b152bcc16103637393940e55357be8f5819da
Mario Torres Jr <105736410+Mattix23@users.noreply.github.com>
2023-01-18 13:53:49 -0600

    docs: revise delete label table code sample, add TODO to clean up sni… (#1466)
    * docs: revise delete label table code sample, add TODO to clean up snippets.py
    
    * changed name of test function to align with file name
    
    Co-authored-by: aribray <45905583+aribray@users.noreply.github.com>
    Co-authored-by: Anthonios Partheniou <partheniou@google.com>

---
+++ bigquery/snippets/delete_label_table_test.py
@@ -0,0 +1,34 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import typing
+
+import delete_label_table
+
+if typing.TYPE_CHECKING:
+    import pytest
+
+
+def test_delete_label_table(
+    capsys: "pytest.CaptureFixture[str]",
+    table_id: str,
+) -> None:
+
+    table = delete_label_table.delete_label_table(table_id, "color")
+
+    out, _ = capsys.readouterr()
+    assert "Deleted" in out
+    assert "color" in out
+    assert table_id in out
+    assert table.labels is None or "color" not in table.labels
