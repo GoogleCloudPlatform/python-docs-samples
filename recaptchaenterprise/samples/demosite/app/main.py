08d213622009fb94fd2d8ae003254bd61d344c5d
Rhy Moore <rhym@google.com>
2023-01-25 08:55:36 -0800

    feat: update app urls

--- recaptchaenterprise/samples/demosite/app/main.py
+++ recaptchaenterprise/samples/demosite/app/main.py
@@ -4,7 +4,10 @@ import urls
 
 app = Flask(__name__)
 
-app.add_url_rule(rule="/", methods=["GET"], view_func=urls.login)
+app.add_url_rule(rule="/", methods=["GET"], view_func=urls.home)
+app.add_url_rule(rule="/store", methods=["GET"], view_func=urls.store)
+app.add_url_rule(rule="/login", methods=["GET"], view_func=urls.login)
+app.add_url_rule(rule="/comment", methods=["GET"], view_func=urls.comment)
 app.add_url_rule(rule="/signup", methods=["GET"], view_func=urls.signup)
 app.add_url_rule(rule="/create_assessment", methods=["POST"], view_func=urls.create_assessment)
 
