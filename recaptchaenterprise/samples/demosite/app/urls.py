16fe9d0dc8c62f4a0bd792757adedf413b1aa377
Rhy Moore <git@morewry.me>
2023-01-26 06:17:55 -0800

    feat(demosite): add gamified demosite (#8857)
    * feat: update demosite pages and theme
    
    - Updates demosite pages to add examples
    - Updates visual design and layout of examples
    
    Still work in progress:
    
    - Visual design
    - Layout
    - Interaction design
    - Copy
    - Replace checkbox examples
    - Add game
    
    * feat: update app urls
    
    * add license headers
    
    Co-authored-by: Sita Lakshmi Sangameswaran <sitalakshmi@google.com>

--- recaptchaenterprise/samples/demosite/app/urls.py
+++ recaptchaenterprise/samples/demosite/app/urls.py
@@ -1,3 +1,17 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import json
 import os
 
@@ -5,6 +19,21 @@ from flask import render_template, request, Response
 
 from backend.recaptcha import execute_create_assessment
 
+def home() -> str:
+    # TODO: These env variables should be made available through Cloud Run's env vars.
+    context = {
+        "project_id": os.environ["GOOGLE_CLOUD_PROJECT"],
+        "site_key": os.environ["SITE_KEY"]
+    }
+    return render_template(template_name_or_list="home.html", context=context)
+
+def signup() -> str:
+    # TODO: These env variables should be made available through Cloud Run's env vars.
+    context = {
+        "project_id": os.environ["GOOGLE_CLOUD_PROJECT"],
+        "checkbox_site_key": os.environ["SITE_KEY"]
+    }
+    return render_template(template_name_or_list="signup.html", context=context)
 
 def login() -> str:
     # TODO: These env variables should be made available through Cloud Run's env vars.
@@ -14,14 +43,20 @@ def login() -> str:
     }
     return render_template(template_name_or_list="login.html", context=context)
 
-
-def signup() -> str:
+def store() -> str:
+    # TODO: These env variables should be made available through Cloud Run's env vars.
     context = {
         "project_id": os.environ["GOOGLE_CLOUD_PROJECT"],
-        "checkbox_site_key": os.environ["CHECKBOX_SITE_KEY"]
+        "site_key": os.environ["SITE_KEY"]
     }
-    return render_template(template_name_or_list="signup.html", context=context)
+    return render_template(template_name_or_list="store.html", context=context)
 
+def comment() -> str:
+    context = {
+        "project_id": os.environ["GOOGLE_CLOUD_PROJECT"],
+        "checkbox_site_key": os.environ["SITE_KEY"]
+    }
+    return render_template(template_name_or_list="comment.html", context=context)
 
 def create_assessment() -> Response:
     json_data = json.loads(request.data)
