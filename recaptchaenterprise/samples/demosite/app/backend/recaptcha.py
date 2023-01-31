64c116afae48dbcbc45106ceb03408c66bd9ac22
Sita Lakshmi Sangameswaran <sitalakshmi@google.com>
2023-01-31 20:12:30 +0530

    refactored to remove comments and print statements

--- recaptchaenterprise/samples/demosite/app/backend/recaptcha.py
+++ recaptchaenterprise/samples/demosite/app/backend/recaptcha.py
@@ -1,19 +1,31 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import json
 
 from flask import jsonify, Response
 
-from backend.create_assessment import get_result
+from backend.create_assessment import create_assessment
 
-
+# Wrapper method for create assessment calls.
 def execute_create_assessment(project_id, json_data: json) -> Response:
-    print("Inside the backend wrapper")
     threshold_score = 0.50
 
     try:
-        score, reasons = get_result(project_id=project_id,
+        score, reasons = create_assessment(project_id=project_id,
                                     recaptcha_site_key=json_data["recaptcha_cred"]["sitekey"],
                                     recaptcha_action=json_data["recaptcha_cred"]["action"], token=json_data["recaptcha_cred"]["token"])
-        print("Obtained the score from recaptcha")
         if score < threshold_score:
             verdict = "Not a human"
         else:
