6c676d95ec0ab057dc84d06928740be7780d0ada
Sita Lakshmi Sangameswaran <sitalakshmi@google.com>
2023-01-31 19:07:06 +0530

    moved context to global

--- recaptchaenterprise/samples/demosite/app/urls.py
+++ recaptchaenterprise/samples/demosite/app/urls.py
@@ -19,51 +19,35 @@ from flask import render_template, request, Response
 
 from backend.recaptcha import execute_create_assessment
 
+context = {
+    "project_id": os.environ["GOOGLE_CLOUD_PROJECT"],
+    "site_key": os.environ["SITE_KEY"]
+}
+
+
 def home() -> str:
-    # TODO: These env variables should be made available through Cloud Run's env vars.
-    context = {
-        "project_id": os.environ["GOOGLE_CLOUD_PROJECT"],
-        "site_key": os.environ["SITE_KEY"]
-    }
     return render_template(template_name_or_list="home.html", context=context)
 
+
 def signup() -> str:
-    # TODO: These env variables should be made available through Cloud Run's env vars.
-    context = {
-        "project_id": os.environ["GOOGLE_CLOUD_PROJECT"],
-        "site_key": os.environ["SITE_KEY"]
-    }
     return render_template(template_name_or_list="signup.html", context=context)
 
+
 def login() -> str:
-    # TODO: These env variables should be made available through Cloud Run's env vars.
-    context = {
-        "project_id": os.environ["GOOGLE_CLOUD_PROJECT"],
-        "site_key": os.environ["SITE_KEY"]
-    }
     return render_template(template_name_or_list="login.html", context=context)
 
+
 def store() -> str:
-    # TODO: These env variables should be made available through Cloud Run's env vars.
-    context = {
-        "project_id": os.environ["GOOGLE_CLOUD_PROJECT"],
-        "site_key": os.environ["SITE_KEY"]
-    }
     return render_template(template_name_or_list="store.html", context=context)
 
+
 def comment() -> str:
-    context = {
-        "project_id": os.environ["GOOGLE_CLOUD_PROJECT"],
-        "site_key": os.environ["SITE_KEY"]
-    }
     return render_template(template_name_or_list="comment.html", context=context)
 
+
 def game() -> str:
-    context = {
-        "project_id": os.environ["GOOGLE_CLOUD_PROJECT"],
-        "site_key": os.environ["SITE_KEY"]
-    }
     return render_template(template_name_or_list="game.html", context=context)
+
 
 def create_assessment() -> Response:
     json_data = json.loads(request.data)
