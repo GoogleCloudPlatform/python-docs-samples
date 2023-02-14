8e79e91b14dcf939cb41f0ab37f6034076fd100d
Maciej Strzelczyk <strzelczyk@google.com>
2023-02-14 19:41:31 +0100

    Changing region

--- compute/client_library/snippets/tests/test_disks.py
+++ compute/client_library/snippets/tests/test_disks.py
@@ -39,8 +39,8 @@ from ..snapshots.create import create_snapshot
 from ..snapshots.delete import delete_snapshot
 
 PROJECT = google.auth.default()[1]
-ZONE = 'europe-west1-c'
-REGION = 'europe-west1'
+ZONE = 'europe-west2-c'
+REGION = 'europe-west2'
 KMS_KEYRING_NAME = 'compute-test-keyring'
 KMS_KEY_NAME = 'compute-test-key'
 
