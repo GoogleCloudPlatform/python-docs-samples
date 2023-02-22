3e329c23edfc61df10a517d7b7e4cf000b8141b4
Sampath M <sampathm@google.com>
2023-02-22 11:08:23 +0100

    Update the dualtoken_test.py

--- media_cdn/dualtoken_test.py
+++ media_cdn/dualtoken_test.py
@@ -14,13 +14,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for snippets."""
+"""Tests for dualtoken."""
 
 import datetime
 
 import pytest
 
-import snippets
+import dualtoken
 
 
 startTime = datetime.datetime.strptime("2022-09-13T00:00:00Z", "%Y-%m-%dT%H:%M:%S%z")
@@ -57,7 +57,7 @@ with open("/tmp/shared.secret", "wb") as fp:
 
 def test_sign_token_for_ed25519_url_prefix(capsys: pytest.LogCaptureFixture) -> None:
     expected = "URLPrefix=aHR0cDovLzEwLjIwLjMwLjQwLw~Expires=1663070400~Signature=OQLXEjnApFGJaGZ_jvp2R7VY5q3ic-HT3igFpi9iPsJRXtQuvPF4cxZUT-rtCqzteXx3vSRhk09FxgDQauO_DA"
-    result = snippets.sign_token(
+    result = dualtoken.sign_token(
         base64_key=b"DJUcnLguVFKmVCFnWGubG1MZg7fWAnxacMjKDhVZMGI=",
         signature_algorithm="ed25519",
         expiration_time=expiresTime,
@@ -68,7 +68,7 @@ def test_sign_token_for_ed25519_url_prefix(capsys: pytest.LogCaptureFixture) -> 
 
 def test_sign_token_for_ed25519_path_glob(capsys: pytest.LogCaptureFixture) -> None:
     expected = "PathGlobs=/*~Expires=1663070400~Signature=9pBdD_6O6LB-4V67HZ_SOc2G_jIkSZ_tMsKnVqElmPlwKB_xDiW7DKAnv8L8CmweeZquaLFlnLogbMcIV8bNCQ"
-    result = snippets.sign_token(
+    result = dualtoken.sign_token(
         base64_key=b"DJUcnLguVFKmVCFnWGubG1MZg7fWAnxacMjKDhVZMGI=",
         signature_algorithm="ed25519",
         expiration_time=expiresTime,
@@ -79,7 +79,7 @@ def test_sign_token_for_ed25519_path_glob(capsys: pytest.LogCaptureFixture) -> N
 
 def test_sign_token_for_ed25519_full_path(capsys: pytest.LogCaptureFixture) -> None:
     expected = "FullPath~Expires=1663070400~Signature=X74OTNjtseIUmsab-YiOTZ8jyX_KG7v4YQWwcFpfFmjhzaX8NdweMc9Wglj8wxEsEW85g3_MBG3T9jzLZFQDCw"
-    result = snippets.sign_token(
+    result = dualtoken.sign_token(
         base64_key=b"DJUcnLguVFKmVCFnWGubG1MZg7fWAnxacMjKDhVZMGI=",
         signature_algorithm="ed25519",
         expiration_time=expiresTime,
@@ -90,7 +90,7 @@ def test_sign_token_for_ed25519_full_path(capsys: pytest.LogCaptureFixture) -> N
 
 def test_sign_token_for_sha1_url_prefix(capsys: pytest.LogCaptureFixture) -> None:
     expected = "URLPrefix=aHR0cDovLzEwLjIwLjMwLjQwLw~Expires=1663070400~hmac=6f5b4bb82536810d5ee111cba3e534d49c6ac3cb"
-    result = snippets.sign_token(
+    result = dualtoken.sign_token(
         base64_key=b"g_SlMILiIWKqsC6Z2L7gy0sReDOqtSrJrE7CXNr5Nl8=",
         signature_algorithm="sha1",
         expiration_time=expiresTime,
@@ -101,7 +101,7 @@ def test_sign_token_for_sha1_url_prefix(capsys: pytest.LogCaptureFixture) -> Non
 
 def test_sign_token_for_sha1_path_glob(capsys: pytest.LogCaptureFixture) -> None:
     expected = "PathGlobs=/*~Expires=1663070400~hmac=c1c446eea24faa31392519f975fea7eefb945625"
-    result = snippets.sign_token(
+    result = dualtoken.sign_token(
         base64_key=b"g_SlMILiIWKqsC6Z2L7gy0sReDOqtSrJrE7CXNr5Nl8=",
         signature_algorithm="sha1",
         expiration_time=expiresTime,
@@ -114,7 +114,7 @@ def test_sign_token_for_sha1_full_path(capsys: pytest.LogCaptureFixture) -> None
     expected = (
         "FullPath~Expires=1663070400~hmac=7af78177d6bc001d5626eefe387b1774a4a99ca2"
     )
-    result = snippets.sign_token(
+    result = dualtoken.sign_token(
         base64_key=b"g_SlMILiIWKqsC6Z2L7gy0sReDOqtSrJrE7CXNr5Nl8=",
         signature_algorithm="sha1",
         expiration_time=expiresTime,
@@ -125,7 +125,7 @@ def test_sign_token_for_sha1_full_path(capsys: pytest.LogCaptureFixture) -> None
 
 def test_sign_token_for_sha256_url_prefix(capsys: pytest.LogCaptureFixture) -> None:
     expected = "URLPrefix=aHR0cDovLzEwLjIwLjMwLjQwLw~Expires=1663070400~hmac=409722313cf6d987da44bb360e60dccc3d79764520fc5e3b57654e1d4d2c862e"
-    result = snippets.sign_token(
+    result = dualtoken.sign_token(
         base64_key=b"g_SlMILiIWKqsC6Z2L7gy0sReDOqtSrJrE7CXNr5Nl8=",
         signature_algorithm="sha256",
         expiration_time=expiresTime,
@@ -136,7 +136,7 @@ def test_sign_token_for_sha256_url_prefix(capsys: pytest.LogCaptureFixture) -> N
 
 def test_sign_token_for_sha256_path_globs(capsys: pytest.LogCaptureFixture) -> None:
     expected = "PathGlobs=/*~Expires=1663070400~hmac=9439ecdd5c4919f76f915dea72afa85a045579794e63d8cda664f5a1140c8d93"
-    result = snippets.sign_token(
+    result = dualtoken.sign_token(
         base64_key=b"g_SlMILiIWKqsC6Z2L7gy0sReDOqtSrJrE7CXNr5Nl8=",
         signature_algorithm="sha256",
         expiration_time=expiresTime,
@@ -147,7 +147,7 @@ def test_sign_token_for_sha256_path_globs(capsys: pytest.LogCaptureFixture) -> N
 
 def test_sign_token_for_sha256_full_path(capsys: pytest.LogCaptureFixture) -> None:
     expected = "FullPath~Expires=1663070400~hmac=365b41fd77297371d890fc9a56e4e3d3baa4c7afbd230a0e9a81c8e1bcab9420"
-    result = snippets.sign_token(
+    result = dualtoken.sign_token(
         base64_key=b"g_SlMILiIWKqsC6Z2L7gy0sReDOqtSrJrE7CXNr5Nl8=",
         signature_algorithm="sha256",
         expiration_time=expiresTime,
@@ -158,7 +158,7 @@ def test_sign_token_for_sha256_full_path(capsys: pytest.LogCaptureFixture) -> No
 
 def test_sign_token_for_ed25519_all_params(capsys: pytest.LogCaptureFixture) -> None:
     expected = "PathGlobs=/*~Starts=1663027200~Expires=1663070400~SessionID=test-id~Data=test-data~Headers=Foo,BAZ~IPRanges=MjAzLjAuMTEzLjAvMjQsMjAwMTpkYjg6NGE3ZjphNzMyLzY0~Signature=A7u67hveGxGvP8KBWZlUuH0IsqhS4a2lcsXwy3uc4X3zaVuw7LY-2FQT1ZF8UxkSFAsDS3_0LYnXwXB2XdepDg"
-    result = snippets.sign_token(
+    result = dualtoken.sign_token(
         base64_key=b"DJUcnLguVFKmVCFnWGubG1MZg7fWAnxacMjKDhVZMGI=",
         signature_algorithm="ed25519",
         start_time=startTime,
@@ -174,7 +174,7 @@ def test_sign_token_for_ed25519_all_params(capsys: pytest.LogCaptureFixture) -> 
 
 def test_sign_token_for_sha1_all_params(capsys: pytest.LogCaptureFixture) -> None:
     expected = "PathGlobs=/*~Starts=1663027200~Expires=1663070400~SessionID=test-id~Data=test-data~Headers=Foo,BAZ~IPRanges=MjAzLjAuMTEzLjAvMjQsMjAwMTpkYjg6NGE3ZjphNzMyLzY0~hmac=b8242e8b76cbfbbd61b3540ed0eb60a2ec2fdbdb"
-    result = snippets.sign_token(
+    result = dualtoken.sign_token(
         base64_key=b"g_SlMILiIWKqsC6Z2L7gy0sReDOqtSrJrE7CXNr5Nl8=",
         signature_algorithm="sha1",
         start_time=startTime,
@@ -190,7 +190,7 @@ def test_sign_token_for_sha1_all_params(capsys: pytest.LogCaptureFixture) -> Non
 
 def test_sign_token_for_sha256_all_params(capsys: pytest.LogCaptureFixture) -> None:
     expected = "PathGlobs=/*~Starts=1663027200~Expires=1663070400~SessionID=test-id~Data=test-data~Headers=Foo,BAZ~IPRanges=MjAzLjAuMTEzLjAvMjQsMjAwMTpkYjg6NGE3ZjphNzMyLzY0~hmac=dda9c3d6f3b2e867a09fbb76209ea138dd81f8512210f970d1e92f90927bef4b"
-    result = snippets.sign_token(
+    result = dualtoken.sign_token(
         base64_key=b"g_SlMILiIWKqsC6Z2L7gy0sReDOqtSrJrE7CXNr5Nl8=",
         signature_algorithm="sha256",
         start_time=startTime,
