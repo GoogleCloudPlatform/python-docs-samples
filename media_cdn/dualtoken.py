9e6585165a518eb38025490410c1917c60ecf780
Sampath M <sampathm@google.com>
2023-02-22 11:04:11 +0100

    Change the directory structure
    changed dualtoken/snippet.py(_test.py) to dualtoken.py(_test.py)

---
+++ media_cdn/dualtoken.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+#
+# Copyright 2022 Google, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START  mediacdn_dual_token_sign_token]
+import base64
+import datetime
+import hashlib
+import hmac
+
+import cryptography.hazmat.primitives.asymmetric.ed25519 as ed25519
+
+
+def base64_encoder(value: bytes) -> str:
+    """
+    Returns a base64-encoded string compatible with Media CDN.
+
+    Media CDN uses URL-safe base64 encoding and strips off the padding at the
+    end.
+    """
+    encoded_bytes = base64.urlsafe_b64encode(value)
+    encoded_str = encoded_bytes.decode("utf-8")
+    return encoded_str.rstrip("=")
+
+
+def sign_token(
+    base64_key: bytes,
+    signature_algorithm: str,
+    start_time: datetime.datetime = None,
+    expiration_time: datetime.datetime = None,
+    url_prefix: str = None,
+    full_path: str = None,
+    path_globs: str = None,
+    session_id: str = None,
+    data: str = None,
+    headers: str = None,
+    ip_ranges: str = None,
+) -> str:
+    """Gets the Signed URL Suffix string for the Media CDN' Short token URL requests.
+    One of (`url_prefix`, `full_path`, `path_globs`) must be included in each input.
+    Args:
+        base64_key: Secret key as a base64 encoded string.
+        signature_algorithm: Algorithm can be either `SHA1` or `SHA256` or `Ed25519`.
+        start_time: Start time as a UTC datetime object.
+        expiration_time: Expiration time as a UTC datetime object. If None, an expiration time 1 hour from now will be used.
+        url_prefix: the URL prefix to sign, including protocol.
+                    For example: http://example.com/path/ for URLs under /path or http://example.com/path?param=1
+        full_path:  A full path to sign, starting with the first '/'.
+                    For example: /path/to/content.mp4
+        path_globs: a set of ','- or '!'-delimited path glob strings.
+                    For example: /tv/*!/film/* to sign paths starting with /tv/ or /film/ in any URL.
+        session_id: a unique identifier for the session
+        data: data payload to include in the token
+        headers: header name and value to include in the signed token in name=value format.  May be specified more than once.
+                    For example: [{'name': 'foo', 'value': 'bar'}, {'name': 'baz', 'value': 'qux'}]
+        ip_ranges: A list of comma separated ip ranges. Both IPv4 and IPv6 ranges are acceptable.
+                    For example: b"203.0.113.0/24,2001:db8:4a7f:a732/64"
+
+    Returns:
+        The Signed URL appended with the query parameters based on the
+        specified URL prefix and configuration.
+    """
+
+    decoded_key = base64.urlsafe_b64decode(base64_key)
+    algo = signature_algorithm.lower()
+
+    # For most fields, the value we put in the token and the value we must sign
+    # are the same.  The FullPath and Headers use a different string for the
+    # value to be signed compared to the token.  To illustrate this difference,
+    # we'll keep the token and the value to be signed separate.
+    tokens = []
+    to_sign = []
+
+    # check for `full_path` or `path_globs` or `url_prefix`
+    if full_path:
+        tokens.append("FullPath")
+        to_sign.append(f"FullPath={full_path}")
+    elif path_globs:
+        path_globs = path_globs.strip()
+        field = f"PathGlobs={path_globs}"
+        tokens.append(field)
+        to_sign.append(field)
+    elif url_prefix:
+        field = "URLPrefix=" + base64_encoder(url_prefix.encode("utf-8"))
+        tokens.append(field)
+        to_sign.append(field)
+    else:
+        raise ValueError(
+            "User Input Missing: One of `url_prefix`, `full_path` or `path_globs` must be specified"
+        )
+
+    # check & parse optional params
+    if start_time:
+        epoch_duration = start_time.astimezone(
+            tz=datetime.timezone.utc
+        ) - datetime.datetime.fromtimestamp(0, tz=datetime.timezone.utc)
+        field = f"Starts={int(epoch_duration.total_seconds())}"
+        tokens.append(field)
+        to_sign.append(field)
+
+    if not expiration_time:
+        expiration_time = datetime.datetime.now() + datetime.timedelta(hours=1)
+        epoch_duration = expiration_time.astimezone(
+            tz=datetime.timezone.utc
+        ) - datetime.datetime.fromtimestamp(0, tz=datetime.timezone.utc)
+    else:
+        epoch_duration = expiration_time.astimezone(
+            tz=datetime.timezone.utc
+        ) - datetime.datetime.fromtimestamp(0, tz=datetime.timezone.utc)
+    field = f"Expires={int(epoch_duration.total_seconds())}"
+    tokens.append(field)
+    to_sign.append(field)
+
+    if session_id:
+        field = f"SessionID={session_id}"
+        tokens.append(field)
+        to_sign.append(field)
+
+    if data:
+        field = f"Data={data}"
+        tokens.append(field)
+        to_sign.append(field)
+
+    if headers:
+        header_names = []
+        header_pairs = []
+        for each in headers:
+            header_names.append(each["name"])
+            header_pairs.append("%s=%s" % (each["name"], each["value"]))
+        tokens.append(f"Headers={','.join(header_names)}")
+        to_sign.append(f"Headers={','.join(header_pairs)}")
+
+    if ip_ranges:
+        field = f"IPRanges={base64_encoder(ip_ranges.encode('ascii'))}"
+        tokens.append(field)
+        to_sign.append(field)
+
+    # generating token
+    to_sign = "~".join(to_sign)
+    to_sign_bytes = to_sign.encode("utf-8")
+    if algo == "ed25519":
+        digest = ed25519.Ed25519PrivateKey.from_private_bytes(decoded_key).sign(
+            to_sign_bytes
+        )
+        tokens.append("Signature=" + base64_encoder(digest))
+    elif algo == "sha256":
+        signature = hmac.new(
+            decoded_key, to_sign_bytes, digestmod=hashlib.sha256
+        ).hexdigest()
+        tokens.append("hmac=" + signature)
+    elif algo == "sha1":
+        signature = hmac.new(
+            decoded_key, to_sign_bytes, digestmod=hashlib.sha1
+        ).hexdigest()
+        tokens.append("hmac=" + signature)
+    else:
+        raise ValueError(
+            "Input Missing Error: `signature_algorithm` can only be one of `sha1`, `sha256` or `ed25519`"
+        )
+    return "~".join(tokens)
+# [end  mediacdn_dual_token_sign_token]
+
---
+++ media_cdn/dualtoken_test.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+#
+# Copyright 2022 Google, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for snippets."""
+
+import datetime
+
+import pytest
+
+import snippets
+
+
+startTime = datetime.datetime.strptime("2022-09-13T00:00:00Z", "%Y-%m-%dT%H:%M:%S%z")
+expiresTime = datetime.datetime.strptime("2022-09-13T12:00:00Z", "%Y-%m-%dT%H:%M:%S%z")
+sessionID = "test-id"
+data = "test-data"
+ipRanges = "203.0.113.0/24,2001:db8:4a7f:a732/64"
+headers = [
+    {
+        "name": "Foo",
+        "value": "bar",
+    },
+    {
+        "name": "BAZ",
+        "value": "quux",
+    },
+]
+
+with open("/tmp/example.key", "wb") as fp:
+    fp.write(
+        b"\x0c\x95\x1c\x9c\xb8.TR\xa6T!gXk\x9b\x1bS\x19\x83\xb7\xd6\x02|Zp\xc8\xca\x0e\x15Y0b\x9f\xb9\xf0\xbe\x1c\xda\xd7P\xb4J\xe5=k-nZ0\xd2\x7f1\xfe\n\x92\x01\x81|j#?\x98w\xd4"
+    )
+
+with open("/tmp/example.pub", "wb") as fp:
+    fp.write(
+        b"\x9f\xb9\xf0\xbe\x1c\xda\xd7P\xb4J\xe5=k-nZ0\xd2\x7f1\xfe\n\x92\x01\x81|j#?\x98w\xd4"
+    )
+
+with open("/tmp/shared.secret", "wb") as fp:
+    fp.write(
+        b"\x83\xf4\xa50\x82\xe2!b\xaa\xb0.\x99\xd8\xbe\xe0\xcbK\x11x3\xaa\xb5*\xc9\xacN\xc2\\\xda\xf96_"
+    )
+
+
+def test_sign_token_for_ed25519_url_prefix(capsys: pytest.LogCaptureFixture) -> None:
+    expected = "URLPrefix=aHR0cDovLzEwLjIwLjMwLjQwLw~Expires=1663070400~Signature=OQLXEjnApFGJaGZ_jvp2R7VY5q3ic-HT3igFpi9iPsJRXtQuvPF4cxZUT-rtCqzteXx3vSRhk09FxgDQauO_DA"
+    result = snippets.sign_token(
+        base64_key=b"DJUcnLguVFKmVCFnWGubG1MZg7fWAnxacMjKDhVZMGI=",
+        signature_algorithm="ed25519",
+        expiration_time=expiresTime,
+        url_prefix="http://10.20.30.40/",
+    )
+    assert result == expected
+
+
+def test_sign_token_for_ed25519_path_glob(capsys: pytest.LogCaptureFixture) -> None:
+    expected = "PathGlobs=/*~Expires=1663070400~Signature=9pBdD_6O6LB-4V67HZ_SOc2G_jIkSZ_tMsKnVqElmPlwKB_xDiW7DKAnv8L8CmweeZquaLFlnLogbMcIV8bNCQ"
+    result = snippets.sign_token(
+        base64_key=b"DJUcnLguVFKmVCFnWGubG1MZg7fWAnxacMjKDhVZMGI=",
+        signature_algorithm="ed25519",
+        expiration_time=expiresTime,
+        path_globs="/*",
+    )
+    assert result == expected
+
+
+def test_sign_token_for_ed25519_full_path(capsys: pytest.LogCaptureFixture) -> None:
+    expected = "FullPath~Expires=1663070400~Signature=X74OTNjtseIUmsab-YiOTZ8jyX_KG7v4YQWwcFpfFmjhzaX8NdweMc9Wglj8wxEsEW85g3_MBG3T9jzLZFQDCw"
+    result = snippets.sign_token(
+        base64_key=b"DJUcnLguVFKmVCFnWGubG1MZg7fWAnxacMjKDhVZMGI=",
+        signature_algorithm="ed25519",
+        expiration_time=expiresTime,
+        full_path="/example.m3u8",
+    )
+    assert result == expected
+
+
+def test_sign_token_for_sha1_url_prefix(capsys: pytest.LogCaptureFixture) -> None:
+    expected = "URLPrefix=aHR0cDovLzEwLjIwLjMwLjQwLw~Expires=1663070400~hmac=6f5b4bb82536810d5ee111cba3e534d49c6ac3cb"
+    result = snippets.sign_token(
+        base64_key=b"g_SlMILiIWKqsC6Z2L7gy0sReDOqtSrJrE7CXNr5Nl8=",
+        signature_algorithm="sha1",
+        expiration_time=expiresTime,
+        url_prefix="http://10.20.30.40/",
+    )
+    assert result == expected
+
+
+def test_sign_token_for_sha1_path_glob(capsys: pytest.LogCaptureFixture) -> None:
+    expected = "PathGlobs=/*~Expires=1663070400~hmac=c1c446eea24faa31392519f975fea7eefb945625"
+    result = snippets.sign_token(
+        base64_key=b"g_SlMILiIWKqsC6Z2L7gy0sReDOqtSrJrE7CXNr5Nl8=",
+        signature_algorithm="sha1",
+        expiration_time=expiresTime,
+        path_globs="/*",
+    )
+    assert result == expected
+
+
+def test_sign_token_for_sha1_full_path(capsys: pytest.LogCaptureFixture) -> None:
+    expected = (
+        "FullPath~Expires=1663070400~hmac=7af78177d6bc001d5626eefe387b1774a4a99ca2"
+    )
+    result = snippets.sign_token(
+        base64_key=b"g_SlMILiIWKqsC6Z2L7gy0sReDOqtSrJrE7CXNr5Nl8=",
+        signature_algorithm="sha1",
+        expiration_time=expiresTime,
+        full_path="/example.m3u8",
+    )
+    assert result == expected
+
+
+def test_sign_token_for_sha256_url_prefix(capsys: pytest.LogCaptureFixture) -> None:
+    expected = "URLPrefix=aHR0cDovLzEwLjIwLjMwLjQwLw~Expires=1663070400~hmac=409722313cf6d987da44bb360e60dccc3d79764520fc5e3b57654e1d4d2c862e"
+    result = snippets.sign_token(
+        base64_key=b"g_SlMILiIWKqsC6Z2L7gy0sReDOqtSrJrE7CXNr5Nl8=",
+        signature_algorithm="sha256",
+        expiration_time=expiresTime,
+        url_prefix="http://10.20.30.40/",
+    )
+    assert result == expected
+
+
+def test_sign_token_for_sha256_path_globs(capsys: pytest.LogCaptureFixture) -> None:
+    expected = "PathGlobs=/*~Expires=1663070400~hmac=9439ecdd5c4919f76f915dea72afa85a045579794e63d8cda664f5a1140c8d93"
+    result = snippets.sign_token(
+        base64_key=b"g_SlMILiIWKqsC6Z2L7gy0sReDOqtSrJrE7CXNr5Nl8=",
+        signature_algorithm="sha256",
+        expiration_time=expiresTime,
+        path_globs="/*",
+    )
+    assert result == expected
+
+
+def test_sign_token_for_sha256_full_path(capsys: pytest.LogCaptureFixture) -> None:
+    expected = "FullPath~Expires=1663070400~hmac=365b41fd77297371d890fc9a56e4e3d3baa4c7afbd230a0e9a81c8e1bcab9420"
+    result = snippets.sign_token(
+        base64_key=b"g_SlMILiIWKqsC6Z2L7gy0sReDOqtSrJrE7CXNr5Nl8=",
+        signature_algorithm="sha256",
+        expiration_time=expiresTime,
+        full_path="/example.m3u8",
+    )
+    assert result == expected
+
+
+def test_sign_token_for_ed25519_all_params(capsys: pytest.LogCaptureFixture) -> None:
+    expected = "PathGlobs=/*~Starts=1663027200~Expires=1663070400~SessionID=test-id~Data=test-data~Headers=Foo,BAZ~IPRanges=MjAzLjAuMTEzLjAvMjQsMjAwMTpkYjg6NGE3ZjphNzMyLzY0~Signature=A7u67hveGxGvP8KBWZlUuH0IsqhS4a2lcsXwy3uc4X3zaVuw7LY-2FQT1ZF8UxkSFAsDS3_0LYnXwXB2XdepDg"
+    result = snippets.sign_token(
+        base64_key=b"DJUcnLguVFKmVCFnWGubG1MZg7fWAnxacMjKDhVZMGI=",
+        signature_algorithm="ed25519",
+        start_time=startTime,
+        expiration_time=expiresTime,
+        path_globs="/*",
+        session_id=sessionID,
+        data=data,
+        headers=headers,
+        ip_ranges=ipRanges,
+    )
+    assert result == expected
+
+
+def test_sign_token_for_sha1_all_params(capsys: pytest.LogCaptureFixture) -> None:
+    expected = "PathGlobs=/*~Starts=1663027200~Expires=1663070400~SessionID=test-id~Data=test-data~Headers=Foo,BAZ~IPRanges=MjAzLjAuMTEzLjAvMjQsMjAwMTpkYjg6NGE3ZjphNzMyLzY0~hmac=b8242e8b76cbfbbd61b3540ed0eb60a2ec2fdbdb"
+    result = snippets.sign_token(
+        base64_key=b"g_SlMILiIWKqsC6Z2L7gy0sReDOqtSrJrE7CXNr5Nl8=",
+        signature_algorithm="sha1",
+        start_time=startTime,
+        expiration_time=expiresTime,
+        path_globs="/*",
+        session_id=sessionID,
+        data=data,
+        headers=headers,
+        ip_ranges=ipRanges,
+    )
+    assert result == expected
+
+
+def test_sign_token_for_sha256_all_params(capsys: pytest.LogCaptureFixture) -> None:
+    expected = "PathGlobs=/*~Starts=1663027200~Expires=1663070400~SessionID=test-id~Data=test-data~Headers=Foo,BAZ~IPRanges=MjAzLjAuMTEzLjAvMjQsMjAwMTpkYjg6NGE3ZjphNzMyLzY0~hmac=dda9c3d6f3b2e867a09fbb76209ea138dd81f8512210f970d1e92f90927bef4b"
+    result = snippets.sign_token(
+        base64_key=b"g_SlMILiIWKqsC6Z2L7gy0sReDOqtSrJrE7CXNr5Nl8=",
+        signature_algorithm="sha256",
+        start_time=startTime,
+        expiration_time=expiresTime,
+        path_globs="/*",
+        session_id=sessionID,
+        data=data,
+        headers=headers,
+        ip_ranges=ipRanges,
+    )
+    assert result == expected
+
