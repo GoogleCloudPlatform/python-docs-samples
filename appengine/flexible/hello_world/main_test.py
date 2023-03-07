03d82f2b499813807d62ba064125db72b36d13cd
Jinglun Dong <1683035+jinglundong@users.noreply.github.com>
2023-03-07 00:05:34 +0000

    Update python-docs-samples/appengine/flexible/hello_world to Python 3.11 on Ubuntu 22.

--- appengine/flexible/hello_world/main_test.py
+++ appengine/flexible/hello_world/main_test.py
@@ -19,6 +19,6 @@ def test_index():
     main.app.testing = True
     client = main.app.test_client()
 
-    r = client.get('/')
+    r = client.get("/")
     assert r.status_code == 200
-    assert 'Hello World' in r.data.decode('utf-8')
+    assert "Hello World" in r.data.decode("utf-8")
