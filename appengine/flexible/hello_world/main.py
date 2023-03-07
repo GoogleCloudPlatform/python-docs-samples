a179ad2a39ee2a9cd332456e63db1990bb7b353e
Jinglun Dong <1683035+jinglundong@users.noreply.github.com>
2023-03-07 00:05:34 +0000

    Update python-docs-samples/appengine/flexible/hello_world to Python 3.11 on Ubuntu 22.

--- appengine/flexible/hello_world/main.py
+++ appengine/flexible/hello_world/main.py
@@ -19,14 +19,14 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return "Hello World!"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
 # [END gae_flex_quickstart]
