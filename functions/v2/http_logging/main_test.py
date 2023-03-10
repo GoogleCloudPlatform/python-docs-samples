5436021f9a5c7b8d4ee3c46103167abc69774c4c
Charles Engelke <engelke@google.com>
2023-03-10 15:15:42 -0800

    Don't capture output that won't be used

--- functions/v2/http_logging/main_test.py
+++ functions/v2/http_logging/main_test.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Google LLC
+# Copyright 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
@@ -45,7 +45,7 @@ def test_functions_log_http_should_print_message(app, capsys):
     # Force trace with trace header
     with app.test_request_context(headers={'x-cloud-trace-context': f"{mock_trace}/{mock_span};o=1"}):
         main.structured_logging(flask.request)
-        out, err = capsys.readouterr()
+        _, err = capsys.readouterr()
 
         # In some situations, the library adds information before the first
         # intended line. Use the next line in that case.
