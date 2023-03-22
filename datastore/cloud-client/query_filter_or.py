30ddb131857131f9dc9f12e9aabd2e0f9fda963f
Eric Schmidt <erschmid@google.com>
2023-03-22 09:55:45 -0700

    lint

--- datastore/cloud-client/query_filter_or.py
+++ datastore/cloud-client/query_filter_or.py
@@ -21,24 +21,30 @@ Builds a union (OR) query filter.
 
 See https://cloud.google.com/python/docs/reference/datastore/latest before running code.
 """
+
+
 def query_filter_or(project_id: str) -> None:
-    """Builds a union of two queries (OR) filter
-    
+    """Builds a union of two queries (OR) filter.
+
     Arguments:
-        project_id: your Google Cloud Project ID 
+        project_id: your Google Cloud Project ID
     """
 
     client = datastore.Client(project=project_id)
 
     or_query = client.query(kind="Task")
-    or_filter = query.Or([
-        query.PropertyFilter("description", "=", "Buy milk"),
-        query.PropertyFilter("description", "=", "Feed cats"),
-    ])
+    or_filter = query.Or(
+        [
+            query.PropertyFilter("description", "=", "Buy milk"),
+            query.PropertyFilter("description", "=", "Feed cats"),
+        ]
+    )
 
     or_query.add_filter(filter=or_filter)
 
     results = or_query.fetch()
     for result in results:
         print(result["description"])
+
+
 # [END datastore_query_filter_or]
