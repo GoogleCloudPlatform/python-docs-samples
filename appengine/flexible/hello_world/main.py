442abbf756beff7ddd70eb60adca1fe2fa831c7f
Mohamed A. Ali <62313945+s3450966@users.noreply.github.com>
2023-03-27 15:55:28 +1100

    user_data

--- appengine/flexible/hello_world/main.py
+++ appengine/flexible/hello_world/main.py
@@ -25,6 +25,30 @@ def hello() -> str:
     return 'Hello World!'
 
 
+data = [
+    {"id": "s3450966", "user_name": "mohamed ali0", "password": "012345"},
+    {"id": "s3450967", "user_name": "mohamed ali1", "password": "123456"},
+    {"id": "s3450968", "user_name": "mohamed ali2", "password": "234567"},
+    {"id": "s3450969", "user_name": "mohamed ali3", "password": "345678"},
+    {"id": "s3450970", "user_name": "mohamed ali4", "password": "456789"},
+    {"id": "s3450971", "user_name": "mohamed ali5", "password": "567890"},
+    {"id": "s3450972", "user_name": "mohamed ali6", "password": "678901"},
+    {"id": "s3450973", "user_name": "mohamed ali7", "password": "789012"},
+    {"id": "s3450974", "user_name": "mohamed ali8", "password": "890123"},
+    {"id": "s3450975", "user_name": "mohamed ali9", "password": "901234"},
+]
+
+# Print header row
+print("{:<15} {:<20} {:<15}".format("id", "user_name", "password"))
+
+# Print table rows
+for item in data:
+    print("{:<15} {:<20} {:<15}".format(item["id"], item["user_name"], item["password"]))
+
+
+
+
+
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app.
