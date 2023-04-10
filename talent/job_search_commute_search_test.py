0960e02c3064070ced70dce89984b5ece0ac3c6c
nicain <nicholascain@google.com>
2023-04-10 12:37:09 -0700

    chore: Adding temporary skipif while product team fixes server-side error, b/277494438 (#9536)

--- talent/job_search_commute_search_test.py
+++ talent/job_search_commute_search_test.py
@@ -12,13 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from datetime import date
 import os
+
+import pytest
 
 import job_search_commute_search
 
 PROJECT_ID = os.environ["GOOGLE_CLOUD_PROJECT"]
 
 
+@pytest.mark.skipif(
+    date.today() < date(2023, 4, 25),
+    reason="Addressed by product team until this date, b/277494438",
+)
 def test_commute_search(tenant):
     jobs = job_search_commute_search.search_jobs(PROJECT_ID, tenant)
     for job in jobs:
