d47750e8cbdb8ab8616d3e286018a114c6ed96c1
Karl Weinmeister <kweinmeister@google.com>
2023-04-16 19:27:30 -0500

    ci: simplify gae flexible requirements

--- appengine/flexible/hello_world_django/noxfile_config.py
+++ appengine/flexible/hello_world_django/noxfile_config.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Google LLC
+# Copyright 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,20 +22,17 @@
 
 TEST_CONFIG_OVERRIDE = {
     # You can opt out from the test for specific Python versions.
+    # Skipping for Python 3.9 due to pyarrow compilation failure.
     "ignored_versions": ["2.7", "3.7"],
     # Old samples are opted out of enforcing Python type hints
     # All new samples should feature them
-    "enforce_type_hints": True,
+    "enforce_type_hints": False,
     # An envvar key for determining the project id to use. Change it
     # to 'BUILD_SPECIFIC_GCLOUD_PROJECT' if you want to opt in using a
     # build specific Cloud project. You can also use your own string
     # to use your own Cloud project.
     "gcloud_project_env": "GOOGLE_CLOUD_PROJECT",
     # 'gcloud_project_env': 'BUILD_SPECIFIC_GCLOUD_PROJECT',
-    # If you need to use a specific version of pip,
-    # change pip_version_override to the string representation
-    # of the version number, for example, "20.2.4"
-    "pip_version_override": None,
     # A dictionary you want to inject into your test. Don't put any
     # secrets here. These values will override predefined values.
     "envs": {},
