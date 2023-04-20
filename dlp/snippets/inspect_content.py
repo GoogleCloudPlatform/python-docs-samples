bde18d9f5296d64b7c8a0258d95764a02f058431
Avani-Thakker-Crest <avani.thakker@crestdatasys.com>
2023-04-20 19:12:39 +0530

    Correcting order of imports

--- dlp/snippets/inspect_content.py
+++ dlp/snippets/inspect_content.py
@@ -16,11 +16,12 @@
 local file or a file on Google Cloud Storage."""
 
 from __future__ import print_function
-from typing import List
 
 import argparse
 import json
 import os
+
+from typing import List
 
 
 # [START dlp_inspect_string_basic]
