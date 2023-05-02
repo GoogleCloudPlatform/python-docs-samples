bb37615fc7c4d8711cb9bee1f2b9c8d3af2e2757
Avani-Thakker-Crest <avani.thakker@crestdatasys.com>
2023-05-02 17:50:35 +0530

    Implemenetd dlp_get_job with unit test cases

--- dlp/snippets/jobs.py
+++ dlp/snippets/jobs.py
@@ -189,6 +189,35 @@ def create_dlp_job(
 # [END dlp_create_job]
 
 
+# [START dlp_get_job]
+def get_dlp_job(
+    project: str,
+    job_name: str
+) -> None:
+    """Uses the Data Loss Prevention API to retrieve a DLP job.
+    Args:
+        project: The project id to use as a parent resource.
+        job_name: The name of the DlpJob resource to be retrieved.
+    """
+    # Import the client library
+    import google.cloud.dlp
+
+    # Instantiate a client.
+    dlp = google.cloud.dlp_v2.DlpServiceClient()
+
+    # Convert the project id and job name into a full resource id.
+    job_name = f"projects/{project}/dlpJobs/{job_name}"
+
+    # Call the API
+    response = dlp.get_dlp_job(request={
+        "name": job_name
+    })
+
+    print(f"Job: {response.name} Status: {response.state}")
+
+# [END dlp_get_job]
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     subparsers = parser.add_subparsers(
@@ -259,6 +288,17 @@ if __name__ == "__main__":
         help="Limit scan to new content only.",
     )
 
+    get_parser = subparsers.add_parser(
+        "get", help="Get a Data Loss Prevention API job."
+    )
+    get_parser.add_argument(
+        "project", help="The project id to use as a parent resource."
+    )
+    get_parser.add_argument(
+        "job_name",
+        help="The name of the DlpJob resource to be retrieved. " "Example: X-#####",
+    )
+
     args = parser.parse_args()
 
     if args.content == "list":
@@ -274,3 +314,5 @@ if __name__ == "__main__":
             max_findings=args.max_findings,
             auto_populate_timespan=args.auto_populate_timespan,
         )
+    elif args.content == "get":
+        get_dlp_job(args.project, args.job_name)
