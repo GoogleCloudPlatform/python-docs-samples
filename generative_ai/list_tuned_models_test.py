e9c2830c766c5ec5ae96e1f323944636c40c5989
Nick Cain <nicholascain@google.com>
2023-05-10 18:35:27 +0000

    fix: list_tuned_models_test.py added fixture to test against

--- generative_ai/list_tuned_models_test.py
+++ generative_ai/list_tuned_models_test.py
@@ -16,13 +16,24 @@ import os
 
 import list_tuned_models
 
+from google.cloud import aiplatform
+
 
 _PROJECT_ID = os.getenv('GOOGLE_CLOUD_PROJECT')
 _LOCATION = "us-central1"
 
 
 def test_list_tuned_models():
-    tuned_model_names = list_tuned_models.list_tuned_models(_PROJECT_ID, _LOCATION)
-    assert len(tuned_model_names) > 0
+    tuned_model_names = list_tuned_models.list_tuned_models(
+        _PROJECT_ID,
+        _LOCATION,
+    )
+    filtered_models_counter = 0
     for tuned_model_name in tuned_model_names:
-        assert tuned_model_name.startswith('projects/_PROJECT_ID/locations/us-central1/models/')
+        model_registry = aiplatform.models.ModelRegistry(model=tuned_model_name)
+        if (
+            'Vertex LLM Test Fixture '
+            '(list_tuned_models_test.py::test_list_tuned_models)'
+        ) in model_registry.get_version_info('1').model_display_name:
+            filtered_models_counter += 1
+    assert filtered_models_counter == 1
