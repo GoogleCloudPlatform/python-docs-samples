d5fa7822327edca20faa9b0efb612f90ad894971
Megan O'Keefe <megan037@gmail.com>
2023-05-15 18:18:06 -0400

    fix return type hint

--- storage/s3-sdk/s3_sdk_test.py
+++ storage/s3-sdk/s3_sdk_test.py
@@ -68,7 +68,7 @@ def test_blob(test_bucket: str) -> storage.Blob:
 # Retry request because the created key may not be fully propagated for up
 # to 15s.
 @backoff.on_exception(backoff.constant, ClientError, interval=1, max_time=15)
-def test_list_buckets(hmac_fixture: dict, test_bucket: storage.Bucket) -> None:
+def test_list_buckets(hmac_fixture: dict, test_bucket: storage.Bucket) -> str:
     results = list_gcs_buckets.list_gcs_buckets(
         google_access_key_id=hmac_fixture[0].access_id,
         google_access_key_secret=hmac_fixture[1],
@@ -79,7 +79,7 @@ def test_list_buckets(hmac_fixture: dict, test_bucket: storage.Bucket) -> None:
 # Retry request because the created key may not be fully propagated for up
 # to 15s.
 @backoff.on_exception(backoff.constant, ClientError, interval=1, max_time=15)
-def test_list_blobs(hmac_fixture: dict, test_bucket: storage.Bucket, test_blob: storage.Blob) -> None:
+def test_list_blobs(hmac_fixture: dict, test_bucket: storage.Bucket, test_blob: storage.Blob) -> str:
     result = list_gcs_objects.list_gcs_objects(
         google_access_key_id=hmac_fixture[0].access_id,
         google_access_key_secret=hmac_fixture[1],
