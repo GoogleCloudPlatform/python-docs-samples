cfe063c11fab7cfb12930ff1e6129551673d2abf
Jennifer Davis <sigje@google.com>
2023-05-15 11:29:15 -0700

    fix: Update main.py
    pair with @nicain; this is not typical usage of if __name__ == .. but to make sure documentation and intent of the sample for atlas this should work. 
    
    Co-authored-by: Nick Cain <nicain.seattle@gmail.com>

--- run/pubsub/main.py
+++ run/pubsub/main.py
@@ -14,16 +14,27 @@
 
 # [START cloudrun_pubsub_server_setup]
 # [START run_pubsub_server_setup]
+# [START cloudrun_pubsub_server]
+# [START run_pubsub_server]
+
 import base64
 import os
 
 from flask import Flask, request
 
-
 app = Flask(__name__)
+
+if __name__ == "__main__":
+    PORT = int(os.getenv("PORT")) if os.getenv("PORT") else 8080
+
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Cloud Run. See entrypoint in Dockerfile.
+    app.run(host="127.0.0.1", port=PORT, debug=True)
+
 # [END run_pubsub_server_setup]
 # [END cloudrun_pubsub_server_setup]
-
+# [END cloudrun_pubsub_server]
+# [END run_pubsub_server]
 
 # [START cloudrun_pubsub_handler]
 # [START run_pubsub_handler]
@@ -50,19 +61,5 @@ def index():
 
     return ("", 204)
 
-
 # [END run_pubsub_handler]
 # [END cloudrun_pubsub_handler]
-
-# [START cloudrun_pubsub_server]
-# [START run_pubsub_server]
-
-if __name__ == "__main__":
-    PORT = int(os.getenv("PORT")) if os.getenv("PORT") else 8080
-
-    # This is used when running locally. Gunicorn is used to run the
-    # application on Cloud Run. See entrypoint in Dockerfile.
-    app.run(host="127.0.0.1", port=PORT, debug=True)
-
-# [END cloudrun_pubsub_server]
-# [END run_pubsub_server]
