d0fc34d49f43e04040780ba868a20a3e9768f33c
Katie McLaughlin <katie@glasnt.com>
2023-05-16 17:12:49 +1000

    fix: add types to tests

--- secretmanager/snippets/snippets_test.py
+++ secretmanager/snippets/snippets_test.py
@@ -14,6 +14,7 @@
 import base64
 import os
 import time
+from typing import Generator, Tuple
 import uuid
 
 from google.api_core import exceptions
@@ -48,22 +49,24 @@ from update_secret_with_etag import update_secret_with_etag
 
 
 @pytest.fixture()
-def client():
+def client() -> secretmanager.SecretManagerServiceClient:
     return secretmanager.SecretManagerServiceClient()
 
 
 @pytest.fixture()
-def project_id():
+def project_id() -> str:
     return os.environ["GOOGLE_CLOUD_PROJECT"]
 
 
 @pytest.fixture()
-def iam_user():
+def iam_user() -> str:
     return "serviceAccount:" + os.environ["GCLOUD_SECRETS_SERVICE_ACCOUNT"]
 
 
 @pytest.fixture()
-def secret_id(client, project_id):
+def secret_id(
+    client: secretmanager.SecretManagerServiceClient, project_id: str
+) -> Generator[str, None, None]:
     secret_id = f"python-secret-{uuid.uuid4()}"
 
     yield secret_id
@@ -79,7 +82,9 @@ def secret_id(client, project_id):
 
 
 @pytest.fixture()
-def secret(client, project_id, secret_id):
+def secret(
+    client: secretmanager.SecretManagerServiceClient, project_id: str, secret_id: str
+) -> Generator[Tuple[str, str, str], None, None]:
     print(f"creating secret {secret_id}")
 
     parent = f"projects/{project_id}"
@@ -96,7 +101,9 @@ def secret(client, project_id, secret_id):
 
 
 @pytest.fixture()
-def secret_version(client, secret):
+def secret_version(
+    client: secretmanager.SecretManagerServiceClient, secret: Tuple[str, str, dict]
+):
     project_id, secret_id, _ = secret
 
     print(f"adding secret version to {secret_id}")
@@ -114,7 +121,7 @@ another_secret_version = secret_version
 
 
 @pytest.fixture()
-def pubsub_message():
+def pubsub_message() -> dict:
     message = "hello!"
     message_bytes = message.encode()
     base64_bytes = base64.b64encode(message_bytes)
@@ -127,35 +134,41 @@ def pubsub_message():
     }
 
 
-def test_quickstart(project_id, secret_id):
+def test_quickstart(project_id: str, secret_id: str) -> None:
     quickstart(project_id, secret_id)
 
 
-def test_access_secret_version(secret_version):
+def test_access_secret_version(secret_version) -> None:
     project_id, secret_id, version_id, _ = secret_version
     version = access_secret_version(project_id, secret_id, version_id)
     assert version.payload.data == b"hello world!"
 
 
-def test_add_secret_version(secret):
+def test_add_secret_version(secret) -> None:
     project_id, secret_id, _ = secret
     payload = "test123"
     version = add_secret_version(project_id, secret_id, payload)
     assert secret_id in version.name
 
 
-def test_create_secret(client, project_id, secret_id):
+def test_create_secret(
+    client: secretmanager.SecretManagerServiceClient, project_id: str, secret_id: str
+) -> None:
     secret = create_secret(project_id, secret_id)
     assert secret_id in secret.name
 
 
-def test_create_secret_with_user_managed_replication(client, project_id, secret_id):
+def test_create_secret_with_user_managed_replication(
+    client: secretmanager.SecretManagerServiceClient, project_id: str, secret_id: str
+) -> None:
     locations = ["us-east1", "us-east4", "us-west1"]
     secret = create_ummr_secret(project_id, secret_id, locations)
     assert secret_id in secret.name
 
 
-def test_delete_secret(client, secret):
+def test_delete_secret(
+    client: secretmanager.SecretManagerServiceClient, secret
+) -> None:
     project_id, secret_id, _ = secret
     delete_secret(project_id, secret_id)
     with pytest.raises(exceptions.NotFound):
@@ -164,7 +177,9 @@ def test_delete_secret(client, secret):
         client.access_secret_version(request={"name": name})
 
 
-def test_delete_secret_with_etag(client, secret):
+def test_delete_secret_with_etag(
+    client: secretmanager.SecretManagerServiceClient, secret
+) -> None:
     project_id, secret_id, etag = secret
     delete_secret_with_etag(project_id, secret_id, etag)
     with pytest.raises(exceptions.NotFound):
@@ -173,19 +188,25 @@ def test_delete_secret_with_etag(client, secret):
         client.access_secret_version(request={"name": name})
 
 
-def test_destroy_secret_version(client, secret_version):
+def test_destroy_secret_version(
+    client: secretmanager.SecretManagerServiceClient, secret_version
+) -> None:
     project_id, secret_id, version_id, _ = secret_version
     version = destroy_secret_version(project_id, secret_id, version_id)
     assert version.destroy_time
 
 
-def test_destroy_secret_version_with_etag(client, secret_version):
+def test_destroy_secret_version_with_etag(
+    client: secretmanager.SecretManagerServiceClient, secret_version
+) -> None:
     project_id, secret_id, version_id, etag = secret_version
     version = destroy_secret_version_with_etag(project_id, secret_id, version_id, etag)
     assert version.destroy_time
 
 
-def test_enable_disable_secret_version(client, secret_version):
+def test_enable_disable_secret_version(
+    client: secretmanager.SecretManagerServiceClient, secret_version
+) -> None:
     project_id, secret_id, version_id, _ = secret_version
     version = disable_secret_version(project_id, secret_id, version_id)
     assert version.state == secretmanager.SecretVersion.State.DISABLED
@@ -194,7 +215,9 @@ def test_enable_disable_secret_version(client, secret_version):
     assert version.state == secretmanager.SecretVersion.State.ENABLED
 
 
-def test_enable_disable_secret_version_with_etag(client, secret_version):
+def test_enable_disable_secret_version_with_etag(
+    client: secretmanager.SecretManagerServiceClient, secret_version
+) -> None:
     project_id, secret_id, version_id, etag = secret_version
     version = disable_secret_version_with_etag(project_id, secret_id, version_id, etag)
     assert version.state == secretmanager.SecretVersion.State.DISABLED
@@ -205,32 +228,38 @@ def test_enable_disable_secret_version_with_etag(client, secret_version):
     assert version.state == secretmanager.SecretVersion.State.ENABLED
 
 
-def test_get_secret_version(client, secret_version):
+def test_get_secret_version(
+    client: secretmanager.SecretManagerServiceClient, secret_version
+) -> None:
     project_id, secret_id, version_id, _ = secret_version
     version = get_secret_version(project_id, secret_id, version_id)
     assert secret_id in version.name
     assert version_id in version.name
 
 
-def test_get_secret(client, secret):
+def test_get_secret(client: secretmanager.SecretManagerServiceClient, secret) -> None:
     project_id, secret_id, _ = secret
     snippet_secret = get_secret(project_id, secret_id)
     assert secret_id in snippet_secret.name
 
 
-def test_iam_grant_access(client, secret, iam_user):
+def test_iam_grant_access(
+    client: secretmanager.SecretManagerServiceClient, secret, iam_user
+) -> None:
     project_id, secret_id, _ = secret
     policy = iam_grant_access(project_id, secret_id, iam_user)
     assert any(iam_user in b.members for b in policy.bindings)
 
 
-def test_iam_revoke_access(client, secret, iam_user):
+def test_iam_revoke_access(
+    client: secretmanager.SecretManagerServiceClient, secret, iam_user
+) -> None:
     project_id, secret_id, _ = secret
     policy = iam_revoke_access(project_id, secret_id, iam_user)
     assert not any(iam_user in b.members for b in policy.bindings)
 
 
-def test_list_secret_versions(capsys, secret_version, another_secret_version):
+def test_list_secret_versions(capsys, secret_version, another_secret_version) -> None:
     project_id, secret_id, version_id, _ = secret_version
     version_1 = get_secret_version(project_id, secret_id, version_id)
     _, _, another_version_id, _ = another_secret_version
@@ -245,7 +274,7 @@ def test_list_secret_versions(capsys, secret_version, another_secret_version):
 
 def test_list_secret_versions_with_filter(
     capsys, secret_version, another_secret_version
-):
+) -> None:
     project_id, secret_id, version_id, _ = secret_version
     enabled = get_secret_version(project_id, secret_id, version_id)
     _, _, another_version_id, _ = another_secret_version
@@ -259,7 +288,7 @@ def test_list_secret_versions_with_filter(
     assert f"Found secret version: {disabled.name}" not in out
 
 
-def test_list_secrets(capsys, secret):
+def test_list_secrets(capsys, secret) -> None:
     project_id, secret_id, _ = secret
     secret = get_secret(project_id, secret_id)
     list_secrets(project_id)
@@ -268,7 +297,7 @@ def test_list_secrets(capsys, secret):
     assert f"Found secret: {secret.name}" in out
 
 
-def test_list_secrets_with_filter(capsys, secret):
+def test_list_secrets_with_filter(capsys, secret) -> None:
     project_id, secret_id, _ = secret
     unlabeled = get_secret(project_id, secret_id)
     list_secrets_with_filter(project_id, "labels.secretmanager:rocks")
@@ -284,26 +313,26 @@ def test_list_secrets_with_filter(capsys, secret):
     assert f"Found secret: {labeled.name}" in out
 
 
-def test_update_secret(secret):
+def test_update_secret(secret) -> None:
     project_id, secret_id, _ = secret
     secret = update_secret(project_id, secret_id)
     assert secret.labels["secretmanager"] == "rocks"
 
 
-def test_consume_event_notification(pubsub_message):
+def test_consume_event_notification(pubsub_message) -> None:
     got = consume_event_notification(pubsub_message, None)
     assert (
         got == "Received SECRET_UPDATE for projects/p/secrets/s. New metadata: hello!"
     )
 
 
-def test_update_secret_with_etag(secret):
+def test_update_secret_with_etag(secret) -> None:
     project_id, secret_id, etag = secret
     secret = update_secret_with_etag(project_id, secret_id, etag)
     assert secret.labels["secretmanager"] == "rocks"
 
 
-def test_update_secret_with_alias(secret_version):
+def test_update_secret_with_alias(secret_version) -> None:
     project_id, secret_id, version_id, _ = secret_version
     secret = update_secret_with_alias(project_id, secret_id)
     assert secret.version_aliases["test"] == 1
