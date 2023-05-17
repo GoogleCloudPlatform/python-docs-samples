fc5222ff4633890e4bb6c00881fb1439cd628680
Charles Engelke <engelke@google.com>
2023-05-17 14:55:18 -0700

    Update run_tests.sh

--- .kokoro/tests/run_tests.sh
+++ .kokoro/tests/run_tests.sh
@@ -167,12 +167,19 @@ btlr_args=(
     "**/requirements.txt"
 )
 
-# if [[ -n "${NUM_TEST_WORKERS:-}" ]]; then
+if [[ -n "${NUM_TEST_WORKERS:-}" ]]; then
     btlr_args+=(
 	"--max-concurrency"
 	"${NUM_TEST_WORKERS}"
     )
-# fi
+fi
+
+# DEBUG
+
+btlr_args+=(
+    "--max-concurrency"
+    "10"
+)
 
 if [[ -n "${DIFF_FROM:-}"  ]]; then
     btlr_args+=(
