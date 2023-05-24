32a6559a8224b5b2a89b82e99a78b7b1e8dffeb7
Olivier Bourgeois <3271352+bourgeoisor@users.noreply.github.com>
2023-05-24 07:38:12 -0400

    fixit: clean up appengine/flexible/hello_world (#10045)
    ## Description
    
    Fixes #<ISSUE-NUMBER>
    
    Note: Before submitting a pull request, please open an issue for discussion if you are not associated with Google.
    
    ## Checklist
    - [ ] I have followed [Sample Guidelines from AUTHORING_GUIDE.MD](https://togithub.com/GoogleCloudPlatform/python-docs-samples/blob/main/AUTHORING_GUIDE.md)
    - [ ] README is updated to include [all relevant information](https://togithub.com/GoogleCloudPlatform/python-docs-samples/blob/main/AUTHORING_GUIDE.md#readme-file)
    - [ ] **Tests** pass:   `nox -s py-3.9` (see [Test Environment Setup](https://togithub.com/GoogleCloudPlatform/python-docs-samples/blob/main/AUTHORING_GUIDE.md#test-environment-setup))
    - [ ] **Lint** pass:   `nox -s lint` (see [Test Environment Setup](https://togithub.com/GoogleCloudPlatform/python-docs-samples/blob/main/AUTHORING_GUIDE.md#test-environment-setup))
    - [ ] These samples need a new **API enabled** in testing projects to pass (let us know which ones)
    - [ ] These samples need a new/updated **env vars** in testing projects set to pass (let us know which ones)
    - [ ] This sample adds a new sample directory, and I updated the [CODEOWNERS file](https://togithub.com/GoogleCloudPlatform/python-docs-samples/blob/main/.github/CODEOWNERS) with the codeowners for this sample
    - [ ] This sample adds a new **Product API**, and I updated the [Blunderbuss issue/PR auto-assigner](https://togithub.com/GoogleCloudPlatform/python-docs-samples/blob/main/.github/blunderbuss.yml) with the codeowners for this sample
    - [ ] Please **merge** this PR for me once it is approved

--- appengine/flexible/hello_world/main.py
+++ appengine/flexible/hello_world/main.py
@@ -19,14 +19,18 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello() -> str:
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    """Return a friendly HTTP greeting.
+
+    Returns:
+        A string with the words 'Hello World!'.
+    """
+    return "Hello World!"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
 # [END gae_flex_quickstart]
