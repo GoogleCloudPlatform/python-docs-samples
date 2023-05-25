8722cdf642f563b3a95901086965e4446294a9f1
Olivier Bourgeois <bourgeoisor@gmail.com>
2023-05-25 16:48:26 -0400

    fixit: clean up appengine/standard_python3/warmup

--- appengine/standard_python3/warmup/main.py
+++ appengine/standard_python3/warmup/main.py
@@ -20,21 +20,32 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def main():
-    return 'Hello World!'
+    """Serves a predefined placeholder string.
+    
+    Returns:
+        A predefined string saying 'Hello World!'
+    """
+    return "Hello World!"
 
 
-@app.route('/_ah/warmup')
+@app.route("/_ah/warmup")
 def warmup():
-    # Handle your warmup logic here, e.g. set up a database connection pool
-    return '', 200, {}
+    """Served stub function returning no content.
+
+    Your warmup logic can be implemented here (e.g. set up a database connection pool)
+    
+    Returns:
+        An empty string, an HTTP code 200, and an empty object.
+    """
+    return "", 200, {}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
 # [END gae_python3_warmup_app]
 # [END gae_python38_warmup_app]
