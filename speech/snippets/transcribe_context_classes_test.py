2968348f8e5e61cd20e638b17db67950d5431242
Yvonne Li <liyvonne@google.com>
2023-06-02 12:47:53 -0700

    [fixit]add typehints and clean up speech/snippets (#10110)
    * fix typehints speech/snippets
    
    * fix typehints
    
    * fix return type
    
    * fix presubmit
    
    * presubmit fix
    
    * remove storage beta file
    
    * no line end of file
    
    * space
    
    * change test assertion
    
    * change pytest

--- speech/snippets/transcribe_context_classes_test.py
+++ speech/snippets/transcribe_context_classes_test.py
@@ -14,14 +14,19 @@
 
 from google.api_core.retry import Retry
 
+import pytest
+
 import transcribe_context_classes
 
 
 @Retry()
-def test_transcribe_context_classes(capsys):
-    transcribe_context_classes.transcribe_context_classes(
+def test_transcribe_context_classes(
+    capsys: pytest.CaptureFixture
+) -> None:
+    result = transcribe_context_classes.transcribe_context_classes(
         "gs://cloud-samples-data/speech/commercial_mono.wav"
     )
     out, _ = capsys.readouterr()
 
     assert "First alternative of result " in out
+    assert result is not None
