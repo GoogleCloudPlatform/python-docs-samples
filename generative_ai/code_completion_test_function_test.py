e86dcc583492ac13cc315b23dd6f226a7132a34b
Yvonne Li <liyvonne@google.com>
2023-06-14 13:32:01 -0700

    feat(Codey): Add python sdk samples for Codey API (#10144)
    * add python sdk samples for Codey API
    
    * add empty line end of file
    
    * fix type hints
    
    * update according to Anthony suggestions
    
    * push changes
    
    * remove textwrap import
    
    * change parameters
    
    * change to complete function
    
    * change requirement.txt
    
    * remove text wrap
    
    * add minor typo
    
    * change test

--- generative_ai/code_completion_test_function_test.py
+++ generative_ai/code_completion_test_function_test.py
@@ -15,11 +15,10 @@
 import backoff
 from google.api_core.exceptions import ResourceExhausted
 
-import sentiment_analysis
+import code_completion_test_function
 
 
 @backoff.on_exception(backoff.expo, ResourceExhausted, max_time=10)
-def test_sentiment_analysis() -> None:
-
-    content = sentiment_analysis.sentiment_analysis(temperature=0).text
-    assert content is not None
+def test_code_completion_test_function() -> None:
+    content = code_completion_test_function.complete_test_function(temperature=0).text
+    assert '-> None:' in content
