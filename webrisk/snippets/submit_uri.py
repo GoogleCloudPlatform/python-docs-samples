8e18c0bc38c1d59628e09cd4de18fe24b863815d
SitaLakshmi <sita1996@gmail.com>
2023-06-21 16:07:28 -0400

    chore(webrisk): update submitUri method

--- webrisk/snippets/submit_uri.py
+++ webrisk/snippets/submit_uri.py
@@ -21,6 +21,8 @@ def submit_uri(project_id: str, uri: str) -> Submission:
     """Submits a URI suspected of containing malicious content to be reviewed.
 
     Returns a google.longrunning.Operation which, once the review is complete, is updated with its result.
+    You can use the [Pub/Sub API] (https://cloud.google.com/pubsub) to receive notifications for the
+    returned Operation.
     If the result verifies the existence of malicious content, the site will be added to the
     Google's Social Engineering lists in order to protect users that could get exposed to this
     threat in the future. Only allow-listed projects can use this method during Early Access.
@@ -35,15 +37,49 @@ def submit_uri(project_id: str, uri: str) -> Submission:
     """
     webrisk_client = webrisk_v1.WebRiskServiceClient()
 
+    # Set the URI to be submitted.
     submission = webrisk_v1.Submission()
     submission.uri = uri
 
-    request = webrisk_v1.CreateSubmissionRequest()
-    request.parent = f"projects/{project_id}"
-    request.submission = submission
+    # Confidence that a URI is unsafe.
+    threat_confidence = webrisk_v1.ThreatInfo.Confidence(
+        level=webrisk_v1.ThreatInfo.Confidence.ConfidenceLevel.MEDIUM
+    )
 
-    response = webrisk_client.create_submission(request)
+    # Context about why the URI is unsafe.
+    threat_justification = webrisk_v1.ThreatInfo.ThreatJustification(
+        # Labels that explain how the URI was classified.
+        labels=[webrisk_v1.ThreatInfo.ThreatJustification.JustificationLabel.AUTOMATED_REPORT],
+        # Free-form context on why this URI is unsafe.
+        comments=["Testing submission"]
+    )
+
+    # Set the context about the submission including the type of abuse found on the URI and
+    # supporting details.
+    threat_info = webrisk_v1.ThreatInfo(
+        # The abuse type found on the URI.
+        abuse_type=webrisk_v1.types.ThreatType.SOCIAL_ENGINEERING,
+        threat_confidence=threat_confidence,
+        threat_justification=threat_justification
+    )
+
+    # Set the details about how the threat was discovered.
+    threat_discovery = webrisk_v1.ThreatDiscovery(
+        # Platform on which the threat was discovered.
+        platform=webrisk_v1.ThreatDiscovery.Platform.MACOS,
+        # CLDR region code of the countries/regions the URI poses a threat ordered
+        # from most impact to least impact. Example: "US" for United States.
+        region_codes=["US"]
+    )
+
+    request = webrisk_v1.SubmitUriRequest(
+        parent=f"projects/{project_id}",
+        submission=submission,
+        threat_info=threat_info,
+        threat_discovery=threat_discovery
+    )
+
+    response = webrisk_client.submit_uri(request).result(timeout=30)
     return response
-
 
 # [END webrisk_submit_uri]
