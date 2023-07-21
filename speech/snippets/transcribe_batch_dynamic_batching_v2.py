57d2b5ced0d330b74ec7c5903bd25695192ef163
Nitsan Shai <shai.nitsan@gmail.com>
2023-07-21 16:46:17 -0400

    Update speech/snippets/transcribe_batch_dynamic_batching_v2.py
    Co-authored-by: Dan Lee <71398022+dandhlee@users.noreply.github.com>

--- speech/snippets/transcribe_batch_dynamic_batching_v2.py
+++ speech/snippets/transcribe_batch_dynamic_batching_v2.py
@@ -27,7 +27,7 @@ def transcribe_batch_dynamic_batching_v2(
     """Transcribes audio from a Google Cloud Storage URI.
 
     Args:
-        project_id: The GCP project ID.
+        project_id: The Google Cloud project ID.
         gcs_uri: The Google Cloud Storage URI.
 
     Returns:
