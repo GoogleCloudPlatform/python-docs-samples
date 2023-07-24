bae3afffa57f7b3a1c868a7a1aef5d6a4535b0ec
rsamborski <rsamborski@gmail.com>
2023-07-24 15:27:08 +0200

    fix(KMS): update comments to match the code

--- kms/snippets/sign_mac.py
+++ kms/snippets/sign_mac.py
@@ -24,7 +24,7 @@ def sign_mac(
     data: str,
 ) -> kms.MacSignResponse:
     """
-    Sign a message using the public key part of an asymmetric key.
+    Sign a message using the private key part of an asymmetric key.
 
     Args:
         project_id (string): Google Cloud project ID (e.g. 'my-project').
