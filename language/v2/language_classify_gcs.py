1b8926d9891f935391177f515cbcebfba68d916c
Kornraphop (Ken) Kawintiranon <kornraphop@google.com>
2023-08-23 11:40:11 -0700

    Add code samples for language_classify

---
+++ language/v2/language_classify_gcs.py
@@ -0,0 +1,84 @@
+#
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# DO NOT EDIT! This is a generated sample ("Request",  "language_classify_gcs")
+
+# To install the latest published package dependency, execute the following:
+#   pip install google-cloud-language
+
+# sample-metadata
+#   title: Classify Content (GCS)
+#   description: Classifying Content in text file stored in Cloud Storage
+#   usage: python3 samples/v2/language_classify_gcs.py [--gcs_content_uri "gs://cloud-samples-data/language/classify-entertainment.txt"]
+
+# [START language_classify_gcs]
+from google.cloud import language_v2
+
+
+def sample_classify_text(gcs_content_uri: str) -> None:
+    """
+    Classifies Content in text file stored in Cloud Storage
+
+    Args:
+      gcs_content_uri: Google Cloud Storage URI where the file content is located.
+        e.g. gs://[Your Bucket]/[Path to File]. The text file must include at least 20 words.
+    """
+
+    client = language_v2.LanguageServiceClient()
+
+    # Available types: PLAIN_TEXT, HTML
+    document_type_in_plain_text = language_v2.Document.Type.PLAIN_TEXT
+
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+    language_code = "en"
+    document = {
+        "gcs_content_uri": gcs_content_uri,
+        "type_": document_type_in_plain_text,
+        "language_code": language_code,
+    }
+
+    response = client.classify_text(request={"document": document})
+    # Loop through classified categories returned from the API
+    for category in response.categories:
+        # Get the name of the category representing the document.
+        # See the predefined taxonomy of categories:
+        # https://cloud.google.com/natural-language/docs/categories
+        print(f"Category name: {category.name}")
+        # Get the confidence. Number representing how certain the classifier
+        # is that this category represents the provided text.
+        print(f"Confidence: {category.confidence}")
+
+
+# [END language_classify_gcs]
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--gcs_content_uri",
+        type=str,
+        default="gs://cloud-samples-data/language/classify-entertainment.txt",
+    )
+    args = parser.parse_args()
+
+    sample_classify_text(args.gcs_content_uri)
+
+
+if __name__ == "__main__":
+    main()
---
+++ language/v2/language_classify_text.py
@@ -0,0 +1,85 @@
+#
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# DO NOT EDIT! This is a generated sample ("Request",  "language_classify_text")
+
+# To install the latest published package dependency, execute the following:
+#   pip install google-cloud-language
+
+# sample-metadata
+#   title: Classify Content
+#   description: Classifying Content in a String
+#   usage: python3 samples/v2/language_classify_text.py [--text_content "That actor on TV makes movies in Hollywood and also stars in a variety of popular new TV shows."]
+
+# [START language_classify_text]
+from google.cloud import language_v2
+
+
+def sample_classify_text(text_content):
+    """
+    Classifies Content in a String
+
+    Args:
+      text_content: The text content to analyze.
+    """
+
+    client = language_v2.LanguageServiceClient()
+
+    # Available types: PLAIN_TEXT, HTML
+    document_type_in_plain_text = language_v2.Document.Type.PLAIN_TEXT
+
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+    language_code = "en"
+    document = {
+        "content": text_content,
+        "type_": document_type_in_plain_text,
+        "language_code": language_code,
+    }
+
+    response = client.classify_text(
+        request={"document": document}
+    )
+    # Loop through classified categories returned from the API
+    for category in response.categories:
+        # Get the name of the category representing the document.
+        # See the predefined taxonomy of categories:
+        # https://cloud.google.com/natural-language/docs/categories
+        print(f"Category name: {category.name}")
+        # Get the confidence. Number representing how certain the classifier
+        # is that this category represents the provided text.
+        print(f"Confidence: {category.confidence}")
+
+
+# [END language_classify_text]
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--text_content",
+        type=str,
+        default="That actor on TV makes movies in Hollywood and also stars in a variety of popular new TV shows.",
+    )
+    args = parser.parse_args()
+
+    sample_classify_text(args.text_content)
+
+
+if __name__ == "__main__":
+    main()
