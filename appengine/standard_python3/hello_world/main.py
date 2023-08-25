00b2f20538700b28b654f1245a88254cd5aaf9f1
MariaTariq123 <52117930+MariaTariq123@users.noreply.github.com>
2023-08-25 12:57:05 +1000

    Update main.py

--- appengine/standard_python3/hello_world/main.py
+++ appengine/standard_python3/hello_world/main.py
@@ -23,19 +23,34 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    """Return a friendly HTTP greeting.
-
-    Returns:
-        A string with the words 'Hello World!'.
-    """
-    return "Hello World!"
-
-
-if __name__ == "__main__":
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. You
-    # can configure startup instructions by adding `entrypoint` to app.yaml.
-    app.run(host="127.0.0.1", port=8080, debug=True)
-# [END gae_python3_app]
-# [END gae_python38_app]
+<!DOCTYPE html>
+  <!-- Fashion Blog: Pro Project 11.30.20 -->
+<html>     
+  <head>
+    <title>Everyday with Isa</title>
+    </head>
+    <body>
+      <a href="#contact"><img src="https://content.codecademy.com/courses/learn-html/elements-and-structure/profile.jpg" /></a>      
+      <h3>by Isabelle Rodriguez | 1 day ago</h3>
+      <h1>An Insider's Guide to NYFW</h1>
+      <img src="https://content.codecademy.com/courses/learn-html/elements-and-structure/image-one.jpeg" />
+      <p><a href="https://en.wikipedia.org/wiki/New_York_Fashion_Week" target="_blank">NYFW</a> can be both amazingly fun & incredibly overwhelming, especially if you’ve never been. Luckily, I’m here to give you an insider’s guide and make your first show a pleasurable experience. By taking my tips and tricks, and following your gut, you’ll have an unforgettable experience!</p>
+      <h2>Getting Tickets & Picking the Shows</h2>
+        <img src="https://content.codecademy.com/courses/learn-html/elements-and-structure/image-two.jpeg" />
+        <p>If you’re lucky or connected you can get an invite, sans the price tag. But I wasn’t so lucky or connected my first 2 years so I’m here to help you out. First, plan out which shows are most important to you and make a schedule and this is a biggie: SET A BUDGET. If you’re worrying about blowing your cash the whole time you won’t have fun. Then check out prices, days, and times and prioritize the designers you want to see most. Lastly, purchase your tickets and get excited!</p>
+      <h2>Dressing for the Shows</h2>
+      <img src="https://content.codecademy.com/courses/learn-html/elements-and-structure/image-three.jpeg" />
+        <p>Always be true to your own sense of style, if you don’t you’ll be uncomfortable the whole time and it will show. Remember, NYFW is about expressing yourself and taking in what the designers have chosen to express through their new lines. Also it’s important to wear shoes you’ll be comfortable in all day. Obviously you want to look good, but you’ll be on your feet all day long, so be prepared.</p>
+        <h4>Related Content</h4>
+        <ul>
+          <li>How to Style Boyfriend Jeans</li>
+          <li>When Print is Too Much</li>
+          <li>The Overalls Trend</li>
+          <li>Fall's It Color: Blush</li>
+        </ul>
+        <div id="contact">
+          <p><strong>email:</strong> isa@fashionblog.com | <strong>phone:</strong> 917-555-1098 |
+              <strong>address:</strong> 371 284th St., New York, NY, 10001</p>
+              </div>
+      </body>
+  </html>
