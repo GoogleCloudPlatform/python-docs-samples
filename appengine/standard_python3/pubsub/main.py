27068d371a917c3e4af4e39cc706c445564f5e0e
jwdjj <jsscawdjj@gmail.com>
2023-09-07 12:25:46 -0400

    Update main.py
    To match other examples maybe it's best to comment this out or remove it
    
    context: 
    - https://github.com/GoogleCloudPlatform/python-docs-samples/blob/main/appengine/flexible/pubsub/main.py#L64-L75
    - https://github.com/GoogleCloudPlatform/python-docs-samples/blob/main/appengine/flexible_python37_and_earlier/pubsub/main.py#L64-L75
    - https://github.com/GoogleCloudPlatform/python-docs-samples/blob/main/appengine/standard/pubsub/main.py#L62-L73

--- appengine/standard_python3/pubsub/main.py
+++ appengine/standard_python3/pubsub/main.py
@@ -72,33 +72,33 @@ def receive_messages_handler():
         return "Invalid request", 400
 
     # Verify that the push request originates from Cloud Pub/Sub.
-    try:
-        # Get the Cloud Pub/Sub-generated JWT in the "Authorization" header.
-        bearer_token = request.headers.get("Authorization")
-        token = bearer_token.split(" ")[1]
-        TOKENS.append(token)
+    # try:
+    #     # Get the Cloud Pub/Sub-generated JWT in the "Authorization" header.
+    #     bearer_token = request.headers.get("Authorization")
+    #     token = bearer_token.split(" ")[1]
+    #     TOKENS.append(token)
 
-        # Verify and decode the JWT. `verify_oauth2_token` verifies
-        # the JWT signature, the `aud` claim, and the `exp` claim.
-        # Note: For high volume push requests, it would save some network
-        # overhead if you verify the tokens offline by downloading Google's
-        # Public Cert and decode them using the `google.auth.jwt` module;
-        # caching already seen tokens works best when a large volume of
-        # messages have prompted a single push server to handle them, in which
-        # case they would all share the same token for a limited time window.
-        claim = id_token.verify_oauth2_token(
-            token, requests.Request(), audience="example.com"
-        )
+    #     # Verify and decode the JWT. `verify_oauth2_token` verifies
+    #     # the JWT signature, the `aud` claim, and the `exp` claim.
+    #     # Note: For high volume push requests, it would save some network
+    #     # overhead if you verify the tokens offline by downloading Google's
+    #     # Public Cert and decode them using the `google.auth.jwt` module;
+    #     # caching already seen tokens works best when a large volume of
+    #     # messages have prompted a single push server to handle them, in which
+    #     # case they would all share the same token for a limited time window.
+    #     claim = id_token.verify_oauth2_token(
+    #         token, requests.Request(), audience="example.com"
+    #     )
 
-        # IMPORTANT: you should validate claim details not covered by signature
-        # and audience verification above, including:
-        #   - Ensure that `claim["email"]` is equal to the expected service
-        #     account set up in the push subscription settings.
-        #   - Ensure that `claim["email_verified"]` is set to true.
+    #     # IMPORTANT: you should validate claim details not covered by signature
+    #     # and audience verification above, including:
+    #     #   - Ensure that `claim["email"]` is equal to the expected service
+    #     #     account set up in the push subscription settings.
+    #     #   - Ensure that `claim["email_verified"]` is set to true.
 
-        CLAIMS.append(claim)
-    except Exception as e:
-        return f"Invalid token: {e}\n", 400
+    #     CLAIMS.append(claim)
+    # except Exception as e:
+    #     return f"Invalid token: {e}\n", 400
 
     envelope = json.loads(request.data.decode("utf-8"))
     payload = base64.b64decode(envelope["message"]["data"])
