23ea0bd36a1dfa7d2c0fb33c5974cf306264511b
Holt Skinner <holtskinner@google.com>
2023-10-13 13:22:19 -0500

    docs(discoveryengine): Initialize Multi-Turn Search Sample

---
+++ discoveryengine/multi_turn_search_sample_test.py
@@ -0,0 +1,57 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import os
+
+from discoveryengine import search_sample
+
+project_id = os.environ["GOOGLE_CLOUD_PROJECT"]
+search_query = "Google"
+
+
+def test_search():
+    location = "global"
+    data_store_id = "test-search-engine_1689960780551"
+    response = search_sample.search_sample(
+        project_id=project_id,
+        location=location,
+        data_store_id=data_store_id,
+        search_query=search_query,
+    )
+
+    assert response
+    assert response.results
+
+    for result in response.results:
+        assert result.document.name
+
+
+def test_search_eu_endpoint():
+    location = "eu"
+    data_store_id = "alphabet-earnings-reports-eu"
+    response = search_sample.search_sample(
+        project_id=project_id,
+        location=location,
+        data_store_id=data_store_id,
+        search_query=search_query,
+    )
+
+    assert response
+    assert response.summary
+    assert response.results
+
+    for result in response.results:
+        assert result.document
+        assert result.document.name
