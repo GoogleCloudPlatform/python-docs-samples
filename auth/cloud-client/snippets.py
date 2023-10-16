9a020ee2b6531ad1af243df01c446c7a89b3ebc9
Nicholas Cook <irataxy@users.noreply.github.com>
2023-10-16 10:52:42 -0700

    feat: add sample and test for getting an access token from an imperso… (#10759)
    * feat: add sample and test for getting an access token from an impersonated SA
    
    * add service account to test
    
    * trigger build

--- auth/cloud-client/snippets.py
+++ auth/cloud-client/snippets.py
@@ -70,6 +70,59 @@ def explicit_compute_engine(project):
 # [END auth_cloud_explicit_compute_engine]
 
 
+# [START auth_cloud_accesstoken_impersonated_credentials]
+def accesstoken_from_impersonated_credentials(
+    impersonated_service_account: str, scope: str
+):
+    from google.auth import impersonated_credentials
+    import google.auth.transport.requests
+
+    """
+      Use a service account (SA1) to impersonate another service account (SA2)
+      and obtain an ID token for the impersonated account.
+      To obtain a token for SA2, SA1 should have the
+      "roles/iam.serviceAccountTokenCreator" permission on SA2.
+
+    Args:
+        impersonated_service_account: The name of the privilege-bearing service account for whom the credential is created.
+            Examples: name@project.service.gserviceaccount.com
+
+        scope: Provide the scopes that you might need to request to access Google APIs,
+            depending on the level of access you need.
+            For this example, we use the cloud-wide scope and use IAM to narrow the permissions.
+            https://cloud.google.com/docs/authentication#authorization_for_services
+            For more information, see: https://developers.google.com/identity/protocols/oauth2/scopes
+    """
+
+    # Construct the GoogleCredentials object which obtains the default configuration from your
+    # working environment.
+    credentials, project_id = google.auth.default()
+
+    # Create the impersonated credential.
+    target_credentials = impersonated_credentials.Credentials(
+        source_credentials=credentials,
+        target_principal=impersonated_service_account,
+        # delegates: The chained list of delegates required to grant the final accessToken.
+        # For more information, see:
+        # https://cloud.google.com/iam/docs/create-short-lived-credentials-direct#sa-credentials-permissions
+        # Delegate is NOT USED here.
+        delegates=[],
+        target_scopes=[scope],
+        lifetime=300,
+    )
+
+    # Get the OAuth2 token.
+    # Once you've obtained the OAuth2 token, use it to make an authenticated call
+    # to the target audience.
+    request = google.auth.transport.requests.Request()
+    target_credentials.refresh(request)
+    # The token field is target_credentials.token.
+    print("Generated OAuth2 token.")
+
+
+# [END auth_cloud_accesstoken_impersonated_credentials]
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
@@ -82,6 +135,12 @@ if __name__ == "__main__":
         "explicit_compute_engine", help=explicit_compute_engine.__doc__
     )
     explicit_gce_parser.add_argument("project")
+    accesstoken_parser = subparsers.add_parser(
+        "accesstoken_from_impersonated_credentials",
+        help=accesstoken_from_impersonated_credentials.__doc__,
+    )
+    accesstoken_parser.add_argument("impersonated_service_account")
+    accesstoken_parser.add_argument("scope")
 
     args = parser.parse_args()
 
@@ -91,3 +150,7 @@ if __name__ == "__main__":
         explicit()
     elif args.command == "explicit_compute_engine":
         explicit_compute_engine(args.project)
+    elif args.command == "accesstoken_from_impersonated_credentials":
+        accesstoken_from_impersonated_credentials(
+            args.impersonated_service_account, args.scope
+        )
