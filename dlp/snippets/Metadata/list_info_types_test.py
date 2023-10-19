2dcf04c178c7794d0db6ef6fe6e0bea7142003de
Avani-Thakker-Crest <avani.thakker@crestdatasys.com>
2023-10-19 19:04:54 +0530

    Corrected import order

--- dlp/snippets/Metadata/list_info_types_test.py
+++ dlp/snippets/Metadata/list_info_types_test.py
@@ -11,10 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import list_info_types as metadata
 
 import pytest
-
-import list_info_types as metadata
 
 
 def test_fetch_info_types(capsys: pytest.CaptureFixture) -> None:
