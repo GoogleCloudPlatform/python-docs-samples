f12bb18c546362b1aa6cc77ff1862b986f7c952d
Avani-Thakker-Crest <avani.thakker@crestdatasys.com>
2023-10-30 17:15:58 +0530

    moved inspect_hotword_rule into separate file

--- dlp/snippets/custom_infotype_test.py
+++ dlp/snippets/custom_infotype_test.py
@@ -147,15 +147,3 @@ def test_inspect_with_medical_record_number_w_custom_hotwords_no_hotwords(
     out, _ = capsys.readouterr()
     assert "Info type: C_MRN" in out
     assert "Likelihood: 3" in out
-
-
-def test_inspect_with_medical_record_number_w_custom_hotwords_has_hotwords(
-    capsys: pytest.LogCaptureFixture,
-) -> None:
-    custom_infotype.inspect_data_w_custom_hotwords(
-        GCLOUD_PROJECT, "Patients MRN 444-5-22222"
-    )
-
-    out, _ = capsys.readouterr()
-    assert "Info type: C_MRN" in out
-    assert "Likelihood: 5" in out
