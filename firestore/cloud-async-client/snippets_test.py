f4794e1bee7bfeee60585bbb04f6b73e2e0f1762
Osteoporosis <tellmewhy@gmail.com>
2023-11-28 02:02:11 +0900

    Firestore - Add argument

--- firestore/cloud-async-client/snippets_test.py
+++ firestore/cloud-async-client/snippets_test.py
@@ -307,7 +307,7 @@ async def test_collection_group_query(db):
 
 async def test_update_document_increment(db):
     await db.collection("cities").document("DC").set({"population": 1})
-    await snippets.update_document_increment()
+    await snippets.update_document_increment(db)
 
 
 async def test_list_document_subcollections():
