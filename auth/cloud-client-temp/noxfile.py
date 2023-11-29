4726bf3fdfaf55c3614e32acc5c89b9eb429fea1
Anthonios Partheniou <partheniou@google.com>
2023-11-28 19:23:29 -0500

    feat: Add support for Python 3.12 (#1421)
    * feat: Add support for Python 3.12
    
    * Add samples lint session
    
    * Add constraints file for python 3.12
    
    * chore: Refresh system test creds.
    
    * only run specified nox session for samples
    
    ---------
    
    Co-authored-by: Carl Lundin <clundin@google.com>

--- auth/cloud-client-temp/noxfile.py
+++ auth/cloud-client-temp/noxfile.py
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import pathlib
-import shutil
 
 import nox
 
@@ -33,7 +31,36 @@ BLACK_PATHS = [
     "docs/conf.py",
 ]
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10"])
+
+# Error if a python version is missing
+nox.options.error_on_missing_interpreters = True
+
+#
+# Style Checks
+#
+
+
+# Linting with flake8.
+#
+# We ignore the following rules:
+#   E203: whitespace before ‘:’
+#   E266: too many leading ‘#’ for block comment
+#   E501: line too long
+#   I202: Additional newline in a section of imports
+#
+# We also need to specify the rules which are ignored by default:
+# ['E226', 'W504', 'E126', 'E123', 'W503', 'E24', 'E704', 'E121']
+FLAKE8_COMMON_ARGS = [
+    "--show-source",
+    "--builtin=gettext",
+    "--max-complexity=20",
+    "--exclude=.nox,.cache,env,lib,generated_pb2,*_pb2.py,*_pb2_grpc.py",
+    "--ignore=E121,E123,E126,E203,E226,E24,E266,E501,E704,W503,W504,I202",
+    "--max-line-length=88",
+]
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11", "3.12"])
 def unit(session):
     # constraints_path = str(
     #     CURRENT_DIRECTORY / "testing" / f"constraints-{session.python}.txt"
@@ -46,3 +73,13 @@ def unit(session):
         "snippets_test.py",
         # "tests_async",
     )
+
+
+@nox.session
+def lint(session: nox.sessions.Session) -> None:
+    session.install("flake8")
+
+    args = FLAKE8_COMMON_ARGS + [
+        ".",
+    ]
+    session.run("flake8", *args)
