b712146f77a4bc371bef6a5e381d58b4c24d3b73
Rick DeBroux <rickde@google.com>
2023-12-21 17:07:28 -0800

    fix: include blob count in printout

--- practice-folder/intermediate-sample/main_test.py
+++ practice-folder/intermediate-sample/main_test.py
@@ -48,7 +48,7 @@ def test_list_blobs(bucket_name, capsys):
 
     captured = capsys.readouterr()
     lines = captured.out.split("\n")
-    assert len(lines) == 3
+    assert len(lines) == 4
     assert "blob-one" in lines
     assert "blob-two" in lines
-    assert lines[2] == ""  # Trailing newline from print leaves empty line
+    assert lines[-1] == ""  # Trailing newline from print leaves empty line
