57d373c75505fe2d9cb5fcbe25429a55f95dd6a1
Stephanie A <129541811+DevStephanie@users.noreply.github.com>
2024-01-25 14:34:17 -0600

    docs: update to use `query_and_wait` in `client_query_w_positional_params.py` (#1786)
    * Updates file
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * Correcting RowIterator
    
    * Variable corrections
    
    ---------
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>
    Co-authored-by: Tim Swast <swast@google.com>

--- bigquery/client_query_w_positional_params.py
+++ bigquery/client_query_w_positional_params.py
@@ -35,8 +35,10 @@ def client_query_w_positional_params() -> None:
             bigquery.ScalarQueryParameter(None, "INT64", 250),
         ]
     )
-    query_job = client.query(query, job_config=job_config)  # Make an API request.
+    results = client.query_and_wait(
+        query, job_config=job_config
+    )  # Make an API request.
 
-    for row in query_job:
+    for row in results:
         print("{}: \t{}".format(row.word, row.word_count))
     # [END bigquery_query_params_positional]
