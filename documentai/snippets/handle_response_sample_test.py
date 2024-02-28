664a5692dd193a2d53cabd3d65c25b665adacfcf
Jiaping Chen <jiapingchen17@gmail.com>
2024-02-28 10:47:18 -0800

    updating processor version for test_process_document_summarizer function

--- documentai/snippets/handle_response_sample_test.py
+++ documentai/snippets/handle_response_sample_test.py
@@ -117,7 +117,6 @@ def test_process_document_quality(capsys):
     location = "us"
     project_id = os.environ["GOOGLE_CLOUD_PROJECT"]
     processor_id = "52a38e080c1a7296"
-    # processor_version = "pretrained-ocr-v1.0-2020-09-23"
     processor_version = "pretrained-ocr-v2.0-2023-06-02"
     poor_quality_file_path = "resources/document_quality_poor.pdf"
     mime_type = "application/pdf"
@@ -201,7 +200,8 @@ def test_process_document_summarizer(capsys):
     location = "us"
     project_id = os.environ["GOOGLE_CLOUD_PROJECT"]
     processor_id = "feacd98c28866ede"
-    processor_version = "stable"
+    # processor_version = "stable"
+    processor_version = "pretrained-foundation-model-v1.0-2023-08-22"
     file_path = "resources/superconductivity.pdf"
     mime_type = "application/pdf"
 
