d96969f43f9e1e2c11a26e408178df8affbbe193
Holt Skinner <holtskinner@google.com>
2024-03-05 10:23:20 -0600

    Change imports

--- discoveryengine/search_sample.py
+++ discoveryengine/search_sample.py
@@ -17,11 +17,7 @@
 from typing import List
 
 from google.api_core.client_options import ClientOptions
-from google.cloud.discoveryengine_v1 import (
-    SearchRequest,
-    SearchResponse,
-    SearchServiceClient,
-)
+from google.cloud import discoveryengine_v1 as discoveryengine
 
 # TODO(developer): Uncomment these variables before running the sample.
 # project_id = "YOUR_PROJECT_ID"
@@ -35,7 +31,7 @@ def search_sample(
     location: str,
     data_store_id: str,
     search_query: str,
-) -> List[SearchResponse]:
+) -> List[discoveryengine.SearchResponse]:
     #  For more information, refer to:
     # https://cloud.google.com/generative-ai-app-builder/docs/locations#specify_a_multi-region_for_your_data_store
     client_options = (
@@ -45,7 +41,7 @@ def search_sample(
     )
 
     # Create a client
-    client = SearchServiceClient(client_options=client_options)
+    client = discoveryengine.SearchServiceClient(client_options=client_options)
 
     # The full resource name of the search engine serving config
     # e.g. projects/{project_id}/locations/{location}/dataStores/{data_store_id}/servingConfigs/{serving_config_id}
@@ -58,39 +54,41 @@ def search_sample(
 
     # Optional: Configuration options for search
     # Refer to the `ContentSearchSpec` reference for all supported fields:
-    # https://cloud.google.com/python/docs/reference/discoveryengine/latest/google.cloud.discoveryengine_v1.types.SearchRequest.ContentSearchSpec
-    content_search_spec = SearchRequest.ContentSearchSpec(
+    # https://cloud.google.com/python/docs/reference/discoveryengine/latest/google.cloud.discoveryengine_v1.types.discoveryengine.SearchRequest.ContentSearchSpec
+    content_search_spec = discoveryengine.SearchRequest.ContentSearchSpec(
         # For information about snippets, refer to:
         # https://cloud.google.com/generative-ai-app-builder/docs/snippets
-        snippet_spec=SearchRequest.ContentSearchSpec.SnippetSpec(return_snippet=True),
+        snippet_spec=discoveryengine.SearchRequest.ContentSearchSpec.SnippetSpec(
+            return_snippet=True
+        ),
         # For information about search summaries, refer to:
         # https://cloud.google.com/generative-ai-app-builder/docs/get-search-summaries
-        summary_spec=SearchRequest.ContentSearchSpec.SummarySpec(
+        summary_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec(
             summary_result_count=5,
             include_citations=True,
             ignore_adversarial_query=True,
             ignore_non_summary_seeking_query=True,
-            model_prompt_spec=SearchRequest.ContentSearchSpec.SummarySpec.ModelPromptSpec(
+            model_prompt_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec.ModelPromptSpec(
                 preamble="YOUR_CUSTOM_PROMPT"
             ),
-            model_spec=SearchRequest.ContentSearchSpec.SummarySpec.ModelSpec(
+            model_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec.ModelSpec(
                 version="stable",  # Options: stable, preview
             ),
         ),
     )
 
-    # Refer to the `SearchRequest` reference for all supported fields:
-    # https://cloud.google.com/python/docs/reference/discoveryengine/latest/google.cloud.discoveryengine_v1.types.SearchRequest
-    request = SearchRequest(
+    # Refer to the `discoveryengine.SearchRequest` reference for all supported fields:
+    # https://cloud.google.com/python/docs/reference/discoveryengine/latest/google.cloud.discoveryengine_v1.types.discoveryengine.SearchRequest
+    request = discoveryengine.SearchRequest(
         serving_config=serving_config,
         query=search_query,
         page_size=10,
         content_search_spec=content_search_spec,
-        query_expansion_spec=SearchRequest.QueryExpansionSpec(
-            condition=SearchRequest.QueryExpansionSpec.Condition.AUTO,
+        query_expansion_spec=discoveryengine.SearchRequest.QueryExpansionSpec(
+            condition=discoveryengine.SearchRequest.QueryExpansionSpec.Condition.AUTO,
         ),
-        spell_correction_spec=SearchRequest.SpellCorrectionSpec(
-            mode=SearchRequest.SpellCorrectionSpec.Mode.AUTO
+        spell_correction_spec=discoveryengine.SearchRequest.SpellCorrectionSpec(
+            mode=discoveryengine.SearchRequest.SpellCorrectionSpec.Mode.AUTO
         ),
     )
 
