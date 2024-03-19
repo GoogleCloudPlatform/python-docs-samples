0b3ff858f940826aea9c749ad11ef9ef4218d45b
Mark Iverson <iverson.mark@gmail.com>
2024-03-18 18:11:49 -0700

    fix: update the last_updated string (#11116)
    * fix: update the last_updated string
    
    * fix: make test use new last_updated value

--- practice-folder/beginner-sample/hello_test.py
+++ practice-folder/beginner-sample/hello_test.py
@@ -27,7 +27,7 @@ def test_home_page(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.text.startswith("Hello. This page was last updated at ")
-    assert response.text.endswith("10:27 PM PST, Friday, December 1, 2023.")
+    assert response.text.endswith("3:01 PM PST, Friday, January 8, 2024.")
 
 
 def test_other_page(client):
