b24c0d406030ad486fee92c52aee9ba65cd23db6
lee1premium <129150001+lee1premium@users.noreply.github.com>
2024-04-03 08:11:35 -0700

    feat: Elastic Text-Embedding Model demo. (#11364)

--- generative_ai/embedding_preview.py
+++ generative_ai/embedding_preview.py
@@ -12,23 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# [START aiplatform_sdk_embedding]
-from typing import List
+# [START generativeaionvertexai_sdk_embedding]
+from typing import List, Optional
 
 from vertexai.language_models import TextEmbeddingInput, TextEmbeddingModel
 
 
 def embed_text(
     texts: List[str] = ["banana muffins? ", "banana bread? banana muffins?"],
-    task: str = "RETRIEVAL_DOCUMENT",
-    model_name: str = "textembedding-gecko@003"
+    task: str = "QUESTION_ANSWERING",
+    model_name: str = "text-embedding-preview-0409",
+    dimensionality: Optional[int] = 256
 ) -> List[List[float]]:
     """Embeds texts with a pre-trained, foundational model."""
     model = TextEmbeddingModel.from_pretrained(model_name)
     inputs = [TextEmbeddingInput(text, task) for text in texts]
-    embeddings = model.get_embeddings(inputs)
+    kwargs = dict(output_dimensionality=dimensionality) if dimensionality else {}
+    embeddings = model.get_embeddings(inputs, **kwargs)
     return [embedding.values for embedding in embeddings]
-# [END aiplatform_sdk_embedding]
+# [END generativeaionvertexai_sdk_embedding]
 
 
 if __name__ == "__main__":
