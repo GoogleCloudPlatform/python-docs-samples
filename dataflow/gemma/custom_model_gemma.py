dcc56c3448f12bf346d8f6327779ec87099e571f
Jack McCluskey <thejackmccluskey@gmail.com>
2024-04-03 14:18:31 -0400

    import linting

--- dataflow/gemma/custom_model_gemma.py
+++ dataflow/gemma/custom_model_gemma.py
@@ -12,10 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
-
 from collections.abc import Iterable
 from collections.abc import Sequence
+
+import logging
+
 from typing import Any
 from typing import Optional
 
