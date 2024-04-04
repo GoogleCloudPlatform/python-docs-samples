0dae63e68ba48dc6d88fdfcd9d21a0173a9f8434
Karl Weinmeister <kweinmeister@google.com>
2024-04-04 07:46:52 -0500

    review feedback

--- appengine/flexible_python37_and_earlier/storage/noxfile_config.py
+++ appengine/flexible_python37_and_earlier/storage/noxfile_config.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Google LLC
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,14 +26,18 @@ TEST_CONFIG_OVERRIDE = {
     "ignored_versions": ["2.7", "3.8", "3.9", "3.10", "3.11", "3.12"],
     # Old samples are opted out of enforcing Python type hints
     # All new samples should feature them
-    "enforce_type_hints": False,
+    "enforce_type_hints": True,
     # An envvar key for determining the project id to use. Change it
     # to 'BUILD_SPECIFIC_GCLOUD_PROJECT' if you want to opt in using a
     # build specific Cloud project. You can also use your own string
     # to use your own Cloud project.
     "gcloud_project_env": "GOOGLE_CLOUD_PROJECT",
     # 'gcloud_project_env': 'BUILD_SPECIFIC_GCLOUD_PROJECT',
+    # If you need to use a specific version of pip,
+    # change pip_version_override to the string representation
+    # of the version number, for example, "20.2.4"
+    "pip_version_override": None,    
     # A dictionary you want to inject into your test. Don't put any
     # secrets here. These values will override predefined values.
-    "envs": {},
+    "envs": {"CLOUD_STORAGE_BUCKET": "python-docs-samples-tests-public"},
 }
