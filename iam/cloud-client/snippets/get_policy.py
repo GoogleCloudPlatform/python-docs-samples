4f9cc5658b2a6ea24a43cf6d4084ea6233543216
BigBlackWolf <srasp@softserveinc.com>
2024-04-22 14:44:46 +0200

    mend

--- iam/cloud-client/snippets/get_policy.py
+++ iam/cloud-client/snippets/get_policy.py
@@ -17,6 +17,7 @@
 # [START iam_service_account_get_policy]
 from google.cloud import iam_admin_v1
 from google.iam.v1 import iam_policy_pb2, policy_pb2
+from google.cloud import resourcemanager_v3
 
 
 def get_policy(project_id: str, account: str) -> policy_pb2.Policy:
@@ -26,11 +27,19 @@ def get_policy(project_id: str, account: str) -> policy_pb2.Policy:
     account: ID or email which is unique identifier of the service account.
     """
 
-    iam_client = iam_admin_v1.IAMClient()
-    request = iam_policy_pb2.GetIamPolicyRequest()
-    request.resource = f"projects/{project_id}/serviceAccounts/{account}"
+    # iam_client = iam_admin_v1.IAMClient()
+    # request = iam_policy_pb2.GetIamPolicyRequest()
+    # request.resource = f"projects/{project_id}/serviceAccounts/{account}"
 
-    policy = iam_client.get_iam_policy(request)
+    # policy = iam_client.get_iam_policy(request)
+    # print(policy)
+    # return policy
+
+    client = resourcemanager_v3.ProjectsClient()
+    request = iam_policy_pb2.GetIamPolicyRequest()
+    request.resource = f"projects/{project_id}"
+
+    policy = client.get_iam_policy(request)
     print(policy)
     return policy
 
