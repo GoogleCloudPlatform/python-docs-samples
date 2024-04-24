8221f1cf375500b04bb51dc2d6f1c523e0382e80
Iurii Rudyk <irudyk@softserveinc.com>
2024-04-24 14:26:19 +0300

    [snipets][compute_ip_address_get_static_address]

--- compute/client_library/snippets/tests/test_ip_address.py
+++ compute/client_library/snippets/tests/test_ip_address.py
@@ -15,6 +15,8 @@
 import uuid
 
 import google.auth
+from google.cloud.compute_v1 import AddressesClient, GlobalAddressesClient
+from google.cloud.compute_v1.types import Address
 import pytest
 
 from ..instances.create_start_instance.create_from_public_image import (
@@ -24,6 +26,7 @@ from ..instances.create_start_instance.create_from_public_image import (
 )
 
 from ..instances.delete import delete_instance
+from ..instances.ip_address.get_static_ip_address import get_static_ip_address
 from ..instances.ip_address.get_vm_address import get_instance_ip_address, IPType
 
 PROJECT = google.auth.default()[1]
@@ -53,6 +56,49 @@ def instance_with_ips(disk_fixture):
     finally:
         # Cleanup after the test
         delete_instance(PROJECT, INSTANCE_ZONE, instance_name)
+
+
+@pytest.fixture
+def static_ip(request):
+    region = request.param["region"]
+    address_name = f"ip-{uuid.uuid4()}"
+
+    client_class = GlobalAddressesClient if region is None else AddressesClient
+    client = client_class()
+
+    # Create an IP address
+    address = Address(
+        name=address_name, address_type="EXTERNAL", network_tier="PREMIUM"
+    )
+    if region:
+        address.region = region
+        operation = client.insert(
+            project=PROJECT, region=region, address_resource=address
+        )
+    else:
+        operation = client.insert(project=PROJECT, address_resource=address)
+    operation.result()
+
+    yield address
+
+    # Cleanup
+    if region:
+        operation = client.delete(project=PROJECT, region=region, address=address_name)
+    else:
+        operation = client.delete(project=PROJECT, address=address_name)
+    operation.result()
+
+
+@pytest.mark.parametrize(
+    "static_ip", [{"region": None}, {"region": "us-central1"}], indirect=True
+)
+def test_get_static_ip(static_ip: Address):
+    region = static_ip.region.split("/")[-1] if static_ip.region else None
+    actual_address = get_static_ip_address(
+        project_id=PROJECT, address_name=static_ip.name, region=region
+    )
+    assert static_ip.region in actual_address.region
+    assert static_ip.name == actual_address.name
 
 
 def test_get_instance_external_ip_address(instance_with_ips):
