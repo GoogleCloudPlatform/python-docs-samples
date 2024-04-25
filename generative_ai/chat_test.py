25dcfc949c5a829fc967758c2b94b64780dc3b1c
Eric Dong <itseric@google.com>
2024-04-25 11:32:54 -0400

    refactor(generative-ai): Update chat sample to the new style (#11587)

--- generative_ai/chat_test.py
+++ generative_ai/chat_test.py
@@ -19,8 +19,5 @@ import chat
 
 
 @backoff.on_exception(backoff.expo, ResourceExhausted, max_time=10)
-def test_science_tutoring() -> None:
-    assert (
-        "There are eight planets in the solar system."
-        == chat.science_tutoring(temperature=0).text
-    )
+def test_send_chat() -> None:
+    assert len(chat.send_chat()) > 0
