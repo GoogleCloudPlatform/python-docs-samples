3ea2863b5b4f34b474be8d20892917f06b3a9413
Stepan Rasputny <srasp@softserveinc.com>
2024-04-29 12:18:01 +0200

    feat: Service account snippet (#11444)
    * feat: Create service account snippet
    
    * feat: list and delete service account snippets
    
    * Comment adjustments
    
    * feat: disable service account sample
    
    * feat: enable service account sample
    
    * Adjusments regarding testing approach
    
    * Updated according to the comments
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    ---------
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

---
+++ iam/cloud-client/snippets/create_service_account.py
@@ -0,0 +1,64 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This file contains code samples that demonstrate how to get create service account.
+
+# [START iam_create_service_account]
+from typing import Optional
+
+from google.cloud import iam_admin_v1
+from google.cloud.iam_admin_v1 import types
+
+
+def create_service_account(
+    project_id: str, account_id: str, display_name: Optional[str] = None
+) -> types.ServiceAccount:
+    """
+    Creates a service account.
+
+    project_id: ID or number of the Google Cloud project you want to use.
+    account_id: ID which will be unique identifier of the service account
+    display_name (optional): human-readable name, which will be assigned to the service account
+    """
+
+    iam_admin_client = iam_admin_v1.IAMClient()
+    request = types.CreateServiceAccountRequest()
+
+    request.account_id = account_id
+    request.name = f"projects/{project_id}"
+
+    service_account = types.ServiceAccount()
+    service_account.display_name = display_name
+    request.service_account = service_account
+
+    account = iam_admin_client.create_service_account(request=request)
+
+    print(f"Created a service account: {account.email}")
+    return account
+
+
+# [END iam_create_service_account]
+
+
+if __name__ == "__main__":
+    # To run the sample you would need
+    # iam.serviceAccounts.create permission (roles/iam.serviceAccountCreator)
+
+    # Your Google Cloud project ID.
+    project_id = "your-google-cloud-project-id"
+
+    # Existing service account name within the project specified above.
+    account_id = account_name = "test-service-account"
+
+    create_service_account(project_id, account_id, account_name)
