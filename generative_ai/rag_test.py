93ad423c910d9624a6a0829fbfcd39c0f1e9ea08
Holt Skinner <holtskinner@google.com>
2024-04-30 11:51:10 -0500

    Re-add test skip

--- generative_ai/rag_test.py
+++ generative_ai/rag_test.py
@@ -25,6 +25,9 @@ PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT")
 LOCATION = "us-central1"
 GCS_FILE = "gs://cloud-samples-data/generative-ai/pdf/earnings_statement.pdf"
 
+# TODO: Remove once Allowlist is removed
+pytest.skip(allow_module_level=True)
+
 vertexai.init(project=PROJECT_ID, location=LOCATION)
 
 
