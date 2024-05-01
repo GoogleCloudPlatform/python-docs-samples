eb43e414fb3b7221596ccad8e48e80aa315797c4
riathakkar <riathakkar16@gmail.com>
2024-05-01 23:39:06 +0000

    Modified lint

--- iap/generate_self_signed_jwt.py
+++ iap/generate_self_signed_jwt.py
@@ -20,71 +20,70 @@ import datetime
 from google.cloud import iam_credentials_v1
 
 def generate_jwt_payload(service_account_email: str, resource_url: str) -> str:
-  """Generates JWT payload for service account.
+   """Generates JWT payload for service account.
 
-  The resource url provided must be the same as the url of the IAP secured resource.
-  Args:
-    service_account_email (str): Specifies service account JWT is created for
-    resource_url (str): Specifies scope of the JWT, the URL that the JWT will be allowed to access.
-  Returns:
-    A signed-jwt that can be used to access IAP protected applications.
-    Sample: curl --verbose --header 'Authorization: Bearer SIGNED_JWT' URL
-  """
-  iat = datetime.datetime.now(tz=datetime.timezone.utc)
-  iat = datetime.datetime.now(tz=datetime.timezone.utc)
-  exp = iat + 3600
-  return json.dumps({
-      'iss': service_account_email,
-      'sub': service_account_email,
-      'aud': resource_url,
-      'iat': iat,
-      'exp': exp,
-  })
+   The resource url provided must be the same as the url of the IAP secured resource.
+   Args:
+     service_account_email (str): Specifies service account JWT is created for
+     resource_url (str): Specifies scope of the JWT, the URL that the JWT will be allowed to access.
+   Returns:
+     A signed-jwt that can be used to access IAP protected applications.
+     Sample: curl --verbose --header 'Authorization: Bearer SIGNED_JWT' URL
+   """
+   iat = datetime.datetime.now(tz=datetime.timezone.utc)
+   exp = iat + 3600
+   return json.dumps({
+       'iss': service_account_email,
+       'sub': service_account_email,
+       'aud': resource_url,
+       'iat': iat,
+       'exp': exp,
+   })
 
 def sign_jwt(target_sa: str, resource_url: str) -> str:
-  """Signs JWT payload using ADC and IAM credentials API 
-  Args:
-    target_sa (str): Service Account JWT is being created for. This service account will require IAM permissions
-    resource_url (str): Audience of the JWT, and scope of the JWT token. This is the url of the IAP protected application.
-  Returns:
-    A signed-jwt that can be used to access IAP protected apps.
-  """
-  # Uses Application Default Credentials
-  source_credentials, _ = google.auth.default()
-  # use the IAM api and the users credentials to sign the JWT
-  iam_client = iam_credentials_v1.IAMCredentialsClient(credentials=source_credentials)
-  return iam_client.sign_jwt(
-      name=iam_client.service_account_path('-', target_sa),
-      payload=generate_jwt_payload(target_sa, resource_url),
-  ).signed_jwt
+   """Signs JWT payload using ADC and IAM credentials API 
+   Args:
+     target_sa (str): Service Account JWT is being created for. This service account will require IAM permissions
+     resource_url (str): Audience of the JWT, and scope of the JWT token. This is the url of the IAP protected application.
+   Returns:
+     A signed-jwt that can be used to access IAP protected apps.
+   """
+   # Uses Application Default Credentials
+   source_credentials, _ = google.auth.default()
+   # use the IAM api and the users credentials to sign the JWT
+   iam_client = iam_credentials_v1.IAMCredentialsClient(credentials=source_credentials)
+   return iam_client.sign_jwt(
+       name=iam_client.service_account_path('-', target_sa),
+       payload=generate_jwt_payload(target_sa, resource_url),
+   ).signed_jwt
 
 def sign_jwt_with_key_file(credential_key_file_path: str, resource_url: str) -> str:
-  """Signs JWT payload using local service account credential key file
-  Args:
-    credential_key_file_path (str): Path to the downloaded JSON credentials of the service account the JWT is being created for.
-    resource_url (str): Scope of JWT token, This is the url of the IAP protected application.
-  Returns:
-    A self-signed JWT created with a downloaded private key.
-  """
-  with open(credential_key_file_path, 'r') as credential_key_file:
-      key_data = json.load(credential_key_file)
+   """Signs JWT payload using local service account credential key file
+   Args:
+     credential_key_file_path (str): Path to the downloaded JSON credentials of the service account the JWT is being created for.
+     resource_url (str): Scope of JWT token, This is the url of the IAP protected application.
+   Returns:
+     A self-signed JWT created with a downloaded private key.
+   """
+   with open(credential_key_file_path, 'r') as credential_key_file:
+       key_data = json.load(credential_key_file)
 
-  PRIVATE_KEY_ID_FROM_JSON = key_data["private_key_id"]
-  PRIVATE_KEY_FROM_JSON = key_data["private_key"]
-  SERVICE_ACCOUNT_EMAIL=key_data["client_email"]
+   PRIVATE_KEY_ID_FROM_JSON = key_data["private_key_id"]
+   PRIVATE_KEY_FROM_JSON = key_data["private_key"]
+   SERVICE_ACCOUNT_EMAIL=key_data["client_email"]
 
-  # Sign JWT with private key and store key id in the header
-  additional_headers = {'kid': PRIVATE_KEY_ID_FROM_JSON}
-  payload = generate_jwt_payload(service_account_email=SERVICE_ACCOUNT_EMAIL, resource_url=resource_url)
+   # Sign JWT with private key and store key id in the header
+   additional_headers = {'kid': PRIVATE_KEY_ID_FROM_JSON}
+   payload = generate_jwt_payload(service_account_email=SERVICE_ACCOUNT_EMAIL, resource_url=resource_url)
 
 
-  signed_jwt = jwt.encode(
-      payload,
-      PRIVATE_KEY_FROM_JSON,
-      headers=additional_headers,
-      algorithm='RS256',
-  )
-  return signed_jwt
+   signed_jwt = jwt.encode(
+       payload,
+       PRIVATE_KEY_FROM_JSON,
+       headers=additional_headers,
+       algorithm='RS256',
+   )
+   return signed_jwt
 
 # sign_jwt("test_email", "resource-url")
 # sign_jwt_with_key_file("/path/to/local/key/file.json", "resource-url")
