62698961577f201c1efd7efa5ea3efbd00547d04
Nick Cook <nickcook@google.com>
2024-05-10 17:24:56 -0700

    keep prompt to one line with no escape char

--- generative_ai/gemini_text_input_example.py
+++ generative_ai/gemini_text_input_example.py
@@ -26,10 +26,7 @@ def generate_from_text_input(project_id: str) -> str:
     model = GenerativeModel(model_name="gemini-1.0-pro-002")
 
     response = model.generate_content(
-        """
-        What\'s a good name for a flower shop that specializes in selling
-        bouquets of dried flowers?
-        """
+        "What's a good name for a flower shop that specializes in selling bouquets of dried flowers?"
     )
 
     print(response.text)
