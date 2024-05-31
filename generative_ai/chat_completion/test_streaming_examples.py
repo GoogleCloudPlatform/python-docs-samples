c361d79b59d21711d9a41da7b9960d6e84c12113
Sampath M <sampathm@google.com>
2024-05-31 18:46:00 +0200

    feat(generative_ai): Add code completion example
    Add code completion examples with gemini flash model

---
+++ generative_ai/chat_completion/test_streaming_examples.py
@@ -0,0 +1,47 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+import non_streaming_image
+import non_streaming_text
+import streaming_image
+import streaming_text
+
+PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT")
+REGION = "us-central1"
+
+
+def test_streaming_text() -> None:
+    response = streaming_text.example()
+    assert type(response) is str, "Expected text response"
+    assert len(response) > 0, "Expected text response"
+
+
+def test_non_streaming_text() -> None:
+    response = streaming_image.example()
+    assert type(response) is str, "Expected text response"
+    assert len(response) > 0, "Expected text response"
+
+
+def test_streaming_image() -> None:
+    response = non_streaming_text.example()
+    assert type(response) is str, "Expected text response"
+    assert len(response) > 0, "Expected text response"
+
+
+def test_non_streaming_image() -> None:
+    response = non_streaming_image.example()
+    assert type(response) is str, "Expected text response"
+    assert len(response) > 0, "Expected text response"
