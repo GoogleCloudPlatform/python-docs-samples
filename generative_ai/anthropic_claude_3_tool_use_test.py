9833deac1b597726f5b6231261960234c88233c4
Huy Ngo <43686519+huythngo@users.noreply.github.com>
2024-05-31 10:44:19 -0700

    Add tool use sample for Claude model (#11807)
    * add sample for anthropic tool use
    
    * lint
    
    ---------
    
    Co-authored-by: Huy Ngo <huyngo@google.com>

---
+++ generative_ai/anthropic_claude_3_tool_use_test.py
@@ -0,0 +1,34 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+import backoff
+from google.api_core.exceptions import ResourceExhausted
+
+import anthropic_claude_3_tool_use
+
+_PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT")
+_LOCATION = "us-east5"
+
+
+@backoff.on_exception(backoff.expo, ResourceExhausted, max_time=10)
+def tool_use_test() -> None:
+    response = anthropic_claude_3_tool_use.tool_use(
+        project_id=_PROJECT_ID, region=_LOCATION
+    )
+    json_response = response.model_dump_json(indent=2)
+    assert "restaurant" in json_response
+    assert "tool_use" in json_response
+    assert "text_search_places_api" in json_response
