254996f536f71a04cc5e0bd8aef42af50269d4c4
Holt Skinner <holtskinner@google.com>
2024-06-14 12:31:06 -0500

    samples(aiplatform): Update Grounding with Google Search sample to use GA endpoint

--- generative_ai/gemini_grounding_example.py
+++ generative_ai/gemini_grounding_example.py
@@ -13,22 +13,26 @@
 # limitations under the License.
 
 
-from vertexai.preview.generative_models import GenerationResponse
+from vertexai.generative_models import GenerationResponse
 
 
 def generate_text_with_grounding_web(project_id: str) -> GenerationResponse:
     # [START generativeaionvertexai_gemini_grounding_with_web]
     import vertexai
 
-    from vertexai.preview.generative_models import grounding
-    from vertexai.generative_models import GenerationConfig, GenerativeModel, Tool
+    from vertexai.generative_models import (
+        GenerationConfig,
+        GenerativeModel,
+        Tool,
+        grounding,
+    )
 
     # TODO(developer): Update and un-comment below line
     # project_id = "PROJECT_ID"
 
     vertexai.init(project=project_id, location="us-central1")
 
-    model = GenerativeModel(model_name="gemini-1.0-pro-002")
+    model = GenerativeModel(model_name="gemini-1.5-pro-001")
 
     # Use Google Search for grounding
     tool = Tool.from_google_search_retrieval(grounding.GoogleSearchRetrieval())
@@ -62,7 +66,7 @@ def generate_text_with_grounding_vertex_ai_search(
 
     vertexai.init(project=project_id, location="us-central1")
 
-    model = GenerativeModel(model_name="gemini-1.0-pro-002")
+    model = GenerativeModel(model_name="gemini-1.5-pro-001")
 
     # Use Vertex AI Search data store
     # Format: projects/{project_id}/locations/{location}/collections/default_collection/dataStores/{data_store_id}
