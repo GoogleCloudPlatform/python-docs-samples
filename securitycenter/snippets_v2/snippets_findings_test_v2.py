72548dbf8a0cf724a75a669b432eaafa098f33e4
Vinod Thokala <vthokala@google.com>
2024-06-25 00:15:06 +0000

    added findings v2 version

---
+++ securitycenter/snippets_v2/snippets_findings_test_v2.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+#
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from itertools import chain
+import os
+
+import pytest
+
+import snippets_findings_v2
+
+
+@pytest.fixture(scope="module")
+def organization_id():
+    """Get Organization ID from the environment variable"""
+    return os.environ["GCLOUD_ORGANIZATION"]
+
+
+@pytest.fixture(scope="module")
+def source_name(organization_id):
+    from google.cloud import securitycenter_v2
+
+    client = securitycenter_v2.SecurityCenterClient()
+    org_name = f"organizations/{organization_id}"
+
+    source = client.create_source(
+        request={
+            "parent": org_name,
+            "source": {
+                "display_name": "Unit test source",
+                "description": "A new custom source that does X",
+            },
+        }
+    )
+    return source.name
+
+def test_list_all_findings(organization_id):
+    count = snippets_findings_v2.list_all_findings(organization_id)
+    assert count > 0
+
+
+def test_list_filtered_findings(source_name):
+    count = snippets_findings_v2.list_filtered_findings(source_name)
+    assert count > 0
+
+
+def test_group_all_findings(organization_id):
+    count = snippets_findings_v2.group_all_findings(organization_id)
+    assert count > 0
+
+
+def test_group_filtered_findings(source_name):
+    count = snippets_findings_v2.group_filtered_findings(source_name)
+    assert count == 0
+
+
+
---
+++ securitycenter/snippets_v2/snippets_findings_v2.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+#
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Examples of working with source and findings in Security Command Center."""
+
+def list_all_findings(organization_id):
+  # [START securitycenter_list_all_findings]
+  from google.cloud import securitycenter_v2
+
+  # Create a client.
+  client = securitycenter_v2.SecurityCenterClient()
+
+  # 'parent' must be in one of the following formats:
+  #   "organizations/{organization_id}"
+  #   "projects/{project_id}"
+  #   "folders/{folder_id}"
+  parent = f"organizations/{organization_id}"
+  # The "sources/-" suffix lists findings across all sources.  You
+  # also use a specific source_name instead.
+  all_sources = f"{parent}/sources/-/locations/global"
+  finding_result_iterator = client.list_findings(request={"parent": all_sources})
+  for i, finding_result in enumerate(finding_result_iterator):
+    print(
+        "{}: name: {} resource: {}".format(
+            i, finding_result.finding.name, finding_result.finding.resource_name
+        )
+    )
+  # [END securitycenter_list_all_findings]
+  return i
+
+
+def list_filtered_findings(source_name):
+  # [START securitycenter_list_filtered_findings]
+  from google.cloud import securitycenter_v2
+
+  # Create a new client.
+  client = securitycenter_v2.SecurityCenterClient()
+
+  # 'source_name' is the resource path for a source that has been
+  # created previously (you can use list_sources to find a specific one).
+  # Its format is:
+  # source_name = f"{parent}/sources/{source_id}"
+  # 'parent' must be in one of the following formats:
+  #   "organizations/{organization_id}"
+  #   "projects/{project_id}"
+  #   "folders/{folder_id}"
+  # You an also use a wild-card "-" for all sources:
+  #   source_name = "organizations/111122222444/sources/-/locations/{location_id}"
+  finding_result_iterator = client.list_findings(
+      request={"parent": source_name, "filter": 'category="MEDIUM_RISK_ONE"'}
+  )
+  # Iterate an print all finding names and the resource they are
+  # in reference to.
+  for i, finding_result in enumerate(finding_result_iterator):
+    print(
+        "{}: name: {} resource: {}".format(
+            i, finding_result.finding.name, finding_result.finding.resource_name
+        )
+    )
+  # [END securitycenter_list_filtered_findings]
+  return i
+
+
+def group_all_findings(organization_id):
+  """Demonstrates grouping all findings across an organization."""
+  i = 0
+  # [START securitycenter_group_all_findings]
+  from google.cloud import securitycenter_v2
+
+  # Create a client.
+  client = securitycenter_v2.SecurityCenterClient()
+
+  # 'parent' must be in one of the following formats:
+  #   "organizations/{organization_id}"
+  #   "projects/{project_id}"
+  #   "folders/{folder_id}"
+  parent = f"organizations/{organization_id}"
+  # The "sources/-" suffix lists findings across all sources.  You
+  # also use a specific source_name instead.
+  all_sources = f"{parent}/sources/-/locations/global"
+  group_result_iterator = client.group_findings(
+      request={"parent": all_sources, "group_by": "category"}
+  )
+  for i, group_result in enumerate(group_result_iterator):
+    print((i + 1), group_result)
+  # [END securitycenter_group_all_findings]
+  return i
+
+
+def group_filtered_findings(source_name):
+  """Demonstrates grouping all findings across an organization."""
+  i = 0
+  # [START securitycenter_group_filtered_findings]
+  from google.cloud import securitycenter_v2
+
+  # Create a client.
+  client = securitycenter_v2.SecurityCenterClient()
+
+  # 'source_name' is the resource path for a source that has been
+  # created previously (you can use list_sources to find a specific one).
+  # Its format is:
+  # source_name = "{parent}/sources/{source_id}"
+  # 'parent' must be in one of the following formats:
+  #   "organizations/{organization_id}"
+  #   "projects/{project_id}"
+  #   "folders/{folder_id}"
+  # source_name = "organizations/111122222444/sources/1234/locations/{location_id}"
+
+  group_result_iterator = client.group_findings(
+      request={
+          "parent": source_name,
+          "group_by": "category",
+          "filter": 'state="ACTIVE"',
+      }
+  )
+  for i, group_result in enumerate(group_result_iterator):
+    print((i + 1), group_result)
+  # [END securitycenter_group_filtered_findings]
+  return i
