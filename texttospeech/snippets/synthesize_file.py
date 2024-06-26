1e6b21c7779cd3426dc0493e2455403eb3d756a5
Alicia Williams <aliciamw@google.com>
2024-06-26 14:02:45 -0700

    delete sample with region tag tts_synthesize_text_file
    delete sample with region tag (tts_synthesize_text_file), plus linting changes

--- texttospeech/snippets/synthesize_file.py
+++ texttospeech/snippets/synthesize_file.py
@@ -18,7 +18,6 @@
 """Google Cloud Text-To-Speech API sample application .
 
 Example usage:
-    python synthesize_file.py --text resources/hello.txt
     python synthesize_file.py --ssml resources/hello.ssml
 """
 
@@ -26,39 +25,6 @@ import argparse
 
 
 # [START tts_synthesize_text_file]
-def synthesize_text_file(text_file):
-    """Synthesizes speech from the input file of text."""
-    from google.cloud import texttospeech
-
-    client = texttospeech.TextToSpeechClient()
-
-    with open(text_file) as f:
-        text = f.read()
-        input_text = texttospeech.SynthesisInput(text=text)
-
-    # Note: the voice can also be specified by name.
-    # Names of voices can be retrieved with client.list_voices().
-    voice = texttospeech.VoiceSelectionParams(
-        language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.FEMALE
-    )
-
-    audio_config = texttospeech.AudioConfig(
-        audio_encoding=texttospeech.AudioEncoding.MP3
-    )
-
-    response = client.synthesize_speech(
-        request={"input": input_text, "voice": voice, "audio_config": audio_config}
-    )
-
-    # The response's audio_content is binary.
-    with open("output.mp3", "wb") as out:
-        out.write(response.audio_content)
-        print('Audio content written to file "output.mp3"')
-
-
-# [END tts_synthesize_text_file]
-
-
 # [START tts_synthesize_ssml_file]
 def synthesize_ssml_file(ssml_file):
     """Synthesizes speech from the input file of ssml.
@@ -102,12 +68,8 @@ if __name__ == "__main__":
         description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
     )
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("--text", help="The text file from which to synthesize speech.")
     group.add_argument("--ssml", help="The ssml file from which to synthesize speech.")
 
     args = parser.parse_args()
 
-    if args.text:
-        synthesize_text_file(args.text)
-    else:
-        synthesize_ssml_file(args.ssml)
+    synthesize_ssml_file(args.ssml)
--- texttospeech/snippets/synthesize_file_test.py
+++ texttospeech/snippets/synthesize_file_test.py
@@ -19,17 +19,7 @@ import os
 
 import synthesize_file
 
-TEXT_FILE = "resources/hello.txt"
 SSML_FILE = "resources/hello.ssml"
-
-
-def test_synthesize_text_file(capsys):
-    synthesize_file.synthesize_text_file(text_file=TEXT_FILE)
-    out, err = capsys.readouterr()
-
-    assert "Audio content written to file" in out
-    statinfo = os.stat("output.mp3")
-    assert statinfo.st_size > 0
 
 
 def test_synthesize_ssml_file(capsys):
