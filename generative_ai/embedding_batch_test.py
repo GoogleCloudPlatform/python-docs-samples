8d1c01b6acea3c4c5769c58857fc8ba88661e8c2
Holt Skinner <holtskinner@google.com>
2024-07-16 10:46:28 -0500

    Fix import groups

--- generative_ai/embedding_batch_test.py
+++ generative_ai/embedding_batch_test.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Google LLC
+# Copyright 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import backoff
 import os
+
+import backoff
 
 from google.api_core.exceptions import ResourceExhausted
 
