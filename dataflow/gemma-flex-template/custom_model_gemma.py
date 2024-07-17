1496cded8312e596736d8614c70060f3a062dd7c
Jack McCluskey <thejackmccluskey@gmail.com>
2024-07-17 12:48:07 -0400

    import ordering

--- dataflow/gemma-flex-template/custom_model_gemma.py
+++ dataflow/gemma-flex-template/custom_model_gemma.py
@@ -12,9 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
-
 from collections.abc import Iterable, Sequence
+import logging
 from typing import Any, Optional
 
 import apache_beam as beam
