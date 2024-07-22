0f60b21dc6693b806eed85b63cb6ff80bd8736ab
David Cavazos <dcavazos@google.com>
2024-07-22 14:07:34 -0700

    format strategy matrix

--- .github/testing/src/main.ts
+++ .github/testing/src/main.ts
@@ -1,7 +1,8 @@
+import { List } from 'immutable';
 import * as affected from './affected';
 import * as git from './git';
-import {global} from './config/global';
-import {python} from './config/python';
+import { global } from './config/global';
+import { python } from './config/python';
 
 const diffsCommit =
   process.argv.length > 2 //
@@ -15,11 +16,16 @@ const baseCommit =
 
 const diffs = git.diffs(diffsCommit, baseCommit).filter(global.matchFile);
 
-const affectedPython = diffs
-  .filter(python.matchFile)
-  .map(python.changes)
-  .groupBy(change => change.package)
-  .map(change => change.map(pkg => pkg.affected))
-  .map(affected.merge);
-
-console.log('affectedPython:', affectedPython.toJS());
+const strategyMatrix = {
+  python: List(
+    diffs
+      .filter(python.matchFile)
+      .map(python.changes)
+      .groupBy(change => change.package)
+      .map(change => change.map(pkg => pkg.affected))
+      .map(affected.merge)
+      .map((tests, pkg) => ({ package: pkg, tests: tests }))
+      .values()
+  ),
+};
+console.log(JSON.stringify(strategyMatrix));
