8dcd580d761500ddfaf35937a8aefbc634be12ac
Maciej Strzelczyk <strzelczyk@google.com>
2024-08-02 17:16:33 +0200

    Trying to debug the tests

--- batch/tests/test_basics.py
+++ batch/tests/test_basics.py
@@ -108,7 +108,8 @@ def _test_body(
         if additional_test:
             additional_test()
     finally:
-        delete_job(project, region, test_job.name.rsplit("/", maxsplit=1)[1]).result()
+        if project == PROJECT:
+            delete_job(project, region, test_job.name.rsplit("/", maxsplit=1)[1]).result()
 
     for job in list_jobs(project, region):
         if job.uid == test_job.uid:
