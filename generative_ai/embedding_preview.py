969dd35a022c37492096a3521854f1f70b7a7297
Kewen Tang <kewent@google.com>
2024-08-13 23:25:15 +0000

    add sample for code retrieval

--- generative_ai/embedding_preview.py
+++ generative_ai/embedding_preview.py
@@ -19,21 +19,24 @@ from vertexai.language_models import TextEmbeddingInput, TextEmbeddingModel
 
 
 def embed_text(
-    texts: List[str] = ["banana muffins? ", "banana bread? banana muffins?"],
+    texts: List[str] = [
+        "Retrieve a function that adds two numbers",
+        "def func(a, b): return a + b",
+    ],
     task: str = "CODE_RETRIEVAL_QUERY",
     model_name: str = "text-embedding-preview-0815",
     dimensionality: Optional[int] = 256,
 ) -> List[List[float]]:
-    """Embeds texts with a pre-trained, foundational model."""
-    model = TextEmbeddingModel.from_pretrained(model_name)
-    inputs = [TextEmbeddingInput(text, task) for text in texts]
-    kwargs = dict(output_dimensionality=dimensionality) if dimensionality else {}
-    embeddings = model.get_embeddings(inputs, **kwargs)
-    return [embedding.values for embedding in embeddings]
+  """Embeds texts with a pre-trained, foundational model."""
+  model = TextEmbeddingModel.from_pretrained(model_name)
+  inputs = [TextEmbeddingInput(text, task) for text in texts]
+  kwargs = dict(output_dimensionality=dimensionality) if dimensionality else {}
+  embeddings = model.get_embeddings(inputs, **kwargs)
+  return [embedding.values for embedding in embeddings]
 
 
 # [END generativeaionvertexai_sdk_embedding]
 
 
 if __name__ == "__main__":
-    embed_text()
+  embed_text()
