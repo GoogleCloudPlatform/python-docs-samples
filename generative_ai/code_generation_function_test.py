8999ba3e50a6a0bf12ea50c69446edcc6faaae7b
Sampath M <sampathm@google.com>
2024-08-23 13:10:07 +0200

    chore: update test case

--- generative_ai/code_generation_function_test.py
+++ generative_ai/code_generation_function_test.py
@@ -21,4 +21,5 @@ import code_generation_function
 @backoff.on_exception(backoff.expo, ResourceExhausted, max_time=10)
 def test_code_generation_function() -> None:
     content = code_generation_function.generate_a_function().text
-    assert "leap year" in content
+    assert len(content) > 0
+    assert "def" in content
