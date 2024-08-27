fa36e7b4a2a0af981276449fed811704553e3927
Daniel Sanche <d.sanche14@gmail.com>
2024-08-27 14:05:59 -0700

    removed Client monkeypatch

--- firestore/cloud-client/snippets_test.py
+++ firestore/cloud-client/snippets_test.py
@@ -45,12 +45,9 @@ class TestFirestoreClient(firestore.Client):
         return f"{collection_name}-{self._UNIQUE_STRING}"
 
 
-snippets.firestore.Client = TestFirestoreClient
-
-
 @pytest.fixture(scope="function")
 def db():
-    client = snippets.firestore.Client()
+    client = TestFirestoreClient()
     yield client
     # Cleanup
     try:
