fcaf4ec39ddf0af392ea7405a2944adfac77c580
Ashutosh sharma <108721320+ashuison@users.noreply.github.com>
2024-08-30 11:37:12 +0530

    Update main.py

--- appengine/standard_python3/hello_world/main.py
+++ appengine/standard_python3/hello_world/main.py
@@ -29,7 +29,7 @@ def hello():
     Returns:
         A string with the words 'Hello World!'.
     """
-    return "Hello World!"
+    return "Hello welcome to BMSIT !"
 
 
 if __name__ == "__main__":
