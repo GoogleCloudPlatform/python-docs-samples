04a792348f6749d790dd279c49d632526dbd3830
Jennifer Davis <sigje@google.com>
2024-09-03 14:40:30 -0700

    fix(gae): resolve linting issues in main_test.py

--- appengine/standard/iap/main_test.py
+++ appengine/standard/iap/main_test.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import main
 import webtest
 
+import main
 
 def test_index(testbed, login):
     app = webtest.TestApp(main.app)
