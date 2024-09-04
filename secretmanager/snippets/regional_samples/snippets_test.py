6b899d7c357aded60b7e2fa235d6672dc2786562
vipul7499 <45483005+vipul7499@users.noreply.github.com>
2024-09-04 23:48:20 +0530

    fix:linting fix

--- secretmanager/snippets/regional_samples/snippets_test.py
+++ secretmanager/snippets/regional_samples/snippets_test.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 
-import base64
 import os
 import time
 from typing import Iterator, Optional, Tuple, Union
