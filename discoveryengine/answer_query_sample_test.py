f14dd8274325e65b8865d91dd654193b256b274b
Holt Skinner <holtskinner@google.com>
2024-09-05 11:06:13 -0500

    docs(samples): Added answer_query_sample.py

---
+++ discoveryengine/answer_query_sample_test.py
@@ -0,0 +1,38 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import os
+
+from discoveryengine import answer_query_sample
+
+project_id = os.environ["GOOGLE_CLOUD_PROJECT"]
+
+
+def test_answer_query():
+    location = "global"
+    data_store_id = "alphabet-earnings-reports_1697472013405"
+    serving_config_id = "default_config"
+    query = "What is Google?"
+
+    response = answer_query_sample.answer_query_sample(
+        project_id=project_id,
+        location=location,
+        data_store_id=data_store_id,
+        serving_config_id=serving_config_id,
+        query=query,
+    )
+
+    assert response
+    assert response.answer
