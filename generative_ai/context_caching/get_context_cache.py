b4e9f5636afeb41e8cf0c64a9061f1638718a796
Mykyta Sherstianykh <112819029+Thoughtseize1@users.noreply.github.com>
2024-09-12 16:38:22 +0200

    refactor: (GenAI) Improved Context Cache Samples (Group A) (#12571)
    ## Description
    I have modified the following files:
    
    - `create_context_cache.py`
    - `delete_context_cache.py`
    - `get_context_cache.py`
    - `update_context_cache.py`
    - `use_context_cache.py`
    
    ❓However, as a developer and user, I can not find a single file `list_context_cache.py`
    
    ❗I have begun developing this file, but I have encountered an issue: the method required for listing context caches is not yet implemented and cannot be used. I have detailed the problem in the attached [document](https://docs.google.com/document/d/1AvNZYbIhrxO414CmPFRbyL24f-3fpncZQOOaaGy-Kcg/edit?usp=sharing).
    
    Proposed region_tag: `generativeaionvertexai_gemini_list_context_cache`
    Could you please review the issue and prioritize the implementation of this method? Having this method available would greatly facilitate the development process.
    
    ## Checklist
    - [x] I have followed [Sample Guidelines from AUTHORING_GUIDE.MD](https://togithub.com/GoogleCloudPlatform/python-docs-samples/blob/main/AUTHORING_GUIDE.md)
    - [ ] README is updated to include [all relevant information](https://togithub.com/GoogleCloudPlatform/python-docs-samples/blob/main/AUTHORING_GUIDE.md#readme-file)
    - [ ] **Tests** pass:   `nox -s py-3.9` (see [Test Environment Setup](https://togithub.com/GoogleCloudPlatform/python-docs-samples/blob/main/AUTHORING_GUIDE.md#test-environment-setup))
    - [ ] **Lint** pass:   `nox -s lint` (see [Test Environment Setup](https://togithub.com/GoogleCloudPlatform/python-docs-samples/blob/main/AUTHORING_GUIDE.md#test-environment-setup))
    - [ ] These samples need a new **API enabled** in testing projects to pass (let us know which ones)
    - [ ] These samples need a new/updated **env vars** in testing projects set to pass (let us know which ones)
    - [ ] This sample adds a new sample directory, and I updated the [CODEOWNERS file](https://togithub.com/GoogleCloudPlatform/python-docs-samples/blob/main/.github/CODEOWNERS) with the codeowners for this sample
    - [ ] This sample adds a new **Product API**, and I updated the [Blunderbuss issue/PR auto-assigner](https://togithub.com/GoogleCloudPlatform/python-docs-samples/blob/main/.github/blunderbuss.yml) with the codeowners for this sample
    - [ ] Please **merge** this PR for me once it is approved

--- generative_ai/context_caching/get_context_cache.py
+++ generative_ai/context_caching/get_context_cache.py
@@ -11,23 +11,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
+
+PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT")
 
 
-def get_context_cache(project_id: str, cache_id: str) -> str:
+def get_context_cache(cache_id: str) -> str:
     # [START generativeaionvertexai_gemini_get_context_cache]
     import vertexai
 
     from vertexai.preview import caching
 
     # TODO(developer): Update and un-comment below lines
-    # project_id = "PROJECT_ID"
-    # cache_id = "CACHE_ID"
+    # PROJECT_ID = "your-project-id"
+    # cache_id = "your-cache-id"
 
-    vertexai.init(project=project_id, location="us-central1")
+    vertexai.init(project=PROJECT_ID, location="us-central1")
 
     cached_content = caching.CachedContent(cached_content_name=cache_id)
 
-    print(cached_content.name)
+    print(cached_content.resource_name)
+    # Example response:
+    # projects/[PROJECT_ID]/locations/us-central1/cachedContents/1234567890
     # [END generativeaionvertexai_gemini_get_context_cache]
+    return cached_content.resource_name
 
-    return cached_content.name
+
+if __name__ == "__main__":
+    get_context_cache("1234567890")
