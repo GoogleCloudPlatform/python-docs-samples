e13155a1e7f2673a5d76e8ec5eb9849e65efe2dd
Veronica Wasson <3992422+VeronicaWasson@users.noreply.github.com>
2024-09-12 10:15:09 -0700

    docs(samples): Add Dataflow snippet to read from multiple Kafka topics (#12530)

--- dataflow/snippets/tests/test_read_kafka.py
+++ dataflow/snippets/tests/test_read_kafka.py
@@ -18,6 +18,7 @@ import uuid
 
 import docker
 
+from docker import DockerClient
 from kafka import KafkaProducer
 from kafka.admin import KafkaAdminClient, NewTopic
 from kafka.errors import NoBrokersAvailable
@@ -25,69 +26,92 @@ from kafka.errors import NoBrokersAvailable
 import pytest
 
 
-BOOTSTRAP_SERVER = "localhost:9092"
-TOPIC_NAME = f"topic-{uuid.uuid4()}"
-CONTAINER_IMAGE_NAME = "kafka-pipeline:1"
+BOOTSTRAP_SERVER = 'localhost:9092'
+TOPIC_NAMES = ['topic1', 'topic2']
+CONTAINER_IMAGE_NAME = 'kafka-pipeline:1'
 
 
-@pytest.fixture(scope="module", autouse=True)
-def kafka_container() -> None:
+@pytest.fixture(scope='module')
+def docker_client() -> DockerClient:
+    # Build a container image for the pipeline.
+    client = docker.from_env()
+    client.images.build(path='./', tag=CONTAINER_IMAGE_NAME)
+    yield client
+
+
+@pytest.fixture(scope='module', autouse=True)
+def kafka_container(docker_client: DockerClient) -> None:
     # Start a containerized Kafka server.
-    docker_client = docker.from_env()
-    container = docker_client.containers.run(
-        "apache/kafka:3.7.0", network_mode="host", detach=True
-    )
+    container = docker_client.containers.run('apache/kafka:3.7.0', network_mode='host', detach=True)
     try:
-        create_topic()
+        create_topics()
+        send_messages(TOPIC_NAMES[0])
+        send_messages(TOPIC_NAMES[1])
         yield
     finally:
         container.stop()
 
 
-def create_topic() -> None:
-    # Try to create a Kafka topic. We might need to wait for the Kafka service to start.
+@pytest.fixture
+def file_name_prefix() -> str:
+    return f'output-{uuid.uuid4()}'
+
+
+def create_topics() -> None:
+    # Try to create Kafka topics. We might need to wait for the Kafka service to start.
     for _ in range(1, 10):
         try:
             client = KafkaAdminClient(bootstrap_servers=BOOTSTRAP_SERVER)
             topics = []
-            topics.append(
-                NewTopic(name=TOPIC_NAME, num_partitions=1, replication_factor=1)
-            )
+            topics.append(NewTopic(name=TOPIC_NAMES[0], num_partitions=1, replication_factor=1))
+            topics.append(NewTopic(name=TOPIC_NAMES[1], num_partitions=1, replication_factor=1))
             client.create_topics(topics)
             break
         except NoBrokersAvailable:
             time.sleep(5)
 
 
-def test_read_from_kafka(tmp_path: Path) -> None:
-    file_name_prefix = f"output-{uuid.uuid4()}"
-    file_name = f"{tmp_path}/{file_name_prefix}-00000-of-00001.txt"
-
+def send_messages(topic: str) -> None:
     # Send some messages to Kafka
     producer = KafkaProducer(bootstrap_servers=BOOTSTRAP_SERVER)
     for i in range(0, 5):
-        message = f"event-{i}"
-        producer.send(TOPIC_NAME, message.encode())
+        message = f'{topic}-{i}'
+        producer.send(topic, message.encode())
 
-    # Build a container image for the pipeline.
-    client = docker.from_env()
-    client.images.build(path="./", tag=CONTAINER_IMAGE_NAME)
 
-    # Run the pipeline.
-    client.containers.run(
-        image=CONTAINER_IMAGE_NAME,
-        command=f"/pipeline/read_kafka.py --output /out/{file_name_prefix} --bootstrap_server {BOOTSTRAP_SERVER} --topic {TOPIC_NAME}",
-        volumes=["/var/run/docker.sock:/var/run/docker.sock", f"{tmp_path}/:/out"],
-        network_mode="host",
-        entrypoint="python",
-    )
-
+def verify_output(file_name: str, topic: str) -> None:
     # Verify the pipeline wrote the Kafka messages to the output file.
-    with open(file_name, "r") as f:
+    with open(file_name, 'r') as f:
         text = f.read()
         for i in range(0, 5):
-            assert f"event-{i}" in text
+            assert f'{topic}-{i}' in text
 
 
-if __name__ == "__main__":
-    test_read_from_kafka()
+def test_read_kafka(docker_client: DockerClient, tmp_path: Path, file_name_prefix: str) -> None:
+    topic = TOPIC_NAMES[0]
+
+    # Run the containerized Dataflow pipeline.
+    docker_client.containers.run(
+        image=CONTAINER_IMAGE_NAME,
+        command=f'/pipeline/read_kafka.py --output /out/{file_name_prefix} --bootstrap_server {BOOTSTRAP_SERVER} --topic {topic}',
+        volumes=['/var/run/docker.sock:/var/run/docker.sock', f'{tmp_path}/:/out'],
+        network_mode='host',
+        entrypoint='python')
+
+    # Verify the pipeline wrote the Kafka messages to the output file.
+    verify_output(f'{tmp_path}/{file_name_prefix}-00000-of-00001.txt', topic)
+
+
+def test_read_kafka_multi_topic(docker_client: DockerClient, tmp_path: Path, file_name_prefix: str) -> None:
+    # Run the containerized Dataflow pipeline.
+    docker_client.containers.run(
+        image=CONTAINER_IMAGE_NAME,
+        command=f'/pipeline/read_kafka_multi_topic.py --output /out/{file_name_prefix} --bootstrap_server {BOOTSTRAP_SERVER}',
+        volumes=['/var/run/docker.sock:/var/run/docker.sock', f'{tmp_path}/:/out'],
+        network_mode='host',
+        entrypoint='python')
+
+    # Verify the pipeline wrote the Kafka messages to the output files.
+    # This code snippet writes outputs to separate directories based on the topic name.
+    for topic in TOPIC_NAMES:
+        verify_output(f'{tmp_path}/{file_name_prefix}/{topic}/output-00000-of-00001.txt', topic)
