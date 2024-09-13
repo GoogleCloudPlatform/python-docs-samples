fdda7ae8d89894bf8ef57462e80656a90c2cd285
Thoughtseize1 <msher@softserveinc.com>
2024-09-13 13:23:11 +0200

    Renamed tests

--- generative_ai/function_calling/examples_function_calling_test.py
+++ generative_ai/function_calling/examples_function_calling_test.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+# TODO: Rename this file after deleting /generative_ai/function_calling_test.py
 import advanced_example
 
 import backoff
