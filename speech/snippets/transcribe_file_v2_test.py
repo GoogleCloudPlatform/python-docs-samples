612eae40b149a0395f157803d536600cc5e312d2
Mykyta Sherstianykh <112819029+Thoughtseize1@users.noreply.github.com>
2024-09-17 10:34:05 +0200

    Simplified next set of Speech Samples (set-3) (#12158)
    * Simplified set of Speech Samples
    * Update comments and imports

--- speech/snippets/transcribe_file_v2_test.py
+++ speech/snippets/transcribe_file_v2_test.py
@@ -12,23 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import re
 
 import pytest
 
 import transcribe_file_v2
 
-_RESOURCES = os.path.join(os.path.dirname(__file__), "resources")
-
 
 def test_transcribe_file_v2(capsys: pytest.CaptureFixture) -> None:
-    project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
-
-    response = transcribe_file_v2.transcribe_file_v2(
-        project_id, os.path.join(_RESOURCES, "audio.wav")
-    )
-
+    response = transcribe_file_v2.transcribe_file_v2()
     assert re.search(
         r"how old is the Brooklyn Bridge",
         response.results[0].alternatives[0].transcript,
