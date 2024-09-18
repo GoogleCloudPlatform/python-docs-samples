04d5329dc35bd7743334b080dde5f1166166be8e
EmanuelB25 <emanuelburgess@google.com>
2024-09-18 17:51:30 -0400

    changing lambda name

--- generative_ai/batch_predict/test_batch_predict.py
+++ generative_ai/batch_predict/test_batch_predict.py
@@ -77,6 +77,8 @@ def test_batch_code_predict(output_folder: pytest.fixture()) -> None:
 def test_batch_gemini_predict(output_folder: pytest.fixture()) -> None:
     input_uri = f"gs://{INPUT_BUCKET}/batch/prompt_for_batch_gemini_predict.jsonl"
     job = _main_test(
-        test_func=lambda: batch_gemini_predict.gemini_batch_prediction_example(input_uri, output_folder)
+        test_func=lambda: batch_gemini_predict.gemini_batch_prediction(
+            input_uri, output_folder
+        )
     )
     assert job.output_info.gcs_output_directory
