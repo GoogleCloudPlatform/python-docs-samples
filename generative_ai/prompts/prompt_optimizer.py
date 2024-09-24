7d7f35ae0d6b0fa55386e4c7cd54b743d1629533
sitalakshmi <sitalakshmi@google.com>
2024-09-24 01:49:40 -0400

    update comments

--- generative_ai/prompts/prompt_optimizer.py
+++ generative_ai/prompts/prompt_optimizer.py
@@ -14,17 +14,25 @@
 
 import os
 
-from google.cloud import aiplatform
-
 
 #  [START aiplatform_prompt_optimizer]
-def optimize_prompts(
-    project: str,
-    location: str,
-    staging_bucket: str,
-    configuration_path: str,
-) -> None:
 
+def optimize_prompts(
+        project: str,
+        location: str,
+        staging_bucket: str,
+        configuration_path: str,
+) -> str:
+    """ Improve prompts by evaluating the model's response to sample prompts against specified evaluation metric(s).
+    Args:
+        project: Google Cloud Project ID.
+        location: Location where you want to run the Vertex AI prompt optimizer.
+        staging_bucket: Specify the Google Cloud Storage bucket to store outputs and metadata. For example, gs://bucket-name
+        configuration_path: URI of the configuration file in your Google Cloud Storage bucket. For example, gs://bucket-name/configuration.json.
+    Returns:
+        custom_job.resource_name: Returns the resource name of the job created of type: projects/project-id/locations/location/customJobs/job-id
+    """
+    from google.cloud import aiplatform
     aiplatform.init(project=project, location=location, staging_bucket=staging_bucket)
 
     worker_pool_specs = [{
@@ -43,9 +51,13 @@ def optimize_prompts(
         worker_pool_specs=worker_pool_specs,
     )
     custom_job.submit()
+    return custom_job.resource_name
+
 
 #  [END aiplatform_prompt_optimizer]
 
-
 if __name__ == "__main__":
-    optimize_prompts(os.environ["PROJECT_ID"], "us-central1", os.environ["PROMPT_OPTIMIZER_BUCKET_NAME"], os.environ["CONFIG_PATH"])
+    optimize_prompts(os.environ["PROJECT_ID"],
+                     "us-central1",
+                     os.environ["PROMPT_OPTIMIZER_BUCKET_NAME"],
+                     os.environ["JSON_CONFIG_PATH"])
