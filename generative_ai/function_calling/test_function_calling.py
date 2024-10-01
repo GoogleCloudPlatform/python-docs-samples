fd019ec2bf47355397456de9497e32d0d6b0ce21
Valeriy Burlaka <valerii@fluxon.com>
2024-10-01 20:23:21 +0200

    move function_calling tests into a single file

--- generative_ai/function_calling/test_function_calling.py
+++ generative_ai/function_calling/test_function_calling.py
@@ -15,7 +15,71 @@ import backoff
 
 from google.api_core.exceptions import ResourceExhausted
 
-import parallel_function_calling_example
+from . import (
+    advanced_example,
+    basic_example,
+    chat_example,
+    chat_function_calling_basic,
+    chat_function_calling_config,
+    parallel_function_calling_example,
+)
+
+
+@backoff.on_exception(backoff.expo, ResourceExhausted, max_time=10)
+def test_function_calling() -> None:
+    response = basic_example.generate_function_call()
+
+    expected_summary = [
+        "Boston",
+    ]
+    expected_responses = [
+        "candidates",
+        "content",
+        "role",
+        "model",
+        "parts",
+        "Boston",
+    ]
+    assert all(x in str(response.text) for x in expected_summary)
+    assert all(x in str(response) for x in expected_responses)
+
+
+@backoff.on_exception(backoff.expo, ResourceExhausted, max_time=10)
+def test_function_calling_advanced_function_selection() -> None:
+    response = advanced_example.generate_function_call_advanced()
+    assert (
+        "Pixel 8 Pro 128GB"
+        in response.candidates[0].function_calls[0].args["product_name"]
+    )
+
+
+@backoff.on_exception(backoff.expo, ResourceExhausted, max_time=10)
+def test_function_calling_basic() -> None:
+    response = chat_function_calling_basic.generate_text()
+    assert "get_current_weather" in response.choices[0].message.tool_calls[0].id
+
+
+@backoff.on_exception(backoff.expo, ResourceExhausted, max_time=10)
+def test_function_calling_config() -> None:
+    response = chat_function_calling_config.generate_text()
+    assert "Boston" in response.choices[0].message.tool_calls[0].function.arguments
+
+
+@backoff.on_exception(backoff.expo, ResourceExhausted, max_time=10)
+def test_function_calling_chat() -> None:
+    chat = chat_example.generate_function_call_chat()
+
+    assert chat
+    assert chat.history
+
+    expected_summaries = [
+        "Pixel 8 Pro",
+        "stock",
+        "store",
+        "2000 N Shoreline Blvd",
+        "Mountain View",
+    ]
+    assert any(x in str(chat.history) for x in expected_summaries)
 
 
 @backoff.on_exception(backoff.expo, ResourceExhausted, max_time=10)
