078df9cd9c3c29531d0f2cc51da1c229489e91b5
Thoughtseize1 <msher@softserveinc.com>
2024-10-15 16:12:55 +0200

    Added new TPU Sample. Updated tests.

--- tpu/test_tpu.py
+++ tpu/test_tpu.py
@@ -19,9 +19,9 @@ from google.cloud.tpu_v2.types import Node
 import pytest
 
 import create_tpu
+import create_tpu_with_script
 import delete_tpu
 import get_tpu
-
 
 TPU_NAME = "test-tpu-" + uuid.uuid4().hex[:10]
 PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT")
@@ -42,7 +42,18 @@ def test_creating_tpu(tpu_instance: Node) -> None:
     assert tpu_instance.state == Node.State.READY
 
 
-def test_get_tpu(tpu_instance: Node) -> None:
+def test_creating_with_startup_script() -> None:
+    tpu_name_with_script = "script-ptu" + uuid.uuid4().hex[:5]
+    try:
+        tpu_with_script = create_tpu_with_script.create_cloud_tpu_with_script(
+            PROJECT_ID, ZONE, tpu_name_with_script
+        )
+        assert "--upgrade numpy" in tpu_with_script.metadata["startup-script"]
+    finally:
+        delete_tpu.delete_cloud_tpu(PROJECT_ID, ZONE, tpu_name_with_script)
+
+
+def test_get_tpu() -> None:
     tpu = get_tpu.get_cloud_tpu(PROJECT_ID, ZONE, TPU_NAME)
     assert tpu.state == Node.State.READY
     assert tpu.name == f"projects/{PROJECT_ID}/locations/{ZONE}/nodes/{TPU_NAME}"
