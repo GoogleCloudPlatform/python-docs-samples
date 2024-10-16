7a12b532e38f559ef237cd29fd8bf0620d42550c
Thoughtseize1 <msher@softserveinc.com>
2024-10-16 15:18:22 +0200

    Made sure resources are clean. Added basic tests

--- tpu/test_tpu.py
+++ tpu/test_tpu.py
@@ -32,15 +32,6 @@ ZONE = "us-south1-a"
 TPU_TYPE = "v5litepod-1"
 TPU_VERSION = "tpu-vm-tf-2.17.0-pjrt"
 
-# For tests and checking the TPU in different zones
-list_of_zones = [
-    "us-central1-a",
-    "us-central1-b",
-    "us-central1-c",
-    "us-central1-f",
-    "us-south1-a",
-]
-
 
 # Instance of TPU
 @pytest.fixture(scope="session")
@@ -51,42 +42,39 @@ def tpu_instance() -> Node:
     except Exception as e:
         print(f"Error during cleanup: {e}")
 
-#
-# def test_creating_tpu(tpu_instance: Node) -> None:
-#     assert tpu_instance.state == Node.State.READY
-#
-#
-# def test_creating_with_startup_script() -> None:
-#     tpu_name_with_script = "script-tpu-" + uuid.uuid4().hex[:5]
-#     try:
-#         tpu_with_script = create_tpu_with_script.create_cloud_tpu_with_script(
-#             PROJECT_ID, ZONE, tpu_name_with_script, TPU_TYPE, TPU_VERSION
-#         )
-#         assert "--upgrade numpy" in tpu_with_script.metadata["startup-script"]
-#     finally:
-#         print(f"\n\n ------------ Deleting TPU {TPU_NAME}\n ------------" )
-#         delete_tpu.delete_cloud_tpu(PROJECT_ID, ZONE, tpu_name_with_script)
-#
-#
-# def test_get_tpu() -> None:
-#     tpu = get_tpu.get_cloud_tpu(PROJECT_ID, ZONE, TPU_NAME)
-#     assert tpu.state == Node.State.READY
-#     assert tpu.name == f"projects/{PROJECT_ID}/locations/{ZONE}/nodes/{TPU_NAME}"
+
+def test_creating_tpu(tpu_instance: Node) -> None:
+    assert tpu_instance.state == Node.State.READY
+
+
+def test_creating_with_startup_script() -> None:
+    tpu_name_with_script = "script-tpu-" + uuid.uuid4().hex[:5]
+    try:
+        tpu_with_script = create_tpu_with_script.create_cloud_tpu_with_script(
+            PROJECT_ID, ZONE, tpu_name_with_script, TPU_TYPE, TPU_VERSION
+        )
+        assert "--upgrade numpy" in tpu_with_script.metadata["startup-script"]
+    finally:
+        print(f"\n\n ------------ Deleting TPU {TPU_NAME}\n ------------")
+        delete_tpu.delete_cloud_tpu(PROJECT_ID, ZONE, tpu_name_with_script)
+
+
+def test_get_tpu() -> None:
+    tpu = get_tpu.get_cloud_tpu(PROJECT_ID, ZONE, TPU_NAME)
+    assert tpu.state == Node.State.READY
+    assert tpu.name == f"projects/{PROJECT_ID}/locations/{ZONE}/nodes/{TPU_NAME}"
 
 
 def test_list_tpu() -> None:
     nodes = list_tpu.list_cloud_tpu(PROJECT_ID, ZONE)
-    for zone in list_of_zones:
-        nodes = list_tpu.list_cloud_tpu(PROJECT_ID, zone)
-        print("KEYWORD: NODES", nodes)
     assert len(list(nodes)) > 0
 
 
-# def test_stop_tpu() -> None:
-#     node = stop_tpu.stop_cloud_tpu(PROJECT_ID, ZONE, TPU_NAME)
-#     assert node.state == Node.State.STOPPED
-#
-#
-# def test_start_tpu() -> None:
-#     node = start_tpu.start_cloud_tpu(PROJECT_ID, ZONE, TPU_NAME)
-#     assert node.state == Node.State.READY
+def test_stop_tpu() -> None:
+    node = stop_tpu.stop_cloud_tpu(PROJECT_ID, ZONE, TPU_NAME)
+    assert node.state == Node.State.STOPPED
+
+
+def test_start_tpu() -> None:
+    node = start_tpu.start_cloud_tpu(PROJECT_ID, ZONE, TPU_NAME)
+    assert node.state == Node.State.READY
