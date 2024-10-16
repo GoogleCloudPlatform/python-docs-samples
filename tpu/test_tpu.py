c778e1dc5ed2efca3f1ef2f298fc11ff42a43b9a
Thoughtseize1 <msher@softserveinc.com>
2024-10-16 14:29:56 +0200

    Test for checking TPU's

--- tpu/test_tpu.py
+++ tpu/test_tpu.py
@@ -22,12 +22,24 @@ import create_tpu
 import create_tpu_with_script
 import delete_tpu
 import get_tpu
+import list_tpu
+import start_tpu
+import stop_tpu
 
 TPU_NAME = "test-tpu-" + uuid.uuid4().hex[:10]
 PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT")
 ZONE = "us-south1-a"
 TPU_TYPE = "v5litepod-1"
 TPU_VERSION = "tpu-vm-tf-2.17.0-pjrt"
+
+# For tests and checking the TPU in different zones
+list_of_zones = [
+    "us-central1-a",
+    "us-central1-b",
+    "us-central1-c",
+    "us-central1-f",
+    "us-south1-a",
+]
 
 
 # Instance of TPU
@@ -39,23 +51,41 @@ def tpu_instance() -> Node:
     except Exception as e:
         print(f"Error during cleanup: {e}")
 
+#
+# def test_creating_tpu(tpu_instance: Node) -> None:
+#     assert tpu_instance.state == Node.State.READY
+#
+#
+# def test_creating_with_startup_script() -> None:
+#     tpu_name_with_script = "script-tpu-" + uuid.uuid4().hex[:5]
+#     try:
+#         tpu_with_script = create_tpu_with_script.create_cloud_tpu_with_script(
+#             PROJECT_ID, ZONE, tpu_name_with_script, TPU_TYPE, TPU_VERSION
+#         )
+#         assert "--upgrade numpy" in tpu_with_script.metadata["startup-script"]
+#     finally:
+#         print(f"\n\n ------------ Deleting TPU {TPU_NAME}\n ------------" )
+#         delete_tpu.delete_cloud_tpu(PROJECT_ID, ZONE, tpu_name_with_script)
+#
+#
+# def test_get_tpu() -> None:
+#     tpu = get_tpu.get_cloud_tpu(PROJECT_ID, ZONE, TPU_NAME)
+#     assert tpu.state == Node.State.READY
+#     assert tpu.name == f"projects/{PROJECT_ID}/locations/{ZONE}/nodes/{TPU_NAME}"
 
-def test_creating_tpu(tpu_instance: Node) -> None:
-    assert tpu_instance.state == Node.State.READY
+
+def test_list_tpu() -> None:
+    nodes = list_tpu.list_cloud_tpu(PROJECT_ID, ZONE)
+    for zone in list_of_zones:
+        list_tpu.list_cloud_tpu(PROJECT_ID, zone)
+    # assert len(list(nodes)) > 0
 
 
-def test_creating_with_startup_script() -> None:
-    tpu_name_with_script = "script-tpu-" + uuid.uuid4().hex[:5]
-    try:
-        tpu_with_script = create_tpu_with_script.create_cloud_tpu_with_script(
-            PROJECT_ID, ZONE, tpu_name_with_script, TPU_TYPE, TPU_VERSION
-        )
-        assert "--upgrade numpy" in tpu_with_script.metadata["startup-script"]
-    finally:
-        delete_tpu.delete_cloud_tpu(PROJECT_ID, ZONE, tpu_name_with_script)
-
-
-def test_get_tpu() -> None:
-    tpu = get_tpu.get_cloud_tpu(PROJECT_ID, ZONE, TPU_NAME)
-    assert tpu.state == Node.State.READY
-    assert tpu.name == f"projects/{PROJECT_ID}/locations/{ZONE}/nodes/{TPU_NAME}"
+# def test_stop_tpu() -> None:
+#     node = stop_tpu.stop_cloud_tpu(PROJECT_ID, ZONE, TPU_NAME)
+#     assert node.state == Node.State.STOPPED
+#
+#
+# def test_start_tpu() -> None:
+#     node = start_tpu.start_cloud_tpu(PROJECT_ID, ZONE, TPU_NAME)
+#     assert node.state == Node.State.READY
