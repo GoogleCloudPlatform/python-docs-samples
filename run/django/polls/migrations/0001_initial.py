7be22931d3d24f42f935b23b518d9353bad7a11b
Katie McLaughlin <katie@glasnt.com>
2024-10-23 14:46:30 +1100

    lint [skip ci]

--- run/django/polls/migrations/0001_initial.py
+++ run/django/polls/migrations/0001_initial.py
@@ -13,8 +13,8 @@
 # limitations under the License.
 
 
-import django.db.models.deletion
 from django.db import migrations, models
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
