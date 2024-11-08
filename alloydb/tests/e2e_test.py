729e12c1f10ea59aa58c7e5b5c9dd6356b60c443
Twisha Bansal <twishabansal@google.com>
2024-11-08 22:30:36 +0530

    Added license header

--- alloydb/tests/e2e_test.py
+++ alloydb/tests/e2e_test.py
@@ -1,3 +1,21 @@
+# Copyright 2022 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This sample creates a secure two-service application running on Cloud Run.
+# This test builds and deploys the two secure services
+# to test that they interact properly together.
+
 import asyncpg  # type: ignore
 import conftest as conftest  # python-docs-samples/alloydb/conftest.py
 import pytest
