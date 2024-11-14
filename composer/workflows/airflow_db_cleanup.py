fbb447f439842129a81b6d6ca91a00f4dd2c62db
Artur Piotr Izaak Laskowski <artur.pi.laskowski@gmail.com>
2024-11-14 17:18:27 +0100

    Linter update.

--- composer/workflows/airflow_db_cleanup.py
+++ composer/workflows/airflow_db_cleanup.py
@@ -325,7 +325,7 @@ def build_query(
         newest_dagrun = session.query(airflow_db_model) \
             .filter(airflow_db_model.dag_id == dag_id) \
             .order_by(airflow_db_model.execution_date).first()
-        if newest_dagrun != None:
+        if newest_dagrun is not None:
             query = query.filter(DagRun.external_trigger.is_(False)) \
                 .filter(airflow_db_model.dag_id == dag_id) \
                 .filter(age_check_column <= max_date) \
