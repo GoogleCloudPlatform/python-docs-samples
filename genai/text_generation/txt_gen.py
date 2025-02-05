deb7a5e6f9113c661963fc670fdedd14b203db70
Sita Lakshmi Sangameswaran <sitalakshmi@google.com>
2025-02-05 14:29:30 -0500

    test(genai-samples): test genai samples

---
+++ genai/text_generation/txt_gen.py
@@ -0,0 +1,13 @@
+from google import genai
+
+client = genai.Client(http_options={'api_version': 'v1'})
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents="How does AI work?"
+)
+print(response.text)
+# Example response:
+# Okay, let's break down how AI works. It's a broad field, so I'll focus on the ...
+#
+# Here's a simplified overview:
+# ...
