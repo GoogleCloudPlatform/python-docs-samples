f128b23961889bc9b2a64238ee94789623f98c8d
Sita Lakshmi Sangameswaran <sita1996@gmail.com>
2025-02-20 23:00:14 -0500

    feat(modelarmor-samples): add model armor template and prompt samples (#13143)
    * feat(modelarmor-samples): add model armor template and prompt samples
    
    * header fix
    
    * add noxfile
    
    * add pytest
    
    * address comments and lint fix
    
    * lint fix
    
    * updated templates
    
    * addressed review comments
    
    * addressed review comments
    
    * addressed review comments

---
+++ model_armor/delete_template.py
@@ -0,0 +1,43 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def delete_model_armor_template(project_id: str, location: str, template_id: str) -> None:
+    # [START modelarmor_delete_template]
+
+    from google.api_core.client_options import ClientOptions
+    from google.cloud.modelarmor_v1 import (
+        ModelArmorClient,
+        DeleteTemplateRequest,
+    )
+
+    client = ModelArmorClient(
+        transport="rest",
+        client_options=ClientOptions(api_endpoint=f"modelarmor.{location}.rep.googleapis.com"),
+    )
+
+    # TODO(Developer): Uncomment these variables and initialize
+    # project_id = "YOUR_PROJECT_ID"
+    # location = "us-central1"
+    # template_id = "template_id"
+
+    request = DeleteTemplateRequest(
+        name=f"projects/{project_id}/locations/{location}/templates/{template_id}",
+    )
+
+    # Make the request
+    client.delete_template(request=request)
+
+
+# [END modelarmor_delete_template]
