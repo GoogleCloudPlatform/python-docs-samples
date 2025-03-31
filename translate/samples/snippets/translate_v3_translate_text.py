bd431f63eff3120150d43528cc10f6450c7f33d8
Emmanuel Alejandro Parada Licea <paradalicea@google.com>
2025-03-31 16:01:27 -0600

    fix(translate): restore import to 'translate_v3' after testing compatibility with '_v3beta1'

--- translate/samples/snippets/translate_v3_translate_text.py
+++ translate/samples/snippets/translate_v3_translate_text.py
@@ -17,7 +17,7 @@ import os
 
 # Import the Google Cloud Translation library.
 # [START translate_v3_import_client_library]
-from google.cloud import translate_v3beta1 as translate
+from google.cloud import translate_v3
 # [END translate_v3_import_client_library]
 
 PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT")
@@ -27,7 +27,7 @@ def translate_text(
     text: str = "YOUR_TEXT_TO_TRANSLATE",
     source_language_code: str = "en-US",
     target_language_code: str = "fr",
-) -> translate.TranslationServiceClient:
+) -> translate_v3.TranslationServiceClient:
     """Translate Text from a Source language to a Target language.
     Args:
         text: The content to translate.
@@ -39,7 +39,7 @@ def translate_text(
     """
 
     # Initialize Translation client.
-    client = translate.TranslationServiceClient()
+    client = translate_v3.TranslationServiceClient()
     parent = f"projects/{PROJECT_ID}/locations/global"
 
     # MIME type of the content to translate.
