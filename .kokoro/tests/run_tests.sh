41fad1b16c3bbf394ebb3e8785f7cd4c6ea4fc44
brandonluong-lgtm <brandonluong@google.com>
2025-05-06 13:08:53 -0400

    Add back sudo for install libpcsclite-dev command

--- .kokoro/tests/run_tests.sh
+++ .kokoro/tests/run_tests.sh
@@ -85,7 +85,7 @@ fi
 cd "${PROJECT_ROOT}"
 
 # add libpcsclite
-apt install libpcsclite-dev
+sudo apt install libpcsclite-dev 
 
 # add user's pip binary path to PATH
 export PATH="${HOME}/.local/bin:${PATH}"
