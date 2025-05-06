11d0e0a182e895b41e0ba83a831950d4b199a9d1
eapl.me <64097272+eapl-gemugami@users.noreply.github.com>
2025-05-06 00:07:37 -0600

    fix(composer): fix version check logic for 'airflow_db_cleanup.py' (#13295)
    * fix(composer): WIP fix version check logic
    
    - Create a function to convert version string to list of semantic versioning
    - Create unit test to validate a few scenarios
    
    * fix(composer): add TODO for developer
    
    * fix(composer): move the version parsing to a separate function
    
    * fix(composer): apply linting fixes
    
    * fix(composer): remove unneccessary argument

--- composer/workflows/noxfile_config.py
+++ composer/workflows/noxfile_config.py
@@ -39,7 +39,7 @@ TEST_CONFIG_OVERRIDE = {
         "3.10",
         "3.12",
         "3.13",
-    ],  # Composer w/ Airflow 2 only supports Python 3.8
+    ],
     # Old samples are opted out of enforcing Python type hints
     # All new samples should feature them
     "enforce_type_hints": False,
