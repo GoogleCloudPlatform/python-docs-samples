0bcc412173e1241f60c1599d655e2cbdf913c6e9
brandonluong-lgtm <brandonluong@google.com>
2025-05-06 14:07:44 -0400

    “”

--- .kokoro/tests/run_tests.sh
+++ .kokoro/tests/run_tests.sh
@@ -84,8 +84,8 @@ fi
 
 cd "${PROJECT_ROOT}"
 
-# add libpcsclite
-sudo apt install libpcsclite-dev 
+# # add libpcsclite
+# sudo apt install -q -y libpcsclite-dev 
 
 # add user's pip binary path to PATH
 export PATH="${HOME}/.local/bin:${PATH}"
