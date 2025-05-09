7a7fb0f1f2b3af55bb2b3b996bd0d943be48c202
Emmanuel Alejandro Parada Licea <paradalicea@google.com>
2025-05-09 11:59:34 -0600

    fix(functions): add a variable for the developer to simulate disabling billing
    - Style fixes
    - Create the billing client at the top of the script

--- functions/billing/stop_billing/stop_billing.py
+++ functions/billing/stop_billing/stop_billing.py
@@ -23,6 +23,8 @@ import functions_framework
 from google.api_core import exceptions
 from google.cloud import billing_v1
 
+billing_client = billing_v1.CloudBillingClient()
+
 
 def get_project_id_from_metadata() -> str:
     url = "http://metadata.google.internal/computeMetadata/v1/project/project-id"
@@ -31,21 +33,26 @@ def get_project_id_from_metadata() -> str:
     project_id = urllib.request.urlopen(req).read().decode()
 
     if project_id is None:
-        raise ValueError("PROJECT_ID not found.")
+        raise ValueError("project-id metadata not found.")
 
     return project_id
 
 
 @functions_framework.cloud_event
 def stop_billing(cloud_event: CloudEvent) -> None:
+    # TODO(developer): As stoping billing is a destructive action
+    # for your project, change the following constant to False
+    # after you validate with a test budget.
+    SIMULATE_DEACTIVATION = True
+
     PROJECT_ID = get_project_id_from_metadata()
     PROJECT_NAME = f"projects/{PROJECT_ID}"
 
     event_data = base64.b64decode(
         cloud_event.data["message"]["data"]
     ).decode("utf-8")
-    event_dict = json.loads(event_data)
 
+    event_dict = json.loads(event_data)
     cost_amount = event_dict["costAmount"]
     budget_amount = event_dict["budgetAmount"]
     print(f"Cost: {cost_amount} Budget: {budget_amount}")
@@ -54,21 +61,21 @@ def stop_billing(cloud_event: CloudEvent) -> None:
         print("No action required. Current cost is within budget.")
         return
 
-    # Create a Billing client.
-    billing_client = billing_v1.CloudBillingClient()
-
-    is_billing_enabled = _is_billing_enabled(PROJECT_NAME, billing_client)
-
     # Disable billing if required
+    print(f"Disabling billing for project '{PROJECT_NAME}'...")
+
+    is_billing_enabled = _is_billing_enabled(PROJECT_NAME)
+
     if is_billing_enabled:
-        _disable_billing_for_project(PROJECT_NAME, billing_client)
+        _disable_billing_for_project(
+            PROJECT_NAME,
+            SIMULATE_DEACTIVATION
+        )
     else:
         print("Billing is already disabled.")
 
 
-def _is_billing_enabled(
-    project_name: str, billing_client: billing_v1.CloudBillingClient
-) -> bool:
+def _is_billing_enabled(project_name: str) -> bool:
     """Determine whether billing is enabled for a project.
 
     Args:
@@ -94,34 +101,35 @@ def _is_billing_enabled(
 
 
 def _disable_billing_for_project(
-    project_name: str, billing_client: billing_v1.CloudBillingClient
+    project_name: str,
+    simulate_deactivation: bool,
 ) -> None:
     """Disable billing for a project by removing its billing account.
 
     Args:
         project_name: Name of project to disable billing.
         projects: Resource for interacting with the Billing API.
+        simulate_deactivation:
+            If True, it won't actually disable billing.
+            Useful to validate with test budgets.
     """
 
-    print(f"Disabling billing for project '{project_name}'...")
-
-    # To disable billing set the `billing_account_name` field to empty
-    # LINT: Commented out to pass linter
-    # project_billing_info = billing_v1.ProjectBillingInfo(
-    #     billing_account_name=""
-    # )
+    if simulate_deactivation:
+        print("Billing disabled. (Simulated)")
+        return
 
     # Find more information about `updateBillingInfo` API method here:
     # https://cloud.google.com/billing/docs/reference/rest/v1/projects/updateBillingInfo
     try:
-        # DEBUG: Simulate disabling billing
-        print("Billing disabled. (Simulated)")
-
-        # response = billing_client.update_project_billing_info(
-        #     name=project_name,
-        #     project_billing_info=project_billing_info
-        # )
-        # print(f"Billing disabled: {response}")
+        # To disable billing set the `billing_account_name` field to empty
+        project_billing_info = billing_v1.ProjectBillingInfo(
+            billing_account_name=""
+        )
+        response = billing_client.update_project_billing_info(
+            name=project_name,
+            project_billing_info=project_billing_info
+        )
+        print(f"Billing disabled: {response}")
     except exceptions.PermissionDenied:
         print("Failed to disable billing, check permissions.")
 # [END functions_billing_stop]
