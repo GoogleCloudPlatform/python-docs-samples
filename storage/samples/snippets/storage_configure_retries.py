478bd1ed3430dd463e9960f8b0edae1b28f3e230
cojenco <cathyo@google.com>
2025-05-19 13:08:32 -0700

    samples: update retry sample and comments (#1485)

--- storage/samples/snippets/storage_configure_retries.py
+++ storage/samples/snippets/storage_configure_retries.py
@@ -38,16 +38,15 @@ def configure_retries(bucket_name, blob_name):
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
 
-    # Customize retry with a deadline of 500 seconds (default=120 seconds).
-    modified_retry = DEFAULT_RETRY.with_deadline(500.0)
+    # Customize retry with a timeout of 500 seconds (default=120 seconds).
+    modified_retry = DEFAULT_RETRY.with_timeout(500.0)
     # Customize retry with an initial wait time of 1.5 (default=1.0).
     # Customize retry with a wait time multiplier per iteration of 1.2 (default=2.0).
     # Customize retry with a maximum wait time of 45.0 (default=60.0).
     modified_retry = modified_retry.with_delay(initial=1.5, multiplier=1.2, maximum=45.0)
 
-    # blob.delete() uses DEFAULT_RETRY_IF_GENERATION_SPECIFIED by default.
-    # Override with modified_retry so the function retries even if the generation
-    # number is not specified.
+    # blob.delete() uses DEFAULT_RETRY by default.
+    # Pass in modified_retry to override the default retry behavior.
     print(
         f"The following library method is customized to be retried according to the following configurations: {modified_retry}"
     )
