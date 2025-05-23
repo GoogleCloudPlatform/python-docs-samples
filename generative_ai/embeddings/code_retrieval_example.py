cf33c748f2c4050819aef32935a17f4097f49072
Feiyang <feiyangc@google.com>
2025-05-23 23:39:26 +0000

    Update model name to gemini-embedding-001 in code snipets

--- generative_ai/embeddings/code_retrieval_example.py
+++ generative_ai/embeddings/code_retrieval_example.py
@@ -17,14 +17,14 @@ from __future__ import annotations
 # [START generativeaionvertexai_embedding_code_retrieval]
 from vertexai.language_models import TextEmbeddingInput, TextEmbeddingModel
 
-MODEL_NAME = "text-embedding-005"
+MODEL_NAME = "gemini-embedding-001"
 DIMENSIONALITY = 256
 
 
 def embed_text(
     texts: list[str] = ["Retrieve a function that adds two numbers"],
     task: str = "CODE_RETRIEVAL_QUERY",
-    model_name: str = "text-embedding-005",
+    model_name: str = "gemini-embedding-001",
     dimensionality: int | None = 256,
 ) -> list[list[float]]:
     """Embeds texts with a pre-trained, foundational model."""
