8ef0f5585be5ce6edea79c704865b59fca75c969
Emmanuel Alejandro Parada Licea <paradalicea@google.com>
2025-05-29 16:29:09 -0600

    fix(translate): implement support to list[str] in `translate_text` sample

--- translate/samples/snippets/snippets.py
+++ translate/samples/snippets/snippets.py
@@ -119,7 +119,7 @@ def translate_text_with_model(target: str, text: str, model: str = "nmt") -> dic
 
 # [START translate_translate_text]
 def translate_text(
-    text: str | bytes = "¡Hola amigos y amigas!",
+    text: str | bytes | list[str] = "¡Hola amigos y amigas!",
     target_language: str = "en",
     source_language: str | None = None,
 ) -> dict:
@@ -129,7 +129,7 @@ def translate_text(
     https://cloud.google.com/translate/docs/languages#nmt
 
     Args:
-        text: The text to translate. Can be a string or bytes.
+        text: The text to translate. Can be a string, bytes or a list of strings.
               If bytes, it will be decoded as UTF-8.
         target_language: The ISO 639 language code to translate the text into
                          (e.g., 'en' for English, 'es' for Spanish).
@@ -146,26 +146,32 @@ def translate_text(
     translate_client = translate.Client()
 
     if isinstance(text, bytes):
-        text = text.decode("utf-8")
+        text = [text.decode("utf-8")]
 
-    # Values can also be a list of strings, in which case this method
-    # will return a list of results for each text.
+    if isinstance(text, str):
+        text = [text]
+
+    # If a string is supplied, a single dictionary will be returned.
+    # In case a list of strings is supplied, this method
+    # will return a list of dictionaries.
 
     # Find more information about translate function here:
     # https://cloud.google.com/python/docs/reference/translate/latest/google.cloud.translate_v2.client.Client#google_cloud_translate_v2_client_Client_translate
-    result = translate_client.translate(
+    results = translate_client.translate(
         values=text,
         target_language=target_language,
         source_language=source_language
     )
 
-    if "detectedSourceLanguage" in result:
-        print(f"Detected source language: {result['detectedSourceLanguage']}")
+    for result in results:
+        if "detectedSourceLanguage" in result:
+            print(f"Detected source language: {result['detectedSourceLanguage']}")
 
-    print(f"Input text: {result['input']}")
-    print(f"Translated text: {result['translatedText']}")
+        print(f"Input text: {result['input']}")
+        print(f"Translated text: {result['translatedText']}")
+        print()
 
-    return result
+    return results
 # [END translate_translate_text]
 
 
