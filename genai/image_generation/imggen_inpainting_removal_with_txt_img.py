9b548b898248c3a157dbaa1514a5b21231f25b92
Katie Nguyen <21978337+katiemn@users.noreply.github.com>
2025-06-06 07:07:18 -0700

    feat(genai): Add new samples for image editing (#13408)

---
+++ genai/image_generation/imggen_inpainting_removal_with_txt_img.py
@@ -0,0 +1,58 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from google.genai.types import Image
+
+
+def edit_inpainting_removal(output_file: str) -> Image:
+    # [START googlegenaisdk_imggen_inpainting_removal_with_txt_img]
+    from google import genai
+    from google.genai.types import RawReferenceImage, MaskReferenceImage, MaskReferenceConfig, EditImageConfig
+
+    client = genai.Client()
+
+    # TODO(developer): Update and un-comment below line
+    # output_file = "output-image.png"
+
+    raw_ref = RawReferenceImage(
+        reference_image=Image.from_file(location='test_resources/fruit.png'), reference_id=0)
+    mask_ref = MaskReferenceImage(
+        reference_id=1,
+        reference_image=None,
+        config=MaskReferenceConfig(
+            mask_mode="MASK_MODE_FOREGROUND",
+        ),
+    )
+
+    image = client.models.edit_image(
+        model="imagen-3.0-capability-001",
+        prompt="",
+        reference_images=[raw_ref, mask_ref],
+        config=EditImageConfig(
+            edit_mode="EDIT_MODE_INPAINT_REMOVAL",
+        ),
+    )
+
+    image.generated_images[0].image.save(output_file)
+
+    print(f"Created output image using {len(image.generated_images[0].image.image_bytes)} bytes")
+    # Example response:
+    # Created output image using 1234567 bytes
+
+    # [END googlegenaisdk_imggen_inpainting_removal_with_txt_img]
+    return image.generated_images[0].image
+
+
+if __name__ == "__main__":
+    edit_inpainting_removal(output_file="test_resources/fruit_edit.png")
