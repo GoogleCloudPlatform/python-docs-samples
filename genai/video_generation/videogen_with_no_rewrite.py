e4c3e16bfdedeeb06a08f616e418194feb9c86c2
Katie Nguyen <21978337+katiemn@users.noreply.github.com>
2025-07-03 08:04:57 -0700

    feat: veo samples for advanced control features (#13485)

---
+++ genai/video_generation/videogen_with_no_rewrite.py
@@ -0,0 +1,55 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def generate_videos_no_rewriter(output_gcs_uri: str) -> str:
+    # [START googlegenaisdk_videogen_with_no_rewrite]
+    import time
+    from google import genai
+    from google.genai.types import GenerateVideosConfig
+
+    client = genai.Client()
+
+    # TODO(developer): Update and un-comment below line
+    # output_gcs_uri = "gs://your-bucket/your-prefix"
+
+    operation = client.models.generate_videos(
+        model="veo-2.0-generate-001",
+        prompt="a cat reading a book",
+        config=GenerateVideosConfig(
+            aspect_ratio="16:9",
+            output_gcs_uri=output_gcs_uri,
+            number_of_videos=1,
+            duration_seconds=5,
+            person_generation="dont_allow",
+            enhance_prompt=False,
+        ),
+    )
+
+    while not operation.done:
+        time.sleep(15)
+        operation = client.operations.get(operation)
+        print(operation)
+
+    if operation.response:
+        print(operation.result.generated_videos[0].video.uri)
+
+    # Example response:
+    # gs://your-bucket/your-prefix
+    # [END googlegenaisdk_videogen_with_no_rewrite]
+    return operation.result.generated_videos[0].video.uri
+
+
+if __name__ == "__main__":
+    generate_videos_no_rewriter(output_gcs_uri="gs://your-bucket/your-prefix")
