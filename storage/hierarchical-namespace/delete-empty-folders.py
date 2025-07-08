8d68dce26d257e29fbd063fea5043f0b38e4f825
mabsaleh <mabsaleh@google.com>
2025-07-08 22:21:22 +0000

    Add licensing agreement, prefix check, and fix typo.

--- storage/hierarchical-namespace/delete-empty-folders.py
+++ storage/hierarchical-namespace/delete-empty-folders.py
@@ -1,3 +1,17 @@
+# Copyright 2025 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from concurrent import futures
 import concurrent.futures
 import logging
@@ -31,7 +45,7 @@ ThreadPoolExecutor = futures.ThreadPoolExecutor
 #    `gcloud auth application-default login`
 # 2. Update configuration variables as needed to specify the target bucket,
 #    prefix, number of concurrent threads, etc.
-# 3. Run the script: `python3 delete-folders-script.py`
+# 3. Run the script: `python3 delete-empty-folders.py`
 
 # --- Configuration ---
 BUCKET_NAME = "your-gcs-bucket-name"
@@ -251,7 +265,11 @@ if __name__ == "__main__":
             "\nERROR: Please update the BUCKET_NAME variable in the script before"
             " running."
         )
-        exit(0)
+        exit(1)
+
+    if FOLDER_PREFIX and not FOLDER_PREFIX.endswith("/"):
+        print("\nERROR: FOLDER_PREFIX must end with a '/' if specified.")
+        exit(1)
 
     start_time = time.time()
 
