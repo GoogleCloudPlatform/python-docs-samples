063082ad239e7d41308ef502dee6a121b948cb2a
Jack Wotherspoon <jackwoth@google.com>
2025-07-29 15:10:33 -0400

    chore: add fixture to Streamlit test

--- run/helloworld-streamlit/main_test.py
+++ run/helloworld-streamlit/main_test.py
@@ -13,22 +13,28 @@
 # limitations under the License.
 
 from streamlit.testing.v1 import AppTest
+import pytest
 
-def test_balloons():
+
+@pytest.fixture
+def app_test() -> AppTest:
+    """Fixture for creating a test app instance and setting a name."""
+    at = AppTest.from_file("main.py").run()
+    at.text_input[0].set_value("Foo").run()
+    return at
+
+
+def test_balloons(app_test: AppTest):
     """A user presses the balloons button"""
-    at = AppTest.from_file("main.py").run()
-    at.text_input[0].set_value("Foo").run()
-    at.button[0].click().run()
-    assert at.markdown.values[0] == "This is a demo Streamlit app.\n\nEnter your name in the text box below and press a button to see some fun features in Streamlit."
-    assert at.markdown.values[1] == "Time to celebrate Foo! 🥳"
-    assert at.markdown.values[2] == "You deployed a Streamlit app! 👏"
+    app_test.button[0].click().run()
+    assert app_test.markdown.values[0] == "This is a demo Streamlit app.\n\nEnter your name in the text box below and press a button to see some fun features in Streamlit."
+    assert app_test.markdown.values[1] == "Time to celebrate Foo! 🥳"
+    assert app_test.markdown.values[2] == "You deployed a Streamlit app! 👏"
 
-def test_snow():
+
+def test_snow(app_test: AppTest):
     """A user presses the snow button"""
-    at = AppTest.from_file("main.py").run()
-    at.text_input[0].set_value("Foo").run()
-    at.button[1].click().run()
-    assert at.markdown.values[0] == "This is a demo Streamlit app.\n\nEnter your name in the text box below and press a button to see some fun features in Streamlit."
-    assert at.markdown.values[1] == "Let it snow Foo! 🌨️"
-    assert at.markdown.values[2] == "You deployed a Streamlit app! 👏"
-    
+    app_test.button[1].click().run()
+    assert app_test.markdown.values[0] == "This is a demo Streamlit app.\n\nEnter your name in the text box below and press a button to see some fun features in Streamlit."
+    assert app_test.markdown.values[1] == "Let it snow Foo! 🌨️"
+    assert app_test.markdown.values[2] == "You deployed a Streamlit app! 👏"
