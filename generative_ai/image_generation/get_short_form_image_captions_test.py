d955fee48ec66b06e645a9c60c734e79598211e1
Katie McLaughlin <katie@glasnt.com>
2025-10-20 16:55:49 +1100

    merge #13608 skips, to solve snippets-scoped tests

--- generative_ai/image_generation/get_short_form_image_captions_test.py
+++ generative_ai/image_generation/get_short_form_image_captions_test.py
@@ -17,6 +17,7 @@ import os
 import backoff
 
 from google.api_core.exceptions import ResourceExhausted
+import pytest
 
 import get_short_form_image_captions
 
@@ -25,6 +26,7 @@ _RESOURCES = os.path.join(os.path.dirname(__file__), "test_resources")
 _INPUT_FILE = os.path.join(_RESOURCES, "cat.png")
 
 
+@pytest.mark.skip("Sample pending deprecation b/452720552")
 @backoff.on_exception(backoff.expo, ResourceExhausted, max_time=60)
 def test_get_short_form_image_captions() -> None:
     response = get_short_form_image_captions.get_short_form_image_captions(
