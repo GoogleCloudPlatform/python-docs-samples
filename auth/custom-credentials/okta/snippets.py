b6fb71876da21a6af43a80cc3d369bcf18f58ec2
Jennifer Davis <sigje@google.com>
2025-12-11 19:48:31 -0800

    fix: starting region tag

--- auth/custom-credentials/okta/snippets.py
+++ auth/custom-credentials/okta/snippets.py
@@ -11,6 +11,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# [START auth_custom_credential_supplier_okta]
+
 import json
 import os
 import sys
