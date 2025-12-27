a69bd808381ff46627d56e880eb20a199ee9b116
Chris Hampartsoumian <champart@google.com>
2025-12-27 00:15:09 +0000

    feat(livestream): Modified get_input script to provide more of the information that is returned from the API response

--- video/live-stream/get_input.py
+++ video/live-stream/get_input.py
@@ -42,7 +42,11 @@ def get_input(
 
     name = f"projects/{project_id}/locations/{location}/inputs/{input_id}"
     response = client.get_input(name=name)
-    print(f"Input: {response.name}")
+    print(f"Name: {response.name}")
+    print(f"Type: {live_stream_v1.types.Input.Type(response.type_).name}")
+    print(f"Tier: {live_stream_v1.types.Input.Tier(response.tier).name}")
+    if response.uri:
+        print(f"URI: {response.uri}")
 
     return response
 
