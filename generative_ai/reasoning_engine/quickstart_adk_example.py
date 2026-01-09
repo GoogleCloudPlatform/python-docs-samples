9c654f51779407da383e6b869cd0b1a7a799765a
Yeesian Ng <ngyeesian@gmail.com>
2026-01-09 15:21:44 -0500

    Update quickstart_adk_example.py with region tags

--- generative_ai/reasoning_engine/quickstart_adk_example.py
+++ generative_ai/reasoning_engine/quickstart_adk_example.py
@@ -6,6 +6,7 @@ LOCATION = os.getenv("GOOGLE_CLOUD_LOCATION")
 
 client = vertexai.Client(project=PROJECT_ID, location=LOCATION)
 
+# [START agentengine_quickstart_adk_tool]
 def get_exchange_rate(
     currency_from: str = "USD",
     currency_to: str = "EUR",
@@ -19,9 +20,11 @@ def get_exchange_rate(
         params={"from": currency_from, "to": currency_to},
     )
     return response.json()
+# [END agentengine_quickstart_adk_tool]
 
 def quickstart_adk_example(staging_bucket: str) -> reasoning_engines.ReasoningEngine:
 
+    # [START agentengine_quickstart_adk_agent_init]
     from google.adk.agents import Agent
     from vertexai import agent_engines
     
@@ -32,27 +35,34 @@ def quickstart_adk_example(staging_bucket: str) -> reasoning_engines.ReasoningEn
     )
     
     app = agent_engines.AdkApp(agent=agent)
-    
+    # [END agentengine_quickstart_adk_agent_init]
+
+    # [START agentengine_quickstart_adk_testlocally]
     async for event in app.async_stream_query(
         user_id="USER_ID",
         message="What is the exchange rate from US dollars to SEK today?",
     ):
         print(event)
-    
+    # [END agentengine_quickstart_adk_testlocally]
+
+    # [START agentengine_quickstart_adk_deploy]
     remote_agent = client.agent_engines.create(
         agent=app,
         config={
             "requirements": ["google-cloud-aiplatform[agent_engines,adk]"],
-            "staging_bucket": "STAGING_BUCKET",
+            "staging_bucket": staging_bucket,
         }
     )
+    # [END agentengine_quickstart_adk_deploy]
 
     events = []
+    # [START agentengine_quickstart_adk_testremotely]
     async for event in remote_agent.async_stream_query(
         user_id="USER_ID",
         message="What is the exchange rate from US dollars to SEK today?",
     ):
         print(event)
+    # [END agentengine_quickstart_adk_testremotely]
         events.append(event)
     
     remote_agent.delete(force=True)
