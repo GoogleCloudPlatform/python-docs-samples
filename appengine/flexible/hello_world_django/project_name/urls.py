80079d4d85d30d9455a36fe31ebd5baa92a4efca
David del Real <warhead_1090@hotmail.com>
2026-02-06 11:11:13 -0600

    fix(django): update dependencies and settings for Django samples (#13742)
    * chore: updating Django and python versions in requirements, CI-CD/Nox files.
    
    b/470283288
    Updated Django version to 6.0.1.
    Updated required python version to 3.12 (minimum version required by Django 6.0.1)
    
    * chore: Updated requirements for App Engine Django Flexible Environment
    
    b/470283244
    - Updated Django and python version in requirements file.
    - Updated and added missing attributes for frameworks's execution.
    
    * chore: Updated requirements for Run Django
    
    b/470283502
    - Updated Django and python version in requirements and nox files.
    
    * chore: Update requirements for Django Cloud SQL
    
    - Updated requirements and nox files for latest Django version.
    
    * Setting up Django to use signed URLs to workaourn UBLA for buckets.
    
    * Fixed linting issues.
    
    * Removed remnants from testing that shouldnt be here.
    
    * Removed changes to run/django to avoid issue with Pipeline test.
    Will attempt to merge other changes only.

--- appengine/flexible/hello_world_django/project_name/urls.py
+++ appengine/flexible/hello_world_django/project_name/urls.py
@@ -13,12 +13,12 @@
 # limitations under the License.
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 import helloworld.views
 
 
 urlpatterns = [
-    path("admin/", include(admin.site.urls)),
+    path("admin/", admin.site.urls),
     path("", helloworld.views.index),
 ]
