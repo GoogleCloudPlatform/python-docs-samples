83aba34089df78b015e58c9fbfb319934f57d7b6
Sampath Kumar <sam1990kumar@gmail.com>
2026-05-25 17:58:14 +0200

    feat(genai): migrate Vertex AI env var to Enterprise in core tests

--- genai/thinking/test_thinking_examples.py
+++ genai/thinking/test_thinking_examples.py
@@ -17,7 +17,7 @@ import thinking_budget_with_txt
 import thinking_includethoughts_with_txt
 import thinking_with_txt
 
-os.environ["GOOGLE_GENAI_USE_VERTEXAI"] = "True"
+os.environ["GOOGLE_GENAI_USE_ENTERPRISE"] = "True"
 os.environ["GOOGLE_CLOUD_LOCATION"] = "global"  # "us-central1"
 # The project name is included in the CICD pipeline
 # os.environ['GOOGLE_CLOUD_PROJECT'] = "add-your-project-name"
