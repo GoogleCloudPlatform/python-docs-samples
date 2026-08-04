3348d292815ad70946f10cdd2251a86db7568bd6
David del Real Sifuentes <deldavid@google.com>
2026-08-04 23:05:15 +0000

    Migrate code retrieval example for embeddings to genai.

---
+++ genai/embeddings/code_retrieval_example.py
@@ -0,0 +1,80 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START aiplatform_genai_embedding_code_retrieval]
+
+import os
+
+from google import genai
+from google.genai import types
+
+
+# TODO (Developer) set the following environment variables.
+PROJECT_ID = os.getenv("PROJECT_ID")
+LOCATION_ID = os.getenv("LOCATION_ID", "us-central1")
+MODEL_NAME = os.getenv("MODEL_NAME", "gemini-embedding-001")
+DIMENSIONALITY = 3072
+
+QUERY_LINES = ["Retrieve a function that adds two numbers"]
+CODE_RETRIEVAL_QUERY = "CODE_RETRIEVAL_QUERY"
+RETRIEVAL_DOCUMENT = "RETRIEVAL_DOCUMENT"
+SOURCE_CODE = [
+        "def func(a, b): return a + b",
+        "def func(a, b): return a - b",
+        "def func(a, b): return (a ** 2 + b ** 2) ** 0.5",
+]
+
+
+def embed_test() -> tuple[types.EmbedContentConfig, types.EmbedContentConfig]:
+    client = genai.Client(enterprise=True, project=PROJECT_ID, location=LOCATION_ID)
+
+    # Index Source Code
+    for line in SOURCE_CODE:
+        config = genai.types.EmbedContentConfig(
+            task_type=RETRIEVAL_DOCUMENT
+        )
+
+        index_response = client.models.embed_content(
+            model=MODEL_NAME,
+            contents=line,
+            config=config
+        )
+
+        print(f"Task: {RETRIEVAL_DOCUMENT} | "
+              f"Vector length: {len(index_response.embedding.values)} | "
+              f"Preview: {index_response.embedding.values[:3]}...")
+
+    # Embed Search Prompts
+    for line in QUERY_LINES:
+        config = genai.types.EmbedContentConfig(
+            task_type=CODE_RETRIEVAL_QUERY
+        )
+
+        query_response = client.models.embed_content(
+            model=MODEL_NAME,
+            contents=line,
+            config=config
+        )
+
+        print(f"Task: {CODE_RETRIEVAL_QUERY} | "
+              f"Vector length: {len(query_response.embedding.values)} | "
+              f"Preview: {query_response.embedding.values[:3]}...")
+
+    return index_response, query_response
+
+
+if __name__ == "__main__":
+    embed_test()
+
+# [END aiplatform_genai_embedding_code_retrieval]
